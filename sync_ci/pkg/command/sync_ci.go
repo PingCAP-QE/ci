5fed2ff25ecb3f7bd6126d557f093caf46e0d71e
Kiv Chen <34561254+KivenChen@users.noreply.github.com>
2020-11-25 14:30:24 +0800

    Update default rule path

--- sync_ci/pkg/command/sync_ci.go
+++ sync_ci/pkg/command/sync_ci.go
@@ -25,7 +25,7 @@ func (s *SyncCICommand) SetFlags(f *flag.FlagSet) {
 	f.StringVar(&s.Dsn, "dsn", "root:@tcp(127.0.0.1:3306)/sync_ci_data", "dsn")
 	f.StringVar(&s.Port, "port", "36000", "http service port")
 	f.StringVar(&s.LogPath, "lp", "log", "log path")
-	f.StringVar(&s.RulePath, "rp", "envrules.json", "env rule file path")
+	f.StringVar(&s.RulePath, "rp", "pkg/parser/envrules.json", "env rule file path")
 }
 
 func (s *SyncCICommand) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
