4a490fd253dfe5610c4bb6929202c9f098db7f29
Qiang Zhou <zhouqiang.cl@gmail.com>
2021-03-17 18:32:32 +0800

    add misc tools

---
+++ misc_tools/create_branch.sh
@@ -0,0 +1,13 @@
+# First your should have $TOKEN
+base_branch=master
+new_branch=release-5.0
+# Should check if tikv/importer's base branch is master, if not, remove it from repo list and create the tikv/importer's branch by hand
+repo_list="pingcap/tidb pingcap/parser tikv/tikv tikv/pd pingcap/tics pingcap/br pingcap/tidb-binlog pingcap/tidb-lightning pingcap/tidb-tools pingcap/ticdc pingcap/dumpling pingcap/tiflash"
+
+set -x
+for repo in $repo_list;do
+sha=$(curl -H "Authorization: token $TOKEN" https://api.github.com/repos/$repo/git/refs/heads/$base_branch | jq -r '.object.sha')
+
+curl -X POST -H "Authorization: token $TOKEN" \
+-d  "{\"ref\": \"refs/heads/$new_branch\",\"sha\": \"$sha\"}" "https://api.github.com/repos/$repo/git/refs"
+done
---
+++ misc_tools/create_tag.py
@@ -0,0 +1,106 @@
+import re
+import sys
+import logging
+
+
+from github import Github
+
+fmt="%(levelname)s: %(asctime)s: %(filename)s:%(lineno)d * %(thread)d %(message)s"
+datefmt="%m-%d %H:%M:%S"
+logging.basicConfig(level=logging.INFO, format=fmt, datefmt=datefmt)
+logger = logging.getLogger()
+
+
+class repo_info(object):
+    def __init__(self, repo_name, repo_addr, tag_githash='', version=''):
+        self.repo_name = repo_name
+        self.repo_addr = repo_addr
+        self.version = version
+        self.tag_githash = tag_githash
+
+
+repo_list = [
+    repo_info(
+        repo_name='TiKV',
+        repo_addr='tikv/tikv',
+        tag_githash='',
+        version='',),
+    repo_info(
+        repo_name='TiDB',
+        repo_addr='pingcap/tidb',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='PD',
+        repo_addr='tikv/pd',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='TiFlash',
+        repo_addr='pingcap/tics',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='BR',
+        repo_addr='pingcap/br',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='binlog',
+        repo_addr='pingcap/tidb-binlog',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='lightning',
+        repo_addr='pingcap/tidb-lightning',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='importer',
+        repo_addr='tikv/importer',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='tidb-tools',
+        repo_addr='pingcap/tidb-tools',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='ticdc',
+        repo_addr='pingcap/ticdc',
+        tag_githash='',
+        version=''),
+    repo_info(
+        repo_name='dumpling',
+        repo_addr='pingcap/dumpling',
+        tag_githash='',
+        version=''),
+]
+
+
+
+def get_repo_tags():
+    for repo_obj in repo_list:
+        repo_obj.tag_githash = get_githash(repo_obj.repo_addr)
+
+def get_githash(repo):
+    r = g.get_repo(repo).get_branch("release-5.0")
+    return r.commit
+
+def create_git_tag():
+    for repo_info in repo_list:
+        repo = g.get_repo(repo_info.repo_addr)
+
+        name = repo_info.repo_name
+        release_message = 'There is no release note for this version.'
+
+        logger.info("creating release tag:%s %s %s",repo_info.repo_name, repo_info.version, repo_info.tag_githash)
+        ret = repo.create_git_ref('refs/tags/{}'.format("v5.0.0-nightly"),repo_info.tag_githash.sha)
+        logger.info("done creating release: %s", ret)
+
+
+if __name__ == '__main__':
+    token = sys.argv[1]
+    g = Github(token) # personal token 
+    get_repo_tags()
+    create_git_tag()
