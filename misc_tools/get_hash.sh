287165c978e4fb0ab679b12b8768ff60f3949c2d
Qiang Zhou <zhouqiang.cl@gmail.com>
2021-03-22 09:57:26 +0800

    add get hash

--- misc_tools/get_hash.sh
+++ misc_tools/get_hash.sh
@@ -2,10 +2,17 @@
 # TOKEN=xxx sh get_hash.sh release-5.0 2>/dev/null
 
 branch=$1
-# Should check if tikv/importer's base branch is master, if not, remove it from repo list and create the tikv/importer's branch by hand
-repo_list="pingcap/tidb pingcap/parser tikv/tikv tikv/pd pingcap/tics pingcap/br pingcap/tidb-binlog pingcap/tidb-lightning pingcap/tidb-tools pingcap/ticdc pingcap/dumpling pingcap/tiflash"
+
+repo_list="pingcap/tidb tikv/tikv tikv/pd pingcap/tics pingcap/br pingcap/tidb-binlog pingcap/tidb-tools pingcap/ticdc tikv/importer"
 
 for repo in $repo_list;do
+sha=$(curl -H "Authorization: token $TOKEN" https://api.github.com/repos/$repo/git/refs/heads/$branch | jq -r '.object.sha')
+echo ${repo} ":" ${sha}
+done
+
+master_base_repo_list="pingcap/dumpling"
+branch="master"
+for repo in $master_base_repo_list;do
 sha=$(curl -H "Authorization: token $TOKEN" https://api.github.com/repos/$repo/git/refs/heads/$branch | jq -r '.object.sha')
 echo ${repo} ":" ${sha}
 done
