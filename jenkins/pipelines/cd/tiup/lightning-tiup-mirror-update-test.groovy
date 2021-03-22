41a61ddee13fb09335758b2a9927ab14657c9d04
Qiang Zhou <zhouqiang.cl@gmail.com>
2021-03-22 11:55:33 +0800

    using br repo instead of lighting repo

--- jenkins/pipelines/cd/tiup/lightning-tiup-mirror-update-test.groovy
+++ jenkins/pipelines/cd/tiup/lightning-tiup-mirror-update-test.groovy
@@ -132,10 +132,8 @@ try {
                 if (TIDB_VERSION == "") {
                     TIDB_VERSION = RELEASE_TAG
                 }
-                lightning_sha1 = sh(returnStdout: true, script: "python gethash.py -repo=tidb-lightning -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
-                if(RELEASE_TAG=="nightly"){
-                    lightning_sha1 = sh(returnStdout: true, script: "python gethash.py -repo=br -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
-                }
+                // After v4.0.11, we use br repo instead of br repo, and we should not maintain old version, if we indeed need, we can use the old version of this groovy file
+                lightning_sha1 = sh(returnStdout: true, script: "python gethash.py -repo=br -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
             }
 
             stage("tiup release tidb-lightning linux amd64") {
