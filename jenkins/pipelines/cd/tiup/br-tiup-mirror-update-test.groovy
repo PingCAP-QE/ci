17dffd08c3f28277ed35c248e1be0ba36b77c014
Qiang Zhou <zhouqiang.cl@gmail.com>
2021-03-22 15:53:01 +0800

    try to extract util

--- jenkins/pipelines/cd/tiup/br-tiup-mirror-update-test.groovy
+++ jenkins/pipelines/cd/tiup/br-tiup-mirror-update-test.groovy
@@ -3,107 +3,7 @@ def br_desc = "TiDB/TiKV cluster backup restore tool"
 
 def br_sha1, tarball_name, dir_name
 
-def install_tiup = { bin_dir ->
-    sh """
-    wget -q https://tiup-mirrors.pingcap.com/tiup-linux-amd64.tar.gz
-    sudo tar -zxf tiup-linux-amd64.tar.gz -C ${bin_dir}
-    sudo chmod 755 ${bin_dir}/tiup
-    rm -rf ~/.tiup
-    mkdir -p /home/jenkins/.tiup/bin/
-    curl https://tiup-mirrors.pingcap.com/root.json -o /home/jenkins/.tiup/bin/root.json
-    mkdir -p ~/.tiup/keys
-    set +x
-    echo ${PINGCAP_PRIV_KEY} | base64 -d > ~/.tiup/keys/private.json
-    set -x
-    """
-}
-
-def install_qshell = { bin_dir ->
-    sh """
-    wget -q https://tiup-mirrors.pingcap.com/qshell-linux-amd64.tar.gz
-    sudo tar -zxf qshell-linux-amd64.tar.gz -C ${bin_dir}
-    sudo chmod 755 ${bin_dir}/qshell
-    set +x
-    qshell account ${QSHELL_KEY} ${QSHELL_SEC} tiup-mirror-update --overwrite
-    set -x
-    """
-}
-
-def download = { name, version, os, arch ->
-    if (os == "linux") {
-        platform = "centos7"
-    } else if (os == "darwin") {
-        platform = "darwin"
-    } else {
-        sh """
-        exit 1
-        """
-    }
-
-    if (arch == "arm64") {
-        tarball_name = "${name}-${os}-${arch}.tar.gz"
-    } else {
-        tarball_name = "${name}.tar.gz"
-    }
-    if (RELEASE_TAG != "nightly") {
-        sh """
-    wget ${FILE_SERVER_URL}/download/builds/pingcap/${name}/optimization/${tag}/${br_sha1}/${platform}/${tarball_name}
-    """
-    } else {
-        sh """
-    wget ${FILE_SERVER_URL}/download/builds/pingcap/${name}/${tag}/${br_sha1}/${platform}/${tarball_name}
-    """
-    }
-}
-
-def unpack = { name, version, os, arch ->
-    if (arch == "arm64") {
-        tarball_name = "${name}-${os}-${arch}.tar.gz"
-    } else {
-        tarball_name = "${name}.tar.gz"
-    }
-
-    sh """
-    tar -zxf ${tarball_name}
-    """
-}
-
-def pack = { name, version, os, arch ->
-
-    sh """
-    rm -rf ${name}*.tar.gz
-    [ -d package ] || mkdir package
-    """
-
-    if (os == "linux" && arch == "amd64") {
-        sh """
-        tar -C bin -czvf package/${name}-${version}-${os}-${arch}.tar.gz br
-        rm -rf bin
-        """
-    } else {
-        sh """
-        tar -C ${name}-*/bin -czvf package/${name}-${version}-${os}-${arch}.tar.gz br
-        rm -rf ${name}-*
-        """
-    }
-
-    sh """
-    tiup mirror publish ${name} ${TIDB_VERSION} package/${name}-${version}-${os}-${arch}.tar.gz ${name} --standalone --arch ${arch} --os ${os} --desc="${br_desc}"
-    """
-}
-
-def upload = { dir ->
-    sh """
-    rm -rf ~/.qshell/qupload
-    qshell qupload2 --src-dir=${dir} --bucket=tiup-mirrors --overwrite
-    """
-}
-
-def update = { name, version, os, arch ->
-    download name, version, os, arch
-    unpack name, version, os, arch
-    pack name, version, os, arch
-}
+def util = load "tiup_utils.groovy"
 
 node("build_go1130") {
     container("golang") {
@@ -131,18 +31,16 @@ node("build_go1130") {
             }
 
             stage("tiup release br linux amd64") {
-                update "br", RELEASE_TAG, "linux", "amd64"
+                util.update "br", RELEASE_TAG, "linux", "amd64"
             }
 
             stage("tiup release br linux arm64") {
-                update "br", RELEASE_TAG, "linux", "arm64"
+                util.update "br", RELEASE_TAG, "linux", "arm64"
             }
 
             stage("tiup release br darwin amd64") {
-                update "br", RELEASE_TAG, "darwin", "amd64"
+                util.update "br", RELEASE_TAG, "darwin", "amd64"
             }
         }
-
-        // upload "package"
     }
 }
---
+++ jenkins/pipelines/cd/tiup/tiup_utils.groovy
@@ -0,0 +1,103 @@
+def install_tiup = { bin_dir ->
+    sh """
+    wget -q https://tiup-mirrors.pingcap.com/tiup-linux-amd64.tar.gz
+    sudo tar -zxf tiup-linux-amd64.tar.gz -C ${bin_dir}
+    sudo chmod 755 ${bin_dir}/tiup
+    rm -rf ~/.tiup
+    mkdir -p /home/jenkins/.tiup/bin/
+    curl https://tiup-mirrors.pingcap.com/root.json -o /home/jenkins/.tiup/bin/root.json
+    mkdir -p ~/.tiup/keys
+    set +x
+    echo ${PINGCAP_PRIV_KEY} | base64 -d > ~/.tiup/keys/private.json
+    set -x
+    """
+}
+
+def install_qshell = { bin_dir ->
+    sh """
+    wget -q https://tiup-mirrors.pingcap.com/qshell-linux-amd64.tar.gz
+    sudo tar -zxf qshell-linux-amd64.tar.gz -C ${bin_dir}
+    sudo chmod 755 ${bin_dir}/qshell
+    set +x
+    qshell account ${QSHELL_KEY} ${QSHELL_SEC} tiup-mirror-update --overwrite
+    set -x
+    """
+}
+
+def download = { name, version, os, arch ->
+    if (os == "linux") {
+        platform = "centos7"
+    } else if (os == "darwin") {
+        platform = "darwin"
+    } else {
+        sh """
+        exit 1
+        """
+    }
+
+    if (arch == "arm64") {
+        tarball_name = "${name}-${os}-${arch}.tar.gz"
+    } else {
+        tarball_name = "${name}.tar.gz"
+    }
+    if (RELEASE_TAG != "nightly") {
+        sh """
+    wget ${FILE_SERVER_URL}/download/builds/pingcap/${name}/optimization/${tag}/${br_sha1}/${platform}/${tarball_name}
+    """
+    } else {
+        sh """
+    wget ${FILE_SERVER_URL}/download/builds/pingcap/${name}/${tag}/${br_sha1}/${platform}/${tarball_name}
+    """
+    }
+}
+
+def unpack = { name, version, os, arch ->
+    if (arch == "arm64") {
+        tarball_name = "${name}-${os}-${arch}.tar.gz"
+    } else {
+        tarball_name = "${name}.tar.gz"
+    }
+
+    sh """
+    tar -zxf ${tarball_name}
+    """
+}
+
+def pack = { name, version, os, arch ->
+
+    sh """
+    rm -rf ${name}*.tar.gz
+    [ -d package ] || mkdir package
+    """
+
+    if (os == "linux" && arch == "amd64") {
+        sh """
+        tar -C bin -czvf package/${name}-${version}-${os}-${arch}.tar.gz br
+        rm -rf bin
+        """
+    } else {
+        sh """
+        tar -C ${name}-*/bin -czvf package/${name}-${version}-${os}-${arch}.tar.gz br
+        rm -rf ${name}-*
+        """
+    }
+
+    sh """
+    tiup mirror publish ${name} ${TIDB_VERSION} package/${name}-${version}-${os}-${arch}.tar.gz ${name} --standalone --arch ${arch} --os ${os} --desc="${br_desc}"
+    """
+}
+
+def upload = { dir ->
+    sh """
+    rm -rf ~/.qshell/qupload
+    qshell qupload2 --src-dir=${dir} --bucket=tiup-mirrors --overwrite
+    """
+}
+
+def update = { name, version, os, arch ->
+    download name, version, os, arch
+    unpack name, version, os, arch
+    pack name, version, os, arch
+}
+
+return this
