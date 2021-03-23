5a0b13ad0e386ec61100f7eb73064e6dab1917f5
Qiang Zhou <zhouqiang.cl@gmail.com>
2021-03-23 20:35:34 +0800

    add maintain script

---
+++ jenkins/maintain/delete_useless_job_by_label.groovy
@@ -0,0 +1,10 @@
+def jobName = "tikv_ghpr_test"
+
+// you can set maxNumber to reserve not delete job
+def maxNumber = 60000
+ 
+Jenkins.instance.getItemByFullName(jobName).builds.findAll {
+  it.number <= maxNumber
+}.each {
+  it.delete()
+}
