6f6a18cae37c815ea3d67bd10e5d086bbf983b57
Qiang Zhou <zhouqiang.cl@gmail.com>
2021-04-07 16:02:15 +0800

    add build linux arm64

---
+++ jenkins/pipelines/build-linux-arm64-enterprise.groovy
@@ -0,0 +1,184 @@
+/*
+* @RELEASE_TAG
+*/
+def slackcolor = 'good'
+def githash
+def os = "linux"
+def arch = "arm64"
+def TIDB_HASH
+def TIKV_HASH
+def PD_HASH
+def TIFLASH_HASH
+
+def label = "build-arm-tiflash"
+
+try {
+    node("arm") {
+        def ws = pwd()
+        deleteDir()
+
+        stage("GO node") {
+            println "arm node: ${NODE_NAME}"
+            println "${ws}"
+            sh "curl -s ${FILE_SERVER_URL}/download/builds/pingcap/ee/gethash.py > gethash.py"
+            TIDB_HASH = sh(returnStdout: true, script: "python gethash.py -repo=tidb -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
+            TIKV_HASH = sh(returnStdout: true, script: "python gethash.py -repo=tikv -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
+            PD_HASH = sh(returnStdout: true, script: "python gethash.py -repo=pd -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
+        }
+
+        stage("Build tidb") {
+            dir("go/src/github.com/pingcap/tidb") {
+                if (sh(returnStatus: true, script: '[ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                    deleteDir()
+                }
+
+                def target = "tidb-${RELEASE_TAG}-${os}-${arch}"
+                // def refspath = "refs/pingcap/tidb/${RELEASE_TAG}/sha1"
+                def filepath = "builds/pingcap/tidb/${TIDB_HASH}/centos7/tidb-server-${RELEASE_TAG}-${os}-${arch}-enterprise.tar.gz"
+
+                // checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name:  "${TIDB_HASH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'git@github.com:pingcap/tidb.git']]]
+                if(RELEASE_TAG == "master") {
+                    checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name:  "${TIDB_HASH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'git@github.com:pingcap/tidb.git']]]
+                } else {
+                    checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name:  "${RELEASE_TAG}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch'],[$class: 'CloneOption', noTags: true]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: "+refs/tags/${RELEASE_TAG}:refs/tags/${RELEASE_TAG}", url: 'git@github.com:pingcap/tidb.git']]]
+                }
+
+                sh """
+                make clean
+                go version
+                TIDB_EDITION=Enterprise make
+                rm -rf ${target}
+                mkdir -p ${target}/bin
+                cp bin/* ${target}/bin
+                tar czvf ${target}.tar.gz ${target}
+                curl -F ${filepath}=@${target}.tar.gz ${FILE_SERVER_URL}/upload
+                """
+            }
+        }
+
+        stage("Build pd") {
+            dir("go/src/github.com/pingcap/pd") {
+                if (sh(returnStatus: true, script: '[ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                    deleteDir()
+                }
+
+                def target = "pd-${RELEASE_TAG}-${os}-${arch}"
+                // def refspath = "refs/pingcap/pd/${RELEASE_TAG}/sha1"
+                def filepath = "builds/pingcap/pd/${PD_HASH}/centos7/pd-server-${RELEASE_TAG}-${os}-${arch}-enterprise.tar.gz"
+
+                // checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name: "${PD_HASH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'git@github.com:pingcap/pd.git']]]
+                if(RELEASE_TAG == "master") {
+                    checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name: "${PD_HASH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'git@github.com:tikv/pd.git']]]
+                } else {
+                    checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name:  "${RELEASE_TAG}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch'],[$class: 'CloneOption', noTags: true]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: "+refs/tags/${RELEASE_TAG}:refs/tags/${RELEASE_TAG}", url: 'git@github.com:tikv/pd.git']]]
+                }
+
+                sh """
+                go version
+                PD_EDITION=Enterprise make
+                PD_EDITION=Enterprise make tools
+                rm -rf ${target}
+                mkdir -p ${target}/bin
+                cp bin/* ${target}/bin
+                tar czvf ${target}.tar.gz ${target}
+                curl -F ${filepath}=@${target}.tar.gz ${FILE_SERVER_URL}/upload
+                """
+            }
+        }
+
+        stage("Build TiKV") {
+            dir("go/src/github.com/pingcap/tikv") {
+                if (sh(returnStatus: true, script: '[ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                    deleteDir()
+                }
+
+                def target = "tikv-${RELEASE_TAG}-${os}-${arch}"
+                // def refspath = "refs/pingcap/tikv/${RELEASE_TAG}/sha1"
+                def filepath = "builds/pingcap/tikv/${TIKV_HASH}/centos7/tikv-server-${RELEASE_TAG}-${os}-${arch}-enterprise.tar.gz"
+
+                // checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name: "${TIKV_HASH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'git@github.com:tikv/tikv.git']]]
+                if(RELEASE_TAG == "master") {
+                    checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name: "${TIKV_HASH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'git@github.com:tikv/tikv.git']]]
+                } else {
+                    checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name:  "${RELEASE_TAG}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch'],[$class: 'CloneOption', noTags: true]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: "+refs/tags/${RELEASE_TAG}:refs/tags/${RELEASE_TAG}", url: 'git@github.com:tikv/tikv.git']]]
+                }
+
+                sh """
+                grpcio_ver=`grep -A 1 'name = "grpcio"' Cargo.lock | tail -n 1 | cut -d '"' -f 2`
+                if [[ ! "0.8.0" > "\$grpcio_ver" ]]; then
+                    echo using gcc 8
+                    source /opt/rh/devtoolset-8/enable
+                fi
+                TIKV_EDITION=Enterprise CARGO_TARGET_DIR=.target ROCKSDB_SYS_STATIC=1 ROCKSDB_SYS_SSE=0 make dist_release
+                rm -rf ${target}
+                mkdir -p ${target}/bin
+                cp bin/* ${target}/bin
+                tar czvf ${target}.tar.gz ${target}
+                curl -F ${filepath}=@${target}.tar.gz ${FILE_SERVER_URL}/upload
+                """
+            }
+        }
+    }
+
+    podTemplate(cloud: 'kubernetes-arm64', name: label, label: label,
+                instanceCap: 5, idleMinutes: 30,
+                workspaceVolume: emptyDirWorkspaceVolume(memory: true),
+                containers: [
+                    containerTemplate(name: 'tiflash-build-arm', image: 'hub.pingcap.net/tiflash/tiflash-builder:arm64',
+                                      alwaysPullImage: true, ttyEnabled: true, privileged: true, command: 'cat',
+                                      resourceRequestCpu: '12000m', resourceRequestMemory: '20Gi',
+                                      resourceLimitCpu: '16000m', resourceLimitMemory: '48Gi'),
+                    containerTemplate(name: 'jnlp', image: 'hub.pingcap.net/jenkins/jnlp-slave-arm64:0.0.1'),
+                ]) {
+        node(label) {
+            container("tiflash-build-arm") {
+                stage("Build tiflash - prepare") {
+                    deleteDir()
+                    println "arm debug command:\nkubectl -n jenkins-ci exec -ti ${NODE_NAME} bash"
+                    sh "curl -s ${FILE_SERVER_URL}/download/builds/pingcap/ee/gethash.py > gethash.py"
+                    TIFLASH_HASH = sh(returnStdout: true, script: "python gethash.py -repo=tics -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
+                }
+
+                stage("build tiflash - build") {
+                    dir("tics") {
+                        def target = "tiflash-${RELEASE_TAG}-${os}-${arch}"
+                        def filepath
+                        
+                        filepath = "builds/pingcap/tiflash/${RELEASE_TAG}/${TIFLASH_HASH}/centos7/tiflash-${RELEASE_TAG}-${os}-${arch}-enterprise.tar.gz"
+
+                        retry(10) {
+                            if(RELEASE_TAG == "master") {
+                                checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name: "${TIFLASH_HASH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 30], [$class: 'CloneOption', timeout: 60], [$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout'], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, trackingSubmodules: false, reference: '', shallow: true, threads: 8], [$class: 'LocalBranch']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'git@github.com:pingcap/tics.git']]]
+                            } else {
+                                checkout changelog: false, poll: true, scm: [$class: 'GitSCM', branches: [[name: "${RELEASE_TAG}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'CheckoutOption', timeout: 30], [$class: 'CloneOption', timeout: 60], [$class: 'LocalBranch'], [$class: 'CloneOption', noTags: true], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, trackingSubmodules: false, reference: '', shallow: true, threads: 8]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: "+refs/tags/${RELEASE_TAG}:refs/tags/${RELEASE_TAG}", url: 'git@github.com:pingcap/tics.git']]]
+                            }
+                        }
+
+                        sh """
+                        TIFLASH_EDITION=Enterprise NPROC=12 release-centos7/build/build-release.sh
+                        cd release-centos7/
+                        mv tiflash ${target}
+                        tar --exclude=${target}.tar.gz -czvf ${target}.tar.gz ${target}
+                        curl -F ${filepath}=@${target}.tar.gz ${FILE_SERVER_URL}/upload
+                        """
+                    }
+                }
+            }
+        }
+
+    }
+
+    currentBuild.result = "SUCCESS"
+} catch (Exception e) {
+    currentBuild.result = "FAILURE"
+    slackcolor = 'danger'
+    echo "${e}"
+}
+
+stage('Summary') {
+    echo "Send slack here ..."
+    //slackSend channel: "", color: "${slackcolor}", teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+    // if (currentBuild.result != "SUCCESS") {
+    //     slackSend channel: '#jenkins-ci-build-critical', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+    // }
+}
