f175ee21590a45c8f8192431e6d0649c3648b533
purelind <purelind@gmail.com>
2021-04-30 13:31:22 +0800

    debug update code: use relative path to update softlink

--- jenkins/pipelines/update-code.groovy
+++ jenkins/pipelines/update-code.groovy
@@ -12,11 +12,13 @@ node("ci-admin-utils") {
     println "datePart : " + datePart + "\ttimePart : " + timePart
 
     def repoPathMap = [:]
+    def codeArchiveDir = "temp-git-archive"
+    def codeCurrentDir = "temp-git"
 
     stage('Collect info') {
         container("golang") {
-            sh "ls -ld /nfs/cache/git"
-            dir("/nfs/cache/git") {
+            sh "ls -ld /nfs/cache/${codeCurrentDir}"
+            dir("/nfs/cache/${codeCurrentDir}") {
                 def files = findFiles()
                 files.each { f ->
                     if (f.directory) {
@@ -41,12 +43,12 @@ node("ci-admin-utils") {
 
     stage('Copy') {
         container('golang') {
-            res = sh(script: "test -d /nfs/cache/git-archive/${datePart} && echo '1' || echo '0' ", returnStdout: true).trim()
+            res = sh(script: "test -d /nfs/cache/${codeArchiveDir}/${datePart} && echo '1' || echo '0' ", returnStdout: true).trim()
             if (res == '1') {
                 echo "dir already exist, quit job in case of delete current using code cache dir"
                 error("Build failed because of dir exist now: /nfs/cache/git-archive/${datePart}")
             }
-            dir("/nfs/cache/git-archive/${datePart}") {
+            dir("/nfs/cache/${codeArchiveDir}/${datePart}") {
                 repoPathMap.each { entry ->
                     sh """
                         cp -R ${entry.value} ./ && [ -d ${entry.key}/.git ]
@@ -60,27 +62,22 @@ node("ci-admin-utils") {
                     dir("${entry.key}@tmp") {
                         deleteDir()
                     }
-//           sh """
-//             tar -C ${entry.key} -czf src-${entry.key}.tar.gz .
-//             md5sum src-${entry.key}.tar.gz > src-${entry.key}.tar.gz.md5sum
-//           """
                     sh """
                         tar -czf src-${entry.key}.tar.gz ${entry.key}
                         md5sum src-${entry.key}.tar.gz > src-${entry.key}.tar.gz.md5sum
                     """
                 }
             }
-            dir("/nfs/cache/git-archive/${datePart}@tmp") {
+            dir("/nfs/cache/${codeArchiveDir}/${datePart}@tmp") {
                 deleteDir()
             }
         }
     }
 
     stage('Update softlink') {
-        // sh " ln -sfn /nfs/cache/git-archive/${datePart}  /nfs/cache/git-test"
-        // source dir absolute path must be same between nfs host server and agent pod
-        // agent pod nfs volume mount must set "Server path" and "Mount path" to same value
-        sh " ln -sfn /mnt/ci.pingcap.net-nfs/git-archive/${datePart}  /nfs/cache/git"
+        dir("/nfs/cache"){
+            sh " ln -sfn ${codeArchiveDir}/${datePart}  ${codeCurrentDir}"
+        }
     }
 
 }
