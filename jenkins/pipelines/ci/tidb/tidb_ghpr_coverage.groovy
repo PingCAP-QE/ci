be69997fa554df4eee90999fbc46c1c918ad6ee1
Cadmus <CadmusJiang@gmail.com>
2021-05-14 11:05:15 +0800

    fix sync-ci analysis-res is null
    Signed-off-by: Cadmus <CadmusJiang@gmail.com>

--- jenkins/pipelines/ci/tidb/tidb_ghpr_coverage.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_coverage.groovy
@@ -128,7 +128,7 @@ try {
 // excluded from job watch list
 // stage("upload status"){
 //     node{
-//         sh """curl --connect-timeout 2 --max-time 4 -d '{"job":"$JOB_NAME","id":$BUILD_NUMBER}' http://172.16.5.13:36000/api/v1/ci/job/sync || true"""
+//         sh """curl --connect-timeout 2 --max-time 4 -d '{"job":"$JOB_NAME","id":$BUILD_NUMBER}' http://172.16.5.25:36000/api/v1/ci/job/sync || true"""
 //     }
 // }
 
