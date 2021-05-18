ef7b67858e17e0e731d11888f6ce64765de8d073
purelind <purelind@gmail.com>
2021-05-18 11:44:31 +0800

    add cdc_ghpr_integration

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_integration_test.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_integration_test.groovy
@@ -1,23 +1,99 @@
-def script_path = "go/src/github.com/pingcap/ticdc/scripts/jenkins_ci/integration_test_common.groovy"
-def common = load script_path
+echo "release test: ${params.containsKey("release_test")}"
+if (params.containsKey("release_test")) {
+    ghprbActualCommit = params.release_test__cdc_commit
+    ghprbPullId = ""
+    ghprbCommentBody = ""
+    ghprbPullLink = "release-test"
+    ghprbPullTitle = "release-test"
+    ghprbPullDescription = "release-test"
+}
+
+//def ciRepeUrl = "https://github.com/PingCAP-QE/ci.git"
+//def ciRepoBranch = "main"
+def ciRepeUrl = "https://github.com/purelind/ci.git"
+def ciRepoBranch = "patch-ticdc-init"
+def specStr = "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*"
+if (ghprbPullId == null || ghprbPullId == "") {
+    specStr = "+refs/heads/*:refs/remotes/origin/*"
+}
 
 catchError {
-    common.prepare_binaries()
-    common.tests("mysql", "${GO_TEST_SLAVE}")
-    common.coverage()
-    currentBuild.result = "SUCCESS"
-}
+    withEnv(['CODECOV_TOKEN=c6ac8b7a-7113-4b3f-8e98-9314a486e41e',
+             'COVERALLS_TOKEN=HTRawMvXi9p5n4OyBvQygxd5iWjNUKd1o']) {
+        node("${GO_TEST_SLAVE}") {
+            stage('Prepare') {
+                def ws = pwd()
+                deleteDir()
 
-stage('Summary') {
-    def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
-    def slackmsg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
-    "${ghprbPullLink}" + "\n" +
-    "${ghprbPullDescription}" + "\n" +
-    "Integration Test Result: `${currentBuild.result}`" + "\n" +
-    "Elapsed Time: `${duration} mins` " + "\n" +
-    "${env.RUN_DISPLAY_URL}"
+                dir("${ws}/go/src/github.com/pingcap/ticdc") {
+                    if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                        echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ticdc"
+                        deleteDir()
+                    }
+                    try {
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/ticdc.git']]]
+                    } catch (info) {
+                        retry(2) {
+                            echo "checkout failed, retry.."
+                            sleep 5
+                            if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                                deleteDir()
+                            }
+                            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/ticdc.git']]]
+                        }
+                    }
+                    sh "git checkout -f ${ghprbActualCommit}"
+                }
 
-    if (currentBuild.result != "SUCCESS") {
-        slackSend channel: '#jenkins-ci', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+                dir("/home/jenkins/agent/git/ci") {
+                    if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                        echo "Not a valid git folder: /home/jenkins/agent/git/ci"
+                        deleteDir()
+                    }
+                    try {
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepeUrl}"]]]
+                    } catch (info) {
+                        retry(2) {
+                            echo "checkout failed, retry.."
+                            sleep 5
+                            if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                                echo "Not a valid git folder: /home/jenkins/agent/git/ci"
+                                deleteDir()
+                            }
+                            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepeUrl}"]]]
+                        }
+                    }
+
+                }
+
+                stash includes: "go/src/github.com/pingcap/ticdc/**", name: "ticdc", useDefaultExcludes: false
+            }
+
+            def script_path = "/home/jenkins/agent/git/ci/jenkins/pipelines/ci/ticdc/integration_test_common.groovy"
+            def common = load script_path
+            catchError {
+                common.prepare_binaries()
+                common.tests("mysql", "${GO_TEST_SLAVE}")
+                common.coverage()
+                currentBuild.result = "SUCCESS"
+            }
+
+            stage('Summary') {
+                def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+                def slackmsg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
+                        "${ghprbPullLink}" + "\n" +
+                        "${ghprbPullDescription}" + "\n" +
+                        "Integration Test Result: `${currentBuild.result}`" + "\n" +
+                        "Elapsed Time: `${duration} mins` " + "\n" +
+                        "${env.RUN_DISPLAY_URL}"
+
+                if (currentBuild.result != "SUCCESS") {
+                    slackSend channel: '#jenkins-ci', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+                }
+            }
+        }
     }
 }
+
+
+
