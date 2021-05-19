754112f1e5cca35be1d5fc91acd8a236b84d6e9f
Cadmus <CadmusJiang@gmail.com>
2021-05-19 19:11:35 +0800

    master and 5.x use go1.16, other use go1.13
    Signed-off-by: Cadmus <CadmusJiang@gmail.com>

--- jenkins/pipelines/ci/tidb/tidb_ghpr_unit_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_unit_test.groovy
@@ -38,6 +38,22 @@ if (params.containsKey("release_test")) {
     specStr = "+refs/heads/*:refs/remotes/origin/*"
 }
 
+@Library("pingcap") _
+
+println "$GO1160_BUILD_SLAVE"
+println "$GO1160_TEST_SLAVE"
+
+def isNeedGo1160 = isBranchMatched(["master", "release-5."], ghprbTargetBranch)
+if (isNeedGo1160) {
+    println "This build use go1.16"
+    GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
+    GO_TEST_SLAVE = GO1160_TEST_SLAVE
+} else {
+    println "This build use go1.13"
+}
+println "BUILD_NODE_NAME=${GO_BUILD_SLAVE}"
+println "TEST_NODE_NAME=${GO_TEST_SLAVE}"
+
 try {
     timestamps {
         stage("Pre-check"){
