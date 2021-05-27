c3640c9ae6c211ba66d8f5c7bcf79708d718909a
purelind <purelind@gmail.com>
2021-05-27 15:21:06 +0800

    remove useless print message

--- jenkins/pipelines/ci/tikv/tikv_check_release_note.groovy
+++ jenkins/pipelines/ci/tikv/tikv_check_release_note.groovy
@@ -8,9 +8,6 @@ def boolean isBranchMatched(List<String> branches, String targetBranch) {
     return false
 }
 
-println "$GO1160_BUILD_SLAVE"
-println "$GO1160_TEST_SLAVE"
-
 def isNeedGo1160 = isBranchMatched(["master"], ghprbTargetBranch)
 if (isNeedGo1160) {
     println "This build use go1.16 because ghprTargetBranch=master"
