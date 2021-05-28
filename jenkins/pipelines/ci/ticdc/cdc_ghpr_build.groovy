c4fb580d0c8872dc65a2764e4ce39d558461102c
amyangfei <amyangfei@gmail.com>
2021-05-28 18:42:04 +0800

    ci: remove GO111MODULE=off in ticdc pipelines (#232)

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_build.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_build.groovy
@@ -90,7 +90,7 @@ catchError {
 
                         dir("go/src/github.com/pingcap/ticdc") {
                             sh """
-                                GO111MODULE=off GOPATH=\$GOPATH:${ws}/go PATH=\$GOPATH/bin:${ws}/go/bin:\$PATH make
+                                GOPATH=\$GOPATH:${ws}/go PATH=\$GOPATH/bin:${ws}/go/bin:\$PATH make
                             """
                         }
                     }
