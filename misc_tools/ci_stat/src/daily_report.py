0d9ac70bd363bdc7fa19974642fa84bbde019adb
MartinNose <liucmartin@qq.com>
2021-06-03 11:46:55 +0800

    daily report bot done

--- misc_tools/ci_stat/src/daily_report.py
+++ misc_tools/ci_stat/src/daily_report.py
@@ -1,9 +1,13 @@
 import ci_stat as stat
+from ci_stat import RedirectStdStreams
 import json
 import os
 import copy
 from datetime import datetime,date,timedelta
 from hourly_bot import send, bot_post, add_content
+import schedule
+import time
+from pathlib import Path
 
 def append_rerun(card, res):
     pr_map = res.pr_map
@@ -29,7 +33,7 @@ def append_rerun(card, res):
         add_content(fields[6], "**🟡: **" + str(job.abort_cnt) + " ")
         add_content(fields[6], "**🔄: **" + str(job.rerun_cnt) + " ")
         rerun_list = list(filter(lambda x: x[1].rerun_cnt(job_name) > 0, sorted(job.local_prs.items(), key=lambda x: x[1].rerun_cnt(job_name), reverse=True)))
-        for pr_number, pr in rerun_list[:5]:
+        for pr_number, pr in rerun_list[:3]:
             add_content(fields[6], "\n  " + pr.repo + " [#" + str(pr.number) + "]("+pr.link +") **rerun cnt:** " + str(pr.rerun_cnt(job_name)))
             run_list = list(filter(lambda x: x.job_name == job_name and len(x.get_fail_info()) > 0, pr.runs))
             for run in run_list[:3]:
@@ -39,16 +43,13 @@ def append_rerun(card, res):
                     add_content(fields[6], "\n             " + info)
             if len(pr.runs) > 3 and len(run_list) > 0:
                 add_content(fields[6], "\n         ... **" + str(pr.rerun_cnt(job_name) + 1 - min(3, len(run_list))) + "** more runs. ")
-
-        if len(rerun_list) > 4:
+        if len(rerun_list) > 3:
             add_content(fields[6], "\n... **" + str(len(rerun_list) - 3) + "** *more rerunning prs and totally* **" + str(job.rerun_cnt) + "** *reran runs.*")
     if len(job_list) > 10:
         add_content(fields[6], "\n... **" + str(len(job_list) - 10) + "** *more rerunning jobs.")
     return card
 
 def main():
-    # env = json.load(open(os.getenv("STAT_ENV_PATH") + "/env.json"))
-
     end_time = date.today()
     begin_time = end_time - timedelta(days=1)
     res = stat.get_result(begin_time, end_time)
@@ -71,6 +72,21 @@ def main():
     print(json.dumps(card))
     bot_post(card)
 
+def daily_report():
+    print((date.today() - timedelta(days=1)).strftime('%Y-%m-%d-%H:%M:%S') + " Dailt report running")
+    Path("/root/ci_stat/daily_log/").mkdir(parents=True, exist_ok=True)
+    tmp_res = "/root/ci_stat/daily_log/" + (date.today() - timedelta(days=1)).strftime('%Y-%m-%d-%H:%M:%S') + ".log"
+    with RedirectStdStreams(stdout=open(tmp_res, "w")):
+        res = main()
+
 
 if __name__ == "__main__":
-    main()
+    schedule.every().day.at("09:49").do(daily_report)
+
+    try:
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
+        pass
+    # main()
