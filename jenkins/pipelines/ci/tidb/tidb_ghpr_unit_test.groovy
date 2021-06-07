4744028e31792c1dd05b2b723c74c5db8710377f
purelind <purelind@gmail.com>
2021-06-07 18:29:11 +0800

    ci release-5.1 use go1.16.4

--- jenkins/pipelines/ci/tidb/tidb_ghpr_unit_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_unit_test.groovy
@@ -51,7 +51,7 @@ def boolean isBranchMatched(List<String> branches, String targetBranch) {
 println "$GO1160_BUILD_SLAVE"
 println "$GO1160_TEST_SLAVE"
 
-def isNeedGo1160 = isBranchMatched(["master"], ghprbTargetBranch)
+def isNeedGo1160 = isBranchMatched(["master", "release-5.1"], ghprbTargetBranch)
 if (isNeedGo1160) {
     println "This build use go1.16"
     GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
