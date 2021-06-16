62edd6ae26f08a61bbd5f828f3ec8ec097f46306
zhouqiang <zhouqiang.cl@gmail.com>
2021-06-16 12:17:21 +0800

    Revert "update commit status (#277)"
    This reverts commit 5525fad81058900bdc23f59ed54c2a797c6b175c.

--- jenkins/pipelines/ci/tidb/tidb_ghpr_integration_copr_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_integration_copr_test.groovy
@@ -239,37 +239,7 @@ finally {
 }
 
 stage("upload status"){
-    node("master") {
+    node{
         sh """curl --connect-timeout 2 --max-time 4 -d '{"job":"$JOB_NAME","id":$BUILD_NUMBER}' http://172.16.5.25:36000/api/v1/ci/job/sync || true"""
-    }
-}
-
-if (params.containsKey("triggered_by_upstream_ci")) {
-    stage("update commit status") {
-        node("master") {
-            if (currentBuild.result = "ABORTED") {
-                PARAM_DESCRIPTION = 'Jenkins job aborted'
-                // Commit state. Possible values are 'pending', 'success', 'error' or 'failure'
-                PARAM_STATUS = 'error'
-            } else if (currentBuild.result = "FAILURE") {
-                PARAM_DESCRIPTION = 'Jenkins job failed'
-                PARAM_STATUS = 'failure'
-            } else if (currentBuild.result = "SUCCESS") {
-                PARAM_DESCRIPTION = 'Jenkins job success'
-                PARAM_STATUS = 'success'
-            } else {
-                PARAM_DESCRIPTION = 'Jenkins job meets something wrong'
-                PARAM_STATUS = 'error'
-            }
-            def default_params = [
-                    string(name: 'TIDB_COMMIT_ID', value: ghprbActualCommit ),
-                    string(name: 'CONTEXT', value: 'idc-jenkins-ci-tidb/integration-copr-test'),
-                    string(name: 'DESCRIPTION', value: PARAM_DESCRIPTION ),
-                    string(name: 'BUILD_URL', value: RUN_DISPLAY_URL ),
-                    string(name: 'STATUS', value: PARAM_STATUS ),
-            ]
-            echo("default params: ${default_params}")
-            build(job: "tidb_update_commit_status", parameters: default_params, wait: true)
-        }
     }
 }
