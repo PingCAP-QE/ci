4b6cb2009367f8f3fcb9d1831156cda533593bac
purelind <purelind@gmail.com>
2021-06-25 11:31:46 +0800

    fix: correct status context

--- jenkins/pipelines/ci/tikv/tikv_ghpr_test.groovy
+++ jenkins/pipelines/ci/tikv/tikv_ghpr_test.groovy
@@ -394,7 +394,7 @@ if (params.containsKey("triggered_by_upstream_ci")) {
             }
             def default_params = [
                     string(name: 'TIKV_COMMIT_ID', value: ghprbActualCommit ),
-                    string(name: 'CONTEXT', value: 'idc-jenkins-ci-tikv/test'),
+                    string(name: 'CONTEXT', value: 'idc-jenkins-ci/test'),
                     string(name: 'DESCRIPTION', value: PARAM_DESCRIPTION ),
                     string(name: 'BUILD_URL', value: RUN_DISPLAY_URL ),
                     string(name: 'STATUS', value: PARAM_STATUS ),
