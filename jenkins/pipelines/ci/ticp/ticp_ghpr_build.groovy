7c1385e4f997c5f33e4baa9cd0b93a2ac8ec1c74
Sen Han <00hnes@gmail.com>
2021-06-30 22:53:42 +0800

    add ticp ci support

---
+++ jenkins/pipelines/ci/ticp/ticp_ghpr_build.groovy
@@ -0,0 +1,96 @@
+def boolean isBranchMatched(List<String> branches, String targetBranch) {
+    for (String item : branches) {
+        if (targetBranch.startsWith(item)) {
+            println "targetBranch=${targetBranch} matched in ${branches}"
+            return true
+        }
+    }
+    return false
+}
+
+def isNeedGo1160 = true
+if (isNeedGo1160) {
+    println "This build use go1.16"
+    GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
+    GO_TEST_SLAVE = GO1160_TEST_SLAVE
+} else {
+    println "This build use go1.13"
+}
+println "BUILD_NODE_NAME=${GO_BUILD_SLAVE}"
+println "TEST_NODE_NAME=${GO_TEST_SLAVE}"
+
+node("${GO_BUILD_SLAVE}") {
+  def ws = pwd()
+  deleteDir()
+
+    dir("${ws}/go/src/github.com/pingcap/ticp") {
+        stage("Prepare"){
+            container("golang") {
+                println "debug command:\nkubectl -n jenkins-ci exec -ti ${NODE_NAME} bash" 
+
+                if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                    deleteDir()
+                }
+                try {
+                    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:pingcap/ticp.git']]]
+                } catch (error) {
+                    retry(2) {
+                        echo "checkout failed, retry.."
+                        sleep 5
+                        if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                            deleteDir()
+                        }
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:pingcap/ticp.git']]]
+                    }
+                }
+
+                sh "git checkout -f ${ghprbActualCommit}"
+            }
+        }
+    }
+
+    catchError {
+        container("golang") {
+            dir("go/src/github.com/pingcap/ticp") {
+                stage('Build') {
+                    sh """
+                        GOPATH=\$GOPATH:${ws}/go make build
+                    """
+                }
+                stage("Upload") {
+                    def filepath = "builds/pingcap/ticp/pr/${ghprbActualCommit}/centos7/ticp.tar.gz"
+                    def refspath = "refs/pingcap/ticp/pr/${ghprbPullId}/sha1"
+
+                    timeout(10) {
+                        sh """
+                        rm -rf .git
+                        tar --exclude=ticp.tar.gz -czvf ticp.tar.gz bin/*
+                        curl -F ${filepath}=@ticp.tar.gz ${FILE_SERVER_URL}/upload
+                        echo "pr/${ghprbActualCommit}" > sha1
+                        curl -F ${refspath}=@sha1 ${FILE_SERVER_URL}/upload
+                        """
+                        // cleanup
+                        sh "rm -rf sha1 ticp.tar.gz"
+                    }
+                }
+            }
+        }
+
+        currentBuild.result = "SUCCESS"
+    }
+
+    stage('Summary') {
+        def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+        def slackmsg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
+        "${ghprbPullLink}" + "\n" +
+        "${ghprbPullDescription}" + "\n" +
+        "Build Result: `${currentBuild.result}`" + "\n" +
+        "Elapsed Time: `${duration} mins` " + "\n" +
+        "${env.RUN_DISPLAY_URL}"
+
+        if (currentBuild.result != "SUCCESS") {
+            slackSend channel: '#jenkins-ci', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+        }
+    }
+}
+
---
+++ jenkins/pipelines/ci/ticp/ticp_ghpr_test.groovy
@@ -0,0 +1,78 @@
+def boolean isBranchMatched(List<String> branches, String targetBranch) {
+    for (String item : branches) {
+        if (targetBranch.startsWith(item)) {
+            println "targetBranch=${targetBranch} matched in ${branches}"
+            return true
+        }
+    }
+    return false
+}
+
+def isNeedGo1160 = true
+if (isNeedGo1160) {
+    println "This build use go1.16"
+    GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
+    GO_TEST_SLAVE = GO1160_TEST_SLAVE
+} else {
+    println "This build use go1.13"
+}
+println "BUILD_NODE_NAME=${GO_BUILD_SLAVE}"
+println "TEST_NODE_NAME=${GO_TEST_SLAVE}"
+
+node("${GO_TEST_SLAVE}") {
+    def ws = pwd()
+    deleteDir()
+  
+    dir("${ws}/go/src/github.com/pingcap/ticp") {
+        stage("Prepare"){
+            container("golang") {
+                println "debug command:\nkubectl -n jenkins-ci exec -ti ${NODE_NAME} bash" 
+
+                if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                    deleteDir()
+                }
+                try {
+                    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:pingcap/ticp.git']]]
+                } catch (error) {
+                    retry(2) {
+                        echo "checkout failed, retry.."
+                        sleep 5
+                        if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                            deleteDir()
+                        }
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:pingcap/ticp.git']]]
+                    }
+                }
+
+                sh "git checkout -f ${ghprbActualCommit}"
+            }
+        }
+    }
+    
+    catchError {
+        stage("Test") {
+            container("golang") {
+                dir("go/src/github.com/pingcap/ticp") {
+                    sh """
+                        GOPATH=\$GOPATH:${ws}/go PATH=\$GOPATH/bin:${ws}/go/bin:\$PATH make test
+                    """
+                }
+            }   
+        }
+        currentBuild.result = "SUCCESS"
+    }
+
+    stage('Summary') {
+        def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+        def slackmsg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
+                "${ghprbPullLink}" + "\n" +
+                "${ghprbPullDescription}" + "\n" +
+                "Test Result: `${currentBuild.result}`" + "\n" +
+                "Elapsed Time: `${duration} mins` " + "\n" +
+                "${env.RUN_DISPLAY_URL}"
+
+        if (currentBuild.result != "SUCCESS") {
+            slackSend channel: '#jenkins-ci', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+        }
+    }
+}
