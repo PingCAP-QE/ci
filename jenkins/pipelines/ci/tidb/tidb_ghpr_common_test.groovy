1c871d29b2e08d195e8242dbf8e659a460d89d56
Lifu Wu <purelind@users.noreply.github.com>
2021-07-27 10:30:37 +0800

    branch hz-poc use go1164 (#340)

--- jenkins/pipelines/ci/tidb/tidb_ghpr_common_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_common_test.groovy
@@ -35,7 +35,7 @@ def boolean isBranchMatched(List<String> branches, String targetBranch) {
     return false
 }
 
-def isNeedGo1160 = isBranchMatched(["master", "release-5.1"], ghprbTargetBranch)
+def isNeedGo1160 = isBranchMatched(["master", "release-5.1", "hz-poc"], ghprbTargetBranch)
 if (isNeedGo1160) {
     println "This build use go1.16"
     GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
