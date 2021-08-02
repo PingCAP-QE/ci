1456a920b47ca1450fc8ab267da5e7235a9d7588
3pointer <luancheng@pingcap.com>
2021-08-02 19:52:42 +0800

    debug remove

--- jenkins/pipelines/ci/br/br_ghpr_unit_and_integration_test.groovy
+++ jenkins/pipelines/ci/br/br_ghpr_unit_and_integration_test.groovy
@@ -535,9 +535,6 @@ catchError {
                         specStr = "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*"
                     }
 
-                    println "ghprbPullId: ${ghprbPullId}"
-                    println "specStr: ${specStr}"
-
                     checkout changelog: false, poll: false, scm: [$class: 'GitSCM', shallow: true, branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: git_repo_url]]]
 
                     sh label: "Build and Compress testing binaries", script: """
