f87373898e82cecea68c6abaee263c59af6a82f6
3pointer <luancheng@pingcap.com>
2021-08-02 14:03:02 +0800

    refine TiDB's ci for BR merged into TiDB

--- jenkins/pipelines/ci/tidb/tidb_ghpr_integration_br_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_integration_br_test.groovy
@@ -14,35 +14,38 @@ def tidb_url = "${FILE_SERVER_URL}/download/builds/pingcap/tidb/pr/${ghprbActual
 
 catchError {
     node("${GO_TEST_SLAVE}") {
+        // After BR merged into TiDB, every PR should trigger this test.
         stage('Trigger BRIE Test') {
-            if (ghprbTargetBranch == "master" || ghprbTargetBranch == "release-4.0" || ghprbTargetBranch == "release-5.0") {
-                container("golang") {
-                    println "debug command:\nkubectl -n jenkins-ci exec -ti ${NODE_NAME} bash"
-                    // Wait build finish.
-                    timeout(60) {
-                        sh """
-                        while ! curl --output /dev/null --silent --head --fail "${tidb_url}"; do sleep 5; done
-                        """
-                    }
-
-                    def default_params = [
-                            booleanParam(name: 'force', value: true),
-                            string(name: 'triggered_by_upstream_pr_ci', value: "tidb"),
-                            string(name: 'upstream_pr_ci_ghpr_target_branch', value: "${ghprbTargetBranch}"),
-                            // We tests BR on the same branch as TiDB's.
-                            string(name: 'upstream_pr_ci_ghpr_actual_commit', value: "${ghprbTargetBranch}"),
-                            string(name: 'upstream_pr_ci_ghpr_pull_id', value: "${ghprbPullId}"),
-                            string(name: 'upstream_pr_ci_ghpr_pull_title', value: "${ghprbPullTitle}"),
-                            string(name: 'upstream_pr_ci_ghpr_pull_link', value: "${ghprbPullLink}"),
-                            string(name: 'upstream_pr_ci_ghpr_pull_description', value: "${ghprbPullDescription}"),
-                            string(name: 'upstream_pr_ci_override_tidb_download_link', value: "${tidb_url}"),
-                    ]
-
-                    // Trigger BRIE test without waiting its finish.
-                    build(job: "br_ghpr_unit_and_integration_test", parameters: default_params, wait: true)
+            container("golang") {
+                println "debug command:\nkubectl -n jenkins-ci exec -ti ${NODE_NAME} bash"
+                // Wait build finish.
+                timeout(60) {
+                    sh """
+                    while ! curl --output /dev/null --silent --head --fail "${tidb_url}"; do sleep 5; done
+                    """
                 }
-            } else {
-                println "skip trigger BRIE tests as this PR targets to ${ghprbTargetBranch}"
+
+                def default_params = [
+                        booleanParam(name: 'force', value: true),
+                        // "Origin" means run every br integration tests.
+                        string(name: 'triggered_by_upstream_pr_ci', value: "Origin"),
+                        string(name: 'upstream_pr_ci_ghpr_target_branch', value: "${ghprbTargetBranch}"),
+                        // We tests BR on the same branch as TiDB's.
+                        string(name: 'upstream_pr_ci_ghpr_actual_commit', value: "${ghprbTargetBranch}"),
+                        string(name: 'upstream_pr_ci_ghpr_pull_id', value: "${ghprbPullId}"),
+                        string(name: 'upstream_pr_ci_ghpr_pull_title', value: "${ghprbPullTitle}"),
+                        string(name: 'upstream_pr_ci_ghpr_pull_link', value: "${ghprbPullLink}"),
+                        string(name: 'upstream_pr_ci_ghpr_pull_description', value: "${ghprbPullDescription}"),
+                        string(name: 'upstream_pr_ci_override_tidb_download_link', value: "${tidb_url}"),
+                ]
+
+                // these three branch don't have br integrated.
+                if (ghprbTargetBranch == "release-4.0" || ghprbTargetBranch == "release-5.0" || ghprbTargetBranch == "release-5.1") {
+                    default_params.triggered_by_upstream_pr_ci = "tidb"
+
+                }
+                // Trigger BRIE test without waiting its finish.
+                build(job: "br_ghpr_unit_and_integration_test", parameters: default_params, wait: true)
             }
             currentBuild.result = "SUCCESS"
         }
