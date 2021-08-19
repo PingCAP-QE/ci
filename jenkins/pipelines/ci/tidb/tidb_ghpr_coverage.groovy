c7505aa440b850d8ea35860e3cfaea7716de5333
Lifu Wu <purelind@users.noreply.github.com>
2021-08-19 13:31:21 +0800

    fix tidb coverage (#390)
    * tidb: fix coverage para project absolute path

--- jenkins/pipelines/ci/tidb/tidb_ghpr_coverage.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_coverage.groovy
@@ -1,3 +1,14 @@
+echo "release test: ${params.containsKey("release_test")}"
+if (params.containsKey("release_test")) {
+    ghprbTargetBranch = params.getOrDefault("release_test__ghpr_target_branch", params.release_test__release_branch)
+    ghprbCommentBody = params.getOrDefault("release_test__ghpr_comment_body", "")
+    ghprbActualCommit = params.getOrDefault("release_test__ghpr_actual_commit", params.release_test__tidb_commit)
+    ghprbPullId = params.getOrDefault("release_test__ghpr_pull_id", "")
+    ghprbPullTitle = params.getOrDefault("release_test__ghpr_pull_title", "")
+    ghprbPullLink = params.getOrDefault("release_test__ghpr_pull_link", "")
+    ghprbPullDescription = params.getOrDefault("release_test__ghpr_pull_description", "")
+}
+
 @NonCPS
 boolean isMoreRecentOrEqual( String a, String b ) {
     if (a == b) {
@@ -40,64 +51,47 @@ if (!isNeedGo1160 && ghprbTargetBranch.startsWith("release-")) {
 if (isNeedGo1160) {
     println "This build use go1.16"
     GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
-    GO_TEST_SLAVE = GO1160_TEST_SLAVE
 } else {
     println "This build use go1.13"
 }
 println "BUILD_NODE_NAME=${GO_BUILD_SLAVE}"
-println "TEST_NODE_NAME=${GO_TEST_SLAVE}"
 
-def notRun = 1
-if (!params.force){
-    node("${GO_BUILD_SLAVE}"){
-		container("golang"){
-		 	notRun = sh(returnStatus: true, script: """
-			if curl --output /dev/null --silent --head --fail ${FILE_SERVER_URL}/download/ci_check/${JOB_NAME}/${ghprbActualCommit}; then exit 0; else exit 1; fi
-			""")   
-		}
-	}
+
+def specStr = "+refs/heads/*:refs/remotes/origin/*"
+if (ghprbPullId != null && ghprbPullId != "") {
+    specStr = "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*"
 }
 
-if (notRun == 0){
-	println "the ${ghprbActualCommit} has been tested"
-	return
-}
 def slackcolor = 'good'
 def githash
-env.TRAVIS_COVERAGE = 1
-env.CODECOV_TOKEN = '2114fff2-bd95-43eb-9483-a351f0184eae'
 
 try {
 
     def buildSlave = "${GO_BUILD_SLAVE}"
-    def testSlave = "${GO_TEST_SLAVE}"
-
 
     node(buildSlave) {
-
         def ws = pwd()
-        // deleteDir()
+        //deleteDir()
+
+        stage("debuf info"){
+            println "debug command:\nkubectl -n jenkins-ci exec -ti ${NODE_NAME} bash"
+            println "work space path:\n${ws}"
+        }
 
         stage("Checkout") {
-
-            // container("root") {
-            //     sh "chown -R 10000:10000 ./"
-            // }
-
             container("golang") {
                 sh "whoami && go version"
             }
-
             // update code
             dir("/home/jenkins/agent/code-archive") {
                 // delete to clean workspace in case of agent pod reused lead to conflict.
                 deleteDir()
                 // copy code from nfs cache
                 container("golang") {
-                    if(fileExists("/nfs/cache/git-test/src-tidb.tar.gz")){
+                    if(fileExists("/home/jenkins/agent/ci-cached-code-daily/src-tidb.tar.gz")){
                         timeout(5) {
                             sh """
-                                cp -R /nfs/cache/git-test/src-tidb.tar.gz*  ./
+                                cp -R /home/jenkins/agent/ci-cached-code-daily/src-tidb.tar.gz*  ./
                                 mkdir -p ${ws}/go/src/github.com/pingcap/tidb
                                 tar -xzf src-tidb.tar.gz -C ${ws}/go/src/github.com/pingcap/tidb --strip-components=1
                             """
@@ -105,23 +99,8 @@ try {
                     }
                 }
                 dir("${ws}/go/src/github.com/pingcap/tidb") {
-                    if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                        echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/tidb"
-                        echo "Clean dir then get tidb src code from fileserver"
-                        deleteDir()
-                    }
-                    if(!fileExists("${ws}/go/src/github.com/pingcap/tidb/Makefile")) {
-                        dir("${ws}/go/src/github.com/pingcap/tidb") {
-                            sh """
-                                rm -rf /home/jenkins/agent/code-archive/tidb.tar.gz
-                                rm -rf /home/jenkins/agent/code-archive/tidb
-                                wget -O /home/jenkins/agent/code-archive/tidb.tar.gz  ${FILE_SERVER_URL}/download/source/tidb.tar.gz -q --show-progress
-                                tar -xzf /home/jenkins/agent/code-archive/tidb.tar.gz -C ./ --strip-components=1
-                            """
-                        }
-                    }
                     try {
-                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', timeout: 2]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tidb.git']]]
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', timeout: 10]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tidb.git']]]
                     }   catch (info) {
                             retry(2) {
                                 echo "checkout failed, retry.."
@@ -129,28 +108,17 @@ try {
                                 if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
                                     deleteDir()
                                 }
-                                // if checkout one pr failed, we fallback to fetch all thre pr data
-                                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tidb.git']]]
+                                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', timeout: 10]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tidb.git']]]
                             }
-                        }
-                    sh "git checkout -f ${ghprbActualCommit}"
-                }
-            }
-        }
-
-        stage("Build & Test") {
-            if(ghprbTargetBranch == "master" || ghprbTargetBranch.startsWith("release-3")) {
-                dir("go/src/github.com/pingcap/tidb") {
+                    }
                     container("golang") {
-                        timeout(30) {
+                        def tidb_path = "${ws}/go/src/github.com/pingcap/tidb"
+                        timeout(5) {
                             sh """
-                            set +x
-                            export CODECOV_TOKEN='2114fff2-bd95-43eb-9483-a351f0184eae'
-                            export TRAVIS_COVERAGE=1
-                            set -x
-                            # we will change TiDB Makefile directly after the actual effect is stable
-                            sed -ir 's/bash <(curl -s https:\\/\\/codecov.io\\/bash)/curl -s https:\\/\\/codecov.io\\/bash | bash -s -- -X s3/g' Makefile
-                            make gotest upload-coverage
+                            git checkout -f ${ghprbActualCommit}
+                            """
+                            sh """
+                            sed -ir "s:-project=github.com/pingcap/tidb:-project=${tidb_path}:g" Makefile
                             """
                         }
                     }
@@ -158,40 +126,60 @@ try {
             }
         }
 
+
+        stage("Test & Coverage") {
+            def tidb_path = "${ws}/go/src/github.com/pingcap/tidb"
+            dir("go/src/github.com/pingcap/tidb") {
+                container("golang") {
+                    withCredentials([string(credentialsId: 'codecov-token-tidb', variable: 'CODECOV_TOKEN')]) {
+                        timeout(30) {
+                            sh '''
+                            set +x
+                            export CODECOV_TOKEN=${CODECOV_TOKEN}
+                            export TRAVIS_COVERAG=1
+                            set -x 
+                            make gotest
+                            make upload-coverages
+                            '''
+                        }
+                    }
+                }
+            }
+        }
     }
     currentBuild.result = "SUCCESS"
-    node("${GO_BUILD_SLAVE}"){
-		container("golang"){
-		    sh """
-		    echo "done" > done
-		    curl -F ci_check/${JOB_NAME}/${ghprbActualCommit}=@done ${FILE_SERVER_URL}/upload
-		    """
-	    }
-	}
 } catch (Exception e) {
     currentBuild.result = "FAILURE"
     slackcolor = 'danger'
     echo "${e}"
 }
 
-// excluded from job watch list
-// stage("upload status"){
-//     node{
-//         sh """curl --connect-timeout 2 --max-time 4 -d '{"job":"$JOB_NAME","id":$BUILD_NUMBER}' http://172.16.5.25:36000/api/v1/ci/job/sync || true"""
-//     }
-// }
-
-stage('Summary') {
-    echo "Send slack here ..."
-    def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
-    def slackmsg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
-    "${ghprbPullLink}" + "\n" +
-    "${ghprbPullDescription}" + "\n" +
-    "Build Result: `${currentBuild.result}`" + "\n" +
-    "Elapsed Time: `${duration} mins` " + "\n" +
-    "${env.RUN_DISPLAY_URL}"
-
-    if (currentBuild.result != "SUCCESS") {
-        slackSend channel: '#jenkins-ci', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+if (params.containsKey("triggered_by_upstream_ci")) {
+    stage("update commit status") {
+        node("master") {
+            if (currentBuild.result == "ABORTED") {
+                PARAM_DESCRIPTION = 'Jenkins job aborted'
+                // Commit state. Possible values are 'pending', 'success', 'error' or 'failure'
+                PARAM_STATUS = 'error'
+            } else if (currentBuild.result == "FAILURE") {
+                PARAM_DESCRIPTION = 'Jenkins job failed'
+                PARAM_STATUS = 'failure'
+            } else if (currentBuild.result == "SUCCESS") {
+                PARAM_DESCRIPTION = 'Jenkins job success'
+                PARAM_STATUS = 'success'
+            } else {
+                PARAM_DESCRIPTION = 'Jenkins job meets something wrong'
+                PARAM_STATUS = 'error'
+            }
+            def default_params = [
+                    string(name: 'TIDB_COMMIT_ID', value: ghprbActualCommit ),
+                    string(name: 'CONTEXT', value: 'idc-jenkins-ci-tidb/code-coverage'),
+                    string(name: 'DESCRIPTION', value: PARAM_DESCRIPTION ),
+                    string(name: 'BUILD_URL', value: RUN_DISPLAY_URL ),
+                    string(name: 'STATUS', value: PARAM_STATUS ),
+            ]
+            echo("default params: ${default_params}")
+            build(job: "tidb_update_commit_status", parameters: default_params, wait: true)
+        }
     }
 }
