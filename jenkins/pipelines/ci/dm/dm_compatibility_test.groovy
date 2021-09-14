c7f317140684fdc7a0ca45f9bf5163e02902efdf
purelind <purelind@gmail.com>
2021-09-14 20:31:29 +0800

    dm: correct image project

--- jenkins/pipelines/ci/dm/dm_compatibility_test.groovy
+++ jenkins/pipelines/ci/dm/dm_compatibility_test.groovy
@@ -68,7 +68,7 @@ if (isNeedGo1160) {
     println "This build use go1.16"
     GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
     GO_TEST_SLAVE = GO1160_TEST_SLAVE
-    POD_GO_DOCKER_IMAGE = "hub.pingcap.net/pingcap/centos7_golang-1.16:latest"
+    POD_GO_DOCKER_IMAGE = "hub.pingcap.net/jenkins/centos7_golang-1.16:latest"
 } else {
     println "This build use go1.13"
     POD_GO_DOCKER_IMAGE = "hub.pingcap.net/jenkins/centos7_golang-1.13:cached"
