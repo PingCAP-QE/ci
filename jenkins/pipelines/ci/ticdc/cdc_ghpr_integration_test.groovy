4d66b1595ff7c3aba29959dca515db9d23d27ecb
purelind <purelind@gmail.com>
2021-09-17 15:36:56 +0800

    cdc: ajuest request memory

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_integration_test.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_integration_test.groovy
@@ -135,7 +135,7 @@ catchError {
                                 containerTemplate(
                                         name: 'golang', alwaysPullImage: true,
                                         image: "${POD_GO_DOCKER_IMAGE}", ttyEnabled: true,
-                                        resourceRequestCpu: '4000m', resourceRequestMemory: '8Gi',
+                                        resourceRequestCpu: '4000m', resourceRequestMemory: '12Gi',
                                         resourceLimitCpu: '30000m', resourceLimitMemory: "20Gi",
                                         command: '/bin/sh -c', args: 'cat',
                                         envVars: [containerEnvVar(key: 'GOMODCACHE', value: '/nfs/cache/mod'),
