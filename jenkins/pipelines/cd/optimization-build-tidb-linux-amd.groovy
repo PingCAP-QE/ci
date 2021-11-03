02e444ba3c02117b221e16c01ffd94fbfb77c72f
zhouqiang <zhouqiang.cl@gmail.com>
2021-11-03 22:49:19 +0800

    fix repo error (#460)

--- jenkins/pipelines/cd/optimization-build-tidb-linux-amd.groovy
+++ jenkins/pipelines/cd/optimization-build-tidb-linux-amd.groovy
@@ -396,6 +396,10 @@ try {
                     if (RELEASE_TAG >= "v5.3.0" ) {
                         repo = "git@github.com:pingcap/tidb.git"
                     }
+                    
+                    if (RELEASE_TAG >= "v5.3.0" && product == "dumpling") {
+                        repo = "git@github.com:pingcap/tidb.git"
+                    }
 
                     dir("go/src/github.com/pingcap/dumpling") {
                         if (sh(returnStatus: true, script: '[ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1') != 0) {
