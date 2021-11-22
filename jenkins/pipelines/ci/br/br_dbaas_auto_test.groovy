7a628064ffee1c2220dea996ae0c302af1e0d12b
fengou1 <85682690+fengou1@users.noreply.github.com>
2021-11-22 11:53:17 +0800

    Enhancement tools DBaaS CI (#474)
    * remove test cases those could added into test infra and merged CI
    
    * add dbaas and s3 automation test
    
    * update
    
    * update the dbaas automation
    
    * add report
    
    * update dbase

--- jenkins/pipelines/ci/br/br_dbaas_auto_test.groovy
+++ jenkins/pipelines/ci/br/br_dbaas_auto_test.groovy
@@ -1,44 +1,94 @@
 
+access_token = ""
+cloud_tier = "aws"
 catchError {
-    def test_case_names = []
-    node("build_go1160") {
-            container("golang") {
-                println "debug command:\nkubectl -n jenkins-tidb exec -ti ${NODE_NAME} bash"
-                def ws = pwd()
-                deleteDir()
+     podTemplate(name: "build_go1160-${BUILD_NUMBER}", label: "build_go1160-${BUILD_NUMBER}", instanceCap: 10, containers: [
+        containerTemplate(name: 'golang', image: 'registry-mirror.pingcap.net/library/golang:1.16', alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'selenium', image: 'elgalu/selenium', alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
+    ]) {
+        node("build_go1160-${BUILD_NUMBER}") {
+                stage("Checkout") {
+                    container("selenium") { sh("chown -R 1000:1000 ./") }
 
-                // Checkout and build testing binaries.
-                dir("${ws}go/src/github.com/AutoTest") {
-                    stage('Prepare') {
-                        if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                            deleteDir()
+                    checkout(changelog: false, poll: false, scm: [
+                        $class           : "GitSCM",
+                        branches         : [[name: "*/main"]],
+                        userRemoteConfigs: [[url: "https://github.com/fengou1/AutoTest.git",
+                                            credentialsId: "FengOu"]],
+                        extensions       : [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']],
+                    ])
+                }
+
+                stage("GetToken") {
+                        container("selenium") {
+                            def projectDir = pwd()
+                            catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE'){
+                                sh("cd ${projectDir}/dbaas/token")
+                                withCredentials([string(credentialsId: 'dataplatform_dbaas', variable: 'dbaas_token_credentials')]) {
+                                    sh("python ${projectDir}/dbaas/token/get_auth0_token.py --url=https://staging.tidbcloud.com/")
+                                }
+                                //acount_token = sh(returnStdout: true, script: 'cat /home/jenkins/agent/tokens/*')
+                                access_token = readFile('/home/jenkins/agent/tokens/feng.ou@pingcap.com')
+                             //println "token is ${access_token}"
+                            }
                         }
 
-                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'main']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'FengOu', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'https://github.com/fengou1/AutoTest.git']]]
+                 }
+                container("golang") {
+                    println "debug command:\nkubectl -n jenkins-tidb exec -ti ${NODE_NAME} bash"
+                    def ws = pwd()
+                    deleteDir()
+                    println "token is ${access_token}"
+                    // Checkout and build testing binaries.
+                    dir("${ws}go/src/github.com/AutoTest") {
+                        stage('Prepare') {
+                            if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                                deleteDir()
+                            }
 
-                        sh label: "Build testing binaries", script: """
-                        echo 'begin to build'
-                        go version
-                        make dbaas
-                        """
-                    }
+                            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'main']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'FengOu', refspec: '+refs/heads/*:refs/remotes/origin/*', url: 'https://github.com/fengou1/AutoTest.git']]]
 
-                    stage("DBaaS Test") {
-                    println "DBaaS Test Started"
-                    sh label: "DBaaS test start", script: """
-                    make dbaas_test
-                    """
-                    }
-                    
-                    currentBuild.result = "SUCCESS"
-                    stage('Summary') {
-                    def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
-                    println duration
-                    }
+                            sh label: "Build testing binaries", script: """
+                            echo 'begin to build'
+                            go version
+                            make dbaas
+                            make build_report
+                            """
+                        }
+
+                        stage("DBaaS AWS Test") {
+                            withEnv(["access_token=${access_token}", "cloud_tier=${cloud_tier}"]) {
+                                println "DBaaS Test Started"
+                                sh label: "DBaaS test start", script: """
+                                make dbaas_test
+                            """
+                            }
+                        }
+
+                        stage("DBaaS GCP Test") {
+                        cloud_tier = "gcp"
+                            withEnv(["access_token=${access_token}", "cloud_tier=${cloud_tier}"]) {
+                                println "DBaaS Test Started"
+                                sh label: "DBaaS test start", script: """
+                                make dbaas_test
+                            """
+                            }
+                        }
+                        
+                        currentBuild.result = "SUCCESS"
+                        stage('Report') {
+                            def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+                            println duration
+                            println "Test Result Reported to Feishu"
+                                sh label: "Feishu Test Report", script: """
+                                make report
+                            """
+                        }
+                }
+                
             }
         }
     }
 
+
 }
-
-
