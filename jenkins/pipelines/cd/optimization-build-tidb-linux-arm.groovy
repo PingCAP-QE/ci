2aa9412eea6a9bf3acd48c1a4f3f76b8f2521e90
zhouqiang <zhouqiang.cl@gmail.com>
2021-12-20 12:02:02 +0800

    rename ticdc to tiflow (#510)

--- jenkins/pipelines/cd/optimization-build-tidb-linux-arm.groovy
+++ jenkins/pipelines/cd/optimization-build-tidb-linux-arm.groovy
@@ -80,6 +80,9 @@ def build_upload = { product, hash, binary ->
             if (RELEASE_TAG >= "v5.3.0" && product == "dumpling") {
                 repo = "git@github.com:pingcap/tidb.git"
             }
+            if (product == "ticdc") {
+                repo = "git@github.com:pingcap/tiflow.git"
+            }
             def workspace = WORKSPACE
             dir("${workspace}/go/src/github.com/pingcap/${product}") {
                 deleteDir()
