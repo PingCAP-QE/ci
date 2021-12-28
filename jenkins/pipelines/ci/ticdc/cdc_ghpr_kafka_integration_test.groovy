0c34c73abeaeb453f1593c9757fed7121531b39f
purelind <purelind@gmail.com>
2021-12-28 11:54:31 +0800

    ticdc: reduce kafka it test cpu request resource

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_kafka_integration_test.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_kafka_integration_test.groovy
@@ -159,12 +159,12 @@ catchError {
                                         resourceRequestCpu: '2000m', resourceRequestMemory: '4Gi',
                                         ttyEnabled: true, command: 'cat'),
                                 containerTemplate(name: 'zookeeper', alwaysPullImage: false, image: 'wurstmeister/zookeeper',
-                                        resourceRequestCpu: '2000m', resourceRequestMemory: '4Gi',
+                                        resourceRequestCpu: '200m', resourceRequestMemory: '4Gi',
                                         ttyEnabled: true),
                                 containerTemplate(
                                         name: 'kafka',
                                         image: "wurstmeister/kafka:${KAFKA_TAG}",
-                                        resourceRequestCpu: '2000m', resourceRequestMemory: '4Gi',
+                                        resourceRequestCpu: '500m', resourceRequestMemory: '4Gi',
                                         ttyEnabled: true,
                                         alwaysPullImage: false,
                                         envVars: [
