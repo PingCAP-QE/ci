441190e3e39f36ec904f4c2d36f0b397f5dd4dc4
zhouqiang <zhouqiang.cl@gmail.com>
2021-12-28 19:34:39 +0800

    remove tikv/pd multi branch trigger always fail docker image build (#537)
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- jenkins/pipelines/cd/multibranch/build_pd_multi_branch.groovy
+++ jenkins/pipelines/cd/multibranch/build_pd_multi_branch.groovy
@@ -1,3 +1,19 @@
+@NonCPS
+boolean isMoreRecentOrEqual( String a, String b ) {
+    if (a == b) {
+        return true
+    }
+
+    [a,b]*.tokenize('.')*.collect { it as int }.with { u, v ->
+       Integer result = [u,v].transpose().findResult{ x,y -> x <=> y ?: null } ?: u.size() <=> v.size()
+       return (result == 1)
+    } 
+}
+
+string trimPrefix = {
+    it.startsWith('release-') ? it.minus('release-') : it 
+}
+
 def boolean isBranchMatched(List<String> branches, String targetBranch) {
     for (String item : branches) {
         if (targetBranch.startsWith(item)) {
@@ -8,7 +24,21 @@ def boolean isBranchMatched(List<String> branches, String targetBranch) {
     return false
 }
 
-def isNeedGo1160 = isBranchMatched(["master", "release-5.1"], env.BRANCH_NAME)
+def isNeedGo1160 = false
+releaseBranchUseGo1160 = "release-5.1"
+
+if (!isNeedGo1160) {
+    isNeedGo1160 = isBranchMatched(["master", "hz-poc"], env.BRANCH_NAME)
+}
+if (!isNeedGo1160 && env.BRANCH_NAME.startsWith("v") && env.BRANCH_NAME >= "v5.1") {
+    isNeedGo1160 = true
+}
+if (!isNeedGo1160 && env.BRANCH_NAME.startsWith("release-")) {
+    isNeedGo1160 = isMoreRecentOrEqual(trimPrefix(env.BRANCH_NAME), trimPrefix(releaseBranchUseGo1160))
+    if (isNeedGo1160) {
+        println "targetBranch=${env.BRANCH_NAME}  >= ${releaseBranchUseGo1160}"
+    }
+}
 if (isNeedGo1160) {
     println "This build use go1.16"
     GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
@@ -29,6 +59,27 @@ def branchNodeMap = [
     "release-2.1" : "build_go1130",
     "release-3.0" : "build_go1130",
 ]
+
+def release_one(repo,hash) {
+    def binary = "builds/pingcap/test/${repo}/${hash}/centos7/${repo}-linux-arm64.tar.gz"
+    echo "release binary: ${FILE_SERVER_URL}/download/${binary}"
+    def paramsBuild = [
+        string(name: "ARCH", value: "arm64"),
+        string(name: "OS", value: "linux"),
+        string(name: "EDITION", value: "community"),
+        string(name: "OUTPUT_BINARY", value: binary),
+        string(name: "REPO", value: repo),
+        string(name: "PRODUCT", value: repo),
+        string(name: "GIT_HASH", value: hash),
+        string(name: "TARGET_BRANCH", value: env.BRANCH_NAME),
+        string(name: "RELEASE_TAG", value: env.BRANCH_NAME),
+        [$class: 'BooleanParameterValue', name: 'FORCE_REBUILD', value: true],
+    ]
+    build job: "build-common",
+            wait: true,
+            parameters: paramsBuild
+}
+
 
 try {
     // 如果不在 map 里，如 release-3.1 分支或者 tag 分支，就使用和 master 一样的环境
@@ -94,23 +145,24 @@ try {
         stage("Upload") {
             dir("go/src/github.com/pingcap/pd") {
                 def refspath = "refs/pingcap/pd/${env.BRANCH_NAME}/sha1"
-                def filepath = "builds/pingcap/pd/${githash}/centos7/pd-server.tar.gz"
+                def filepath = "builds/pingcap/pd/${env.BRANCH_NAME}/${githash}/centos7/pd-server.tar.gz"
+                def filepath2 = "builds/pingcap/pd/${githash}/centos7/pd-server.tar.gz"
                 container("golang") {
+                    release_one("pd","${githash}")
                     timeout(10) {
                         sh """
                         echo "${githash}" > sha1
                         curl -F ${refspath}=@sha1 ${FILE_SERVER_URL}/upload
                         tar --exclude=pd-server.tar.gz -czvf pd-server.tar.gz *
                         curl -F ${filepath}=@pd-server.tar.gz ${FILE_SERVER_URL}/upload
+                        curl -F ${filepath2}=@pd-server.tar.gz ${FILE_SERVER_URL}/upload
                         """
                     }
                 }
             }
         }
     }
-    stage("Push pd Docker") {
-        build job: 'build_image_hash', wait: false, parameters: [[$class: 'StringParameterValue', name: 'REPO', value: "pd"], [$class: 'StringParameterValue', name: 'COMMIT_ID', value: githash], [$class: 'StringParameterValue', name: 'IMAGE_TAG', value: env.BRANCH_NAME]]
-    }
+   
     currentBuild.result = "SUCCESS"
 } catch (Exception e) {
     currentBuild.result = "FAILURE"
