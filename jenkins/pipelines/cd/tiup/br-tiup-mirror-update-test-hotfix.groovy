a9c1c8829246aa65785f0fbb722f11dc83584c9a
Qiang Zhou <zhouqiang.cl@gmail.com>
2021-12-31 21:43:45 +0800

    try to fix cdc error

--- jenkins/pipelines/cd/tiup/br-tiup-mirror-update-test-hotfix.groovy
+++ jenkins/pipelines/cd/tiup/br-tiup-mirror-update-test-hotfix.groovy
@@ -30,11 +30,8 @@ def download = { name, version, os, arch ->
         """
     }
 
-    if (arch == "arm64" && os != "darwin") {
-        tarball_name = "${name}-${os}-${arch}.tar.gz"
-    } else {
-        tarball_name = "${name}.tar.gz"
-    }
+    tarball_name = "${name}-${os}-${arch}.tar.gz"
+
     if (HOTFIX_TAG != "nightly") {
         sh """
     wget ${FILE_SERVER_URL}/download/builds/pingcap/${name}/optimization/${tag}/${br_sha1}/${platform}/${tarball_name}
@@ -47,12 +44,7 @@ def download = { name, version, os, arch ->
 }
 
 def unpack = { name, version, os, arch ->
-    if (arch == "arm64" && os != "darwin") {
-        tarball_name = "${name}-${os}-${arch}.tar.gz"
-    } else {
-        tarball_name = "${name}.tar.gz"
-    }
-
+    tarball_name = "${name}-${os}-${arch}.tar.gz"
     sh """
     tar -zxf ${tarball_name}
     """
