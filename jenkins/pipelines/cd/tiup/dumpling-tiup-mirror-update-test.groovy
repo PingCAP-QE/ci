b8c34fe63e3287431e9c8cbd7743d5404e5fedbb
Qiang Zhou <zhouqiang.cl@gmail.com>
2022-01-11 11:11:36 +0800

    unify tarball name
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- jenkins/pipelines/cd/tiup/dumpling-tiup-mirror-update-test.groovy
+++ jenkins/pipelines/cd/tiup/dumpling-tiup-mirror-update-test.groovy
@@ -16,11 +16,9 @@ def download = { name, version, os, arch ->
         """
     }
 
-    if (arch == "arm64" || os == "darwin") {
-        tarball_name = "${name}-${os}-${arch}.tar.gz"
-    } else {
-        tarball_name = "${name}.tar.gz"
-    }
+
+    tarball_name = "${name}-${os}-${arch}.tar.gz"
+ 
     if (RELEASE_TAG != "nightly") {
         sh """
     wget ${FILE_SERVER_URL}/download/builds/pingcap/${name}/optimization/${tag}/${dumpling_sha1}/${platform}/${tarball_name}
@@ -34,11 +32,7 @@ def download = { name, version, os, arch ->
 }
 
 def unpack = { name, version, os, arch ->
-    if (arch == "arm64" || os == "darwin") {
-        tarball_name = "${name}-${os}-${arch}.tar.gz"
-    } else {
-        tarball_name = "${name}.tar.gz"
-    }
+    tarball_name = "${name}-${os}-${arch}.tar.gz"
 
     sh """
     tar -zxf ${tarball_name}
--- jenkins/pipelines/cd/tiup/importer-tiup-mirror-update-test.groovy
+++ jenkins/pipelines/cd/tiup/importer-tiup-mirror-update-test.groovy
@@ -16,11 +16,8 @@ def download = { name, version, os, arch ->
         """
     }
 
-    if (arch == "arm64" && os != "darwin") {
-        tarball_name = "${name}-${os}-${arch}.tar.gz"
-    } else {
-        tarball_name = "${name}.tar.gz"
-    }
+    tarball_name = "${name}-${os}-${arch}.tar.gz"
+
     if (RELEASE_TAG != "nightly" && RELEASE_TAG > "v4.0.0") {
         sh """
     wget ${FILE_SERVER_URL}/download/builds/pingcap/${name}/optimization/${tag}/${importer_sha1}/${platform}/${tarball_name}
@@ -33,11 +30,7 @@ def download = { name, version, os, arch ->
 }
 
 def unpack = { name, version, os, arch ->
-    if (arch == "arm64" && os != "darwin") {
-        tarball_name = "${name}-${os}-${arch}.tar.gz"
-    } else {
-        tarball_name = "${name}.tar.gz"
-    }
+    tarball_name = "${name}-${os}-${arch}.tar.gz"
 
     sh """
     tar -zxf ${tarball_name}
