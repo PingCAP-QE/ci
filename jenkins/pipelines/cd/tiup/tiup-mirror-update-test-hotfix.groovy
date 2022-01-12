0a8c21df634339965664adcf55f465733fea6376
zhouqiang <zhouqiang.cl@gmail.com>
2022-01-12 18:55:50 +0800

    add new hotfix (#601)
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- jenkins/pipelines/cd/tiup/tiup-mirror-update-test-hotfix.groovy
+++ jenkins/pipelines/cd/tiup/tiup-mirror-update-test-hotfix.groovy
@@ -355,7 +355,7 @@ node("build_go1130") {
             }
 
             def paramsPROMETHEUS = [
-                    string(name: "HOTFIX_TAG", value: "${HOTFIX_TAG}"),
+                    string(name: "RELEASE_TAG", value: "${HOTFIX_TAG}"),
                     string(name: "TIDB_VERSION", value: "${tidb_version}"),
                     string(name: "TIUP_MIRRORS", value: "${TIUP_MIRRORS}"),
                     string(name: "ORIGIN_TAG", value: "${TIFLASH_TAG}"),
