58391f922b9e054e2311d5b4e1e99ffef6b46421
shiyuhang <1136742008@qq.com>
2022-01-14 12:37:37 +0800

    tispark:add daily

---
+++ jenkins/pipelines/ci/tispark/daily.groovy
@@ -0,0 +1,70 @@
+def get_sha(repo,branch) {
+    sh "curl -s ${FILE_SERVER_URL}/download/builds/pingcap/ee/get_hash_from_github.py > gethash.py"
+    return sh(returnStdout: true, script: "python gethash.py -repo=${repo} -version=${branch} -s=${FILE_SERVER_URL}").trim()
+}
+
+node("lightweight_pod") {
+    container("golang"){
+
+
+        stage("Debug INFO") {
+            commitID = get_sha("tispark","master")
+            println "tispark master latest commit id: ${commitID}"
+        }
+
+        stage("Trigger job") {
+            parallel(
+                    test1: {
+                        println "tidb=master pd=master tiflash=master tikv=master profile=3.0  test-spark-catalog=yes"
+                        build(job: "tispark_ghpr_integration_test",
+                                parameters: [
+                                        string(name: 'triggered_by_upstream_ci', value: "daily-trigger-tispark"),
+                                        booleanParam(name: 'daily_test', value: true),
+                                        string(name: 'ghprbActualCommit', value: commitID),
+                                        string(name: 'ghprbCommentBody', value: "tidb=master pd=master tiflash=master tikv=master profile=3.0  test-spark-catalog=yes"),
+                                ],
+                                wait: true, propagate: true)
+
+                    },
+
+                    test2: {
+                        println "tidb=master pd=master tiflash=master tikv=master profile=3.0  test-spark-catalog=no"
+                        build(job: "tispark_ghpr_integration_test",
+                                parameters: [
+                                        string(name: 'triggered_by_upstream_ci', value: "daily-trigger-tispark"),
+                                        booleanParam(name: 'daily_test', value: true),
+                                        string(name: 'ghprbActualCommit', value: commitID),
+                                        string(name: 'ghprbCommentBody', value: "tidb=master pd=master tiflash=master tikv=master profile=3.0  test-spark-catalog=yes"),
+                                ],
+                                wait: true, propagate: true)
+                    },
+
+                    test3: {
+                        println "tidb=master pd=master tiflash=master tikv=master profile=3.1  test-spark-catalog=yes"
+                        build(job: "tispark_ghpr_integration_test",
+                                parameters: [
+                                        string(name: 'triggered_by_upstream_ci', value: "daily-trigger-tispark"),
+                                        booleanParam(name: 'daily_test', value: true),
+                                        string(name: 'ghprbActualCommit', value: commitID),
+                                        string(name: 'ghprbCommentBody', value: "tidb=master pd=master tiflash=master tikv=master profile=3.1  test-spark-catalog=yes"),
+                                ],
+                                wait: true, propagate: true)
+                    },
+
+                    test4: {
+                        println "tidb=master pd=master tiflash=master tikv=master profile=3.1  test-spark-catalog=no"
+                        build(job: "tispark_ghpr_integration_test",
+                                parameters: [
+                                        string(name: 'triggered_by_upstream_ci', value: "daily-trigger-tispark"),
+                                        booleanParam(name: 'daily_test', value: true),
+                                        string(name: 'ghprbActualCommit', value: commitID),
+                                        string(name: 'ghprbCommentBody', value: "tidb=master pd=master tiflash=master tikv=master profile=3.1  test-spark-catalog=yes"),
+                                ],
+                                wait: true, propagate: true)
+                    },
+            )
+
+        }
+
+    }
+}
