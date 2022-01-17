1c0a380b55c0a3a9877bfa99e6a36594047bae0e
zhouqiang <zhouqiang.cl@gmail.com>
2022-01-17 11:24:02 +0800

    refine lightning & dumpling build (#634)
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- jenkins/pipelines/cd/tiup/dumpling-tiup-mirror-update-test.groovy
+++ jenkins/pipelines/cd/tiup/dumpling-tiup-mirror-update-test.groovy
@@ -74,31 +74,33 @@ node("build_go1130") {
             stage("Get hash") {
                 sh "curl -s ${FILE_SERVER_URL}/download/builds/pingcap/ee/gethash.py > gethash.py"
 
-                if (RELEASE_TAG == "nightly") {
-                    tag = "master"
-                } else {
-                    tag = RELEASE_TAG
-                }
-                if (RELEASE_TAG == "nightly" || RELEASE_TAG >= "v5.3.0"){
+
+                tag = RELEASE_TAG
+                if(ORIGIN_TAG != "") {
+                    dumpling_sha1 = ORIGIN_TAG
+                } else if ( RELEASE_TAG >= "v5.3.0"){
                     dumpling_sha1 = sh(returnStdout: true, script: "python gethash.py -repo=tidb -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
                 } else {
                     dumpling_sha1 = sh(returnStdout: true, script: "python gethash.py -repo=dumpling -version=${RELEASE_TAG} -s=${FILE_SERVER_URL}").trim()
                 }
             }
 
-            stage("tiup release dumpling linux amd64") {
-                update "dumpling", RELEASE_TAG, "linux", "amd64"
+            if (params.ARCH_X86) {
+                stage("tiup release dumpling linux amd64") {
+                    update "dumpling", RELEASE_TAG, "linux", "amd64"
+                }
             }
-
-            stage("tiup release dumpling linux arm64") {
-                update "dumpling", RELEASE_TAG, "linux", "arm64"
+            if (params.ARCH_ARM) {
+                stage("tiup release dumpling linux arm64") {
+                    update "dumpling", RELEASE_TAG, "linux", "arm64"
+                }
             }
-
-            stage("tiup release dumpling darwin amd64") {
-                update "dumpling", RELEASE_TAG, "darwin", "amd64"
+            if (params.ARCH_MAC) {
+                stage("tiup release dumpling darwin amd64") {
+                    update "dumpling", RELEASE_TAG, "darwin", "amd64"
+                }
             }
-
-            if (RELEASE_TAG >="v5.1.0" || RELEASE_TAG =="nightly") {
+            if (params.ARCH_MAC_ARM && RELEASE_TAG >="v5.1.0") {
                 stage("tiup release dumpling darwin arm64") {
                     update "dumpling", RELEASE_TAG, "darwin", "arm64"
                 }
