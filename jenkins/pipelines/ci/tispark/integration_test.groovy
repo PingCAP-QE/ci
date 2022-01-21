aa9dda361fe9685d6f1d4b1947fada5a515b2818
shiyuhang0 <52435083+shiyuhang0@users.noreply.github.com>
2022-01-21 14:04:11 +0800

    add tispark memory (#679)
    * add tispark memory
    
    * alter MaxPermSize

--- jenkins/pipelines/ci/tispark/integration_test.groovy
+++ jenkins/pipelines/ci/tispark/integration_test.groovy
@@ -119,7 +119,7 @@ podTemplate(name: label, label: label, instanceCap: 20, namespace: 'jenkins-tisp
                 ], alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
         containerTemplate(name: 'java', image: 'hub.pingcap.net/jenkins/centos7_golang-1.13_java:cached',
                 resourceRequestCpu: '4000m',
-                resourceRequestMemory: '8Gi',
+                resourceRequestMemory: '12Gi',
                 envVars: [
                         envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375'),
                 ], alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
@@ -258,7 +258,7 @@ podTemplate(name: label, label: label, instanceCap: 20, namespace: 'jenkins-tisp
                         if [ ! "\$(ls -A /maven/.m2/repository)" ]; then curl -sL \$archive_url | tar -zx -C /maven || true; fi
                     """
                     sh """
-                        export MAVEN_OPTS="-Xmx6G -XX:MaxPermSize=512M"
+                        export MAVEN_OPTS="-Xmx6G -XX:MaxPermSize=1024M"
                         mvn clean package ${MVN_PROFILE} -DskipTests
                         mvn test ${MVN_PROFILE} -Dtest=moo ${mvnStr}
                     """
@@ -276,7 +276,7 @@ podTemplate(name: label, label: label, instanceCap: 20, namespace: 'jenkins-tisp
                             if [ ! "\$(ls -A /maven/.m2/repository)" ]; then curl -sL \$archive_url | tar -zx -C /maven || true; fi
                         """
                         sh """
-                            export MAVEN_OPTS="-Xmx6G -XX:MaxPermSize=512M"
+                            export MAVEN_OPTS="-Xmx6G -XX:MaxPermSize=1024M"
                             mvn test ${MVN_PROFILE} -am -pl tikv-client
                         """
                         sh 'curl -s https://codecov.io/bash | bash -s'
