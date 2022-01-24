d67b1196e1b7b8f36064a0d82b1f0637f7203f7c
VelocityLight <yjchit@126.com>
2022-01-24 22:20:01 +0800

    fixme:  nexus repository

--- jenkins/pipelines/cd/release-tikv-java-client.groovy
+++ jenkins/pipelines/cd/release-tikv-java-client.groovy
@@ -64,25 +64,25 @@ pipeline {
                         echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
 
                         // 上传到中央Nexus仓库
-                        nexusArtifactUploader(
-                            nexusVersion: NEXUS_VERSION,
-                            protocol: NEXUS_PROTOCOL,
-                            nexusUrl: NEXUS_URL,
-                            groupId: pom.groupId,
-                            version: pom.version,
-                            repository: NEXUS_REPOSITORY,
-                            credentialsId: NEXUS_CREDENTIAL_ID,
-                            artifacts: [
-                                [artifactId: pom.artifactId,
-                                classifier: '',
-                                file: artifactPath,
-                                type: pom.packaging],
-                                [artifactId: pom.artifactId,
-                                classifier: '',
-                                file: "pom.xml",
-                                type: "pom"]
-                            ]
-                        );
+                        // nexusArtifactUploader(
+                        //     nexusVersion: NEXUS_VERSION,
+                        //     protocol: NEXUS_PROTOCOL,
+                        //     nexusUrl: NEXUS_URL,
+                        //     groupId: pom.groupId,
+                        //     version: pom.version,
+                        //     repository: NEXUS_REPOSITORY,
+                        //     credentialsId: NEXUS_CREDENTIAL_ID,
+                        //     artifacts: [
+                        //         [artifactId: pom.artifactId,
+                        //         classifier: '',
+                        //         file: artifactPath,
+                        //         type: pom.packaging],
+                        //         [artifactId: pom.artifactId,
+                        //         classifier: '',
+                        //         file: "pom.xml",
+                        //         type: "pom"]
+                        //     ]
+                        // );
                     } else {
                         error "*** File: ${artifactPath}, could not be found";
                     }
