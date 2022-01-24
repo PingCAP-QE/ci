1e02a78289cd1e569720f3de67b6be6bc1b16206
shiyuhang <1136742008@qq.com>
2022-01-24 13:26:13 +0800

    tispark:update daily ci

--- jenkins/pipelines/ci/tispark/daily.groovy
+++ jenkins/pipelines/ci/tispark/daily.groovy
@@ -1,12 +1,10 @@
-
-
-def get_sha(repo,branch) {
+def get_sha(repo, branch) {
     sh "curl -s ${FILE_SERVER_URL}/download/builds/pingcap/ee/get_hash_from_github.py > gethash.py"
     return sh(returnStdout: true, script: "python gethash.py -repo=${repo} -version=${branch} -s=${FILE_SERVER_URL}").trim()
 }
 
-def test(ghprbActualCommit,ghprbCommentBody){
-    println ghprbCommentBody
+def test_base(commitName, ghprbActualCommit, ghprbCommentBody) {
+    println commitName + " : " + ghprbCommentBody
     build(job: "tispark_ghpr_integration_test",
             parameters: [
                     string(name: 'triggered_by_upstream_ci', value: "daily-trigger-tispark"),
@@ -17,179 +15,79 @@ def test(ghprbActualCommit,ghprbCommentBody){
             wait: true, propagate: true)
 }
 
+def test_master(commitID, version) {
+    println "tispark master"
+    parallel(
+            test1: {
+                test_base("master", commitID, "tidb=$version pd=$version tiflash=$version tikv$version test-spark-catalog=true")
+            },
+
+            test2: {
+                test_base("master", commitID, "tidb=$version pd=$version tiflash=$version tikv=$version test-spark-catalog=false")
+            },
+
+            test3: {
+                test_base("master", commitID, "tidb=$version pd=$version tiflash=$version tikv=$version profile=spark-3.1.1 test-spark-catalog=true")
+            },
+
+            test4: {
+                test_base("master", commitID, "tidb=$version pd=$version tiflash=$version tikv=$version profile=spark-3.1.1 test-spark-catalog=false")
+            },
+    )
+}
+
+def test_release2_4(commitID, version) {
+    parallel(
+            test5: {
+                test_base("release-2.4", commitID, "tidb=$version pd=$version tiflash=$version tikv=$version profile=scala-2.11 profile=spark-2.3")
+            },
+
+            test6: {
+                test_base("release-2.4", commitID, "tidb=$version pd=$version tiflash=$version tikv=$version profile=scala-2.11")
+            },
+
+            test7: {
+                test_base("release-2.4", commitID, "tidb=$version pd=$version tiflash=$version tikv=$version profile=scala-2.12")
+            },
+    )
+}
+
 node("lightweight_pod") {
-    container("golang"){
+    container("golang") {
 
 
         stage("Debug INFO") {
-            commitID = get_sha("tispark","master")
-            println "tispark master latest commit id: ${commitID}"
+            master = get_sha("tispark", "master")
+            println "tispark master latest commit id: ${master}"
 
-            commitID2 = get_sha("tispark","release-2.4")
-            println "tispark master latest commit2 id: ${commitID2}"
+            release_2_4 = get_sha("tispark", "release-2.4")
+            println "tispark master release_2_4 commit id: ${release_2_4}"
         }
 
         stage("Trigger job") {
-            parallel(
-                    test1: {
-                        test(commitID,"tidb=v5.3.0 pd=v5.3.0 tiflash=v5.3.0 tikv=v5.3.0 test-spark-catalog=true")
-                    },
-
-                    test2: {
-                        test(commitID,"tidb=v5.3.0 pd=v5.3.0 tiflash=v5.3.0 tikv=v5.3.0 test-spark-catalog=false")
-                    },
-
-                    test3: {
-                        test(commitID,"tidb=v5.3.0 pd=v5.3.0 tiflash=v5.3.0 tikv=v5.3.0 profile=spark-3.1.1 test-spark-catalog=true")
-                    },
-
-                    test4: {
-                        test(commitID,"tidb=v5.3.0 pd=v5.3.0 tiflash=v5.3.0 tikv=v5.3.0 profile=spark-3.1.1 test-spark-catalog=false")
-                    },
-            )
-
-            parallel(
-                    test5: {
-                        test(commitID,"tidb=v5.2.3  pd=v5.2.3 tiflash=v5.2.3 tikv=v5.2.3 test-spark-catalog=true")
-                    },
-
-                    test6: {
-                        test(commitID,"tidb=v5.2.3  pd=v5.2.3  tiflash=v5.2.3 tikv=v5.2.3 test-spark-catalog=false")
-                    },
-
-                    test7: {
-                        test(commitID,"tidb=v5.2.3  pd=v5.2.3  tiflash=v5.2.3 tikv=v5.2.3 profile=spark-3.1.1 test-spark-catalog=true")
-                    },
-
-                    test8: {
-                        test(commitID,"tidb=v5.2.3  pd=v5.2.3 tiflash=v5.2.3 tikv=v5.2.3 profile=spark-3.1.1 test-spark-catalog=false")
-                    },
-            )
-
-            parallel(
-                    test9: {
-                        test(commitID,"tidb=v5.1.3  pd=v5.1.3 tiflash=v5.1.3 tikv=v5.1.3 test-spark-catalog=true")
-                    },
-
-                    test10: {
-                        test(commitID,"tidb=v5.1.3  pd=v5.1.3  tiflash=v5.1.3 tikv=v5.1.3 test-spark-catalog=false")
-                    },
-
-                    test11: {
-                        test(commitID,"tidb=v5.1.3  pd=v5.1.3  tiflash=v5.1.3 tikv=v5.1.3 profile=spark-3.1.1 test-spark-catalog=true")
-                    },
-
-                    test12: {
-                        test(commitID,"tidb=v5.1.3  pd=v5.1.3 tiflash=v5.1.3 tikv=v5.1.3 profile=spark-3.1.1 test-spark-catalog=false")
-                    },
-            )
-
-            parallel(
-                    test13: {
-                        test(commitID,"tidb=v5.0.6  pd=v5.0.6 tiflash=v5.0.6 tikv=v5.0.6 test-spark-catalog=true")
-                    },
-
-                    test14: {
-                        test(commitID,"tidb=v5.0.6  pd=v5.0.6  tiflash=v5.0.6 tikv=v5.0.6 test-spark-catalog=false")
-                    },
-
-                    test15: {
-                        test(commitID,"tidb=v5.0.6  pd=v5.0.6  tiflash=v5.0.6 tikv=v5.0.6 profile=spark-3.1.1 test-spark-catalog=true")
-                    },
-
-                    test16: {
-                        test(commitID,"tidb=v5.0.6  pd=v5.0.6 tiflash=v5.0.6 tikv=v5.0.6 profile=spark-3.1.1 test-spark-catalog=false")
-                    },
-            )
-
-            parallel(
-                    test17: {
-                        test(commitID,"tidb=v4.0.16  pd=v4.0.16 tiflash=v4.0.16 tikv=v4.0.16 test-spark-catalog=true")
-                    },
-
-                    test18: {
-                        test(commitID,"tidb=v4.0.16  pd=v4.0.16  tiflash=v4.0.16 tikv=v4.0.16 test-spark-catalog=false")
-                    },
-
-                    test19: {
-                        test(commitID,"tidb=v4.0.16  pd=v4.0.16  tiflash=v4.0.16 tikv=v4.0.16 profile=spark-3.1.1 test-spark-catalog=true")
-                    },
-
-                    test20: {
-                        test(commitID,"tidb=v4.0.16  pd=v4.0.16 tiflash=v4.0.16 tikv=v4.0.16 profile=spark-3.1.1 test-spark-catalog=false")
-                    },
-            )
-
-            parallel(
-                    test21: {
-                        test(commitID2,"tidb=v5.3.0 pd=v5.3.0 tiflash=v5.3.0 tikv=v5.3.0 profile=scala-2.11 profile=spark-2.3")
-                    },
-
-                    test22: {
-                        test(commitID2,"tidb=v5.3.0 pd=v5.3.0 tiflash=v5.3.0 tikv=v5.3.0 profile=scala-2.11")
-                    },
-
-                    test23: {
-                        test(commitID2,"tidb=v5.3.0 pd=v5.3.0 tiflash=v5.3.0 tikv=v5.3.0 profile=scala-2.12")
-                    },
-            )
-
-            parallel(
-                    test24: {
-                        test(commitID2,"tidb=v5.2.3 pd=v5.2.3 tiflash=v5.2.3 tikv=v5.2.3 profile=scala-2.11 profile=spark-2.3")
-                    },
-
-                    test25: {
-                        test(commitID2,"tidb=v5.2.3 pd=v5.2.3 tiflash=v5.2.3 tikv=v5.2.3 profile=scala-2.11")
-                    },
-
-                    test26: {
-                        test(commitID2,"tidb=v5.2.3 pd=v5.2.3 tiflash=v5.2.3 tikv=v5.2.3 profile=scala-2.12")
-                    },
-            )
-
-            parallel(
-                    test27: {
-                        test(commitID2,"tidb=v5.1.3 pd=v5.1.3 tiflash=v5.1.3 tikv=v5.1.3 profile=scala-2.11 profile=spark-2.3")
-                    },
-
-                    test28: {
-                        test(commitID2,"tidb=v5.1.3 pd=v5.1.3 tiflash=v5.1.3 tikv=v5.1.3 profile=scala-2.11")
-                    },
-
-                    test29: {
-                        test(commitID2,"tidb=v5.1.3 pd=v5.1.3 tiflash=v5.1.3 tikv=v5.1.3 profile=scala-2.12")
-                    },
-            )
-
-            parallel(
-                    test30: {
-                        test(commitID2,"tidb=v5.0.6 pd=v5.0.6 tiflash=v5.0.6 tikv=v5.0.6 profile=scala-2.11 profile=spark-2.3")
-                    },
-
-                    test31: {
-                        test(commitID2,"tidb=v5.0.6 pd=v5.0.6 tiflash=v5.1.3 tikv=v5.0.6 profile=scala-2.11")
-                    },
-
-                    test32: {
-                        test(commitID2,"tidb=v5.0.6 pd=v5.0.6 tiflash=v5.0.6 tikv=v5.0.6 profile=scala-2.12")
-                    },
-            )
-
-            parallel(
-                    test33: {
-                        test(commitID2,"tidb=v4.0.16 pd=v4.0.16 tiflash=v4.0.16 tikv=v4.0.16 profile=scala-2.11 profile=spark-2.3")
-                    },
-
-                    test34: {
-                        test(commitID2,"tidb=v4.0.16 pd=v4.0.16 tiflash=v4.0.16 tikv=v4.0.16 profile=scala-2.11")
-                    },
-
-                    test35: {
-                        test(commitID2,"tidb=v4.0.16 pd=v4.0.16 tiflash=v4.0.16 tikv=v4.0.16 profile=scala-2.12")
-                    },
-            )
-
+            def now = new Date().getDay()
+            switch (now) {
+                case 1:
+                    test_master(master, "v5.3.0")
+                    test_release2_4(release_2_4, "v5.3.0")
+                    break
+                case 2:
+                    test_master(master, "v5.2.2")
+                    test_release2_4(release_2_4, "v5.2.2")
+                    break
+                case 3:
+                    test_master(master, "v5.1.2")
+                    test_release2_4(release_2_4, "v5.1.2")
+                    break
+                case 4:
+                    test_master(master, "v5.0.6")
+                    test_release2_4(release_2_4, "v5.0.6")
+                    break
+                case 5:
+                    test_master(master, "release-4.0")
+                    test_release2_4(release_2_4, "release-4.0")
+                    break
+            }
         }
 
     }
