33ff2c6299aa3604dfd7484907729f6743a7a050
VelocityLight <yjchit@126.com>
2022-01-24 22:06:02 +0800

    add clean

--- jenkins/pipelines/cd/release-tikv-java-client.groovy
+++ jenkins/pipelines/cd/release-tikv-java-client.groovy
@@ -41,7 +41,7 @@ pipeline {
         stage("Maven Build") {
             steps {
                 script {
-                    sh "mvn package -DskipTests=true"
+                    sh "mvn clean package -DskipTests=true"
                 }
             }
         }
