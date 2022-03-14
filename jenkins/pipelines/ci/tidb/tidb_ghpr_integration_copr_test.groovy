1b9c725b5b8bf63f23ce1c79155b697da8f5cc0a
purelind <purelind@gmail.com>
2022-03-14 15:59:30 +0800

    fix pod label map

--- jenkins/pipelines/ci/tidb/tidb_ghpr_integration_copr_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_integration_copr_test.groovy
@@ -53,11 +53,11 @@ GO_IMAGE_MAP = [
     "go1.16": "hub.pingcap.net/jenkins/centos7_golang-1.16:latest",
     "go1.18": "hub.pingcap.net/jenkins/centos7_golang-1.18:latest",
 ]
-POD_LABEL_MAP = {
-    "go1.13": "${JOB_NAME}-go1130-${BUILD_NUMBER}",
-    "go1.16": "${JOB_NAME}-go1160-${BUILD_NUMBER}",
-    "go1.18": "${JOB_NAME}-go1180-${BUILD_NUMBER}",
-}
+POD_LABEL_MAP = [
+    "go1.13": "tidb-ghpr-common-test-go1130-${BUILD_NUMBER}",
+    "go1.16": "tidb-ghpr-common-test-go1160-${BUILD_NUMBER}",
+    "go1.18": "tidb-ghpr-common-test-go1180-${BUILD_NUMBER}",
+]
 
 node("master") {
     deleteDir()
