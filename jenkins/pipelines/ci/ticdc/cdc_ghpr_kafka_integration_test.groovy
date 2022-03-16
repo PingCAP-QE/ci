220a4f269fc9dde2231b8425e5d645000ccca120
Lifu Wu <purelind@users.noreply.github.com>
2022-03-16 23:12:00 +0800

    Revert "[WIP] tiflow master upgrade go version to go1.18 (#806)" (#813)
    This reverts commit 915d6a3f9ceab8c234ad31d42c65a91c4299fa23.

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_kafka_integration_test.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_kafka_integration_test.groovy
@@ -17,130 +17,56 @@ if (ghprbPullId == null || ghprbPullId == "") {
     specStr = "+refs/heads/*:refs/remotes/origin/*"
 }
 
-GO_VERSION = "go1.18"
-POD_GO_IMAGE = ""
-GO_IMAGE_MAP = [
-    "go1.13": "hub.pingcap.net/jenkins/centos7_golang-1.13:latest",
-    "go1.16": "hub.pingcap.net/jenkins/centos7_golang-1.16:latest",
-    "go1.18": "hub.pingcap.net/jenkins/centos7_golang-1.18:latest",
-]
-POD_LABEL_MAP = [
-    "go1.13": "${JOB_NAME}-go1130-${BUILD_NUMBER}",
-    "go1.16": "${JOB_NAME}-go1160-${BUILD_NUMBER}",
-    "go1.18": "${JOB_NAME}-go1180-${BUILD_NUMBER}",
-]
+@NonCPS
+boolean isMoreRecentOrEqual(String a, String b) {
+    if (a == b) {
+        return true
+    }
 
-feature_branch_use_go13 = []
-feature_branch_use_go16 = ["hz-poc", "ft-data-inconsistency", "br-stream", "release-multi-source"]
-feature_branch_use_go18 = []
-
-// Version Selector
-// branch or tag
-// == branch
-//  master use go1.18
-//  release branch >= release-6.0 use go1.18
-//  release branch >= release-5.1 use go1.16
-//  release branch < release-5.0 use go1.13
-//  other feature use corresponding go version
-//  the default go version is go1.18
-// == tag
-// any tag greater or eqaul to v6.0.xxx use go1.18
-// any tag smaller than v6.0.0 and graeter or equal to v5.1.xxx use go1.16
-// any tag smaller than v5.1.0 use go1.13
-
-
-def selectGoVersion(branchNameOrTag) {
-    if (branchNameOrTag.startsWith("v")) {
-        println "This is a tag"
-        if (branchNameOrTag >= "v6.0") {
-            println "tag ${branchNameOrTag} use go 1.18"
-            return "go1.18"
-        }
-        if (branchNameOrTag >= "v5.1") {
-            println "tag ${branchNameOrTag} use go 1.16"
-            return "go1.16"
-        }
-        if (branchNameOrTag < "v5.1") {
-            println "tag ${branchNameOrTag} use go 1.13"
-            return "go1.13"
-        }
-        println "tag ${branchNameOrTag} use default version go 1.18"
-        return "go1.18"
-    } else { 
-        println "this is a branch"
-        if (branchNameOrTag in feature_branch_use_go13) {
-            println "feature branch ${branchNameOrTag} use go 1.13"
-            return "go1.13"
-        }
-        if (branchNameOrTag in feature_branch_use_go16) {
-            println "feature branch ${branchNameOrTag} use go 1.16"
-            return "go1.16"
-        }
-        if (branchNameOrTag in feature_branch_use_go18) {
-            println "feature branch ${branchNameOrTag} use go 1.18"
-            return "go1.18"
-        }
-        if (branchNameOrTag == "master") {
-            println("branchNameOrTag: master  use go1.18")
-            return "go1.18"
-        }
-
-
-        if (branchNameOrTag.startsWith("release-") && branchNameOrTag >= "release-6.0") {
-            println("branchNameOrTag: ${branchNameOrTag}  use go1.18")
-            return "go1.18"
-        }
-        if (branchNameOrTag.startsWith("release-") && branchNameOrTag < "release-6.0" && branchNameOrTag >= "release-5.1") {
-            println("branchNameOrTag: ${branchNameOrTag}  use go1.16")
-            return "go1.16"
-        }
-
-        if (branchNameOrTag.startsWith("release-") && branchNameOrTag < "release-5.1") {
-            println("branchNameOrTag: ${branchNameOrTag}  use go1.13")
-            return "go1.13"
-        }
-        println "branchNameOrTag: ${branchNameOrTag}  use default version go1.18"
-        return "go1.18"
+    [a, b]*.tokenize('.')*.collect { it as int }.with { u, v ->
+        Integer result = [u, v].transpose().findResult { x, y -> x <=> y ?: null } ?: u.size() <=> v.size()
+        return (result == 1)
     }
 }
 
-GO_VERSION = selectGoVersion(ghprbTargetBranch)
-POD_GO_IMAGE = GO_IMAGE_MAP[GO_VERSION]
-println "go version: ${GO_VERSION}"
-println "go image: ${POD_GO_IMAGE}"
+string trimPrefix = {
+    it.startsWith('release-') ? it.minus('release-').split("-")[0] : it
+}
 
-
-def run_with_pod(Closure body) {
-    def label = POD_LABEL_MAP[GO_VERSION]
-    def cloud = "kubernetes"
-    def namespace = "jenkins-ticdc"
-    def jnlp_docker_image = "jenkins/inbound-agent:4.3-4"
-    podTemplate(label: label,
-            cloud: cloud,
-            namespace: namespace,
-            idleMinutes: 0,
-            containers: [
-                    containerTemplate(
-                        name: 'golang', alwaysPullImage: true,
-                        image: "${POD_GO_IMAGE}", ttyEnabled: true,
-                        resourceRequestCpu: '4000m', resourceRequestMemory: '8Gi',
-                        command: '/bin/sh -c', args: 'cat',
-                        envVars: [containerEnvVar(key: 'GOPATH', value: '/go')]     
-                    )
-            ],
-            volumes: [
-                    nfsVolume(mountPath: '/home/jenkins/agent/ci-cached-code-daily', serverAddress: '172.16.5.22',
-                            serverPath: '/mnt/ci.pingcap.net-nfs/git', readOnly: false),
-                    emptyDirVolume(mountPath: '/tmp', memory: false),
-                    emptyDirVolume(mountPath: '/home/jenkins', memory: false)
-                    ],
-    ) {
-        node(label) {
-            println "debug command:\nkubectl -n ${namespace} exec -ti ${NODE_NAME} bash"
-            body()
+def boolean isBranchMatched(List<String> branches, String targetBranch) {
+    for (String item : branches) {
+        if (targetBranch.startsWith(item)) {
+            println "targetBranch=${targetBranch} matched in ${branches}"
+            return true
         }
     }
+    return false
 }
+
+def isNeedGo1160 = false
+releaseBranchUseGo1160 = "release-5.1"
+
+if (!isNeedGo1160) {
+    isNeedGo1160 = isBranchMatched(["master", "hz-poc", "release-multi-source"], ghprbTargetBranch)
+}
+if (!isNeedGo1160 && ghprbTargetBranch.startsWith("release-")) {
+    isNeedGo1160 = isMoreRecentOrEqual(trimPrefix(ghprbTargetBranch), trimPrefix(releaseBranchUseGo1160))
+    if (isNeedGo1160) {
+        println "targetBranch=${ghprbTargetBranch}  >= ${releaseBranchUseGo1160}"
+    }
+}
+
+if (isNeedGo1160) {
+    println "This build use go1.16"
+    GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
+    GO_TEST_SLAVE = GO1160_TEST_SLAVE
+    POD_GO_DOCKER_IMAGE = 'hub.pingcap.net/jenkins/centos7_golang-1.16:latest'
+} else {
+    println "This build use go1.13"
+    POD_GO_DOCKER_IMAGE = "hub.pingcap.net/jenkins/centos7_golang-1.13:latest"
+}
+println "BUILD_NODE_NAME=${GO_BUILD_SLAVE}"
+println "TEST_NODE_NAME=${GO_TEST_SLAVE}"
 
 
 /**
@@ -198,64 +124,64 @@ if (ghprbPullId != null && ghprbPullId != "" && !params.containsKey("triggered_b
 
 
 catchError {
-    run_with_pod {
-        container("golang") {
-            stage('Prepare') {
-                def ws = pwd()
-                deleteDir()
+    node("${GO_TEST_SLAVE}") {
+        stage('Prepare') {
+            def ws = pwd()
+            deleteDir()
 
-                dir("${ws}/go/src/github.com/pingcap/tiflow") {
-                    if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                        echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/tiflow"
-                        deleteDir()
-                    }
-                    try {
+            dir("${ws}/go/src/github.com/pingcap/tiflow") {
+                if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                    echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/tiflow"
+                    deleteDir()
+                }
+                try {
+                    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tiflow.git']]]
+                } catch (info) {
+                    retry(2) {
+                        echo "checkout failed, retry.."
+                        sleep 5
+                        if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                            deleteDir()
+                        }
                         checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tiflow.git']]]
-                    } catch (info) {
-                        retry(2) {
-                            echo "checkout failed, retry.."
-                            sleep 5
-                            if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                                deleteDir()
-                            }
-                            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tiflow.git']]]
-                        }
                     }
-                    sh "git checkout -f ${ghprbActualCommit}"
                 }
-
-                dir("${ws}/go/src/github.com/pingcap/ci") {
-                    if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                        echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ci"
-                        deleteDir()
-                    }
-                    try {
-                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepoUrl}"]]]
-                    } catch (info) {
-                        retry(2) {
-                            echo "checkout failed, retry.."
-                            sleep 5
-                            if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                                echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ci"
-                                deleteDir()
-                            }
-                            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepoUrl}"]]]
-                        }
-                    }
-
-                }
-
-                stash includes: "go/src/github.com/pingcap/tiflow/**", name: "ticdc", useDefaultExcludes: false
+                sh "git checkout -f ${ghprbActualCommit}"
             }
+
+            dir("${ws}/go/src/github.com/pingcap/ci") {
+                if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                    echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ci"
+                    deleteDir()
+                }
+                try {
+                    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepoUrl}"]]]
+                } catch (info) {
+                    retry(2) {
+                        echo "checkout failed, retry.."
+                        sleep 5
+                        if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                            echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ci"
+                            deleteDir()
+                        }
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepoUrl}"]]]
+                    }
+                }
+
+            }
+
+            stash includes: "go/src/github.com/pingcap/tiflow/**", name: "ticdc", useDefaultExcludes: false
         }
+
         def script_path = "go/src/github.com/pingcap/ci/jenkins/pipelines/ci/ticdc/integration_test_common.groovy"
         def common = load script_path
+
         // HACK! Download jks by injecting RACK_COMMAND
         // https://git.io/JJZXX -> https://github.com/pingcap/tiflow/raw/6e62afcfecc4e3965d8818784327d4bf2600d9fa/tests/_certificates/kafka.server.keystore.jks
         // https://git.io/JJZXM -> https://github.com/pingcap/tiflow/raw/6e62afcfecc4e3965d8818784327d4bf2600d9fa/tests/_certificates/kafka.server.truststore.jks
         def download_jks = 'curl -sfL https://git.io/JJZXX -o /tmp/kafka.server.keystore.jks && curl -sfL https://git.io/JJZXM -o /tmp/kafka.server.truststore.jks'
 
-        container("golang") {
+        catchError {
             def KAFKA_TAG = "2.12-2.4.1"
             def KAFKA_VERSION = "2.4.1"
             // parse kafka tag
@@ -279,20 +205,16 @@ catchError {
             common.prepare_binaries()
 
             def label = "cdc-kafka-integration-test"
-            if (GO_VERSION == "go1.13") {
-                label = "cdc-kafka-integration-test-go1130-${BUILD_NUMBER}"
-            }
-            if (GO_VERSION == "go1.16") {
-                label = "cdc-kafka-integration-test-go1160-${BUILD_NUMBER}"
-            }
-            if (GO_VERSION == "go1.18") {
-                label = "cdc-kafka-integration-test-go1180-${BUILD_NUMBER}"
+            if (isNeedGo1160) {
+                label = "cdc-kafka-integration-test-go1160-build-${BUILD_NUMBER}"
+            } else {
+                label = "cdc-kafka-integration-test-go1130-build-${BUILD_NUMBER}"
             }
             podTemplate(label: label,
                     idleMinutes: 0,
                     namespace: "jenkins-ticdc",
                     containers: [
-                            containerTemplate(name: 'golang', alwaysPullImage: true, image: "${POD_GO_IMAGE}",
+                            containerTemplate(name: 'golang', alwaysPullImage: true, image: "${POD_GO_DOCKER_IMAGE}",
                                     resourceRequestCpu: '2000m', resourceRequestMemory: '12Gi',
                                     ttyEnabled: true, command: 'cat'),
                             containerTemplate(name: 'zookeeper', alwaysPullImage: false, image: 'wurstmeister/zookeeper',
@@ -348,7 +270,7 @@ catchError {
 
             currentBuild.result = "SUCCESS"
         }
-        
+
         stage('Summary') {
             def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
             def slackmsg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
