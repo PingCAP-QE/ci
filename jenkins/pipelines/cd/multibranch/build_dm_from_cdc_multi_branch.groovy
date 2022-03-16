682353f82238a60e77e7c5f989ca48aaa33a8db0
dianqihanwangzi <dianqihanwangzi@gmail.com>
2022-03-16 16:13:43 +0800

    update dm multibranch

--- jenkins/pipelines/cd/multibranch/build_dm_from_cdc_multi_branch.groovy
+++ jenkins/pipelines/cd/multibranch/build_dm_from_cdc_multi_branch.groovy
@@ -1,3 +1,4 @@
+def GO_BIN_PATH="/usr/local/go/bin"
 def BUILD_URL = 'git@github.com:pingcap/ticdc.git'
 def build_path = 'go/src/github.com/pingcap/dm'
 def UCLOUD_OSS_URL = "http://pingcap-dev.hk.ufileos.com"
@@ -5,7 +6,6 @@ def branch = (env.TAG_NAME==null) ? "${env.BRANCH_NAME}" : "refs/tags/${env.TAG_
 def slackcolor = 'good'
 def githash
 def ws
-
 
 // choose which go version to use. 
 def String selectGoVersion(String branchORTag) {
@@ -31,7 +31,7 @@ def String selectGoVersion(String branchORTag) {
 println "This build use ${goVersion}"
 
 def GO_BUILD_SLAVE = GO1180_BUILD_SLAVE
-def GO_BIN_PATH = "/usr/local/go1.18rc1/bin"
+def GO_BIN_PATH = "/usr/local/go1.18/bin"
 goVersion = selectGoVersion(env.BRANCH_NAME)
 if ( goVersion == "go1.16" ) {
     GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
@@ -88,10 +88,23 @@ try {
             dir(build_path) {
                 container("golang") {
                     timeout(30) {
-                        sh """
-                        mkdir -p \$GOPATH/pkg/mod && mkdir -p ${ws}/go/pkg && ln -sf \$GOPATH/pkg/mod ${ws}/go/pkg/mod
-                        GOPATH=\$GOPATH:${ws}/go make dm
-                        """
+                        if ((branch.startsWith("release-") && branch <"release-6.0") || (branch.startsWith("v") && branch <"v6.0.0")) {
+                            sh """
+                            mkdir -p \$GOPATH/pkg/mod && mkdir -p ${ws}/go/pkg && ln -sf \$GOPATH/pkg/mod ${ws}/go/pkg/mod
+                            GOPATH=\$GOPATH:${ws}/go make dm
+                            """
+                        }else {
+                            cwd = pwd()
+                            sh """
+                            wget http://fileserver.pingcap.net/download/ee-tools/node-v16.14.0-linux-x64.tar.gz
+                            tar -xvf node-v16.14.0-linux-x64.tar.gz
+                            export PATH=${cwd}/node-v16.14.0-linux-x64/bin:\$PATH
+                            node -v
+                            npm install -g yarn
+                            mkdir -p \$GOPATH/pkg/mod && mkdir -p ${ws}/go/pkg && ln -sf \$GOPATH/pkg/mod ${ws}/go/pkg/mod
+                            GOPATH=\$GOPATH:${ws}/go make dm-master-with-webui dm-worker dmctl dm-syncer
+                            """
+                        }
                     }
                 }
             }
@@ -137,40 +150,39 @@ try {
                 }
             }
         }
+        // do not release dm-ansible  after v6.0.0
+        if ((branch.startsWith("release-") && branch <"release-6.0") || (branch.startsWith("v") && branch <"v6.0.0")) {
+            stage("package dm-ansible") {
+                dir(build_path) {
+                    container("golang") {
+                        timeout(10) {
+                            sh """
+                            cp -r dm/dm/dm-ansible ./
+                            tar -czvf dm-ansible.tar.gz dm-ansible
+                            """
+                        }
+                    }
+                }
+            }
 
-        stage("package dm-ansible") {
-            dir(build_path) {
-                container("golang") {
-                    timeout(10) {
+            stage("Upload dm-ansible") {
+                dir(build_path) {
+                    container("golang") {
+                        def refspath = "refs/pingcap/dm/${env.BRANCH_NAME}/dm-ansible-sha1"
+                        def filepath = "builds/pingcap/dm/${githash}/centos7/dm-ansible.tar.gz"
+
+                        writeFile file: 'dm-ansible-sha1', text: "${githash}"
                         sh """
-                        cp -r dm/dm/dm-ansible ./
-                        tar -czvf dm-ansible.tar.gz dm-ansible
+                        # ./filemgr-linux64 --action mput --bucket pingcap-dev --nobar --key ${refspath} --file dm-ansible-sha1
+                        curl -F ${refspath}=@dm-ansible-sha1 ${FILE_SERVER_URL}/upload 
+
+                        # ./filemgr-linux64 --action mput --bucket pingcap-dev --nobar --key builds/pingcap/dm/${githash}/centos7/dm-ansible.tar.gz --file dm-ansible.tar.gz
+                        curl -F ${filepath}=@dm-ansible.tar.gz ${FILE_SERVER_URL}/upload
                         """
                     }
                 }
             }
-        }
 
-        stage("Upload dm-ansible") {
-            dir(build_path) {
-                container("golang") {
-                    def refspath = "refs/pingcap/dm/${env.BRANCH_NAME}/dm-ansible-sha1"
-                    def filepath = "builds/pingcap/dm/${githash}/centos7/dm-ansible.tar.gz"
-
-                    writeFile file: 'dm-ansible-sha1', text: "${githash}"
-                    sh """
-                    # ./filemgr-linux64 --action mput --bucket pingcap-dev --nobar --key ${refspath} --file dm-ansible-sha1
-                    curl -F ${refspath}=@dm-ansible-sha1 ${FILE_SERVER_URL}/upload 
-
-                    # ./filemgr-linux64 --action mput --bucket pingcap-dev --nobar --key builds/pingcap/dm/${githash}/centos7/dm-ansible.tar.gz --file dm-ansible.tar.gz
-                    curl -F ${filepath}=@dm-ansible.tar.gz ${FILE_SERVER_URL}/upload
-                    """
-                }
-            }
-        }
-
-        // we Build and Push monitoring image here, because no source code in `release_dm` job.
-        if (branch != "release-1.0" && !branch.startsWith("v1.")) {
             stage("Generate monitoring") {
                 dir(build_path) {
                     container("golang") {
@@ -189,6 +201,41 @@ try {
                     }
                 }
                 stash includes: "go/src/github.com/pingcap/dm/dm/monitoring/**", name: "monitoring"
+            }
+        } else {
+            stage("package dm-ansible") {
+                dir(build_path) {
+                    container("golang") {
+                        timeout(10) {
+                            sh """
+                            mkdir -p dm-ansible
+                            mkdir -p dm-ansible/conf
+                            mkdir -p dm-ansible/scripts
+                            cp dm/metrics/alertmanager/dm_worker.rules.yml dm-ansible/conf
+                            cp dm/metrics/grafana/* dm-ansible/scripts
+                            tar -czvf dm-ansible.tar.gz dm-ansible
+                            """
+                        }
+                    }
+                }
+            }
+
+            stage("Upload dm-ansible") {
+                dir(build_path) {
+                    container("golang") {
+                        def refspath = "refs/pingcap/dm/${env.BRANCH_NAME}/dm-ansible-sha1"
+                        def filepath = "builds/pingcap/dm/${githash}/centos7/dm-ansible.tar.gz"
+
+                        writeFile file: 'dm-ansible-sha1', text: "${githash}"
+                        sh """
+                        # ./filemgr-linux64 --action mput --bucket pingcap-dev --nobar --key ${refspath} --file dm-ansible-sha1
+                        curl -F ${refspath}=@dm-ansible-sha1 ${FILE_SERVER_URL}/upload 
+
+                        # ./filemgr-linux64 --action mput --bucket pingcap-dev --nobar --key builds/pingcap/dm/${githash}/centos7/dm-ansible.tar.gz --file dm-ansible.tar.gz
+                        curl -F ${filepath}=@dm-ansible.tar.gz ${FILE_SERVER_URL}/upload
+                        """
+                    }
+                }
             }
         }
     }
@@ -233,12 +280,27 @@ try {
         stage("ARM - Build binary") {
             dir(build_path) {
                 timeout(30) {
-                    sh """
-                    export PATH=${GO_BIN_PATH}:/usr/local/node/bin:/root/go/bin:/root/.cargo/bin:/usr/lib64/ccache:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
-                    go version
-                    mkdir -p \$GOPATH/pkg/mod && mkdir -p ${ws}/go/pkg && ln -sf \$GOPATH/pkg/mod ${ws}/go/pkg/mod
-                    GOPATH=\$GOPATH:${ws}/go make dm
-                    """
+                    if ((branch.startsWith("release-") && branch <"release-6.0") || (branch.startsWith("v") && branch <"v6.0.0")) {
+                        sh """
+                        export PATH=${GO_BIN_PATH}:/usr/local/node/bin:/root/go/bin:/root/.cargo/bin:/usr/lib64/ccache:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+                        go version
+                        mkdir -p \$GOPATH/pkg/mod && mkdir -p ${ws}/go/pkg && ln -sf \$GOPATH/pkg/mod ${ws}/go/pkg/mod
+                        GOPATH=\$GOPATH:${ws}/go make dm
+                        """
+                    }else {
+                        cwd = pwd()
+                        sh """
+                        export PATH=${GO_BIN_PATH}:/usr/local/node/bin:/root/go/bin:/root/.cargo/bin:/usr/lib64/ccache:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+                        wget http://fileserver.pingcap.net/download/ee-tools/node-v16.14.0-linux-arm64.tar.gz
+                        tar -xvf node-v16.14.0-linux-arm64.tar.gz
+                        export PATH=${cwd}/node-v16.14.0-linux-arm64/bin:\$PATH
+                        node -v
+                        npm install -g yarn
+                        go version
+                        mkdir -p \$GOPATH/pkg/mod && mkdir -p ${ws}/go/pkg && ln -sf \$GOPATH/pkg/mod ${ws}/go/pkg/mod
+                        GOPATH=\$GOPATH:${ws}/go make dm-master-with-webui dm-worker dmctl dm-syncer
+                        """
+                    }
                 }
             }
         }
@@ -293,20 +355,23 @@ try {
         } else {
             RELEASE_TAG = "${RELEASE_TAG}-nightly"
         }
-        stage("Publish Monitor Docker Image") {
-            node("delivery") {
-                container("delivery") {
-                    deleteDir()
-                    unstash 'monitoring'
-                    dir("go/src/github.com/pingcap/dm/dm/monitoring") {
-                        withDockerServer([uri: "${env.DOCKER_HOST}"]) {
-                            docker.build("pingcap/dm-monitor-initializer:${RELEASE_TAG}").push()
-                        }
-                        docker.withRegistry("https://uhub.service.ucloud.cn", "ucloud-registry") {
-                            sh """
-                                docker tag pingcap/dm-monitor-initializer:${RELEASE_TAG} uhub.service.ucloud.cn/pingcap/dm-monitor-initializer:${RELEASE_TAG}
-                                docker push uhub.service.ucloud.cn/pingcap/dm-monitor-initializer:${RELEASE_TAG}
-                            """
+        // do not release dm-monitor-initializer after v6.0.0
+        if ((branch.startsWith("release-") && branch <"release-6.0") || (branch.startsWith("v") && branch <"v6.0.0")) {
+            stage("Publish Monitor Docker Image") {
+                node("delivery") {
+                    container("delivery") {
+                        deleteDir()
+                        unstash 'monitoring'
+                        dir("go/src/github.com/pingcap/dm/dm/monitoring") {
+                            withDockerServer([uri: "${env.DOCKER_HOST}"]) {
+                                docker.build("pingcap/dm-monitor-initializer:${RELEASE_TAG}").push()
+                            }
+                            docker.withRegistry("https://uhub.service.ucloud.cn", "ucloud-registry") {
+                                sh """
+                                    docker tag pingcap/dm-monitor-initializer:${RELEASE_TAG} uhub.service.ucloud.cn/pingcap/dm-monitor-initializer:${RELEASE_TAG}
+                                    docker push uhub.service.ucloud.cn/pingcap/dm-monitor-initializer:${RELEASE_TAG}
+                                """
+                            }
                         }
                     }
                 }
