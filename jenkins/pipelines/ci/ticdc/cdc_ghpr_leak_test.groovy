b53b658b816316737ea85557758588b3f1c020fc
Lifu Wu <purelind@users.noreply.github.com>
2022-03-17 11:55:21 +0800

    tiflow ci use go1.18 (#814)
    * tiflow master support go1.18
    
    * fix pod name map error
    
    * fix
    
    * just set func selectGoversion in pipeline
    
    * fix kafka pod failed

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_leak_test.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_leak_test.groovy
@@ -8,79 +8,63 @@ if (ghprbPullId == null || ghprbPullId == "") {
     specStr = "+refs/heads/*:refs/remotes/origin/*"
 }
 
-@NonCPS
-boolean isMoreRecentOrEqual(String a, String b) {
-    if (a == b) {
-        return true
-    }
+GO_VERSION = "go1.18"
+POD_GO_IMAGE = ""
+GO_IMAGE_MAP = [
+    "go1.13": "hub.pingcap.net/jenkins/centos7_golang-1.13:latest",
+    "go1.16": "hub.pingcap.net/jenkins/centos7_golang-1.16:latest",
+    "go1.18": "hub.pingcap.net/jenkins/centos7_golang-1.18:latest",
+]
+POD_LABEL_MAP = [
+    "go1.13": "${JOB_NAME}-go1130-${BUILD_NUMBER}",
+    "go1.16": "${JOB_NAME}-go1160-${BUILD_NUMBER}",
+    "go1.18": "${JOB_NAME}-go1180-${BUILD_NUMBER}",
+]
 
-    [a, b]*.tokenize('.')*.collect { it as int }.with { u, v ->
-        Integer result = [u, v].transpose().findResult { x, y -> x <=> y ?: null } ?: u.size() <=> v.size()
-        return (result == 1)
-    }
+node("master") {
+    deleteDir()
+    def ws = pwd()
+    sh "curl -O https://raw.githubusercontent.com/PingCAP-QE/ci/main/jenkins/pipelines/goversion-select-lib.groovy"
+    def script_path = "${ws}/goversion-select-lib.groovy"
+    def goversion_lib = load script_path
+    GO_VERSION = goversion_lib.selectGoVersion(ghprbTargetBranch)
+    POD_GO_IMAGE = GO_IMAGE_MAP[GO_VERSION]
+    println "go version: ${GO_VERSION}"
+    println "go image: ${POD_GO_IMAGE}"
 }
 
-string trimPrefix = {
-    it.startsWith('release-') ? it.minus('release-').split("-")[0] : it
-}
-
-def boolean isBranchMatched(List<String> branches, String targetBranch) {
-    for (String item : branches) {
-        if (targetBranch.startsWith(item)) {
-            println "targetBranch=${targetBranch} matched in ${branches}"
-            return true
-        }
-    }
-    return false
-}
-
-isNeedGo1160 = false
-releaseBranchUseGo1160 = "release-5.1"
-
-if (!isNeedGo1160) {
-    isNeedGo1160 = isBranchMatched(["master", "hz-poc", "release-multi-source"], ghprbTargetBranch)
-}
-if (!isNeedGo1160 && ghprbTargetBranch.startsWith("release-")) {
-    isNeedGo1160 = isMoreRecentOrEqual(trimPrefix(ghprbTargetBranch), trimPrefix(releaseBranchUseGo1160))
-    if (isNeedGo1160) {
-        println "targetBranch=${ghprbTargetBranch}  >= ${releaseBranchUseGo1160}"
-    }
-}
 
 def run_with_pod(Closure body) {
-    def label = "cdc_ghpr_leak_test_1.13-${BUILD_NUMBER}"
-    pod_go_docker_image = "hub.pingcap.net/jenkins/centos7_golang-1.13:latest"
-    if (isNeedGo1160) {
-        println "Use go1.16.4"
-        pod_go_docker_image = "hub.pingcap.net/pingcap/centos7_golang-1.16:latest"
-        label = "cdc_ghpr_leak_test_1.16-${BUILD_NUMBER}"
-    } else {
-        println "Use go1.13.7"
-    }
+    def label = POD_LABEL_MAP[GO_VERSION]
+    def cloud = "kubernetes"
+    def namespace = "jenkins-tidb"
+    def jnlp_docker_image = "jenkins/inbound-agent:4.3-4"
     podTemplate(label: label,
-            cloud: "kubernetes",
-            namespace: "jenkins-tidb",
-            containers: [containerTemplate(
-                    name: 'golang', alwaysPullImage: true,
-                    image: "${pod_go_docker_image}", ttyEnabled: true,
-                    resourceRequestCpu: '4000m', resourceRequestMemory: '8Gi',
-                    resourceLimitCpu: '10000m', resourceLimitMemory: "20Gi",
-                    command: '/bin/sh -c', args: 'cat',
-                    envVars: [containerEnvVar(key: 'GOPATH', value: '/go')],
-            )],
+            cloud: cloud,
+            namespace: namespace,
+            idleMinutes: 0,
+            containers: [
+                    containerTemplate(
+                        name: 'golang', alwaysPullImage: true,
+                        image: "${POD_GO_IMAGE}", ttyEnabled: true,
+                        resourceRequestCpu: '4000m', resourceRequestMemory: '8Gi',
+                        command: '/bin/sh -c', args: 'cat',
+                        envVars: [containerEnvVar(key: 'GOPATH', value: '/go')]     
+                    )
+            ],
             volumes: [
                     nfsVolume(mountPath: '/home/jenkins/agent/ci-cached-code-daily', serverAddress: '172.16.5.22',
                             serverPath: '/mnt/ci.pingcap.net-nfs/git', readOnly: false),
-            ],
+                    emptyDirVolume(mountPath: '/tmp', memory: false),
+                    emptyDirVolume(mountPath: '/home/jenkins', memory: false)
+                    ],
     ) {
         node(label) {
-            println("${NODE_NAME}")
-            println "current pod use CPU 4000m & Memory 8Gi"
+            println "debug command:\nkubectl -n ${namespace} exec -ti ${NODE_NAME} bash"
             body()
         }
     }
 }
-
 
 catchError {
     run_with_pod() {
@@ -88,48 +72,50 @@ catchError {
             def ws = pwd()
             deleteDir()
 
-            dir("${ws}/go/src/github.com/pingcap/tiflow") {
-                if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                    echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/tiflow"
-                    deleteDir()
-                }
-                try {
-                    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tiflow.git']]]
-                } catch (info) {
-                    retry(2) {
-                        echo "checkout failed, retry.."
-                        sleep 5
-                        if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                            deleteDir()
-                        }
+            container("golang") {
+                dir("${ws}/go/src/github.com/pingcap/tiflow") {
+                    if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                        echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/tiflow"
+                        deleteDir()
+                    }
+                    try {
                         checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tiflow.git']]]
-                    }
-                }
-                sh "git checkout -f ${ghprbActualCommit}"
-            }
-
-            dir("${ws}/go/src/github.com/pingcap/ci") {
-                if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                    echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ci"
-                    deleteDir()
-                }
-                try {
-                    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepoUrl}"]]]
-                } catch (info) {
-                    retry(2) {
-                        echo "checkout failed, retry.."
-                        sleep 5
-                        if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                            echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ci"
-                            deleteDir()
+                    } catch (info) {
+                        retry(2) {
+                            echo "checkout failed, retry.."
+                            sleep 5
+                            if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                                deleteDir()
+                            }
+                            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tiflow.git']]]
                         }
-                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepoUrl}"]]]
                     }
+                    sh "git checkout -f ${ghprbActualCommit}"
                 }
 
-            }
+                dir("${ws}/go/src/github.com/pingcap/ci") {
+                    if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                        echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ci"
+                        deleteDir()
+                    }
+                    try {
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepoUrl}"]]]
+                    } catch (info) {
+                        retry(2) {
+                            echo "checkout failed, retry.."
+                            sleep 5
+                            if (sh(returnStatus: true, script: '[ -d .git ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                                echo "Not a valid git folder: ${ws}/go/src/github.com/pingcap/ci"
+                                deleteDir()
+                            }
+                            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: "${ciRepoBranch}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[refspec: specStr, url: "${ciRepoUrl}"]]]
+                        }
+                    }
 
-            stash includes: "go/src/github.com/pingcap/tiflow/**", name: "ticdc", useDefaultExcludes: false
+                }
+
+                stash includes: "go/src/github.com/pingcap/tiflow/**", name: "ticdc", useDefaultExcludes: false
+            }
         }
 
         catchError {
