817a3bd5256cfe3ea59ca1411e2fc9981650ff59
Lifu Wu <purelind@users.noreply.github.com>
2022-03-17 17:21:35 +0800

    br it test upgrade go1.18 (#819)

--- jenkins/pipelines/ci/br/br_ghpr_unit_and_integration_test.groovy
+++ jenkins/pipelines/ci/br/br_ghpr_unit_and_integration_test.groovy
@@ -173,54 +173,64 @@ if (getImporterBranch(ghprbCommentBody)!=""){
 }
 println "TIKV_IMPORTER_BRANCH=${TIKV_IMPORTER_BRANCH}"
 
-@NonCPS
-boolean isMoreRecentOrEqual( String a, String b ) {
-    if (a == b) {
-        return true
-    }
+GO_VERSION = "go1.18"
+POD_GO_IMAGE = ""
+GO_IMAGE_MAP = [
+    "go1.13": "hub.pingcap.net/jenkins/centos7_golang-1.13:latest",
+    "go1.16": "hub.pingcap.net/jenkins/centos7_golang-1.16:latest",
+    "go1.18": "hub.pingcap.net/jenkins/centos7_golang-1.18:latest",
+]
+POD_LABEL_MAP = [
+    "go1.13": "${JOB_NAME}-go1130-${BUILD_NUMBER}",
+    "go1.16": "${JOB_NAME}-go1160-${BUILD_NUMBER}",
+    "go1.18": "${JOB_NAME}-go1180-${BUILD_NUMBER}",
+]
 
-    [a,b]*.tokenize('.')*.collect { it as int }.with { u, v ->
-       Integer result = [u,v].transpose().findResult{ x,y -> x <=> y ?: null } ?: u.size() <=> v.size()
-       return (result == 1)
-    } 
+node("master") {
+    deleteDir()
+    def ws = pwd()
+    sh "curl -O https://raw.githubusercontent.com/PingCAP-QE/ci/main/jenkins/pipelines/goversion-select-lib.groovy"
+    def script_path = "${ws}/goversion-select-lib.groovy"
+    def goversion_lib = load script_path
+    GO_VERSION = goversion_lib.selectGoVersion(ghprbTargetBranch)
+    POD_GO_IMAGE = GO_IMAGE_MAP[GO_VERSION]
+    println "go version: ${GO_VERSION}"
+    println "go image: ${POD_GO_IMAGE}"
 }
 
-string trimPrefix = {
-        it.startsWith('release-') ? it.minus('release-').split("-")[0] : it 
-    }
 
-def boolean isBranchMatched(List<String> branches, String targetBranch) {
-    for (String item : branches) {
-        if (targetBranch.startsWith(item)) {
-            println "targetBranch=${targetBranch} matched in ${branches}"
-            return true
+def run_with_pod(Closure body) {
+    def label = POD_LABEL_MAP[GO_VERSION]
+    def cloud = "kubernetes"
+    def namespace = "jenkins-tidb"
+    def jnlp_docker_image = "jenkins/inbound-agent:4.3-4"
+    podTemplate(label: label,
+            cloud: cloud,
+            namespace: namespace,
+            idleMinutes: 0,
+            containers: [
+                    containerTemplate(
+                        name: 'golang', alwaysPullImage: true,
+                        image: "${POD_GO_IMAGE}", ttyEnabled: true,
+                        resourceRequestCpu: '4000m', resourceRequestMemory: '8Gi',
+                        command: '/bin/sh -c', args: 'cat',
+                        envVars: [containerEnvVar(key: 'GOPATH', value: '/go')]     
+                    )
+            ],
+            volumes: [
+                    nfsVolume(mountPath: '/home/jenkins/agent/ci-cached-code-daily', serverAddress: '172.16.5.22',
+                            serverPath: '/mnt/ci.pingcap.net-nfs/git', readOnly: false),
+                    emptyDirVolume(mountPath: '/tmp', memory: false),
+                    emptyDirVolume(mountPath: '/home/jenkins', memory: false)
+                    ],
+    ) {
+        node(label) {
+            println "debug command:\nkubectl -n ${namespace} exec -ti ${NODE_NAME} bash"
+            body()
         }
     }
-    return false
 }
 
-def isNeedGo1160 = false
-releaseBranchUseGo1160 = "release-5.1"
-
-if (!isNeedGo1160) {
-    isNeedGo1160 = isBranchMatched(["master", "hz-poc"], ghprbTargetBranch)
-}
-if (!isNeedGo1160 && ghprbTargetBranch.startsWith("release-")) {
-    isNeedGo1160 = isMoreRecentOrEqual(trimPrefix(ghprbTargetBranch), trimPrefix(releaseBranchUseGo1160))
-    if (isNeedGo1160) {
-        println "targetBranch=${ghprbTargetBranch}  >= ${releaseBranchUseGo1160}"
-    }
-}
-
-if (isNeedGo1160) {
-    println "This build use go1.16"
-    GO_BUILD_SLAVE = GO1160_BUILD_SLAVE
-    GO_TEST_SLAVE = GO1160_TEST_SLAVE
-} else {
-    println "This build use go1.13"
-}
-println "BUILD_NODE_NAME=${GO_BUILD_SLAVE}"
-println "TEST_NODE_NAME=${GO_TEST_SLAVE}"
 
 all_task_result = []
 
@@ -251,7 +261,7 @@ def get_commit_hash(prj, branch_or_hash) {
 
 def run_unit_test() {
     try {
-        node("${GO_TEST_SLAVE}") {
+        run_with_pod {
             container("golang") {
                 println "debug command:\nkubectl -n jenkins-tidb exec -ti ${NODE_NAME} bash"
                 def ws = pwd()
@@ -313,7 +323,7 @@ def run_unit_test() {
 
 def run_integration_tests(case_names, tidb, tikv, pd, cdc, importer, tiflashBranch, tiflashCommit) {
     try {
-        node("${GO_TEST_SLAVE}") {
+        run_with_pod {
             container("golang") {
                 println "debug command:\nkubectl -n jenkins-tidb exec -ti ${NODE_NAME} bash"
                 def ws = pwd()
@@ -550,7 +560,7 @@ try {
     ]
 
     stage('Prepare') {
-        node("${GO_BUILD_SLAVE}") {
+        run_with_pod {
             container("golang") {
                 println "debug command:\nkubectl -n jenkins-tidb exec -ti ${NODE_NAME} bash"
                 def ws = pwd()
@@ -773,7 +783,7 @@ try {
 
     stage('Coverage') {
         // Skip upload coverage when the job is initialed by TiDB PRs.
-        node("${GO_TEST_SLAVE}") {
+        run_with_pod {
             container("golang") {
                 println "debug command:\nkubectl -n jenkins-tidb exec -ti ${NODE_NAME} bash"
                 if (params.containsKey("triggered_by_upstream_pr_ci") || params.containsKey("release_test")) {
