789623ed2f2e66a5f2637f0024db87b1d29d883f
dianqihanwangzi <dianqihanwangzi@gmail.com>
2022-03-18 11:38:25 +0800

    update docker user

--- jenkins/pipelines/ci/tikv/tikv_ghpr_cache.groovy
+++ jenkins/pipelines/ci/tikv/tikv_ghpr_cache.groovy
@@ -4,7 +4,7 @@ stage("Build") {
 
         def checkAndBuild = { ghBranch, content, tag, args, allowStaleCache ->
             container("dind"){
-                docker.withRegistry("https://hub.pingcap.net", "harbor-jenkins") {
+                docker.withRegistry("https://hub.pingcap.net", "harbor-pingcap") {
                     // 2 means no image, 1 means stale, 0 means uptodate.
                     def cacheState = 2
                     if (!params.skip_sha_check) {
