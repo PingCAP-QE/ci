d8604dcdca48239cfc45dedbc0fead782ffbbec7
purelind <purelind@gmail.com>
2022-03-22 17:11:24 +0800

    fix

--- jenkins/pipelines/ci/tidb/tidb_ghpr_mysql_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_mysql_test.groovy
@@ -51,7 +51,6 @@ CI_RUN_PART_TEST_CASES = """
     type_binary type_uint union update update_stmt variable 
     with_recursive with_recursive_bugs xd
     index_merge_sqlgen_exprs index_merge_sqlgen_exprs_orandor_1_no_out_trans index_merge1
-    variables case ctype_gbk
     """
 
 // remove test: temp_table
@@ -275,7 +274,7 @@ try {
                 dir("go/src/github.com/pingcap/tidb-test/${test_dir}") {
                     try {
                         timeout(10) {
-                            if (ghprbTargetBranch in ["master", "release-6.0"]) {
+                            if (ghprbTargetBranch in ["master"]) {
                                 sh """
                                 curl -o run-test-part.sh ${FILE_SERVER_URL}/download/cicd/tidb-mysql-test-ci/run-test-part.sh
                                 chmod +x run-test-part.sh
@@ -299,7 +298,7 @@ try {
                                 rm -rf /tmp/tidb
                                 set -e
                                 """
-                            else if (ghprbTargetBranch in ["relase-6.0"]) {
+                            else if (ghprbTargetBranch in ["release-6.0"]) {
                                 sh """
                                 set +e
                                 killall -9 -r tidb-server
