b8ecfdbc6f2ae2baa6754c77ef84fb630d568f70
lvhongmeng <hongmeng.lv@pingcap.com>
2022-04-28 16:24:37 +0800

    update notify

--- jenkins/pipelines/test_send_notify.groovy
+++ jenkins/pipelines/test_send_notify.groovy
@@ -20,23 +20,24 @@ catchError {
             }
 
             stage("Publish arm64 docker images") {
-               echo 'test2'
+                echo 'test2'
+            }
         }
-    }
 
-    currentBuild.result = "SUCCESS"
+        currentBuild.result = "SUCCESS"
 
-    post{
-        always{
-            build job: 'send_notify',
-                    wait: true,
-                    parameters: [
-                            [$class: 'StringParameterValue', name: 'RESULT_JOB_NAME', value: "${JOB_NAME}"],
-                            [$class: 'StringParameterValue', name: 'RESULT_BUILD_RESULT', value: currentBuild.result],
-                            [$class: 'StringParameterValue', name: 'RESULT_BUILD_NUMBER', value: "${BUILD_NUMBER}"],
-                            [$class: 'StringParameterValue', name: 'RESULT_RUN_DISPLAY_URL', value: "${RUN_DISPLAY_URL}"],
-                            [$class: 'StringParameterValue', name: 'RESULT_TASK_START_TS', value: "${taskStartTimeInMillis}"]
-                    ]
+        post {
+            always {
+                build job: 'send_notify',
+                        wait: true,
+                        parameters: [
+                                [$class: 'StringParameterValue', name: 'RESULT_JOB_NAME', value: "${JOB_NAME}"],
+                                [$class: 'StringParameterValue', name: 'RESULT_BUILD_RESULT', value: currentBuild.result],
+                                [$class: 'StringParameterValue', name: 'RESULT_BUILD_NUMBER', value: "${BUILD_NUMBER}"],
+                                [$class: 'StringParameterValue', name: 'RESULT_RUN_DISPLAY_URL', value: "${RUN_DISPLAY_URL}"],
+                                [$class: 'StringParameterValue', name: 'RESULT_TASK_START_TS', value: "${taskStartTimeInMillis}"]
+                        ]
+            }
         }
     }
 }
