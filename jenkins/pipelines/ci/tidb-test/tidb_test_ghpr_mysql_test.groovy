29aa06deb672cfa95eaca2c7ff5c64277d259637
Lifu Wu <purelind@users.noreply.github.com>
2022-04-29 12:50:58 +0800

    run 3 times mysql-test in ci (#929)

--- jenkins/pipelines/ci/tidb-test/tidb_test_ghpr_mysql_test.groovy
+++ jenkins/pipelines/ci/tidb-test/tidb_test_ghpr_mysql_test.groovy
@@ -66,92 +66,193 @@ def run_with_pod(Closure body) {
     }
 }
 
-run_with_pod {
-    container("golang") {
-        def ws = pwd()
-        def tidb_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/tidb/${TIDB_BRANCH}/sha1").trim()
-        def tidb_url = "${FILE_SERVER_URL}/download/builds/pingcap/tidb-check/pr/${tidb_sha1}/centos7/tidb-server.tar.gz"
-        def tidb_done_url = "${FILE_SERVER_URL}/download/builds/pingcap/tidb-check/pr/${tidb_sha1}/centos7/done"
-        
-        stage("Get commits and Set params") {
-            println "TIDB_BRANCH: ${TIDB_BRANCH}"
-            println "tidb_sha1: $tidb_sha1"
-            println "tidb_url: $tidb_url"
-            ghprbCommentBody = ghprbCommentBody + " /tidb-test=pr/$ghprbPullId"
-            println "commentbody: $ghprbCommentBody"
-            basic_params = [
-                string(name: "upstreamJob", value: "tidb_test_ghpr_mysql_test"),
-                string(name: "ghprbCommentBody", value: ghprbCommentBody),
-                string(name: "ghprbPullTitle", value: ghprbPullTitle),
-                string(name: "ghprbPullLink", value: ghprbPullLink),
-                string(name: "ghprbPullDescription", value: ghprbPullDescription),
-                string(name: "ghprbTargetBranch", value: TIDB_BRANCH),
-                string(name: "ghprbActualCommit", value: tidb_sha1)
-            ]
 
-            println "basic_params: $basic_params"
+try {
+    run_with_pod {
+        container("golang") {
+            def ws = pwd()
+            def tidb_sha1 = sh(returnStdout: true, script: "curl ${FILE_SERVER_URL}/download/refs/pingcap/tidb/${TIDB_BRANCH}/sha1").trim()
+            def tidb_url = "${FILE_SERVER_URL}/download/builds/pingcap/tidb-check/pr/${tidb_sha1}/centos7/tidb-server.tar.gz"
+            def tidb_done_url = "${FILE_SERVER_URL}/download/builds/pingcap/tidb-check/pr/${tidb_sha1}/centos7/done"
             
-        }
+            stage("Get commits and Set params") {
+                println "TIDB_BRANCH: ${TIDB_BRANCH}"
+                println "tidb_sha1: $tidb_sha1"
+                println "tidb_url: $tidb_url"
+                ghprbCommentBody = ghprbCommentBody + " /tidb-test=pr/$ghprbPullId"
+                println "commentbody: $ghprbCommentBody"
+                basic_params = [
+                    string(name: "upstreamJob", value: "tidb_test_ghpr_mysql_test"),
+                    string(name: "ghprbCommentBody", value: ghprbCommentBody),
+                    string(name: "ghprbPullTitle", value: ghprbPullTitle),
+                    string(name: "ghprbPullLink", value: ghprbPullLink),
+                    string(name: "ghprbPullDescription", value: ghprbPullDescription),
+                    string(name: "ghprbTargetBranch", value: TIDB_BRANCH),
+                    string(name: "ghprbActualCommit", value: tidb_sha1)
+                ]
 
-        stage("Checkout") {
-            parallel(
-                'tidb-test': {
-                    dir("go/src/github.com/pingcap/tidb-test") {
-                        checkout(changelog: false, poll: false, scm: [
-                            $class: "GitSCM",
-                            branches: [
-                                [name: "${ghprbActualCommit}"],
-                            ],
-                            userRemoteConfigs: [
-                                [
-                                    url: "git@github.com:pingcap/tidb-test.git",
-                                    refspec: "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*",
-                                    credentialsId: 'github-sre-bot-ssh',
-                                ]
-                            ],
-                            extensions: [
-                                [$class: 'PruneStaleBranch'],
-                                [$class: 'CleanBeforeCheckout'],
-                            ],
-                        ])
-                    }
-                }, 
-                'tidb': {
-                    dir("go/src/github.com/pingcap/tidb") {
-                        deleteDir()
-                        timeout(10) {
-                            retry(3){
-                                deleteDir()
-                                sh """
-                                while ! curl --output /dev/null --silent --head --fail ${tidb_done_url}; do sleep 1; done
-                                curl ${tidb_url} | tar xz
-                                """
+                println "basic_params: $basic_params"
+                
+            }
+
+            stage("Checkout") {
+                parallel(
+                    'tidb-test': {
+                        dir("go/src/github.com/pingcap/tidb-test") {
+                            checkout(changelog: false, poll: false, scm: [
+                                $class: "GitSCM",
+                                branches: [
+                                    [name: "${ghprbActualCommit}"],
+                                ],
+                                userRemoteConfigs: [
+                                    [
+                                        url: "git@github.com:pingcap/tidb-test.git",
+                                        refspec: "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*",
+                                        credentialsId: 'github-sre-bot-ssh',
+                                    ]
+                                ],
+                                extensions: [
+                                    [$class: 'PruneStaleBranch'],
+                                    [$class: 'CleanBeforeCheckout'],
+                                ],
+                            ])
+                        }
+                        stash includes: "go/src/github.com/pingcap/tidb-test/**", name: "tidb-test"
+                    }, 
+                    'tidb': {
+                        dir("go/src/github.com/pingcap/tidb") {
+                            deleteDir()
+                            timeout(10) {
+                                retry(3){
+                                    deleteDir()
+                                    sh """
+                                    while ! curl --output /dev/null --silent --head --fail ${tidb_done_url}; do sleep 1; done
+                                    curl ${tidb_url} | tar xz
+                                    """
+                                }
                             }
                         }
                     }
-                }
-            ) 
-        }
+                ) 
+            }
 
-        stage("Tests") {
-            parallel(
-                "run-all-tests": {
-                    dir("go/src/github.com/pingcap/tidb-test") {
-                        timeout(10) {
-                            sh """
-                            export TIDB_SRC_PATH=${ws}/go/src/github.com/pingcap/tidb
-                            export TIDB_SERVER_PATH=${ws}/go/src/github.com/pingcap/tidb/bin/tidb-server
-                            cd mysql_test && ./build.sh
-                            ./test.sh
-                            """
+            stage("Tests") {
+                parallel(
+                    "test1": {
+                        run_with_pod {
+                        container("golang") {
+                            dir("go/src/github.com/pingcap/tidb") {
+                                timeout(10) {
+                                    retry(3){
+                                        sh """
+                                        while ! curl --output /dev/null --silent --head --fail ${tidb_done_url}; do sleep 1; done
+                                        curl ${tidb_url} | tar xz
+                                        """
+                                    }
+                                }
+                            }
+                            unstash "tidb-test"
+                            dir("go/src/github.com/pingcap/tidb-test") {
+                                timeout(10) {
+                                    sh """
+                                    export TIDB_SRC_PATH=${ws}/go/src/github.com/pingcap/tidb
+                                    export TIDB_SERVER_PATH=${ws}/go/src/github.com/pingcap/tidb/bin/tidb-server
+                                    cd mysql_test && ./build.sh
+                                    ./test.sh
+                                    
+                                    pwd && ls -l
+                                    """
+                                }
+                                if (ghprbTargetBranch == "master") {
+                                    sh """
+                                    pwd && ls -l
+                                    cp mysql_test/result.xml test1_result.xml
+                                    """
+                                    junit testResults: "**/test1_result.xml"
+                                }
+                            }
                         }
+                        }
+                    },
+                    "test2": {
+                        run_with_pod {
+                        container("golang") {
+                            dir("go/src/github.com/pingcap/tidb") {
+                                timeout(10) {
+                                    retry(3){
+                                        sh """
+                                        while ! curl --output /dev/null --silent --head --fail ${tidb_done_url}; do sleep 1; done
+                                        curl ${tidb_url} | tar xz
+                                        """
+                                    }
+                                }
+                            }
+                            unstash "tidb-test"
+                            dir("go/src/github.com/pingcap/tidb-test") {
+                                timeout(10) {
+                                    sh """
+                                    export TIDB_SRC_PATH=${ws}/go/src/github.com/pingcap/tidb
+                                    export TIDB_SERVER_PATH=${ws}/go/src/github.com/pingcap/tidb/bin/tidb-server
+                                    cd mysql_test && ./build.sh
+                                    ./test.sh
+                                    pwd && ls -l
+                                    """
+                                    if (ghprbTargetBranch == "master") {
+                                        sh """
+                                        pwd && ls -l
+                                        cp mysql_test/result.xml test2_result.xml
+                                        """
+                                        junit testResults: "**/test2_result.xml"
+                                    }
+                                }
+                            }
+                        }
+                        }
+                    },
+                    "test3": {
+                        run_with_pod {
+                        container("golang") {
+                            dir("go/src/github.com/pingcap/tidb") {
+                                timeout(10) {
+                                    retry(3){
+                                        sh """
+                                        while ! curl --output /dev/null --silent --head --fail ${tidb_done_url}; do sleep 1; done
+                                        curl ${tidb_url} | tar xz
+                                        """
+                                    }
+                                }
+                            }
+                            unstash "tidb-test"
+                            dir("go/src/github.com/pingcap/tidb-test") {
+                                timeout(10) {
+                                    sh """
+                                    export TIDB_SRC_PATH=${ws}/go/src/github.com/pingcap/tidb
+                                    export TIDB_SERVER_PATH=${ws}/go/src/github.com/pingcap/tidb/bin/tidb-server
+                                    cd mysql_test && ./build.sh
+                                    ./test.sh
+                                    pwd && ls -l
+                                    """
+                                    if (ghprbTargetBranch == "master") {
+                                        sh """
+                                        pwd && ls -l
+                                        cp mysql_test/result.xml test3_result.xml
+                                        """
+                                        junit testResults: "**/test3_result.xml"
+                                    }
+                                }
+                            }
+                        }
+                        }
+                    },
+                    "trigger tidb_ghpr_mysql_test": {
+                        build(job: "tidb_ghpr_mysql_test", parameters: basic_params, wait: true)
                     }
-                    
-                },
-                "run-tests-in-wightlist": {
-                    build(job: "tidb_ghpr_mysql_test", parameters: basic_params, wait: true)
-                }
-            )
+                )
+            }
         }
     }
+    currentBuild.result = "SUCCESS"
+} catch (Exception e) {
+    currentBuild.result = "FAILURE"
+    echo "${e}"
 }
