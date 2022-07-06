2d8f61ed45bf02b8d0e5f451d46c13ad34e1882a
lvhongmeng <hongmeng.lv@pingcap.com>
2022-07-06 10:11:40 +0800

    add param for upload

--- jenkins/pipelines/cd/upload-result-to-db.groovy
+++ jenkins/pipelines/cd/upload-result-to-db.groovy
@@ -34,6 +34,12 @@ properties([
                 ),
                 string(
                         defaultValue: '',
+                        name: 'PUSH_GCR',
+                        description: '',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
                         name: 'JOB_NAME',
                         description: '',
                         trim: true
@@ -119,7 +125,8 @@ node("pipeline_data_collection_pod") {
                 "artifact_type":params.ARTIFACT_TYPE,
                 "branch":params.BRANCH,
                 "version": params.VERSION,
-                "build_type": params.BUILD_TYPE
+                "build_type": params.BUILD_TYPE,
+                "push_gcr":params.PUSH_GCR
         ]
 
         collect_pipeline_info(finally_result)
