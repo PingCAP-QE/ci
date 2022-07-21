51fe72bd0e862abad911416cfb16a702da9d1c20
purelind <purelind@gmail.com>
2022-07-21 16:36:29 +0800

    tidb(mysql-test): support release-6.2

--- jenkins/pipelines/ci/tidb/tidb_ghpr_mysql_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_mysql_test.groovy
@@ -219,7 +219,7 @@ try {
                 dir("go/src/github.com/pingcap/tidb-test/${test_dir}") {
                     try {
                         timeout(25) {
-                            if (ghprbTargetBranch in ["master"]) {
+                            if (ghprbTargetBranch in ["master", "release-6.2"]) {
                                 sh """
                                 set +e
                                 killall -9 -r tidb-server
@@ -229,24 +229,6 @@ try {
                                 set -e
                                 TIDB_SERVER_PATH=${ws}/go/src/github.com/pingcap/tidb/bin/tidb-server \
                                 ./test.sh -backlist=1
-
-                                set +e
-                                killall -9 -r tidb-server
-                                killall -9 -r tikv-server
-                                killall -9 -r pd-server
-                                rm -rf /tmp/tidb
-                                set -e
-                                """
-                            } else if (ghprbTargetBranch in ["release-6.1", "release-6.0", "release-5.4", "release-5.3"]) {
-                                sh """
-                                set +e
-                                killall -9 -r tidb-server
-                                killall -9 -r tikv-server
-                                killall -9 -r pd-server
-                                rm -rf /tmp/tidb
-                                set -e
-                                TIDB_SERVER_PATH=${ws}/go/src/github.com/pingcap/tidb/bin/tidb-server \
-                                ./test.sh
 
                                 set +e
                                 killall -9 -r tidb-server
