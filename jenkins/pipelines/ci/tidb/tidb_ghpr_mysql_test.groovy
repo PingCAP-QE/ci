c5a7c20a58e9e77b3d1bb95cb55a79ccba23915f
purelind <purelind@gmail.com>
2022-07-27 15:44:39 +0800

    tidb(mysql-test): uncomment upload data

--- jenkins/pipelines/ci/tidb/tidb_ghpr_mysql_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_mysql_test.groovy
@@ -270,28 +270,28 @@ catch (Exception e) {
     }
 } finally {
     taskFinishTime = System.currentTimeMillis()
-    // build job: 'upload-pipelinerun-data',
-    //     wait: false,
-    //     parameters: [
-    //             [$class: 'StringParameterValue', name: 'PIPELINE_NAME', value: "${JOB_NAME}"],
-    //             [$class: 'StringParameterValue', name: 'PIPELINE_RUN_URL', value: "${env.RUN_DISPLAY_URL}"],
-    //             [$class: 'StringParameterValue', name: 'REPO', value: "${ghprbGhRepository}"],
-    //             [$class: 'StringParameterValue', name: 'COMMIT_ID', value: ghprbActualCommit],
-    //             [$class: 'StringParameterValue', name: 'TARGET_BRANCH', value: ghprbTargetBranch],
-    //             [$class: 'StringParameterValue', name: 'JUNIT_REPORT_URL', value: resultDownloadPath],
-    //             [$class: 'StringParameterValue', name: 'PULL_REQUEST', value: ghprbPullId],
-    //             [$class: 'StringParameterValue', name: 'PULL_REQUEST_AUTHOR', value: ghprbPullAuthorLogin],
-    //             [$class: 'StringParameterValue', name: 'JOB_TRIGGER', value: ghprbPullAuthorLogin],
-    //             [$class: 'StringParameterValue', name: 'TRIGGER_COMMENT_BODY', value: ghprbPullAuthorLogin],
-    //             [$class: 'StringParameterValue', name: 'JOB_RESULT_SUMMARY', value: ""],
-    //             [$class: 'StringParameterValue', name: 'JOB_START_TIME', value: "${taskStartTimeInMillis}"],
-    //             [$class: 'StringParameterValue', name: 'JOB_END_TIME', value: "${taskFinishTime}"],
-    //             [$class: 'StringParameterValue', name: 'POD_READY_TIME', value: ""],
-    //             [$class: 'StringParameterValue', name: 'CPU_REQUEST', value: ""],
-    //             [$class: 'StringParameterValue', name: 'MEMORY_REQUEST', value: ""],
-    //             [$class: 'StringParameterValue', name: 'JOB_STATE', value: currentBuild.result],
-    //             [$class: 'StringParameterValue', name: 'JENKINS_BUILD_NUMBER', value: "${BUILD_NUMBER}"],
-    // ]
+    build job: 'upload-pipelinerun-data',
+        wait: false,
+        parameters: [
+                [$class: 'StringParameterValue', name: 'PIPELINE_NAME', value: "${JOB_NAME}"],
+                [$class: 'StringParameterValue', name: 'PIPELINE_RUN_URL', value: "${env.RUN_DISPLAY_URL}"],
+                [$class: 'StringParameterValue', name: 'REPO', value: "${ghprbGhRepository}"],
+                [$class: 'StringParameterValue', name: 'COMMIT_ID', value: ghprbActualCommit],
+                [$class: 'StringParameterValue', name: 'TARGET_BRANCH', value: ghprbTargetBranch],
+                [$class: 'StringParameterValue', name: 'JUNIT_REPORT_URL', value: resultDownloadPath],
+                [$class: 'StringParameterValue', name: 'PULL_REQUEST', value: ghprbPullId],
+                [$class: 'StringParameterValue', name: 'PULL_REQUEST_AUTHOR', value: ghprbPullAuthorLogin],
+                [$class: 'StringParameterValue', name: 'JOB_TRIGGER', value: ghprbPullAuthorLogin],
+                [$class: 'StringParameterValue', name: 'TRIGGER_COMMENT_BODY', value: ghprbPullAuthorLogin],
+                [$class: 'StringParameterValue', name: 'JOB_RESULT_SUMMARY', value: ""],
+                [$class: 'StringParameterValue', name: 'JOB_START_TIME', value: "${taskStartTimeInMillis}"],
+                [$class: 'StringParameterValue', name: 'JOB_END_TIME', value: "${taskFinishTime}"],
+                [$class: 'StringParameterValue', name: 'POD_READY_TIME', value: ""],
+                [$class: 'StringParameterValue', name: 'CPU_REQUEST', value: ""],
+                [$class: 'StringParameterValue', name: 'MEMORY_REQUEST', value: ""],
+                [$class: 'StringParameterValue', name: 'JOB_STATE', value: currentBuild.result],
+                [$class: 'StringParameterValue', name: 'JENKINS_BUILD_NUMBER', value: "${BUILD_NUMBER}"],
+    ]
 }
 
 
