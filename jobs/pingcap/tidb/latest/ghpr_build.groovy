f0573d71a0a83451ee8d5a52ddd93bb2513fc174
wuhuizuo <wuhuizuo@126.com>
2022-08-02 14:36:50 +0800

    test(jobs/pingcap/tidb/latest): limit trigger only by debug phrase (#1222)
    using `/gray-debug` to debug

--- jobs/pingcap/tidb/latest/ghpr_build.groovy
+++ jobs/pingcap/tidb/latest/ghpr_build.groovy
@@ -18,7 +18,13 @@ pipelineJob('pingcap/tidb/ghpr_build') {
                 ghprbTrigger {
                     cron('H/5 * * * *')
                     gitHubAuthId('8b25795b-a680-4dce-9904-89ef40d73159') // tidb-ci-bot.
-                    triggerPhrase('.*/(merge|run-(all-tests|build).*)')
+
+                    // ### prod
+                    // triggerPhrase('.*/(merge|run-(all-tests|build).*)')
+                    // ### debug
+                    triggerPhrase('^/gray-debug')
+                    onlyTriggerPhrase(true)
+
                     skipBuildPhrase(".*skip-ci.*")
                     buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
                     whitelist("ming-relax LiangShang hsqlu yangwenmai qxhy123 mccxj dreamquster MyonKeminta colinback spongedu lzmhhh123 bb7133 dbjoa")
