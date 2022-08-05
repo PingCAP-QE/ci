8cca943310729260141cba11298fe84bbd4b7e4a
Weizhen Wang <wangweizhen@pingcap.com>
2022-08-05 16:11:16 +0800

    upgrade 20220805
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- jenkins/pipelines/ci/tidb/tidb_ghpr_check.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_check.groovy
@@ -25,7 +25,7 @@ GO_IMAGE_MAP = [
     "go1.13": "hub.pingcap.net/jenkins/centos7_golang-1.13:latest",
     "go1.16": "hub.pingcap.net/jenkins/centos7_golang-1.16:latest",
     "go1.18": "hub.pingcap.net/jenkins/centos7_golang-1.18.5:latest",
-    "bazel_master": "hub.pingcap.net/wangweizhen/tidb_image:20220718",
+    "bazel_master": "hub.pingcap.net/wangweizhen/tidb_image:20220805",
 ]
 RESOURCE_REQUEST_CPU = '4000m'
 
