3d1e882905dcfba256fa9ab94fbddb9eecffbaf8
lijie <lijie0123@users.noreply.github.com>
2022-08-16 19:28:58 +0800

    run basic sql with test-infra (#1264)
    * run basic sql via test-infra
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * refract code use pod template
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * NewLine
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * remove package name
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * use embed yaml
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * default container
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * syntax
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * syntax
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * remove tcctl pod template
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * skip checkout
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * syntax
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    Signed-off-by: lijie <lijie@pingcap.com>

---
+++ jenkins/pipelines/cd/check-images-with-basic-sql.groovy
@@ -0,0 +1,49 @@
+components = ['tidb', 'tikv', 'pd', 'tiflash']
+
+def componentsVersions(String hotfixVersion, String component) {
+    String baseVersion = hotfixVersion.split("-")[0]
+    def componentsVersions = [:]
+    for (comp in components) {
+        componentsVersions[comp] = baseVersion
+    }
+    componentsVersions[component] = hotfixVersion
+    return componentsVersions
+}
+
+pipeline {
+    parameters {
+        string defaultValue: 'master', description: 'example v5.2.4-20220804', name: 'hotfixVersion', trim: true
+        choice choices: components, description: '', name: 'component'
+    }
+    agent {
+        kubernetes {
+            yaml '''
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+    - name: tcctl
+      image: hub.pingcap.net/qa/tcctl
+      command:
+        - sleep
+      args:
+        - infinity
+'''
+            defaultContainer 'tcctl'
+        }
+    }
+    options { skipDefaultCheckout() }
+    environment {
+        token = credentials('tcms-token')
+    }
+    stages {
+        stage('Main') {
+            steps {
+                script {
+                    def versions = componentsVersions(params.hotfixVersion, params.component)
+                    sh "tcctl svc run tidb-basic-sql-check -a tidbVersion=${versions.tidb},tikvVersion=${versions.tikv},pdVersion=${versions.pd},tiflashVersion=${versions.tiflash} -o -"
+                }
+            }
+        }
+    }
+}
