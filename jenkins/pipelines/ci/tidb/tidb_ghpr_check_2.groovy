79c2d9384ab9ac24e56951ae6ec8f82b6e60c45c
Hangjie Mo <mohangjie1995@gmail.com>
2022-08-18 18:19:44 +0800

    *: add feature branch CI for feature[/_].*

--- jenkins/pipelines/ci/tidb/tidb_ghpr_check_2.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_check_2.groovy
@@ -53,7 +53,7 @@ GO_IMAGE_MAP = [
 
 def user_bazel(branch) {
     // set the feature branch at here.
-    if (branch in ["master", "feature/distribute-reorg"]) {
+    if (branch in ["master"] || branch.matches("feature[/_].*")) {
         return GO_IMAGE_MAP["master"]
     }
     return ""
