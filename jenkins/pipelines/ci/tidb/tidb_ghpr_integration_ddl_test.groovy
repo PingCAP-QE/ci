a155c6cb15a962fac6c66f18ba4f38417d9c8f08
Lifu Wu <purelind@users.noreply.github.com>
2022-09-02 20:52:14 +0800

    feat: mergeci master use go1.19 (#1333)
    * feat: mergeci master use go1.19
    
    * chore fix

--- jenkins/pipelines/ci/tidb/tidb_ghpr_integration_ddl_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_integration_ddl_test.groovy
@@ -52,17 +52,18 @@ def tidb_done_url = "${FILE_SERVER_URL}/download/builds/pingcap/tidb/pr/${ghprbA
 all_task_result = []
 POD_NAMESPACE = "jenkins-tidb-mergeci"
 
-GO_VERSION = "go1.18"
+GO_VERSION = "go1.19"
 POD_GO_IMAGE = ""
 GO_IMAGE_MAP = [
     "go1.13": "hub.pingcap.net/jenkins/centos7_golang-1.13:latest",
     "go1.16": "hub.pingcap.net/jenkins/centos7_golang-1.16:latest",
     "go1.18": "hub.pingcap.net/jenkins/centos7_golang-1.18.5:latest",
+    "go1.19": "hub.pingcap.net/jenkins/centos7_golang-1.19:latest",
 ]
 
 node("master") {
     deleteDir()
-    def goversion_lib_url = 'https://raw.githubusercontent.com/PingCAP-QE/ci/main/jenkins/pipelines/goversion-select-lib.groovy'
+    def goversion_lib_url = 'https://raw.githubusercontent.com/purelind/ci-1/purelind/tidb-it-use-go1.19/jenkins/pipelines/ci/tidb/goversion-select-lib.groovy'
     sh "curl -O --retry 3 --retry-delay 5 --retry-connrefused --fail ${goversion_lib_url}"
     def goversion_lib = load('goversion-select-lib.groovy')
     GO_VERSION = goversion_lib.selectGoVersion(ghprbTargetBranch)
@@ -94,6 +95,9 @@ def run_with_pod(Closure body) {
     }
     if (GO_VERSION == "go1.18") {
         label = "tidb-ghpr-integration-ddl-test-go1180-${BUILD_NUMBER}"
+    }
+    if (GO_VERSION == "go1.19") {
+        label = "tidb-ghpr-integration-ddl-test-go1190-${BUILD_NUMBER}"
     }
 
     def cloud = "kubernetes-ksyun"
@@ -137,6 +141,9 @@ def run_with_memory_volume_pod(Closure body) {
     }
     if (GO_VERSION == "go1.18") {
         label = "tidb-ghpr-integration-ddl-test-memory-volume-go1180-${BUILD_NUMBER}"
+    }
+    if (GO_VERSION == "go1.19") {
+        label = "tidb-ghpr-integration-ddl-test-memory-volume-go1190-${BUILD_NUMBER}"
     }
     
     def cloud = "kubernetes-ksyun"
