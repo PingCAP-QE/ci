c31af80829495cbff8b45f86248822c59da38c8e
wuhuizuo <wuhuizuo@126.com>
2022-09-18 03:12:57 +0800

    fix(pipelines/pingcap-qe/ci): fix `self_test` (#1380)
    * refactor(jobs/pingcap-qe): move library to `pingcap-qe` folder
    
    * fix(pipelines/pingcap-qe/ci): fix `self_test`
    
    - add jenkins pipeline validation step

--- pipelines/pingcap-qe/ci/self_test.groovy
+++ pipelines/pingcap-qe/ci/self_test.groovy
@@ -1,20 +1,31 @@
 @Library('tipipeline') _
 
+final POD_TEMPLATE_FILE = 'pipelines/pingcap-qe/ci/pod-self_test.yaml'
+
 pipeline {
     agent {
-        kubernetes { 
-            label ""
+        kubernetes {
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer('main')
         }
     }
     stages{
-        stage('run example') {
+        stage('Validate pipelines') {
             steps{
-                echo(message: env.BUILD_NUMBER)
-                script {
-                    log.info("info from share library's globa var function")
-                    hello
-                }
-                sh '[ $(($(date +%s) % 2)) -eq 0 ]' // random failed with 50% percent.
+                sh '''#!/bin/bash +x
+                
+                # Linting via HTTP POST using curl
+                # curl (REST API)
+                # Assuming "anonymous read access" has been enabled on your Jenkins instance.
+                # JENKINS_URL=[root URL of Jenkins controller]
+                # JENKINS_CRUMB is needed if your Jenkins controller has CRSF protection enabled as it should
+                
+                JENKINS_CRUMB=`curl -fsS "$JENKINS_URL/crumbIssuer/api/json" | jq .crumb`
+                for f in `find pipelines -name "*.groovy"`; do
+                    echo -ne "validating $f:\\t"
+                    curl -fsS -X POST -H $JENKINS_CRUMB -F "jenkinsfile=<${f}" $JENKINS_URL/pipeline-model-converter/validate
+                done
+                '''
             }
         }
     }
