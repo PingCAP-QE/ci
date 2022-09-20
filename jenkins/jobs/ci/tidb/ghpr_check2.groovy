8bfb0965b036ec2063fa580f490e1d74f33e6d20
wuhuizuo <wuhuizuo@126.com>
2022-09-20 20:43:54 +0800

    fix(tidb): fix job dsl trigger phrases (#1388)

--- jenkins/jobs/ci/tidb/ghpr_check2.groovy
+++ jenkins/jobs/ci/tidb/ghpr_check2.groovy
@@ -17,7 +17,7 @@ pipelineJob('tidb_ghpr_check_2') {
                     cron('H/5 * * * *')
                     gitHubAuthId('8b25795b-a680-4dce-9904-89ef40d73159')
 
-                    triggerPhrase('.*/(merge|run-(all-tests|check[-_]dev[-_]?2))')
+                    triggerPhrase('.*/(merge|run-(all-tests|check[-_]dev[-_]?2))\\b')
                     onlyTriggerPhrase(false)
                     skipBuildPhrase(".*skip-ci.*")
                     buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
--- jobs/pingcap/tidb/latest/ghpr_check2.groovy
+++ jobs/pingcap/tidb/latest/ghpr_check2.groovy
@@ -19,7 +19,7 @@ pipelineJob('pingcap/tidb/ghpr_check2') {
                     cron('H/5 * * * *')
                     gitHubAuthId('') // using the default only one.
 
-                    triggerPhrase('.*/(merge|run-(all-tests|check[-_]dev[-_]?2))')
+                    triggerPhrase('.*/(merge|run-(all-tests|check[-_]dev[-_]?2))\\b')
                     onlyTriggerPhrase(false)
                     skipBuildPhrase(".*skip-ci.*")
                     buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
