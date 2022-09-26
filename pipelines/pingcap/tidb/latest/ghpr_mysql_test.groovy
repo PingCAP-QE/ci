7848f64ea1106733b26f7097137b185f2416e142
wuhuizuo <wuhuizuo@126.com>
2022-09-26 20:22:08 +0800

    refactor(pipelines/pingcap/tidb): simplify `ghpr_mysql_test`
    - no need to kill process after tests finished, pods will be terminated.
    - using extracted share lib to checkout `tidb-test` repo.

--- pipelines/pingcap/tidb/latest/ghpr_mysql_test.groovy
+++ pipelines/pingcap/tidb/latest/ghpr_mysql_test.groovy
@@ -1,8 +1,11 @@
 // REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
 // Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
-// should triggerd for master and release-6.2.x branches
+// should triggerd for master and latest release branches
+@Library('tipipeline') _
+
 final K8S_NAMESPACE = "jenkins-tidb"
 final GIT_FULL_REPO_NAME = 'pingcap/tidb'
+final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
 final POD_TEMPLATE_FILE = 'pipelines/pingcap/tidb/latest/pod-ghpr_mysql_test.yaml'
 
 // TODO(wuhuizuo): tidb-test should delivered by docker image.
@@ -35,7 +38,54 @@ pipeline {
                     sh 'dig github.com'
                 }
             }
-        }        
+        }
+        stage('Checkout') {
+            options { timeout(time: 5, unit: 'MINUTES') }
+            steps {
+                dir("tidb") {
+                    cache(path: "./", filter: '**/*', key: "git/pingcap/tidb/rev-${ghprbActualCommit}", restoreKeys: ['git/pingcap/tidb/rev-']) {
+                        retry(2) {
+                            checkout(
+                                changelog: false,
+                                poll: false,
+                                scm: [
+                                    $class: 'GitSCM', branches: [[name: ghprbActualCommit]],
+                                    doGenerateSubmoduleConfigurations: false,
+                                    extensions: [
+                                        [$class: 'PruneStaleBranch'],
+                                        [$class: 'CleanBeforeCheckout'],
+                                        [$class: 'CloneOption', timeout: 15],
+                                    ],
+                                    submoduleCfg: [],
+                                    userRemoteConfigs: [[
+                                        refspec: "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*",
+                                        url: "https://github.com/${GIT_FULL_REPO_NAME}.git",
+                                    ]],
+                                ]
+                            )
+                        }
+                    }
+                }
+                dir("tidb-test") {
+                    cache(path: "./", filter: '**/*', key: "git/pingcap/tidb-test/rev-${ghprbActualCommit}", restoreKeys: ['git/pingcap/tidb-test/rev-']) {
+                        retry(2) {
+                            script {
+                                component.checkout('git@github.com:pingcap/tidb-test.git', 'tidb-test', ghprbTargetBranch, ghprbCommentBody, GIT_CREDENTIALS_ID)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage('Prepare binaries') {
+            steps {
+                dir('tidb') {
+                    cache(path: "./bin", filter: '**/*', key: "binary/pingcap/tidb/tidb-server/rev-${ghprbActualCommit}") {
+                        sh label: 'tidb-server', script: 'ls bin/tidb-server || go build -race -o bin/tidb-server ./tidb-server'
+                    }
+                }
+            }
+        }
         stage('MySQL Tests') {
             matrix {
                 axes {
@@ -52,79 +102,21 @@ pipeline {
                     }
                 }
                 stages {
-                    stage('Prepare') {
-                        options { timeout(time: 10, unit: 'MINUTES') }
-                        steps {
-                            dir("tidb") {
-                                retry(3){
-                                    sh label: 'get tidb-server binnary', script: '''#! /usr/bin/env bash
-                                        tidb_done_url="${FILE_SERVER_URL}/download/builds/pingcap/tidb/pr/${ghprbActualCommit}/centos7/done"
-                                        tidb_url="${FILE_SERVER_URL}/download/builds/pingcap/tidb/pr/${ghprbActualCommit}/centos7/tidb-server.tar.gz"
-                                        while ! curl --output /dev/null --silent --head --fail ${tidb_done_url}; do sleep 1; done
-                                        curl --fail ${tidb_url} | tar xz
-                                        '''
-                                }
-                            }
-                            dir("tidb-test") {
-                                sh label: 'download tidb-test and build mysql_test', script: '''#! /usr/bin/env bash
-                                    trunkBranch="master"
-                                    releaseOrHotfixBranchReg="^(release-)?([0-9]+\\.[0-9]+)(\\.[0-9]+\\-.+)?"
-                                    featureBranchReg="^feature[/_].+"
-                                    commentBodyBranchReg="\\btidb[-_]test\\s*=\\s*(\\S+)\\b"
-
-                                    if [[ "${ghprbCommentBody}" =~ $commentBodyBranchReg ]]; then
-                                        TIDB_TEST_BRANCH=${BASH_REMATCH[1]}
-                                    elif [[ "${ghprbTargetBranch}" =~ $releaseOrHotfixBranchReg ]]; then
-                                        TIDB_TEST_BRANCH="release-${BASH_REMATCH[2]}"
-                                    elif [[ "${ghprbTargetBranch}" =~ $featureBranchReg ]]; then
-                                        TIDB_TEST_BRANCH="${trunkBranch}"
-                                    else
-                                        TIDB_TEST_BRANCH="${ghprbTargetBranch}"
-                                    fi
-
-                                    echo "TIDB_TEST_BRANCH or PR: ${TIDB_TEST_BRANCH}"
-
-                                    tidb_test_refs="${FILE_SERVER_URL}/download/refs/pingcap/tidb-test/${TIDB_TEST_BRANCH}/sha1"
-                                    echo "${tidb_test_refs}"
-                                    while ! curl --output /dev/null --silent --head --fail ${tidb_test_refs}; do sleep 5; done
-                                    tidb_test_sha1="$(curl --fail ${tidb_test_refs})"
-
-                                    tidb_test_url="${FILE_SERVER_URL}/download/builds/pingcap/tidb-test/${tidb_test_sha1}/centos7/tidb-test.tar.gz"
-                                    while ! curl --output /dev/null --silent --head --fail ${tidb_test_url}; do sleep 5; done
-                                    curl --fail ${tidb_test_url} | tar xz
-
-                                    cd mysql_test
-                                    TIDB_SRC_PATH=$(realpath ../../tidb) ./build.sh
-                                    '''
-                            }
-                            // TODO(wuhuizuo): store files:
-                            // - tidb/bin/tidb-server
-                            // - tidb-test/mysql-test
-                        }
-                    }
                     stage("Test") {
                         options { timeout(time: 25, unit: 'MINUTES') }
                         steps {
-                            dir("tidb-test/mysql_test") {
-                                sh label: "part ${PART}", script: '''#! /usr/bin/env bash
-
-                                pwd && ls -alh
-                                exit_code=0
-                                { # try block
-                                    TIDB_SERVER_PATH=../../tidb/bin/tidb-server ./test.sh -backlist=1 -part=${PART}
-                                } || { # catch block
-                                    exit_code="$?"  # exit code of last command which is 44
+                            dir('tidb') {
+                                cache(path: "./bin", filter: '**/*', key: "binary/pingcap/tidb/tidb-server/rev-${ghprbActualCommit}") {
+                                    sh label: 'tidb-server', script: 'ls bin/tidb-server && chmod +x bin/tidb-server'
                                 }
-                                # finally block:
-                                killall -9 -r tidb-server
-                                killall -9 -r tikv-server
-                                killall -9 -r pd-server
-                                rm -rf /tmp/tidb
-
-                                if [[ "exit_code" != '0' ]]; then
-                                    exit \${exit_code}
-                                fi
-                                '''
+                            }
+                            dir('tidb-test') {
+                                cache(path: "./", filter: '**/*', key: "git/pingcap/tidb-test/rev-${ghprbActualCommit}") {                                    
+                                    sh 'ls mysql_test' // if cache missed, fail it(should not miss).
+                                    dir('mysql_test') {
+                                        sh label: "part ${PART}", script: 'TIDB_SERVER_PATH=${WORKSPACE}/tidb/bin/tidb-server ./test.sh -backlist=1 -part=${PART}'
+                                    }
+                                }
                             }
                         }
                         post{
