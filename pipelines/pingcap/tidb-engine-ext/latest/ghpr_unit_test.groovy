22ae7649872186b83463fc1269ae01e0a00595cf
CalvinNeo <calvinneo1995@gmail.com>
2022-09-30 11:33:44 +0800

    f
    Signed-off-by: CalvinNeo <calvinneo1995@gmail.com>

--- pipelines/pingcap/tidb-engine-ext/latest/ghpr_unit_test.groovy
+++ pipelines/pingcap/tidb-engine-ext/latest/ghpr_unit_test.groovy
@@ -68,7 +68,6 @@ pipeline {
             steps {
                 dir('tidb-engine-ext') {
                     sh """
-                    source /opt/rh/devtoolset-8/enable
                     make ci_fmt_check
                     make ci_test
                     """ 
