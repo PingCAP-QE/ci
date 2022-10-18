457e41a1822c38ae831688f2900c744f63aa7ad8
wuhuizuo <wuhuizuo@126.com>
2022-10-18 18:39:27 +0800

    chore(staging): add debug job for jenkins-operator (#1482)

---
+++ staging/jobs/pingcap-qe/ee-ops/prow_debug.groovy
@@ -0,0 +1,30 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For trunk and latest release branches.
+pipelineJob('pingcap-qe/ee-ops/prow_debug') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap-qe/ee-ops")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("staging/pipelines/pingcap-qe/ee-ops/prow_debug.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                }
+            }
+        }
+    }
+}
