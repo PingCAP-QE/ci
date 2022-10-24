48fec6438fa9dfb4112205130b9fd96b6b0b077b
wuhuizuo <wuhuizuo@126.com>
2022-10-24 19:42:12 +0800

    fix(libraries/tipipeline/vars): fix checkout for prow contexts (#1494)

--- libraries/tipipeline/vars/prow.groovy
+++ libraries/tipipeline/vars/prow.groovy
@@ -4,36 +4,26 @@ def checkoutPr(prowDeckUrl, prowJobId, timeout=5, credentialsId='') {
     // get yaml from <prowDeckUrl>/prowjob?prowjob=<prow_job_id>
     def response = httpRequest "${prowDeckUrl}/prowjob?prowjob=${prowJobId}"
 
-    // parse git refs from prowjob's yaml
-    /*
-       spec:
-         agent: jenkins
-         cluster: default
-         context: debug/prow-call-jenkins
-         job: prow_debug
-         namespace: prow-test-pods
-         prowjob_defaults:
-           tenant_id: GlobalDefaultID
-         refs:
-           base_link: https://github.com/PingCAP-QE/ee-ops/commit/258a3136f240fa60be9ca3aa156fc1e86075cc51
-           base_ref: main
-           base_sha: 258a3136f240fa60be9ca3aa156fc1e86075cc51
-           org: PingCAP-QE
-           pulls:
-             - author: wuhuizuo
-               author_link: https://github.com/wuhuizuo
-               commit_link: https://github.com/PingCAP-QE/ee-ops/pull/157/commits/ed162dfefe1041dfae2128e38e458cb96e8bc85c
-               link: https://github.com/PingCAP-QE/ee-ops/pull/157
-               number: 157
-               sha: ed162dfefe1041dfae2128e38e458cb96e8bc85c
-               title: "chore: test jenkins-operator job"
-           repo: ee-ops
-           repo_link: https://github.com/PingCAP-QE/ee-ops
-    */
-    final prowJob = readYaml(text: response.context)
+    // parse git refs from prowjob's yaml, format:    
+    // spec:
+    //   refs:
+    //     base_link: <string>
+    //     base_ref: <base_branch_name>
+    //     base_sha: <base_branch_sha>
+    //     org: <string>
+    //     repo: <string>
+    //     repo_link: <string>
+    //     pulls:
+    //       - author: <string>
+    //         author_link: <string>
+    //         commit_link: <string>
+    //         link: <string>
+    //         number: <pr number>
+    //         sha: <head merge commit sha>
+    //         title: <pr title>    
+    final prowJob = readYaml(text: response.content)
     final refs = prowJob.spec.refs
     assert refs.pulls.size() == 1
-    
     
     // parse values for git checkout.
     final org = refs.org
@@ -56,7 +46,7 @@ def checkoutPr(prowDeckUrl, prowJobId, timeout=5, credentialsId='') {
             submoduleCfg: [],
             userRemoteConfigs: [[
                 refspec: "+refs/pull/${pullId}/*:refs/remotes/origin/pr/${pullId}/*",
-                url: "https://github.com/${org}/${repo}/.git",
+                url: "https://github.com/${org}/${repo}.git",
                 credentialsId: credentialsId
             ]],
         ]
