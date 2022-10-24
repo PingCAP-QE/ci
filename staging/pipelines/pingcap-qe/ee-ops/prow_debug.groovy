15b8732f50cc0c6a791b0a32320c3494883c105f
wuhuizuo <wuhuizuo@126.com>
2022-10-24 16:33:29 +0800

    chore(staging/pipelines): update pipeline to debug

--- staging/pipelines/pingcap-qe/ee-ops/prow_debug.groovy
+++ staging/pipelines/pingcap-qe/ee-ops/prow_debug.groovy
@@ -2,23 +2,28 @@
 // Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
 // should triggerd for master and latest release branches
 
-// TODO(wuhuizuo): tidb-test should delivered by docker image.
+final POD_TEMPLATE_FILE = 'staging/pipelines/pingcap-qe/ee-ops/pod-prow_debug.yaml'
+
 pipeline {
     agent {
         kubernetes {
+            yamlFile POD_TEMPLATE_FILE
         }
     }
     
     stages {
         stage('Debug info') {
             steps {
-                sh label: 'Debug info', script: """
+                sh label: 'Debug info', script: '''
                     printenv                   
                     echo "-------------------------"
-                    pwd
+                    echo "debug, you can rm the file debug.txt to continue"
+                    touch debug.txt
+                    while [ -f debug.txt ]; do sleep 1; done'
+                    echo "continue"
                     echo "-------------------------"
                     ls -la
-                """               
+                '''
             }
         }
     }
