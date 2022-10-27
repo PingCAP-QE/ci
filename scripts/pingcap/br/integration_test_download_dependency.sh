0fbd69249ad00f0b02174781bfc6b5a840b560ef
purelind <purelind@gmail.com>
2022-10-27 16:00:35 +0800

    init prepare tidb br merged integration test

---
+++ scripts/pingcap/br/integration_test_download_dependency.sh
@@ -0,0 +1,11 @@
+
+# br it binary
+# ticdc binary
+# tikv / pd / importer / tiflash 
+# goycsb
+# minio
+# mc
+# kes
+# fake-gcs-server
+# brv4.0.8
+
---
+++ staging/pipelines/pingcap/tidb/latest/merged_integration_br_test.groovy
@@ -0,0 +1,136 @@
+// REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
+// Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
+// should triggerd for master and latest release branches
+@Library('tipipeline') _
+
+final K8S_NAMESPACE = "jenkins-tidb"
+final GIT_FULL_REPO_NAME = 'pingcap/tidb'
+// TODO: remove env GIT_BRANCH and GIT_COMMIT
+final GIT_BRANCH = 'master'
+final GIT_COMMIT = '9743a9a2d2c626acbd7e13d4693cca9c58f329b7'
+final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
+final POD_TEMPLATE_FILE = 'staging/pipelines/pingcap/tidb/latest/pod-merged_integration_br_test.yaml'
+
+pipeline {
+    agent {
+        kubernetes {
+            namespace K8S_NAMESPACE
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer 'golang'
+        }
+    }
+    environment {
+        FILE_SERVER_URL = 'http://fileserver.pingcap.net'
+    }
+    options {
+        timeout(time: 40, unit: 'MINUTES')
+        // parallelsAlwaysFailFast()
+    }
+    stages {
+        stage('Debug info') {
+            steps {
+                sh label: 'Debug info', script: """
+                    printenv
+                    echo "-------------------------"
+                    go env
+                    echo "-------------------------"
+                    echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
+                """
+                container(name: 'net-tool') {
+                    sh 'dig github.com'
+                }
+            }
+        }
+        stage('Checkout') {
+            options { timeout(time: 5, unit: 'MINUTES') }
+            steps {
+                dir("tidb") {
+                    cache(path: "./", filter: '**/*', key: "git/pingcap/tidb/rev-${GIT_COMMIT}", restoreKeys: ['git/pingcap/tidb/rev-']) {
+                        retry(2) {
+                            checkout(
+                                changelog: false,
+                                poll: false,
+                                scm: [
+                                    $class: 'GitSCM', branches: [[name: GIT_COMMIT ]],
+                                    doGenerateSubmoduleConfigurations: false,
+                                    extensions: [
+                                        [$class: 'PruneStaleBranch'],
+                                        [$class: 'CleanBeforeCheckout'],
+                                        [$class: 'CloneOption', timeout: 15],
+                                    ],
+                                    submoduleCfg: [],
+                                    userRemoteConfigs: [[
+                                        refspec: "+refs/heads/*:refs/remotes/origin/*",
+                                        url: "https://github.com/${GIT_FULL_REPO_NAME}.git",
+                                    ]],
+                                ]
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        stage('Prepare') {
+            steps {
+                dir('tidb') {
+                    sh "git branch && git status"
+                    cache(path: "./bin", filter: '**/*', key: "binary/pingcap/tidb/tidb-server/rev-${BUILD_TAG}") {
+                        // FIXME: https://github.com/pingcap/tidb-test/issues/1987
+                        sh label: 'tidb-server', script: 'ls bin/tidb-server || make'
+                    }
+                }
+            }
+        }
+        stage('Tests') {
+            matrix {
+                axes {
+                    axis {
+                        name 'CASES'
+                        values 'region_merge', 'ddl_reentrant', 'http_api_tls', 'generate_column'
+                    }
+                }
+                agent{
+                    kubernetes {
+                        namespace K8S_NAMESPACE
+                        defaultContainer 'golang'
+                        yamlFile POD_TEMPLATE_FILE
+                    }
+                }
+                stages {
+                    
+                    stage("Test") {
+                        options { timeout(time: 25, unit: 'MINUTES') }
+                        steps {
+                            dir('tidb') {
+                                cache(path: "./bin", filter: '**/*', key: "binary/pingcap/tidb/tidb-server/rev-${BUILD_TAG}") {
+                                    sh label: 'tidb-server', script: 'ls bin/tidb-server && chmod +x bin/tidb-server'
+                                }
+                            }
+                            dir('tiflow') {
+                                cache(path: "./", filter: '**/*', key: "ws/${BUILD_TAG}/tiflow") {
+                                    sh 'chmod +x ../scripts/pingcap/tiflow/*.sh'
+                                    sh "${WORKSPACE}/scripts/pingcap/tiflow/ticdc_integration_test_download_dependency.sh master master master master http://fileserver.pingcap.net"
+                                    sh label: "Case ${CASES}", script: """
+                                    mv third_bin/* bin/ && ls -alh bin/
+                                    rm -rf /tmp/tidb_cdc_test
+                                    mkdir -p /tmp/tidb_cdc_test
+                                    cp ../tidb/bin/tidb-server ./bin/
+                                    ./bin/tidb-server -V
+                                    ls -alh ./bin/
+                                    make integration_test_mysql CASE="${CASES}"
+                                    """             
+                                }
+                            }
+                        }
+                        post{
+                            failure {
+                                println "Test failed, archive the log"
+                            }
+                        }
+                    }
+                }
+            }        
+        }
+    }
+}
+
