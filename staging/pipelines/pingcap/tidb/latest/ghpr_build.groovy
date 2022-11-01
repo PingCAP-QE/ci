c19bd2e7c59e498d042680f9daf220b793609efb
Weizhen Wang <wangweizhen@pingcap.com>
2022-11-01 13:20:09 +0800

    update image 20221101
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- staging/pipelines/pingcap/tidb/latest/ghpr_build.groovy
+++ staging/pipelines/pingcap/tidb/latest/ghpr_build.groovy
@@ -14,7 +14,7 @@ spec:
     fsGroup: 1000
   containers:
     - name: golang
-      image: "hub.pingcap.net/wangweizhen/tidb_image:go11920221025"
+      image: "hub.pingcap.net/wangweizhen/tidb_image:go11920221101"
       securityContext:
         privileged: true
       tty: true
