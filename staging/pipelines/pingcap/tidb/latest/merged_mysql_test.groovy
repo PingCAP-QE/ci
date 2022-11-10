27967f0d2a78ba4aeafc0f7f5dd06e6d086fdfac
purelind <purelind@gmail.com>
2022-11-10 16:35:25 +0800

    add podtemplate

--- staging/pipelines/pingcap/tidb/latest/merged_mysql_test.groovy
+++ staging/pipelines/pingcap/tidb/latest/merged_mysql_test.groovy
@@ -9,51 +9,14 @@ final GIT_FULL_REPO_NAME = 'pingcap/tidb'
 final GIT_BRANCH = 'master'
 final GIT_COMMIT = '4aa89a6274f0195195f1d70281aa545007413aa1'
 final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
-// final POD_TEMPLATE_FILE = 'staging/pipelines/pingcap/tidb/latest/pod-merged_mysql_test.yaml'
-
-
-def podYaml = '''
-apiVersion: v1
-kind: Pod
-spec:
-  containers:
-  - name: net-tool
-    image: wbitt/network-multitool
-    tty: true
-    resources:
-    limits:
-        memory: 128Mi
-        cpu: 100m
-  - name: docker
-    image: docker:dind
-    args: ["--registry-mirror=https://registry-mirror.pingcap.net"]
-    env:
-    - name: REGISTRY
-      value: hub.pingcap.net
-    - name: DOCKER_TLS_CERTDIR
-      value: ""
-    - name: DOCKER_HOST
-      value: tcp://localhost:2375
-    securityContext:
-      privileged: true
-    tty: true
-    readinessProbe:
-      exec:
-        command: ["docker", "info"]
-      initialDelaySeconds: 10
-      failureThreshold: 6
-  - name: jnlp
-    image: jenkins/inbound-agent:4.10-3
-'''
-
+final POD_TEMPLATE_FILE = 'staging/pipelines/pingcap/tidb/latest/pod-merged_mysql_test.yaml'
 
 pipeline {
     agent {
         kubernetes {
             namespace K8S_NAMESPACE
-            // yamlFile POD_TEMPLATE_FILE
-            yaml podYaml
-            defaultContainer 'docker'
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer 'golang'
         }
     }
     environment {
@@ -64,20 +27,20 @@ pipeline {
         // parallelsAlwaysFailFast()
     }
     stages {
-        // stage('Debug info') {
-        //     steps {
-        //         sh label: 'Debug info', script: """
-        //             printenv
-        //             echo "-------------------------"
-        //             go env
-        //             echo "-------------------------"
-        //             echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
-        //         """
-        //         container(name: 'net-tool') {
-        //             sh 'dig github.com'
-        //         }
-        //     }
-        // }
+        stage('Debug info') {
+            steps {
+                sh label: 'Debug info', script: """
+                    printenv
+                    echo "-------------------------"
+                    go env
+                    echo "-------------------------"
+                    echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
+                """
+                container(name: 'net-tool') {
+                    sh 'dig github.com'
+                }
+            }
+        }
         stage('Checkout') {
             options { timeout(time: 5, unit: 'MINUTES') }
             steps {
@@ -120,14 +83,15 @@ pipeline {
             steps {
                 dir('tidb') {
                     cache(path: "./bin", filter: '**/*', key: "binary/pingcap/tidb/merged_mysql_test/rev-${BUILD_TAG}") {
-                        // FIXME: https://github.com/pingcap/tidb-test/issues/1987
-                        sh label: 'tidb-server', script: 'ls bin/tidb-server || make'
-                        sh label: 'download binary', script: """
-                        chmod +x ${WORKSPACE}/scripts/pingcap/tidb-test/*.sh
-                        ${WORKSPACE}/scripts/pingcap/tidb-test/download_pingcap_artifact.sh --pd=${GIT_BRANCH} --tikv=${GIT_BRANCH}
-                        mv third_bin/* bin/
-                        ls -alh bin/
-                        """
+                        container("golang") {
+                            sh label: 'tidb-server', script: 'ls bin/tidb-server || make'
+                            sh label: 'download binary', script: """
+                            chmod +x ${WORKSPACE}/scripts/pingcap/tidb-test/*.sh
+                            ${WORKSPACE}/scripts/pingcap/tidb-test/download_pingcap_artifact.sh --pd=${GIT_BRANCH} --tikv=${GIT_BRANCH}
+                            mv third_bin/* bin/
+                            ls -alh bin/
+                            """
+                        }
                     }
                 }
                 dir('tidb-test') {
@@ -144,16 +108,24 @@ pipeline {
                         name 'CACHE_ENABLED'
                         values '0', "1"
                     }
+                    axis {
+                        name 'TEST_PART'
+                        values '1', "2", "3", "4"
+                    }
+                    axis {
+                        name 'TEST_STORE'
+                        values 'unistore', "tikv"
+                    }
                 }
                 agent{
                     kubernetes {
                         namespace K8S_NAMESPACE
-                        defaultContainer 'golang'
                         yamlFile POD_TEMPLATE_FILE
+                        defaultContainer 'golang'
                     }
                 } 
                 stages {
-                    stage("tikv") {
+                    stage("Test") {
                         options { timeout(time: 40, unit: 'MINUTES') }
                         steps {
                             dir('tidb') {
@@ -170,20 +142,25 @@ pipeline {
                                         cp ${WORKSPACE}/tidb/bin/* bin/ && chmod +x bin/*
                                         ls -alh bin/
                                     """
-                                    sh label: "unistore cache_enabled=${CACHE_ENABLED}", script: """
-                                        docker run --rm --tty --net=host \
-                                            --user \$(id -u):\$(id -g) \
-                                            --env TIDB_SERVER_PATH="/workspace/bin/tidb-server" \
-                                            --env CACHE_ENABLED=${CACHE_ENABLED} \
-                                            --env TIKV_PATH='127.0.0.1:2379' \
-                                            --env TIDB_TEST_STORE_NAME="tikv"\
-                                            --ulimit nofile=82920:82920 \
-                                            --ulimit stack=16777216:16777216 \
-                                            --volume "\${PWD}:/workspace" \
-                                            --workdir "/workspace" \
-                                            golang:1.19-bullseye \
-                                            bash ${WORKSPACE}/scripts/pingcap/tidb-test/run-integration-mysql-tests.sh  mysql_test/ 
-                                    """
+                                    container("golang") {
+                                        sh label: "test_store=${TEST_STORE} cache_enabled=${CACHE_ENABLED} test_part=${TEST_PART}", script: """
+                                            #!/usr/bin/env bash
+                                            if [[ "${TEST_STORE}" == "tikv" ]]; then
+                                                echo '[storage]\nreserve-space = "0MB"'> tikv_config.toml
+                                                bash ${WORKSPACE}/scripts/pingcap/tidb-test/start_tikv.sh
+                                                export TIDB_SERVER_PATH="${WORKSPACE}/tidb-test/bin/tidb-server"
+                                                export CACHE_ENABLED=${CACHE_ENABLED}
+                                                export TIKV_PATH="127.0.0.1:2379"
+                                                export TIDB_TEST_STORE_NAME="tikv"
+                                                cd mysql_test/ && ./test.sh -blacklist=1 -part=1
+                                            else
+                                                export TIDB_SERVER_PATH="${WORKSPACE}/tidb-test/bin/tidb-server"
+                                                export CACHE_ENABLED=${CACHE_ENABLED}
+                                                export TIDB_TEST_STORE_NAME="unistore"
+                                                cd mysql_test/ && ./test.sh -blacklist=1 -part=1
+                                            fi
+                                        """
+                                    }
                                 }
                             }
                         }
