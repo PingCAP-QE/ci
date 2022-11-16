d3f7fa66eabb9d5ee91e6215c2eda92e0ac8badb
purelind <purelind@gmail.com>
2022-11-16 11:30:59 +0800

    feat(tidb/ut): upload coverage to codecov

--- pipelines/pingcap/tidb/latest/ghpr_unit_test.groovy
+++ pipelines/pingcap/tidb/latest/ghpr_unit_test.groovy
@@ -66,6 +66,7 @@ pipeline {
             }
         }
         stage('Test') {
+            environment { TIDB_CODECOV_TOKEN = credentials('codecov-token-tidb') }
             steps {
                 dir('tidb') {
                     sh './build/jenkins_unit_test.sh' 
@@ -76,6 +77,15 @@ pipeline {
                     dir('tidb') {
                         archiveArtifacts(artifacts: '**/core.*', allowEmptyArchive: true)
                         archiveArtifacts(artifacts: '**/*.test.bin', allowEmptyArchive: true)
+                    }
+                }
+                success {
+                    dir("tidb") {
+                        sh label: "upload coverage to codecov", script: """
+                        wget -q -O codecov ${FILE_SERVER_URL}/download/cicd/tools/codecov-v0.3.2
+                        chmod +x codecov
+                        ./codecov --dir test_coverage/ --token ${TIDB_CODECOV_TOKEN}
+                        """
                     }
                 }
                 always {
