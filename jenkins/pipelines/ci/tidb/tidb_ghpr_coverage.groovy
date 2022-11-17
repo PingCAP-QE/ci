40d973b2dbdbb4b7aa2b1b460ec037ec27081d26
purelind <purelind@gmail.com>
2022-11-17 18:21:41 +0800

    feat: cron to check coverage of branch

--- jenkins/pipelines/ci/tidb/tidb_ghpr_coverage.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_coverage.groovy
@@ -1,190 +1,110 @@
-echo "release test: ${params.containsKey("release_test")}"
-if (params.containsKey("release_test")) {
-    ghprbTargetBranch = params.getOrDefault("release_test__ghpr_target_branch", params.release_test__release_branch)
-    ghprbCommentBody = params.getOrDefault("release_test__ghpr_comment_body", "")
-    ghprbActualCommit = params.getOrDefault("release_test__ghpr_actual_commit", params.release_test__tidb_commit)
-    ghprbPullId = params.getOrDefault("release_test__ghpr_pull_id", "")
-    ghprbPullTitle = params.getOrDefault("release_test__ghpr_pull_title", "")
-    ghprbPullLink = params.getOrDefault("release_test__ghpr_pull_link", "")
-    ghprbPullDescription = params.getOrDefault("release_test__ghpr_pull_description", "")
-}
-
-def specStr = "+refs/heads/*:refs/remotes/origin/*"
-if (ghprbPullId != null && ghprbPullId != "") {
-    specStr = "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*"
-}
-
-def slackcolor = 'good'
-def githash
-
-GO_VERSION = "go1.18"
-POD_GO_IMAGE = ""
-GO_IMAGE_MAP = [
-    "go1.13": "hub.pingcap.net/jenkins/centos7_golang-1.13:latest",
-    "go1.16": "hub.pingcap.net/jenkins/centos7_golang-1.16:latest",
-    "go1.18": "hub.pingcap.net/jenkins/centos7_golang-1.18.5:latest",
-    "go1.19": "hub.pingcap.net/jenkins/centos7_golang-1.19:latest",
-]
-POD_LABEL_MAP = [
-    "go1.13": "${JOB_NAME}-go1130-${BUILD_NUMBER}",
-    "go1.16": "${JOB_NAME}-go1160-${BUILD_NUMBER}",
-    "go1.18": "${JOB_NAME}-go1180-${BUILD_NUMBER}",
-    "go1.19": "${JOB_NAME}-go1190-${BUILD_NUMBER}",
-]
-
-node("master") {
-    deleteDir()
-    def goversion_lib_url = 'https://raw.githubusercontent.com/PingCAP-QE/ci/main/jenkins/pipelines/goversion-select-lib.groovy'
-    sh "curl -O --retry 3 --retry-delay 5 --retry-connrefused --fail ${goversion_lib_url}"
-    def goversion_lib = load('goversion-select-lib.groovy')
-    GO_VERSION = goversion_lib.selectGoVersion(ghprbTargetBranch)
-    POD_GO_IMAGE = GO_IMAGE_MAP[GO_VERSION]
-    println "go version: ${GO_VERSION}"
-    println "go image: ${POD_GO_IMAGE}"
-}
 
 
-def run_with_pod(Closure body) {
-    def label = POD_LABEL_MAP[GO_VERSION]
-    def cloud = "kubernetes-ng"
-    def namespace = "jenkins-tidb-mergeci"
-    def jnlp_docker_image = "jenkins/inbound-agent:4.3-4"
-    podTemplate(label: label,
-            cloud: cloud,
-            namespace: namespace,
-            idleMinutes: 0,
-            containers: [
-                    containerTemplate(
-                        name: 'golang', alwaysPullImage: true,
-                        image: "${POD_GO_IMAGE}", ttyEnabled: true,
-                        resourceRequestCpu: '4000m', resourceRequestMemory: '8Gi',
-                        command: '/bin/sh -c', args: 'cat',
-                        envVars: [containerEnvVar(key: 'GOPATH', value: '/go')]     
-                    )
-            ],
-            volumes: [
-                    nfsVolume(mountPath: '/home/jenkins/agent/ci-cached-code-daily', serverAddress: '172.16.5.22',
-                            serverPath: '/mnt/ci.pingcap.net-nfs/git', readOnly: false),
-                    emptyDirVolume(mountPath: '/tmp', memory: false),
-                    emptyDirVolume(mountPath: '/home/jenkins', memory: false)
-                    ],
-    ) {
-        node(label) {
-            println "debug command:\nkubectl -n ${namespace} exec -ti ${NODE_NAME} bash"
-            body()
+final GIT_FULL_REPO_NAME="pingcap/tidb"
+
+final podYaml = '''
+apiVersion: v1
+kind: Pod
+spec:
+  securityContext:
+    fsGroup: 1000
+  containers:
+    - name: golang
+      image: "hub.pingcap.net/jenkins/centos7_golang-1.19:latest"
+      tty: true
+      resources:
+        requests:
+          memory: 12Gi
+          cpu: "4"
+        limits:
+          memory: 16Gi
+          cpu: "6"
+    - name: net-tool
+      image: wbitt/network-multitool
+      tty: true
+      resources:
+        limits:
+          memory: 128Mi
+          cpu: 100m
+    - name: report
+      image: hub.pingcap.net/jenkins/python3-requests:latest
+      tty: true
+      resources:
+        limits:
+          memory: 256Mi
+          cpu: 100m
+'''
+
+pipeline {
+    agent {
+        kubernetes {
+            yaml podYaml
+            defaultContainer 'golang'
         }
     }
-}
+    triggers {
+        cron('@daily')
+    }
+    parameters {
+        string(name: 'GIT_BRANCH', defaultValue: 'master', description: '')
+        string(name: 'GIT_COMMIT', defaultValue: 'master', description: '')
+    }
 
-try {
-    run_with_pod {
-        def ws = pwd()
-        //deleteDir()
-
-        stage("debuf info"){
-            println "debug command:\nkubectl -n jenkins-ci exec -ti ${NODE_NAME} bash"
-            println "work space path:\n${ws}"
-        }
-
+    stages {
         stage("Checkout") {
-            container("golang") {
-                sh "whoami && go version"
-            }
-            // update code
-            dir("/home/jenkins/agent/code-archive") {
-                // delete to clean workspace in case of agent pod reused lead to conflict.
-                deleteDir()
-                // copy code from nfs cache
-                container("golang") {
-                    if(fileExists("/home/jenkins/agent/ci-cached-code-daily/src-tidb.tar.gz")){
-                        timeout(5) {
-                            sh """
-                                cp -R /home/jenkins/agent/ci-cached-code-daily/src-tidb.tar.gz*  ./
-                                mkdir -p ${ws}/go/src/github.com/pingcap/tidb
-                                tar -xzf src-tidb.tar.gz -C ${ws}/go/src/github.com/pingcap/tidb --strip-components=1
-                            """
-                        }
-                    }
-                }
-                dir("${ws}/go/src/github.com/pingcap/tidb") {
-                    try {
-                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', timeout: 10]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tidb.git']]]
-                    }   catch (info) {
-                            retry(2) {
-                                echo "checkout failed, retry.."
-                                sleep 5
-                                if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
-                                    deleteDir()
-                                }
-                                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout'], [$class: 'CloneOption', timeout: 10]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: specStr, url: 'git@github.com:pingcap/tidb.git']]]
-                            }
-                    }
-                    container("golang") {
-                        def tidb_path = "${ws}/go/src/github.com/pingcap/tidb"
-                        timeout(5) {
-                            sh """
-                            git checkout -f ${ghprbActualCommit}
-                            """
-                        }
-                    }
-                }
-            }
-        }
-
-
-        stage("Test & Coverage") {
-            dir("go/src/github.com/pingcap/tidb") {
-                container("golang") {
-                    if (ghprbTargetBranch in ["master"]) {
+            options { timeout(time: 5, unit: 'MINUTES') }
+            steps {
+                dir("tidb") {
+                    retry(2) {
                         sh """
-                        export log_level=warn
-                        make br_unit_test_in_verify_ci
-                        mv test_coverage/br_cov.unit_test.out br.coverage
-                        make dumpling_unit_test_in_verify_ci
-                        mv test_coverage/dumpling_cov.unit_test.out dumpling.coverage
-                        make gotest_in_verify_ci
-                        mv test_coverage/tidb_cov.unit_test.out tidb.coverage
+                        wget -O tidb.tar.gz  ${FILE_SERVER_URL}/download/cicd/daily-cache-code/tidb.tar.gz -q --show-progress
+                        tar -xzf tidb.tar.gz -C ./ --strip-components=1  && rm -rf tidb.tar.gz
                         """
-                    } 
+                        checkout(
+                            changelog: false,
+                            poll: false,
+                            scm: [
+                                $class: 'GitSCM', branches: [[name: GIT_BRANCH ]],
+                                doGenerateSubmoduleConfigurations: false,
+                                extensions: [
+                                    [$class: 'PruneStaleBranch'],
+                                    [$class: 'CleanBeforeCheckout'],
+                                    [$class: 'CloneOption', timeout: 15],
+                                ],
+                                submoduleCfg: [],
+                                userRemoteConfigs: [[
+                                    refspec: "+refs/heads/*:refs/remotes/origin/*",
+                                    url: "https://github.com/${GIT_FULL_REPO_NAME}.git",
+                                ]],
+                            ]
+                        )
+                    }  
                 }
             }
         }
-    }
 
-    currentBuild.result = "SUCCESS"
-} catch (Exception e) {
-    currentBuild.result = "FAILURE"
-    currentBuild.description = "unit test failed, coverage data was not available"
-    slackcolor = 'danger'
-    echo "${e}"
-}
-
-if (params.containsKey("triggered_by_upstream_ci")  && params.get("triggered_by_upstream_ci") == "tidb_integration_test_ci") {
-    stage("update commit status") {
-        node("master") {
-            if (currentBuild.result == "ABORTED") {
-                PARAM_DESCRIPTION = 'Jenkins job aborted'
-                // Commit state. Possible values are 'pending', 'success', 'error' or 'failure'
-                PARAM_STATUS = 'error'
-            } else if (currentBuild.result == "FAILURE") {
-                PARAM_DESCRIPTION = 'Jenkins job failed'
-                PARAM_STATUS = 'failure'
-            } else if (currentBuild.result == "SUCCESS") {
-                PARAM_DESCRIPTION = 'Jenkins job success'
-                PARAM_STATUS = 'success'
-            } else {
-                PARAM_DESCRIPTION = 'Jenkins job meets something wrong'
-                PARAM_STATUS = 'error'
+        stage("Test") {
+            options { timeout(time: 30, unit: "MINUTES") }
+            steps {
+                dir("tidb") {
+                    sh label: "ut", script: """
+                    git status && git rev-parse HEAD
+                    ./build/jenkins_unit_test.sh
+                    """
+                }
             }
-            def default_params = [
-                    string(name: 'TIDB_COMMIT_ID', value: ghprbActualCommit ),
-                    string(name: 'CONTEXT', value: 'idc-jenkins-ci-tidb/code-coverage'),
-                    string(name: 'DESCRIPTION', value: PARAM_DESCRIPTION ),
-                    string(name: 'BUILD_URL', value: RUN_DISPLAY_URL ),
-                    string(name: 'STATUS', value: PARAM_STATUS ),
-            ]
-            echo("default params: ${default_params}")
-            build(job: "tidb_update_commit_status", parameters: default_params, wait: true)
+            post {
+                success {
+                    dir("tidb") {
+                        sh label: "upload coverage to codecov", script: """
+                        mv coverage.dat test_coverage/coverage.dat
+                        wget -q -O codecov ${FILE_SERVER_URL}/download/cicd/tools/codecov-v0.3.2
+                        chmod +x codecov
+                        ./codecov --dir test_coverage/ --token ${TIDB_CODECOV_TOKEN}
+                        """
+                    }
+                }
+            }
         }
     }
 }
