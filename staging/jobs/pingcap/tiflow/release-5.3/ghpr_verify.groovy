fa017f288405959f49926fb16c9395f6fd7c55dd
purelind <purelind@gmail.com>
2022-11-21 12:17:20 +0800

    fix: rename pipeline for v5.3 and v6.0

--- staging/jobs/pingcap/tiflow/release-5.3/ghpr_verify.groovy
+++ staging/jobs/pingcap/tiflow/release-5.3/ghpr_verify.groovy
@@ -1,6 +1,6 @@
 // REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
 // For trunk and latest release branches.
-pipelineJob('pingcap/tiflow/ghpr_verify') {
+pipelineJob('pingcap/tiflow/release-5.3/ghpr_verify') {
     logRotator {
         daysToKeep(30)
     }
---
+++ staging/jobs/pingcap/tiflow/release-6.0/aa_folder.groovy
@@ -0,0 +1,3 @@
+folder('pingcap/tiflow/release-6.0') {
+    description("Folder for pipelines of pingcap/tiflow repo for v6.0")
+}
