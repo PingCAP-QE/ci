d5f0ae8408bacecd6ed20a32bfc626b712df85ef
Lifu Wu <purelind@users.noreply.github.com>
2022-11-24 16:39:59 +0800

    feat(tiflow): cdc and dm 6.1 use go1.19 (#1559)

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_test.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_test.groovy
@@ -29,7 +29,7 @@ resultDownloadPath = ""
 
 node("master") {
     deleteDir()
-    def goversion_lib_url = 'https://raw.githubusercontent.com/PingCAP-QE/ci/main/jenkins/pipelines/goversion-select-lib.groovy'
+    def goversion_lib_url = 'https://raw.githubusercontent.com/PingCAP-QE/ci/main/jenkins/pipelines/ci/tidb/goversion-select-lib.groovy'
     sh "curl -O --retry 3 --retry-delay 5 --retry-connrefused --fail ${goversion_lib_url}"
     def goversion_lib = load('goversion-select-lib.groovy')
     GO_VERSION = goversion_lib.selectGoVersion(ghprbTargetBranch)
