9ea805f8e5a7e6f2ada17e1fb671071d702b5cf6
Lifu Wu <purelind@users.noreply.github.com>
2022-11-29 13:28:09 +0800

    feat: migrate tiflow verifyci to ksyun (#1580)
    * feat: add trigger phrase
    
    * fix: correct junit remport file path
    
    * feat: add tiflwo jobs
    
    * feat: add tiflow pipelines
    
    * feat: trigger pipeline for every commit push
    
    * fix: correct script path
    
    * fix: correct pod file
    
    * fix: adjust pod namespace to jenkins-tiflow

---
+++ jobs/pingcap/tiflow/release-5.3/aa_folder.groovy
@@ -0,0 +1,3 @@
+folder('pingcap/tiflow/release-5.3') {
+    description("Folder for pipelines of pingcap/tiflow repo for v5.3")
+}
---
+++ jobs/pingcap/tiflow/release-6.0/ghpr_verify.groovy
@@ -0,0 +1,76 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+pipelineJob('pingcap/tiflow/release-6.0/ghpr_verify') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam("ghprbActualCommit")
+        stringParam("ghprbPullId")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tiflow")
+        pipelineTriggers {
+            triggers {
+                ghprbTrigger {
+                    cron('H/5 * * * *')
+                    gitHubAuthId('') // using the default only one.
+
+                    triggerPhrase('.*/(run-verify.*)|(/merge)')
+                    onlyTriggerPhrase(false)
+                    skipBuildPhrase(".*skip-ci.*")
+                    buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
+                    whitelist('')
+                    orgslist('')
+                    whiteListTargetBranches {
+                        ghprbBranch { branch('^(release-)?6\\.0\\d*(\\.\\d+)?(\\-.*)?$') }
+                    }
+                    // ignore when only those file changed.(
+                    //   multi line regex
+                    // excludedRegions('.*\\.md')
+                    excludedRegions('') // current the context is required in github branch protection.
+
+                    blackListLabels("") // list of GitHub labels for which the build should not be triggered.
+                    whiteListLabels("") // list of GitHub labels for which the build should only be triggered.
+                    adminlist("")
+                    blackListCommitAuthor("")
+                    includedRegions("")
+                    commentFilePath("")
+
+                    allowMembersOfWhitelistedOrgsAsAdmin(true)
+                    permitAll(true)
+                    useGitHubHooks(true)
+                    displayBuildErrorsOnDownstreamBuilds(false)
+                    autoCloseFailedPullRequests(false)
+
+                    // useless, but can not delete.
+                    commitStatusContext("--none--")
+                    msgSuccess("--none--")
+                    msgFailure("--none--")
+
+                    extensions {
+                        ghprbCancelBuildsOnUpdate { overrideGlobal(true) }
+                        ghprbSimpleStatus {
+                            commitStatusContext("jenkins-ticdc/verify")
+                            statusUrl('${RUN_DISPLAY_URL}')
+                            startedStatus("")
+                            triggeredStatus("")
+                            addTestResults(false)
+                            showMatrixStatus(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/pingcap/tiflow/release-6.0/ghpr_verify.groovy")
+            scm {
+                github('PingCAP-QE/ci', 'main')
+            }
+        }
+    }
+}
