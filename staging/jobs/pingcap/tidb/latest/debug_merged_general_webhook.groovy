adec7df675845fbc06d4ab3e85fdc00c2862ae6d
purelind <purelind@gmail.com>
2022-11-30 18:12:32 +0800

    fix: adjust request memory for sqllogic test

---
+++ staging/jobs/pingcap/tidb/latest/debug_merged_general_webhook.groovy
@@ -0,0 +1,24 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+pipelineJob('pingcap/tidb/merged_mysql_test') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam("ghprbActualCommit")
+        stringParam("ghprbTargetBranch")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tidb")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath('staging/pipelines/pingcap/tidb/latest/merged_mysql_test.groovy')
+            scm {
+                github('PingCAP-QE/ci', 'main')
+            }
+        }
+    }
+}
