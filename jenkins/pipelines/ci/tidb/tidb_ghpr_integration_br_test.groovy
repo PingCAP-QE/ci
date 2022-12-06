b987f06d65c61b0c9d9a2af747f13cfa40e77bee
purelind <purelind@gmail.com>
2022-12-06 16:40:41 +0800

    fix: print downstream pipeline link

--- jenkins/pipelines/ci/tidb/tidb_ghpr_integration_br_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_integration_br_test.groovy
@@ -93,7 +93,11 @@ run_with_pod {
                 // Trigger BRIE test without waiting its finish.
                 result = build(job: "br_ghpr_unit_and_integration_test", parameters: default_params, wait: true, propagate: false)
                 if (result.getResult() != "SUCCESS") {
-                    throw new Exception("triggered job: ${triggered_job_name} failed")
+                    currentBuild.result = "FAILURE"
+                    echo "Test failed: https://ci.pingcap.net/blue/organizations/jenkins/br_ghpr_unit_and_integration_test/detail/br_ghpr_unit_and_integration_test/${result.number}/pipeline"
+                } else {
+                    currentBuild.result = "SUCCESS"
+                    echo "Test at: https://ci.pingcap.net/blue/organizations/jenkins/br_ghpr_unit_and_integration_test/detail/br_ghpr_unit_and_integration_test/${result.number}/pipeline"
                 }
             }
             currentBuild.result = "SUCCESS"
