6374d92379764e89f60448df9bd1d9dfab39db90
purelind <purelind@gmail.com>
2022-12-06 16:13:04 +0800

    fix: fix curl download error

--- jenkins/pipelines/ci/tidb/tidb_ghpr_integration_ddl_test.groovy
+++ jenkins/pipelines/ci/tidb/tidb_ghpr_integration_ddl_test.groovy
@@ -200,7 +200,7 @@ try {
                         sh """
                         while ! curl --output /dev/null --silent --head --fail ${tidb_done_url}; do sleep 2; done
                         wget -q --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0  ${tidb_url}
-                        tar -xvz -f tidb-server.tar.gz && rm -rf tidb-server.tar.gz
+                        tar -xz -f tidb-server.tar.gz && rm -rf tidb-server.tar.gz
                         unset GOPROXY && go env -w GOPROXY=${GOPROXY} 
                         if [ \$(grep -E "^ddltest:" Makefile) ]; then
                             make ddltest
@@ -249,20 +249,21 @@ try {
 
                             while ! curl --output /dev/null --silent --head --fail \${tidb_test_url}; do sleep 10; done
                             wget -q --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 \${tidb_test_url}
-                            tar -xvz  -f tidb-test.tar.gz
+                            tar -xz  -f tidb-test.tar.gz
 
                             cd ${test_dir}
 
                             while ! curl --output /dev/null --silent --head --fail \${tikv_url}; do sleep 10; done
                             wget -q --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0  \${tikv_url}
-                            tar -xvz bin/ -f tikv-server.tar.gz && rm -rf tikv-server.tar.gz
+                            tar -xz bin/ -f tikv-server.tar.gz && rm -rf tikv-server.tar.gz
 
                             while ! curl --output /dev/null --silent --head --fail \${pd_url}; do sleep 10; done
                             wget -q --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0  \${pd_url}
-                            tar -xvz bin/ -f pd-server.tar.gz && rm -rf pd-server.tar.gz 
+                            tar -xz bin/ -f pd-server.tar.gz && rm -rf pd-server.tar.gz 
 
                             mkdir -p ${dir}/../tidb/
-                            curl \${tidb_tar_url} | tar -xf - -C ${dir}/../
+                            wget -q --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 \${tidb_tar_url}
+                            tar -xz -f tidb-server.tar -C ${dir}/../ && rm -rf tidb-server.tar
                             mv ${dir}/../tidb/bin/tidb-server ./bin/ddltest_tidb-server
 
                             cd ${dir}/../tidb/
