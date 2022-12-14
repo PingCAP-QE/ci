ddf9e967cd1d108fd27680c2b6ed7167996f480f
lijie <lijie0123@users.noreply.github.com>
2022-12-14 14:26:23 +0800

    chore: bump nightly version to v6.6.0-alpha (#1627)
    Signed-off-by: lijie <lijie@pingcap.com>
    
    Signed-off-by: lijie <lijie@pingcap.com>

--- jenkins/pipelines/cd/nightly/tiup-release-tidb-master-nightly.groovy
+++ jenkins/pipelines/cd/nightly/tiup-release-tidb-master-nightly.groovy
@@ -33,7 +33,7 @@ String PRODUCED_VERSION
 retry(2) {
     try {
         timeout(600) {
-            RELEASE_TAG = "v6.5.0-alpha"
+            RELEASE_TAG = "v6.6.0-alpha"
             node("build_go1130") {
                 container("golang") {
                     def ws = pwd()
