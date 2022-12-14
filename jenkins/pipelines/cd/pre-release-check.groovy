e0c29ed6a1d51408dc341fa18c8fcb124ba94136
lijie <lijie@pingcap.com>
2022-12-14 15:15:29 +0800

    feat: add release check for rocky
    Signed-off-by: lijie <lijie@pingcap.com>

--- jenkins/pipelines/cd/pre-release-check.groovy
+++ jenkins/pipelines/cd/pre-release-check.groovy
@@ -23,7 +23,7 @@ properties([
 def task = "pre-release-check"
 def check_image = { comps, edition, registry, project ->
     podTemplate(name: task, label: task, instanceCap: 5, idleMinutes: 120, containers: [
-            containerTemplate(name: 'dockerd', image: 'docker:18.09.6-dind', privileged: true),
+			containerTemplate(name: 'dockerd', image: 'docker:20-dind', privileged: true, command:'dockerd --host=tcp://localhost:2375'),
             containerTemplate(name: 'docker', image: 'hub.pingcap.net/jenkins/release-checker:master', alwaysPullImage: true, envVars: [
                     envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375'),
             ], ttyEnabled: true, command: 'cat'),
@@ -37,6 +37,13 @@ def check_image = { comps, edition, registry, project ->
                         sh script: "python3 main.py image -c $it --registry ${registry} --project qa ${RELEASE_TAG}.json ${RELEASE_TAG} ${edition} --isrcbuild=true", label: "$it"
                     }
                 }
+                if(RELEASE_TAG>="v6.5.0"){
+                    dir("release-check-rocky") {
+                        comps.each {
+                            sh script: "python3 main.py image -c $it --registry ${registry} --project qa ${RELEASE_TAG}.json ${RELEASE_TAG} ${edition} --isrcbuild=true", label: "$it"
+                        }
+                    }
+				}
             }
         }
     }
@@ -69,7 +76,12 @@ __EOF__
             tar -xzf release-check.tar.gz
             cp ${RELEASE_TAG}.json release-check/
             """
-            stash includes: "release-check/**", name: "release-check"
+            sh """
+            wget ${FILE_SERVER_URL}/download/cicd/scripts/release-check-rocky.tar.gz
+            tar -xzf release-check-rocky.tar.gz
+            cp ${RELEASE_TAG}.json release-check-rocky/
+            """
+            stash includes: "release-check/,release-check-rocky/", name: "release-check"
         }
     }
 }
