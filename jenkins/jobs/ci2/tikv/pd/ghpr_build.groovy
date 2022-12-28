55ed213d01401882f7a00af05a1dd6c6a3a2b3f7
wuhuizuo <wuhuizuo@126.com>
2022-12-28 15:45:38 +0800

    refactor(jobs/tikv/pd): refactor job pd_ghpr_build (#1662)
    current will it will be triggered manually

---
+++ jenkins/jobs/ci2/tikv/pd/ghpr_build.groovy
@@ -0,0 +1,84 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+pipelineJob('pd_ghpr_build') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam("ghprbActualCommit")
+        stringParam("ghprbPullId")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/tikv/pd/")
+        pipelineTriggers {
+            triggers {
+                ghprbTrigger {
+                    cron('H/5 * * * *')
+                    gitHubAuthId('37c47302-ce04-4cae-a76f-b75f439c1464')
+
+                    triggerPhrase('.*/(merge|rebuild|run(-all-tests|-build).*)')
+                    onlyTriggerPhrase(false)
+                    skipBuildPhrase(".*skip-ci.*")
+                    buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
+                    whitelist("")
+                    orgslist("pingcap")
+                    // blackListTargetBranches {
+                    //     ghprbBranch { branch('master') }
+                    //     ghprbBranch { branch('^feature[_|/].*') }
+                    //     ghprbBranch { branch('^(release-)?6\\.[1-9]\\d*(\\.\\d+)?(\\-.*)?$') }
+                    // }
+                    // ignore when only those file changed.(
+                    //   multi line regex
+                    // excludedRegions('.*\\.md')
+                    excludedRegions('') // current the context is required in github branch protection.
+
+                    blackListLabels("")
+                    whiteListLabels("")
+                    adminlist("")
+                    blackListCommitAuthor("")
+                    includedRegions("")
+                    commentFilePath("")
+
+                    allowMembersOfWhitelistedOrgsAsAdmin(true)
+                    permitAll(true)
+                    useGitHubHooks(true)
+                    displayBuildErrorsOnDownstreamBuilds(false)
+                    autoCloseFailedPullRequests(false)
+
+                    // useless, but can not delete.
+                    commitStatusContext("--none--")
+                    msgSuccess("--none--")
+                    msgFailure("--none--")
+
+                    extensions {
+                        ghprbCancelBuildsOnUpdate { overrideGlobal(true) }
+                        ghprbSimpleStatus {
+                            commitStatusContext("idc-jenkins-ci/build")
+                            statusUrl('${RUN_DISPLAY_URL}')
+                            startedStatus("Jenkins job is running.")
+                            triggeredStatus("Jenkins job triggered.")
+                            addTestResults(false)
+                            showMatrixStatus(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("jenkins/pipelines/ci/pd/pd_ghpr_build.groovy")
+            scm {
+                git{
+                    remote {
+                        url('git@github.com:PingCAP-QE/ci.git')
+                        credentials('github-sre-bot-ssh')
+                    }
+                    branch('main')
+                }
+            }
+        }
+    }
+}
