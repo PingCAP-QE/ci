185ef0041617ded3a3df16f47844cb1c45c5e74b
lijie <lijie@pingcap.com>
2022-12-29 19:30:07 +0800

    feat: tidb-dashboard to public
    Signed-off-by: lijie <lijie@pingcap.com>

--- jenkins/pipelines/cd/build-tidb-dashboard.groovy
+++ jenkins/pipelines/cd/build-tidb-dashboard.groovy
@@ -112,6 +112,7 @@ pipeline {
     parameters {
         string(name: 'GitRef', defaultValue: 'master', description: 'branch or commit hash')
         string(name: 'ReleaseTag', defaultValue: 'test', description: 'empty means the same with GitRef')
+        booleanParam(name: 'PushPublic', defaultValue: false, description: 'whether push to public')
     }
     stages {
         stage("build multi-arch"){
@@ -284,6 +285,43 @@ pipeline {
                     tiup mirror publish tidb-dashboard ${ReleaseTag} darwin-amd64.tar.gz tidb-dashboard --os=darwin --arch=amd64 --desc='TiDB Dashboard is a Web UI for monitoring, diagnosing, and managing the TiDB cluster'
                     tiup mirror publish tidb-dashboard ${ReleaseTag} darwin-arm64.tar.gz tidb-dashboard --os=darwin --arch=arm64 --desc='TiDB Dashboard is a Web UI for monitoring, diagnosing, and managing the TiDB cluster'
                    """
+            }
+        }
+        stage("publish tiup"){
+            agent{
+                kubernetes{
+                    yaml tiupYaml
+                    defaultContainer 'tiup'
+                }
+            }
+            when {expression{params.PushPublic.toBoolean()}}
+            environment { TIUPKEY_JSON = credentials('tiup-prod-key') }
+            steps{
+                sh 'set +x;curl https://tiup-mirrors.pingcap.com/root.json -o /root/.tiup/bin/root.json; mkdir -p /root/.tiup/keys; cp $TIUPKEY_JSON  /root/.tiup/keys/private.json'
+                sh """
+                    curl -O http://fileserver.pingcap.net/download/build/tidb-dashboard/${params.ReleaseTag}/linux-amd64.tar.gz
+                    curl -O http://fileserver.pingcap.net/download/build/tidb-dashboard/${params.ReleaseTag}/linux-arm64.tar.gz
+                    curl -O http://fileserver.pingcap.net/download/build/tidb-dashboard/${params.ReleaseTag}/darwin-amd64.tar.gz
+                    curl -O http://fileserver.pingcap.net/download/build/tidb-dashboard/${params.ReleaseTag}/darwin-arm64.tar.gz
+                   """
+                sh """
+                    tiup mirror publish tidb-dashboard ${ReleaseTag} linux-amd64.tar.gz tidb-dashboard --os=linux --arch=amd64 --desc='TiDB Dashboard is a Web UI for monitoring, diagnosing, and managing the TiDB cluster'
+                    tiup mirror publish tidb-dashboard ${ReleaseTag} linux-arm64.tar.gz tidb-dashboard --os=linux --arch=arm64 --desc='TiDB Dashboard is a Web UI for monitoring, diagnosing, and managing the TiDB cluster'
+                    tiup mirror publish tidb-dashboard ${ReleaseTag} darwin-amd64.tar.gz tidb-dashboard --os=darwin --arch=amd64 --desc='TiDB Dashboard is a Web UI for monitoring, diagnosing, and managing the TiDB cluster'
+                    tiup mirror publish tidb-dashboard ${ReleaseTag} darwin-arm64.tar.gz tidb-dashboard --os=darwin --arch=arm64 --desc='TiDB Dashboard is a Web UI for monitoring, diagnosing, and managing the TiDB cluster'
+                   """
+            }
+        }
+        stage("publish image"){
+            agent none
+            when {expression{params.PushPublic.toBoolean()}}
+            steps{
+                build(job: "jenkins-image-syncer",
+                    parameters: [
+                        string(name: 'SOURCE_IMAGE', value: "hub.pingcap.net/rc/tidb-dashboard:${ReleaseTag}"),
+                        string(name: 'TARGET_IMAGE', value: "pingcap/tidb-dashboard:${ReleaseTag}"),
+                    ],
+                    wait: true)
             }
         }
     }
