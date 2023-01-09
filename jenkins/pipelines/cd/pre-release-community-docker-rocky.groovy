51ae3eb8d36cadbfa448b8ebf12a4948b4659ae1
lijie <lijie0123@users.noreply.github.com>
2023-01-09 14:20:50 +0800

    feat: add rocky image for rc (#1628)
    * feat: add rocky dockerfile to ci
    
    becuase ci repo is public
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: fix pd dockerfile for CD
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add dm dockerfile
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: ticdc base image path
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add dumpling dockerfile
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: dumping dockerfile typo
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add ng-monitoring dockerfile
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add tidb binlog dockerfile
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add tidb-lightning-debug dockerfile
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add pre-release-community-docker-rocky
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * style: use dash instead of underscore
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add tidb enterprise dockerfile
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add pipeline to generate enterprise image
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: remove unused comment
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * refract: remove unused code in community-docker
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: groovy clousure ref loop var
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: use def to overcome race condition
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: trigger rocky build at rc
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * refract: remove unnecessary codes
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * improvement: remove unnecessary arguments
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: add bind-utils for dm and cdc
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: tidb use tidb-base base image
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: remove debug images
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: fix debug image
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: add release check for rocky
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * refract: add a seperate stage for rocky
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: jenkins booleanParam is not bool
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: binlog use nslookup
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: compat v5.1 check br
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: tidb-lightning and dumpling use nslookup
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: compat v5.1 check lightning
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: pre release check omit dm for which earlier than 5.2
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * feat: using v1.1.0 base images
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: use .toBoolean() instead of == true for jenkins booleanParam
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * style: use space instead of tab
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * refract: mv dockerfiles to artifact repo
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * refract: move dockerfile to artifacts repo
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    Signed-off-by: lijie <lijie@pingcap.com>

---
+++ jenkins/pipelines/cd/pre-release-community-docker-rocky.groovy
@@ -0,0 +1,347 @@
+properties([
+        parameters([
+                string(
+                        defaultValue: '',
+                        name: 'RELEASE_BRANCH',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'RELEASE_TAG',
+                        trim: true
+                ),
+                booleanParam(
+                        defaultValue: true,
+                        name: 'FORCE_REBUILD'
+                ),
+                booleanParam(
+                        defaultValue: true,
+                        name: 'NEED_DEBUG_IMAGE'
+                ),
+                booleanParam(
+                        defaultValue: false,
+                        name: 'DEBUG_MODE'
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'TIDB_HASH',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'TIKV_HASH',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'PD_HASH',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'TIFLASH_HASH',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'NG_MONITORING_HASH',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'TIDB_BINLOG_HASH',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'TICDC_HASH',
+                        trim: true
+                ),
+        ])
+])
+
+
+HARBOR_REGISTRY_PROJECT_PREFIX = 'hub.pingcap.net/qa'
+if (params.DEBUG_MODE) {
+    HARBOR_REGISTRY_PROJECT_PREFIX = 'hub.pingcap.net/ee-debug'
+    println('DEBUG_MODE is true, use hub.pingcap.net/ee-debug')
+}
+
+def get_dockerfile_url(product, is_enterprise, is_debug){
+    def fileName = product
+    if (product == "tidb" && is_enterprise){
+        fileName = fileName + '-enterprise'
+    }
+    if (is_debug) {
+        fileName = fileName + "-debug"
+    }
+    return "https://raw.githubusercontent.com/PingCAP-QE/artifacts/main/dockerfiles/${fileName}.Dockerfile"
+}
+
+def get_image_str_for_community(product, arch, tag, is_failpoint, is_debug) {
+    def imageTag = tag + "-rocky"+ "-pre"
+    def imageName = product
+    if (product == "monitoring") {
+        imageName = "tidb-monitor-initializer"
+    }
+    if (is_failpoint) {
+        imageTag = imageTag + "-" + "failpoint"
+    }
+    if (arch){
+        imageTag = imageTag + "-" + arch
+    }
+    return "${HARBOR_REGISTRY_PROJECT_PREFIX}/${imageName}:${imageTag}"
+}
+
+def get_sha(hash, repo, branch) {
+    if (hash != "") {
+        return hash
+    } else {
+        sh "curl -s ${FILE_SERVER_URL}/download/builds/pingcap/ee/get_hash_from_github.py > gethash.py"
+        return sh(returnStdout: true, script: "python gethash.py -repo=${repo} -version=${branch} -s=${FILE_SERVER_URL}").trim()
+    }
+}
+
+def test_binary_already_build(binary_url) {
+    cacheExisted = sh(returnStatus: true, script: """
+    if curl --output /dev/null --silent --head --fail ${binary_url}; then exit 0; else exit 1; fi
+    """)
+    if (cacheExisted == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+def release_one(repo, arch, failpoint) {
+    def actualRepo = repo
+    def hash = ""
+    if (repo == "br") {
+        actualRepo = "tidb"
+        hash = "${TIDB_HASH}"
+    }
+    if (repo == "dumpling") {
+        actualRepo = "tidb"
+        hash = "${TIDB_HASH}"
+    }
+    if (repo == "ticdc") {
+        actualRepo = "tiflow"
+        hash = "${TICDC_HASH}"
+    }
+    if (repo == "dm") {
+        actualRepo = "tiflow"
+        hash = "${TICDC_HASH}"
+    }
+    if (repo == "tidb-lightning") {
+        hash = "${TIDB_HASH}"
+    }
+    if (repo == "tidb") {
+        hash = "${TIDB_HASH}"
+    }
+    if (repo == "tikv") {
+        hash = "${TIKV_HASH}"
+    }
+    if (repo == "pd") {
+        hash = "${PD_HASH}"
+    }
+    if (repo == "ng-monitoring") {
+        hash = "${NG_MONITORING_HASH}"
+    }
+    if (repo == "tidb-binlog") {
+        hash = "${TIDB_BINLOG_HASH}"
+    }
+    if (repo == "tiflash") {
+        hash = "${TIFLASH_HASH}"
+    }
+
+    def sha1 = get_sha(hash, actualRepo, RELEASE_BRANCH)
+
+    println "${repo}: ${sha1}"
+    def binary = "builds/pingcap/${repo}/optimization/${RELEASE_TAG}/${sha1}/centos7/${repo}-linux-${arch}.tar.gz"
+    if (failpoint) {
+        binary = "builds/pingcap/${repo}/test/failpoint/${RELEASE_TAG}/${sha1}/linux-${arch}/${repo}.tar.gz"
+    }
+
+    def paramsBuild = [
+            string(name: "ARCH", value: arch),
+            string(name: "OS", value: "linux"),
+            string(name: "EDITION", value: "community"),
+            string(name: "OUTPUT_BINARY", value: binary),
+            string(name: "REPO", value: actualRepo),
+            string(name: "PRODUCT", value: repo),
+            string(name: "GIT_HASH", value: sha1),
+            string(name: "RELEASE_TAG", value: RELEASE_TAG),
+            string(name: "TARGET_BRANCH", value: RELEASE_BRANCH),
+            [$class: 'BooleanParameterValue', name: 'FORCE_REBUILD', value: FORCE_REBUILD],
+    ]
+    if (repo == "tiflash") {
+        paramsBuild = [
+                string(name: "ARCH", value: arch),
+                string(name: "OS", value: "linux"),
+                string(name: "EDITION", value: "community"),
+                string(name: "OUTPUT_BINARY", value: binary),
+                string(name: "REPO", value: "tics"),
+                string(name: "PRODUCT", value: "tics"),
+                string(name: "GIT_HASH", value: sha1),
+                string(name: "RELEASE_TAG", value: RELEASE_TAG),
+                string(name: "TARGET_BRANCH", value: RELEASE_BRANCH),
+                [$class: 'BooleanParameterValue', name: 'FORCE_REBUILD', value: FORCE_REBUILD],
+        ]
+    }
+    if (failpoint) {
+        paramsBuild.push([$class: 'BooleanParameterValue', name: 'FAILPOINT', value: true])
+    }
+    if (repo == "monitoring") {
+        paramsBuild.push([$class: 'StringParameterValue', name: 'RELEASE_TAG', value: RELEASE_BRANCH])
+    }
+    if (test_binary_already_build("${FILE_SERVER_URL}/download/${binary}") && !params.FORCE_REBUILD) {
+        echo "binary already build: ${binary}"
+        echo "forece rebuild: ${params.FORCE_REBUILD}"
+        echo "skip build"
+    } else {
+        echo "force rebuild: ${params.FORCE_REBUILD}"
+        echo "binary not existed or forece_rebuild is true"
+        println "start build binary ${repo} ${arch}"
+        println "params: ${paramsBuild}"
+        build job: "build-common",
+                wait: true,
+                parameters: paramsBuild
+    }
+
+
+    def dockerfile = get_dockerfile_url(repo, false, false)
+    def image = get_image_str_for_community(repo, arch, RELEASE_TAG, failpoint, false)
+
+    def paramsDocker = [
+            string(name: "ARCH", value: arch),
+            string(name: "OS", value: "linux"),
+            string(name: "INPUT_BINARYS", value: binary),
+            string(name: "REPO", value: repo),
+            string(name: "PRODUCT", value: repo),
+            string(name: "RELEASE_TAG", value: RELEASE_TAG),
+            string(name: "DOCKERFILE", value: dockerfile),
+            string(name: "RELEASE_DOCKER_IMAGES", value: image),
+    ]
+    println "start build image ${repo} ${arch}"
+    println "params: ${paramsDocker}"
+    build job: "docker-common",
+            wait: true,
+            parameters: paramsDocker
+
+
+    if (NEED_DEBUG_IMAGE.toBoolean() && arch == "amd64") {
+        def dockerfileForDebug = get_dockerfile_url(repo, false, true)
+        def imageForDebug = get_image_str_for_community(repo, "", RELEASE_TAG, failpoint, true)
+        def paramsDockerForDebug = [
+                string(name: "ARCH", value: "amd64"),
+                string(name: "OS", value: "linux"),
+                string(name: "INPUT_BINARYS", value: binary),
+                string(name: "REPO", value: repo),
+                string(name: "PRODUCT", value: repo),
+                string(name: "RELEASE_TAG", value: RELEASE_TAG),
+                string(name: "DOCKERFILE", value: dockerfileForDebug),
+                string(name: "RELEASE_DOCKER_IMAGES", value: imageForDebug),
+        ]
+        if (repo in ["dumpling", "ticdc", "tidb-binlog", "tidb", "tikv", "pd"]) {
+            println "build debug image ${repo} ${arch}, params: ${paramsDockerForDebug}"
+            build job: "docker-common",
+                    wait: true,
+                    parameters: paramsDockerForDebug
+        } else {
+            println "only amd64 of following repos can build debug image: [dumpling,ticdc,tidb-binlog,tidb,tikv,pd], but got: $repo"
+        }
+    }
+
+    if (repo == "br") {
+        println("start build tidb-lightning")
+        def dockerfileLightning = get_dockerfile_url("tidb-lightning", false, false)
+        def imageLightling = get_image_str_for_community("tidb-lightning", arch, RELEASE_TAG,false,false)
+        def paramsDockerLightning = [
+                string(name: "ARCH", value: arch),
+                string(name: "OS", value: "linux"),
+                string(name: "INPUT_BINARYS", value: binary),
+                string(name: "REPO", value: "lightning"),
+                string(name: "PRODUCT", value: "lightning"),
+                string(name: "RELEASE_TAG", value: RELEASE_TAG),
+                string(name: "DOCKERFILE", value: dockerfileLightning),
+                string(name: "RELEASE_DOCKER_IMAGES", value: imageLightling),
+        ]
+        build job: "docker-common",
+                wait: true,
+                parameters: paramsDockerLightning
+
+        if (NEED_DEBUG_IMAGE.toBoolean() && arch == "amd64") {
+            def dockerfileLightningForDebug = get_dockerfile_url("tidb-lightning", false, true)
+            def imageLightlingForDebug = get_image_str_for_community("tidb-lightning", "",RELEASE_TAG,failpoint,true)
+            def paramsDockerLightningForDebug = [
+                    string(name: "ARCH", value: "amd64"),
+                    string(name: "OS", value: "linux"),
+                    string(name: "INPUT_BINARYS", value: binary),
+                    string(name: "REPO", value: "lightning"),
+                    string(name: "PRODUCT", value: "lightning"),
+                    string(name: "RELEASE_TAG", value: RELEASE_TAG),
+                    string(name: "DOCKERFILE", value: dockerfileLightningForDebug),
+                    string(name: "RELEASE_DOCKER_IMAGES", value: imageLightlingForDebug),
+            ]
+            build job: "docker-common",
+                    wait: true,
+                    parameters: paramsDockerLightningForDebug
+        }
+    }
+}
+
+def release_docker(releaseRepos, builds, arch) {
+    for (item in releaseRepos) {
+        def product = item
+        def product_show = product
+        if (product_show == "br") {
+            product_show = "br && tidb-lightning"
+        }
+        builds["build  " + product_show + " " + arch] = {
+            release_one(product, arch, false)
+        }
+    }
+
+    if (arch == "amd64") {
+        failpointRepos = ["tidb", "pd", "tikv"]
+        for (item in failpointRepos) {
+            def product = "${item}"
+            builds["build ${item} failpoint" + arch] = {
+                release_one(product, arch, true)
+            }
+        }
+    }
+
+}
+
+stage("docker images") {
+    node("${GO_BUILD_SLAVE}") {
+        container("golang") {
+            releaseRepos = ["dumpling", "br", "ticdc", "tidb-binlog", "tiflash", "tidb", "tikv", "pd", "monitoring", "dm", "ng-monitoring"]            
+            builds = [:]
+            release_docker(releaseRepos, builds, "amd64")
+            release_docker(releaseRepos, builds, "arm64")
+            parallel builds
+        }
+    }
+}
+
+stage("create multi-arch image") {
+    def imageNames = ["dumpling", "br", "ticdc", "tidb-binlog", "tiflash", "tidb", "tikv", "pd", "tidb-monitor-initializer", "dm", "tidb-lightning", "ng-monitoring"]
+    def manifest_multiarch_builds = [:]
+    for (item in imageNames) {
+        def product = item // important: groovy closure may use the last status of loop var
+        manifest_multiarch_builds[product] = {
+            def paramsManifest = [
+                    string(name: "AMD64_IMAGE", value: get_image_str_for_community(product, "amd64", RELEASE_TAG, false, false)),
+                    string(name: "ARM64_IMAGE", value: get_image_str_for_community(product, "arm64", RELEASE_TAG, false, false)),
+                    string(name: "MULTI_ARCH_IMAGE", value: get_image_str_for_community(product, "", RELEASE_TAG, false, false)),
+            ]
+            println "paramsManifest: ${paramsManifest}"
+            build job: "manifest-multiarch-common",
+                    wait: true,
+                    parameters: paramsManifest
+        }
+    }
+    parallel manifest_multiarch_builds
+}
---
+++ jenkins/pipelines/cd/pre-release-enterprise-docker-rocky.groovy
@@ -0,0 +1,204 @@
+properties([
+        parameters([
+                string(
+                        defaultValue: '',
+                        name: 'RELEASE_TAG',
+                        description: '',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'RELEASE_BRANCH',
+                        description: '',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'TIDB_HASH',
+                        description: '',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'TIKV_HASH',
+                        description: '',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'PD_HASH',
+                        description: '',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'TIFLASH_HASH',
+                        description: '',
+                        trim: true
+                ),
+                string(
+                        defaultValue: '',
+                        name: 'PLUGIN_HASH',
+                        description: '',
+                        trim: true
+                ),
+                booleanParam(
+                        defaultValue: true,
+                        name: 'FORCE_REBUILD',
+                        description: ''
+                ),
+                booleanParam(
+                        defaultValue: false,
+                        name: 'DEBUG_MODE'
+                )
+        ])
+])
+
+HARBOR_REGISTRY_PROJECT_PREFIX = 'hub.pingcap.net/qa'
+if (params.DEBUG_MODE) {
+    HARBOR_REGISTRY_PROJECT_PREFIX = 'hub.pingcap.net/ee-debug'
+    println('DEBUG_MODE is true, use hub.pingcap.net/ee-debug')
+}
+
+def get_image_str_for_enterprise(product, arch, tag) {
+    def imageTag = tag + "-rocky" + "-pre"
+    def imageName = product
+    if (product == "monitroing") {
+        imageName = "tidb-monitor-initializer"
+    }
+    imageName = imageName + "-enterprise"
+    if (arch != ""){
+        imageTag = imageTag + "-" + arch
+    }
+    def imageStr = "${HARBOR_REGISTRY_PROJECT_PREFIX}/${imageName}:${imageTag}"
+    return imageStr
+}
+
+def build_tidb_enterprise_image(product, sha1, plugin_hash, arch) {
+    def binary = "builds/pingcap/${product}/optimization/${RELEASE_TAG}/${sha1}/centos7/${product}-linux-${arch}-enterprise.tar.gz"
+    def plugin_binary = "builds/pingcap/enterprise-plugin/optimization/${RELEASE_TAG}/${plugin_hash}/centos7/enterprise-plugin-linux-${arch}-enterprise.tar.gz"
+    def dockerfile = "https://raw.githubusercontent.com/PingCAP-QE/artifacts/main/dockerfiles/${product}-enterprise.Dockerfile"
+    def image = get_image_str_for_enterprise("tidb", arch, RELEASE_TAG)
+    def paramsDocker = [
+            string(name: "ARCH", value: arch),
+            string(name: "OS", value: "linux"),
+            string(name: "INPUT_BINARYS", value: "${binary},${plugin_binary}"),
+            string(name: "REPO", value: product),
+            string(name: "PRODUCT", value: product),
+            string(name: "RELEASE_TAG", value: RELEASE_TAG),
+            string(name: "DOCKERFILE", value: dockerfile),
+            string(name: "RELEASE_DOCKER_IMAGES", value: image),
+            string(name: "GIT_BRANCH", value: RELEASE_BRANCH),
+    ]
+    println "build tidb enterprise image: ${paramsDocker}"
+    build job: "docker-common-check",
+            wait: true,
+            parameters: paramsDocker
+}
+
+def build_enterprise_image(product, sha1, arch) {
+    def binary = "builds/pingcap/${product}/optimization/${RELEASE_TAG}/${sha1}/centos7/${product}-linux-${arch}-enterprise.tar.gz"
+    if (product == "tidb-lightning") {
+        binary = "builds/pingcap/br/optimization/${RELEASE_TAG}/${sha1}/centos7/br-linux-${arch}-enterprise.tar.gz"
+    }
+    def dockerfile = "https://raw.githubusercontent.com/PingCAP-QE/artifacts/main/dockerfiles/${product}.Dockerfile"
+    def repo = product
+    def image = get_image_str_for_enterprise(product, arch, RELEASE_TAG)
+
+    def paramsDocker = [
+            string(name: "ARCH", value: arch),
+            string(name: "OS", value: "linux"),
+            string(name: "INPUT_BINARYS", value: binary),
+            string(name: "REPO", value: repo),
+            string(name: "PRODUCT", value: repo),
+            string(name: "RELEASE_TAG", value: RELEASE_TAG),
+            string(name: "DOCKERFILE", value: dockerfile),
+            string(name: "RELEASE_DOCKER_IMAGES", value: image),
+            string(name: "GIT_BRANCH", value: RELEASE_BRANCH),
+    ]
+    println "paramsDocker: ${paramsDocker}"
+    build job: "docker-common-check",
+            wait: true,
+            parameters: paramsDocker
+}
+
+
+stage("build enterprise image"){
+    def builds = [:]
+    builds["tidb-amd64"] = {
+        println "tidb hash : ${TIDB_HASH}"
+        println "tidb plugin hash : ${PLUGIN_HASH}"
+        build_tidb_enterprise_image("tidb", TIDB_HASH, PLUGIN_HASH, "amd64")
+    }
+
+    builds["tikv-amd64"] = {
+        build_enterprise_image("tikv", TIKV_HASH, "amd64")
+    }
+
+    builds["pd-amd64"] = {
+        build_enterprise_image("pd", PD_HASH, "amd64")
+    }
+
+    builds["tiflash-amd64"] = {
+        build_enterprise_image("tiflash", TIFLASH_HASH, "amd64")
+    }
+
+    builds["tidb-arm64"] = {
+        println "tidb hash : ${TIDB_HASH}"
+        println "tidb plugin hash : ${PLUGIN_HASH}"
+        build_tidb_enterprise_image("tidb", TIDB_HASH, PLUGIN_HASH, "arm64")
+    }
+
+    builds["tikv-arm64"] = {
+        build_enterprise_image("tikv", TIKV_HASH, "arm64")
+    }
+
+    builds["pd-arm64"] = {
+        build_enterprise_image("pd", PD_HASH, "arm64")
+    }
+
+    builds["tiflash-arm64"] = {
+        build_enterprise_image("tiflash", TIFLASH_HASH, "arm64")
+    }
+    parallel builds
+}
+
+stage("manifest multi-arch"){
+    def manifest_multiarch_builds = [:]
+    for (item in ["tikv", "tidb", "tiflash", "pd"]) {
+        def product = item
+        manifest_multiarch_builds[product] = {
+            def paramsManifest = [
+                    string(name: "AMD64_IMAGE", value: get_image_str_for_enterprise(product, "amd64", RELEASE_TAG)),
+                    string(name: "ARM64_IMAGE", value: get_image_str_for_enterprise(product, "arm64", RELEASE_TAG)),
+                    string(name: "MULTI_ARCH_IMAGE", value: get_image_str_for_enterprise(product, "", RELEASE_TAG)),
+            ]
+            println "paramsManifest: ${paramsManifest}"
+            build job: "manifest-multiarch-common",
+                    wait: true,
+                    parameters: paramsManifest
+        }
+    }
+    parallel manifest_multiarch_builds
+}
+
+stage("retag community images"){
+    def builds = [:]
+    retagProducts = ["tidb-lightning", "tidb-binlog", "ticdc", "br", "dumpling", "ng-monitoring", "dm", "tidb-monitor-initializer"]
+    for (item in retagProducts) {
+        def product = item
+        builds["retag ${product}"] = {
+            def  community_image = "${HARBOR_REGISTRY_PROJECT_PREFIX}/${product}:${RELEASE_TAG}-rocky-pre"
+            def  enterprise_image = get_image_str_for_enterprise(product, "", RELEASE_TAG)
+            def default_params = [
+                    string(name: 'SOURCE_IMAGE', value: community_image),
+                    string(name: 'TARGET_IMAGE', value: enterprise_image),
+            ]
+            println "retag enterprise image from community image: ${default_params}"
+            build(job: "jenkins-image-syncer",
+                    parameters: default_params,
+                    wait: true)
+        }
+    }
+    parallel builds
+}
