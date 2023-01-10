6661f6e14a011dd89a65497b0b8ec412da41b97b
Lifu Wu <purelind@users.noreply.github.com>
2023-01-10 15:12:44 +0800

    Revert "chore(pingcap/tiflow): set goproxy to proxy.golang.org  (#1699)"
    This reverts commit 36df4b5ffd592946c4fddfdc0b53e3d493b56d2e.

--- pipelines/pingcap/tiflow/release-6.1/ghpr_verify.groovy
+++ pipelines/pingcap/tiflow/release-6.1/ghpr_verify.groovy
@@ -93,7 +93,6 @@ pipeline {
                             dir('tiflow') {
                                 cache(path: "./", filter: '**/*', key: "git/pingcap/tiflow/rev-${ghprbActualCommit}") {
                                     sh label: "${TEST_CMD}", script: """
-                                        unset GOPROXY && go env -w GOPROXY="https://proxy.golang.org,direct"
                                         make ${TEST_CMD}
                                     """
                                 }
