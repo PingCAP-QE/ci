eee726eb828707e79f82a8dc27e8d5a8a2bf6540
purelind <purelind@gmail.com>
2023-01-12 17:36:21 +0800

    fix: remove duplicate cmd

--- staging/pipelines/pingcap/tidb-test/latest/ghpr_build.groovy
+++ staging/pipelines/pingcap/tidb-test/latest/ghpr_build.groovy
@@ -98,7 +98,6 @@ pipeline {
                     sh """
                     TIDB_SRC_PATH=${WORKSPACE}/tidb make check
                     for binCase in {partition_test,coprocessor_test,concurrent-sql}; do
-                        TIDB_SRC_PATH=${WORKSPACE}/tidb make check && break
                         cd \${binCase} && chmod +x build.sh && ./build.sh
                         cd ..
                         sleep 5
