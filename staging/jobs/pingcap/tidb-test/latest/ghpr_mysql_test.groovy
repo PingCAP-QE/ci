8fad43ee9cfea3a1116ade4297483a585d624ac8
Lifu Wu <purelind@users.noreply.github.com>
2023-01-12 16:07:32 +0800

    feat(staging/tidb-test): add tidb-test verifyci to staging (#1705)
    * feat: add 3 tidb-test verifyci pipelines
    
    * feat: add ghpr_build for tidb-test
    
    * feat: add tidb-test ghpr_build
    
    * feat: add tidb-test ghpr_mysql_test
    
    * feat: add tidb-test ghpr_integration_mysql_test
    
    * feat: add tidb-test ghpr_integration_jdbc_test
    
    * feat: add pod template file
    
    * format: add new line
    
    * feat: add job
    
    * chore: scm with git

---
+++ staging/jobs/pingcap/tidb-test/latest/ghpr_mysql_test.groovy
@@ -0,0 +1,84 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For trunk and latest release branches.
+pipelineJob('pingcap/tidb-test/ghpr_mysql_test') {
+    // disabled(true)
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam("ghprbActualCommit")
+        stringParam("ghprbPullId")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tidb-test")
+        pipelineTriggers {
+            triggers {
+                ghprbTrigger {
+                    cron('H/5 * * * *')
+                    gitHubAuthId('') // using the default only one.
+
+                    triggerPhrase('.*/(debug-(all-tests|mysql-test).*)')
+                    onlyTriggerPhrase(true)
+                    skipBuildPhrase(".*skip-ci.*")
+                    buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
+                    whitelist("ming-relax LiangShang hsqlu yangwenmai qxhy123 mccxj dreamquster MyonKeminta colinback spongedu lzmhhh123 bb7133 dbjoa")
+                    orgslist("pingcap")
+                    whiteListTargetBranches {
+                        ghprbBranch { branch('master') }
+                        ghprbBranch { branch('^feature[_|/].*') }
+                    }
+                    // ignore when only those file changed.(
+                    //   multi line regex
+                    // excludedRegions('.*\\.md')
+                    excludedRegions('') // current the context is required in github branch protection.
+
+                    blackListLabels("") // list of GitHub labels for which the build should not be triggered.
+                    whiteListLabels("") // list of GitHub labels for which the build should only be triggered.
+                    adminlist("")
+                    blackListCommitAuthor("")
+                    includedRegions("")
+                    commentFilePath("")
+
+                    allowMembersOfWhitelistedOrgsAsAdmin(true)
+                    permitAll(true)
+                    useGitHubHooks(true)
+                    displayBuildErrorsOnDownstreamBuilds(false)
+                    autoCloseFailedPullRequests(false)
+
+                    // useless, but can not delete.
+                    commitStatusContext("--none--")
+                    msgSuccess("--none--")
+                    msgFailure("--none--")
+
+                    extensions {
+                        ghprbCancelBuildsOnUpdate { overrideGlobal(true) }
+                        ghprbSimpleStatus {
+                            commitStatusContext("idc-jenkins-ci/mysql-test")
+                            statusUrl('${RUN_DISPLAY_URL}')
+                            startedStatus("")
+                            triggeredStatus("")
+                            addTestResults(false)
+                            showMatrixStatus(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("staging/pipelines/pingcap/tidb-test/latest/ghpr_mysql_test.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                }
+            }
+        }
+    }
+}
