462d6ad58003e53ef049bcefd34b47d04f739f47
Lifu Wu <purelind@users.noreply.github.com>
2023-02-08 17:21:59 +0800

    feat(pingcap/tidb-test): prod refactor tidb-test pipeline (#1754)
    * feat: prod add tidb-test prow jobs
    
    * feat: add pipeline of tidb-test
    
    * feat: refactor to prow style pipelines
    
    * feat: add prow presubmit config for tidb-test latest
    
    * feat: add conditional trigger

---
+++ jobs/pingcap/tidb-test/latest/ghpr_mysql_test.groovy
@@ -0,0 +1,32 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For trunk and latest release branches.
+pipelineJob('pingcap/tidb-test/ghpr_mysql_test') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tidb")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/pingcap/tidb-test/latest/ghpr_mysql_test.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                }
+            }
+        }
+    }
+}
