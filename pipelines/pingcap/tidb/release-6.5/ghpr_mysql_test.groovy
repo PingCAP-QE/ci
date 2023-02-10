00b368ee795ea43d9fdf3aab459ef678ae9fec66
purelind <purelind@gmail.com>
2023-02-10 18:10:32 +0800

    chore: remove comments

--- pipelines/pingcap/tidb/release-6.5/ghpr_mysql_test.groovy
+++ pipelines/pingcap/tidb/release-6.5/ghpr_mysql_test.groovy
@@ -1,6 +1,5 @@
 // REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
 // Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
-// should triggerd for master and latest release branches
 @Library('tipipeline') _
 
 final K8S_NAMESPACE = "jenkins-tidb"
