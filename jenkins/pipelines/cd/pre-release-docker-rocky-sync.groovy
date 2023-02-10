003d8cd043d4748fc60af58eeb66ebd87714698a
lijie <lijie@pingcap.com>
2023-02-10 11:06:36 +0800

    fix: v6.6.0 use rocky
    Signed-off-by: lijie <lijie@pingcap.com>

--- jenkins/pipelines/cd/pre-release-docker-rocky-sync.groovy
+++ jenkins/pipelines/cd/pre-release-docker-rocky-sync.groovy
@@ -1,10 +1,48 @@
+import java.text.SimpleDateFormat
 pipeline {
     agent none
     parameters {
         string  description: 'RC release tag', name: 'Version', trim: true
     }
     stages {
-        stage('sync') {
+        stage('prepare'){
+            steps{
+                script{
+                    def date = new Date()
+                    ts13 = date.getTime() / 1000
+                    ts10 = (Long) ts13
+                    sdf = new SimpleDateFormat("yyyyMMdd")
+                    day = sdf.format(date)
+                }
+            }
+        }
+        stage('sync-failpoint') {
+            matrix {
+                axes {
+                    axis {
+                        name 'prod'
+                        values  "pd", "tidb", "tikv"
+                    }
+                }
+                stages {
+                    stage("sync") {
+                        steps {
+                            script{
+                                def source = "hub.pingcap.net/qa/${prod}:${params.Version}-rocky-pre-failpoint-amd64"
+                                def target = "hub.pingcap.net/qa/${prod}:${params.Version}-pre-failpoint"
+                                echo "sync $source to $target"
+                                build job: 'jenkins-image-syncer',
+                                    parameters: [
+                                            string(name: 'SOURCE_IMAGE', value: source),
+                                            string(name: 'TARGET_IMAGE', value: target)
+                                    ]
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage('sync-multiarch') {
             matrix {
                 axes {
                     axis {
@@ -26,6 +64,25 @@ pipeline {
                                     ]
                                 source = "hub.pingcap.net/qa/${prod}-enterprise:${params.Version}-rocky-pre"
                                 target = "hub.pingcap.net/qa/${prod}-enterprise:${params.Version}-pre"
+                                echo "sync $source to $target"
+                                build job: 'jenkins-image-syncer',
+                                    parameters: [
+                                            string(name: 'SOURCE_IMAGE', value: source),
+                                            string(name: 'TARGET_IMAGE', value: target)
+                                    ]
+                                def source = "hub.pingcap.net/qa/${prod}:${params.Version}-rocky-pre"
+                                def target = "pingcap/${prod}:${params.Version}-pre"
+                                echo "sync $source to $target"
+                                build job: 'jenkins-image-syncer',
+                                    parameters: [
+                                            string(name: 'SOURCE_IMAGE', value: source),
+                                            string(name: 'TARGET_IMAGE', value: target)
+                                    ]
+                                source = "hub.pingcap.net/qa/${prod}-enterprise:${params.Version}-rocky-pre"
+                                target = "gcr.io/pingcap-public/dbaas/${prod}:${params.Version}-${day}-${ts10}"
+                                if (prod == "tidb-monitor-initializer"){
+                                    target = "gcr.io/pingcap-public/dbaas/${prod}:${params.Version}"
+                                }
                                 echo "sync $source to $target"
                                 build job: 'jenkins-image-syncer',
                                     parameters: [
