3f7db3ff9e77511be383fbc84d4d279241e90be7
purelind <purelind@gmail.com>
2023-02-16 11:37:16 +0800

    feat: manage tikv jobs wiht jobdsl

---
+++ jenkins/jobs/ci/tikv/tikv/ghpr_integration_br_test.groovy
@@ -0,0 +1,100 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+pipelineJob('tikv_ghpr_integration_br_test') {
+    logRotator {
+        daysToKeep(60)
+        numToKeep(500)
+    }
+    parameters {
+        stringParam{
+            name('ghprbActualCommit')
+            trim(true)
+        }
+        stringParam{
+            name('ghprbPullId')
+            trim(true)
+        }
+        stringParam{
+            name('ghprbTargetBranch')
+            trim(true)
+        }
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/tikv/tikv/")
+        pipelineTriggers {
+            triggers {
+                ghprbTrigger {
+                    cron('H/5 * * * *')
+                    gitHubAuthId('a6f8c5ac-6082-4ad1-b84d-562cc1c37682')
+
+                    triggerPhrase('.*/run(-integration-br-test|-integration-tests).*')
+                    onlyTriggerPhrase(true)
+                    skipBuildPhrase(".*skip-ci.*")
+                    buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
+                    whitelist('')
+                    orgslist('pingcap tikv')
+                    whitelistTargetBranches {
+                        ghprbBranch { branch('master') }
+                        ghprbBranch { branch('^feature[_|/].*') }
+                        ghprbBranch { branch('^release-\\d+\\.\\d+.*') }
+                    }
+                    blackListTargetBranches {
+                        ghprbBranch { branch('release-2.1') }
+                        ghprbBranch { branch('3.0.5.hotfix') }
+                        ghprbBranch { branch('^(release-)?3\\.[1-9]\\d*(\\.\\d+)?(\\-.*)?$') }
+                    }
+                    // ignore when only those file changed.(
+                    //   multi line regex
+                    // excludedRegions('.*\\.md')
+                    excludedRegions('') // current the context is required in github branch protection.
+
+                    blackListLabels("")
+                    whiteListLabels("")
+                    adminlist("")
+                    blackListCommitAuthor("")
+                    includedRegions("")
+                    commentFilePath("")
+
+                    allowMembersOfWhitelistedOrgsAsAdmin(true)
+                    permitAll(true)
+                    useGitHubHooks(true)
+                    displayBuildErrorsOnDownstreamBuilds(false)
+                    autoCloseFailedPullRequests(false)
+
+                    // useless, but can not delete.
+                    commitStatusContext("--none--")
+                    msgSuccess("--none--")
+                    msgFailure("--none--")
+
+                    extensions {
+                        ghprbCancelBuildsOnUpdate { overrideGlobal(true) }
+                        ghprbSimpleStatus {
+                            commitStatusContext('idc-jenkins-ci-tikv/integration-br-test')
+                            statusUrl('${RUN_DISPLAY_URL}')
+                            startedStatus('Jenkins job is running.')
+                            triggeredStatus('Jenkins job triggered.')
+                            addTestResults(false)
+                            showMatrixStatus(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath('jenkins/pipelines/ci/tikv/tikv_ghpr_integration_br_test.groovy')
+            scm {
+                git{
+                    remote {
+                        url('git@github.com:PingCAP-QE/ci.git')
+                        credentials('github-sre-bot-ssh')
+                    }
+                    branch('main')
+                }
+            }
+        }
+    }
+}
