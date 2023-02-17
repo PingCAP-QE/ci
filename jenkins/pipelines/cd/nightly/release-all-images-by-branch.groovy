a477fcfd131ecab2c7f72d8e6ef0c9919faa747b
lijie <lijie0123@users.noreply.github.com>
2023-02-17 18:57:04 +0800

    feat: skip old version nightly build (#1804)

--- jenkins/pipelines/cd/nightly/release-all-images-by-branch.groovy
+++ jenkins/pipelines/cd/nightly/release-all-images-by-branch.groovy
@@ -26,14 +26,15 @@ properties([
                 )
         ]),
         pipelineTriggers([
-                parameterizedCron('''
+                /*
                 H 18 * * * % GIT_BRANCH=release-5.0;FORCE_REBUILD=false;NEED_MULTIARCH=false
                 H 18 * * * % GIT_BRANCH=release-5.1;FORCE_REBUILD=false;NEED_MULTIARCH=false
                 H 18 * * * % GIT_BRANCH=release-5.2;FORCE_REBUILD=false;NEED_MULTIARCH=false
                 H 18 * * * % GIT_BRANCH=release-5.3;FORCE_REBUILD=false;NEED_MULTIARCH=false
+                */
+                parameterizedCron('''
                 H 18 * * * % GIT_BRANCH=release-5.4;FORCE_REBUILD=false;NEED_MULTIARCH=false
                 H 18 * * * % GIT_BRANCH=release-6.1;FORCE_REBUILD=false;NEED_MULTIARCH=true
-                H 18 * * * % GIT_BRANCH=release-6.4;FORCE_REBUILD=false;NEED_MULTIARCH=true
                 H 18 * * * % GIT_BRANCH=release-6.5;FORCE_REBUILD=false;NEED_MULTIARCH=true
                 H 18 * * * % GIT_BRANCH=release-6.6;FORCE_REBUILD=false;NEED_MULTIARCH=true
                 H 19 * * * % GIT_BRANCH=master;FORCE_REBUILD=false;NEED_MULTIARCH=true
