8cc2625b712ad74b2057b82e85f4e9faf699e32d
Purelind <purelind@users.noreply.github.com>
2023-02-17 18:17:04 +0800

    feat(pingcap/tiflow): manage tiflow with jobdsl (#1793)
    * feat: manage tiflow with jobdsl
    
    * fix: correct repo name
    
    * feat: manage tiflow pipeline with jobdsl
    
    * chore: update targetbranch regex

---
+++ jenkins/jobs/ci2/pingcap/tiflow/cdc_ghpr_downstream_coverage.groovy
@@ -0,0 +1,53 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+pipelineJob('cdc_ghpr_downstream_coverage') {
+    logRotator {
+        daysToKeep(90)
+    }
+    parameters {
+        stringParam{
+            name('COVERAGE_FILE')
+            trim(true)
+        }
+        stringParam{
+            name('CI_NAME')
+            defaultValue('jenkins-ci')
+            trim(true)
+        }
+        stringParam{
+            name('CI_BUILD_NUMBER')
+            trim(true)
+        }
+        stringParam{
+            name('CI_BUILD_URL')
+            trim(true)
+        }
+        stringParam{
+            name('CI_BRANCH')
+            trim(true)
+        }
+        stringParam{
+            name('CI_PULL_REQUEST')
+            trim(true)
+        }
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tiflow/")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("jenkins/pipelines/ci/ticdc/cdc_ghpr_downstream_coverage.groovy")
+            scm {
+                git{
+                    remote {
+                        url('git@github.com:PingCAP-QE/ci.git')
+                        credentials('github-sre-bot-ssh')
+                    }
+                    branch('main')
+                }
+            }
+        }
+    }
+}
---
+++ jenkins/jobs/ci2/pingcap/tiflow/cdc_ghpr_test.groovy
@@ -0,0 +1,83 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+pipelineJob('cdc_ghpr_test') {
+    logRotator {
+        daysToKeep(90)
+    }
+    parameters {
+        stringParam("ghprbActualCommit")
+        stringParam("ghprbPullId")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tiflow/")
+        pipelineTriggers {
+            triggers {
+                ghprbTrigger {
+                    cron('H/5 * * * *')
+                    gitHubAuthId('37c47302-ce04-4cae-a76f-b75f439c1464')
+
+                    triggerPhrase('(.*\/run(-all-tests|-unit-test).*)|(\/merge)')
+                    onlyTriggerPhrase(false)
+                    skipBuildPhrase(".*skip-ci.*")
+                    buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
+                    whitelist("")
+                    orgslist("pingcap")
+                    whitelistTargetBranches {
+                        ghprbBranch { branch('^(release-)?4\\.\\d+(\\.\\d+)?(\\-.*)?$') }
+                        ghprbBranch { branch('^(release-)?[5]\\.[0-3](\\.\\d+)?(\\-.*)?$') }
+                    }
+                    // ignore when only those file changed.(
+                    //   multi line regex
+                    // excludedRegions('.*\\.md')
+                    excludedRegions('') // current the context is required in github branch protection.
+
+                    blackListLabels("")
+                    whiteListLabels("")
+                    adminlist("")
+                    blackListCommitAuthor("")
+                    includedRegions("")
+                    commentFilePath("")
+
+                    allowMembersOfWhitelistedOrgsAsAdmin(true)
+                    permitAll(true)
+                    useGitHubHooks(true)
+                    displayBuildErrorsOnDownstreamBuilds(false)
+                    autoCloseFailedPullRequests(false)
+
+                    // useless, but can not delete.
+                    commitStatusContext("--none--")
+                    msgSuccess("--none--")
+                    msgFailure("--none--")
+
+                    extensions {
+                        ghprbCancelBuildsOnUpdate { overrideGlobal(true) }
+                        ghprbSimpleStatus {
+                            commitStatusContext("idc-jenkins-ci-ticdc/unit-test")
+                            statusUrl('${RUN_DISPLAY_URL}')
+                            startedStatus("Jenkins job is running.")
+                            triggeredStatus("Jenkins job triggered.")
+                            addTestResults(false)
+                            showMatrixStatus(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("jenkins/pipelines/ci/ticdc/cdc_ghpr_test.groovy")
+            scm {
+                git{
+                    remote {
+                        url('git@github.com:PingCAP-QE/ci.git')
+                        credentials('github-sre-bot-ssh')
+                    }
+                    branch('main')
+                }
+            }
+        }
+    }
+}
