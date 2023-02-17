71a61aa54793af66ffc7382e9da211b9a272ed3e
Purelind <purelind@users.noreply.github.com>
2023-02-17 18:48:22 +0800

    fix: fix trigger phrase (#1803)

--- jenkins/jobs/ci2/pingcap/tiflow/dm_ghpr_compatibility_test.groovy
+++ jenkins/jobs/ci2/pingcap/tiflow/dm_ghpr_compatibility_test.groovy
@@ -16,7 +16,7 @@ pipelineJob('dm_ghpr_compatibility_test') {
                     cron('H/5 * * * *')
                     gitHubAuthId('37c47302-ce04-4cae-a76f-b75f439c1464')
 
-                    triggerPhrase('(.*\/run(-all-tests|-dm-compatibility-tests?).*)|(\/merge)')
+                    triggerPhrase('(.*/run(-all-tests|-dm-compatibility-tests?).*)|(/merge)')
                     onlyTriggerPhrase(true)
                     skipBuildPhrase(".*skip-ci.*")
                     buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
