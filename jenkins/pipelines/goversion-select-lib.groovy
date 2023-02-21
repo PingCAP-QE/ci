411d8f7ae0cea7d9239ca442816d4210048e9f0d
Purelind <purelind@users.noreply.github.com>
2023-02-21 14:22:26 +0800

    chore: sync two goversion lib (#1817)
    * fix: sync two goversion lib
    
    * fix: sync two goversion lib
    
    * format: add new line at end of file
    
    * fix: fix tag env

--- jenkins/pipelines/goversion-select-lib.groovy
+++ jenkins/pipelines/goversion-select-lib.groovy
@@ -6,13 +6,15 @@ feature_branch_use_go19 = []
 // Version Selector
 // branch or tag
 // == branch
-//  master use go1.18
+//  master use go1.19
+//  release branch >= release-6.1 use go1.19
 //  release branch >= release-6.0 use go1.18
 //  release branch >= release-5.1 use go1.16
 //  release branch < release-5.0 use go1.13
 //  other feature use corresponding go version
 //  the default go version is go1.18
 // == tag
+// any tag greater or eqaul to v6.1.xxx use go1.19
 // any tag greater or eqaul to v6.0.xxx use go1.18
 // any tag smaller than v6.0.0 and graeter or equal to v5.1.xxx use go1.16
 // any tag smaller than v5.1.0 use go1.13
@@ -21,6 +23,10 @@ feature_branch_use_go19 = []
 def selectGoVersion(branchNameOrTag) {
     if (branchNameOrTag.startsWith("v")) {
         println "This is a tag"
+        // special for v6.1 larger than patch 3
+        if (branchNameOrTag.startsWith("v6.1") && branchNameOrTag >= "v6.1.3" || branchNameOrTag=="v6.1.0-nightly") {
+            return "go1.19"
+        }
         if (branchNameOrTag >= "v6.3") {
             println "tag ${branchNameOrTag} use go 1.19"
             return "go1.19"
@@ -63,11 +69,11 @@ def selectGoVersion(branchNameOrTag) {
         }
 
 
-        if (branchNameOrTag.startsWith("release-") && branchNameOrTag >= "release-6.3") {
+        if (branchNameOrTag.startsWith("release-") && branchNameOrTag >= "release-6.1") {
             println("branchNameOrTag: ${branchNameOrTag}  use go1.19")
             return "go1.19"
         }
-        if (branchNameOrTag.startsWith("release-") && branchNameOrTag < "release-6.3"  && branchNameOrTag >= "release-6.0") {
+        if (branchNameOrTag.startsWith("release-") && branchNameOrTag < "release-6.1"  && branchNameOrTag >= "release-6.0") {
             println("branchNameOrTag: ${branchNameOrTag}  use go1.18")
             return "go1.18"
         }
@@ -80,57 +86,9 @@ def selectGoVersion(branchNameOrTag) {
             println("branchNameOrTag: ${branchNameOrTag}  use go1.13")
             return "go1.13"
         }
-        println "branchNameOrTag: ${branchNameOrTag}  use default version go1.18"
+        println "branchNameOrTag: ${branchNameOrTag}  use default version go1.19"
         return "go1.19"
     }
 }
-
-
-// assert selectGoVersion("v6.3.0") == "go1.19"
-// assert selectGoVersion("v6.2.0") == "go1.18"
-// assert selectGoVersion("v6.0.0") == "go1.18"
-// assert selectGoVersion("v6.0.1") == "go1.18"
-// assert selectGoVersion("v6.2.3") == "go1.18"
-// assert selectGoVersion("v6.0.1") == "go1.18"
-// assert selectGoVersion("v6.1.1") == "go1.18"
-// assert selectGoVersion("v5.1.0") == "go1.16"
-// assert selectGoVersion("v5.2.3") == "go1.16"
-// assert selectGoVersion("v5.3.0") == "go1.16"
-// assert selectGoVersion("v5.4.0") == "go1.16"
-// assert selectGoVersion("v5.4.3") == "go1.16"
-// assert selectGoVersion("v5.0.0") == "go1.13"
-// assert selectGoVersion("v5.0.6") == "go1.13"
-// assert selectGoVersion("v4.0.0") == "go1.13"
-// assert selectGoVersion("v3.0.0") == "go1.13"
-// assert selectGoVersion("v5.1.0-20220202") == "go1.16"
-// assert selectGoVersion("v5.2.0-20220202") == "go1.16"
-// assert selectGoVersion("v5.0.1-20220202") == "go1.13"
-// assert selectGoVersion("v5.3.2-20220202") == "go1.16"
-// assert selectGoVersion("v5.4.0-20220202") == "go1.16"
-// assert selectGoVersion("v6.2.1-20220202") == "go1.18"
-// assert selectGoVersion("v6.2.1-20220203-somepoc") == "go1.18"
-// assert selectGoVersion("v5.1.1") == "go1.16"
-// assert selectGoVersion("v5.0.1") == "go1.13"
-
-// assert selectGoVersion("release-6.3") == "go1.19"
-// assert selectGoVersion("release-6.0") == "go1.18"
-// assert selectGoVersion("release-5.2") == "go1.16"
-// assert selectGoVersion("release-5.3") == "go1.16"
-// assert selectGoVersion("release-5.4") == "go1.16"
-// assert selectGoVersion("release-5.1") == "go1.16"
-// assert selectGoVersion("release-5.0") == "go1.13"
-// assert selectGoVersion("master") == "go1.19"
-// assert selectGoVersion("release-4.0") == "go1.13"
-// assert selectGoVersion("release-6.0-20220202") == "go1.18"
-// assert selectGoVersion("release-5.2-20220203") == "go1.16"
-// assert selectGoVersion("release-5.2-20220203-somepoc") == "go1.16"
-// assert selectGoVersion("release-5.3-20220203-somepoc") == "go1.16"
-// assert selectGoVersion("release-5.4-20220203-somepoc") == "go1.16"
-// assert selectGoVersion("release-5.1-20220203-somepoc") == "go1.16"
-// assert selectGoVersion("release-5.0-20220203-somepoc") == "go1.13"
-
-// assert selectGoVersion("hz-poc") == "go1.16"
-// assert selectGoVersion("ft-data-inconsistency") == "go1.16"
-// assert selectGoVersion("br-stream") == "go1.16"
 
 return this
