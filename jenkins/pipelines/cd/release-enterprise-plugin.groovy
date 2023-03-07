1af7d9686aded61e2ce9e122b53b4ad25821031c
lijie <lijie0123@users.noreply.github.com>
2023-03-07 19:19:14 +0800

    feat: upload enterprise plugin to fixed path (#1869)
    * feat: upload enterprise plugin to fixed path
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: sha256
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * fix: use uploader builder
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    * style: pod
    
    Signed-off-by: lijie <lijie@pingcap.com>
    
    ---------
    
    Signed-off-by: lijie <lijie@pingcap.com>

---
+++ jenkins/pipelines/cd/release-enterprise-plugin.groovy
@@ -0,0 +1,50 @@
+final uploaderYaml = '''
+spec:
+  containers:
+  - name: uploader
+    image: hub.pingcap.net/jenkins/uploader
+    args: ["sleep", "infinity"]
+'''
+pipeline{
+    parameters {
+        string(name: 'Version', description: 'important, the version for cli --version and profile choosing, eg. v6.5.0')
+        string(name: 'Hash', description: 'git hash')
+    }
+    agent {
+        kubernetes {
+            yaml uploaderYaml
+            defaultContainer 'uploader'
+        }
+    }
+    stages{
+        stage("multi-arch"){
+            matrix{
+                axes{
+                    axis{
+                        name "arch"
+                        values "amd64", "arm64"
+                    }
+                }
+                stages{
+                stage("upload"){
+                    environment {
+                        QINIU_BUCKET_NAME = 'tidb'
+                        QINIU_ACCESS_KEY = credentials('qn_access_key');
+                        QINIU_SECRET_KEY = credentials('qiniu_secret_key');
+                    }
+                    steps{
+                sh """
+                wget -q http://fileserver.pingcap.net/download/builds/pingcap/enterprise-plugin/optimization/${params.Version}/${params.Hash}/centos7/enterprise-plugin-linux-${arch}-enterprise.tar.gz
+                wget -q http://fileserver.pingcap.net/download/builds/pingcap/enterprise-plugin/optimization/${params.Version}/${params.Hash}/centos7/enterprise-plugin-linux-${arch}-enterprise.tar.gz.sha256
+                curl --fail -F release/enterprise-plugin-${params.Version}-linux-${arch}.tar.gz=@enterprise-plugin-linux-${arch}-enterprise.tar.gz http://fileserver.pingcap.net/upload
+                curl --fail -F release/enterprise-plugin-${params.Version}-linux-${arch}.tar.gz.sha256=@enterprise-plugin-linux-${arch}-enterprise.tar.gz.sha256 http://fileserver.pingcap.net/upload
+                upload_qiniu.py enterprise-plugin-linux-${arch}-enterprise.tar.gz enterprise-plugin-${params.Version}-linux-${arch}.tar.gz
+                upload_qiniu.py enterprise-plugin-linux-${arch}-enterprise.tar.gz.sha256 enterprise-plugin-${params.Version}-linux-${arch}.tar.gz.sha256
+                """
+                    }
+                }
+                }
+            }
+        }
+    }
+}
