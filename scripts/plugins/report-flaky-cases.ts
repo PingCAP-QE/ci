adf2c8001bc33adc1cf8135a661386ccc628460d
wuhuizuo <wuhuizuo@126.com>
2023-03-13 17:48:13 +0800

    feat(scripts/plugins): add report flaky test tool
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- scripts/plugins/report-flaky-cases.ts
+++ scripts/plugins/report-flaky-cases.ts
@@ -1,8 +1,21 @@
 import { Client } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
 import * as flags from "https://deno.land/std@0.179.0/flags/mod.ts";
 
+const SQL_TABLE_DSL = `
+CREATE TABLE IF NOT EXISTS tiinsight_problem_case_runs (
+  id INT auto_increment,
+  repo VARCHAR(255), -- repo full name
+  branch VARCHAR(255), -- base branch
+  suite_name VARCHAR(255), -- suite name, target naem in bazel.
+  case_name VARCHAR(255), -- case name, may be TextXxx.TestYyy format.
+  report_time timestamp, -- unit timestamp
+  flaky BIT, -- true or false
+  timecost_ms INT, -- milliseconds.
+  primary key (id)
+);
+`;
 const SQL_TPL_INSERT =
-  `INSERT INTO problem_case_runs(repo, branch, target, case_name, timestamp, flaky, timecost) values(?, ?, ?, ?, ?, ?, ?, ?)`;
+  `INSERT INTO tiinsight_problem_case_runs(repo, branch, suite_name, case_name, report_time, flaky, timecost_ms) values(?, ?, ?, ?, FROM_UNIXTIME(?), ?, ?);`;
 
 interface _cliParams {
   hostname: string;
@@ -12,7 +25,6 @@ interface _cliParams {
   caseDataFile: string;
   repo: string;
   branch: string;
-  timestamp: number;
 }
 
 interface _problemCasesFromBazel {
@@ -30,6 +42,10 @@ async function main({
   caseDataFile,
   ...rest
 }: _cliParams) {
+  const caseData = JSON.parse(
+    await Deno.readTextFile(caseDataFile),
+  ) as _problemCasesFromBazel;
+
   const client = await new Client().connect({
     hostname,
     username,
@@ -37,19 +53,19 @@ async function main({
     db,
   });
 
-  const caseData = JSON.parse(
-    await Deno.readTextFile(caseDataFile),
-  ) as _problemCasesFromBazel;
-
   try {
-    insert_problem_case_runs(
+    // create table if not exist.
+    await client.execute(SQL_TABLE_DSL);
+    // insert records.
+    await insert_problem_case_runs(
       client,
       caseData,
       rest.repo,
       rest.branch,
-      rest.timestamp,
+      Date.now(),
     );
   } finally {
+    // close the db connection.
     await client.close();
   }
 }
@@ -64,7 +80,7 @@ async function insert_problem_case_runs(
   // insert/update record
   for (const [target, caseResults] of Object.entries(caseData)) {
     caseResults.new_flaky?.forEach(async (flakyCase) => {
-      const result = await dbClient.execute(SQL_TPL_INSERT, [
+      await dbClient.execute(SQL_TPL_INSERT, [
         repo,
         branch,
         target,
@@ -73,20 +89,18 @@ async function insert_problem_case_runs(
         true,
         0,
       ]);
-      console.log(result); // { affectedRows: 1, lastInsertId: 1 }
     });
 
     for (const [tc, timecost] of Object.entries(caseResults?.long_time || [])) {
-      const result = await dbClient.execute(SQL_TPL_INSERT, [
+      await dbClient.execute(SQL_TPL_INSERT, [
         repo,
         branch,
         target,
         tc,
         timestamp,
         false,
-        timecost,
+        timecost > 0 ? Math.ceil(timecost * 1000) : timecost,
       ]);
-      console.log(result); // { affectedRows: 1, lastInsertId: 1 }
     }
   }
 }
@@ -94,14 +108,14 @@ async function insert_problem_case_runs(
 /**
  * ---------entry----------------
  * ****** CLI args **************
- * --hostname        db hostname
- * --username        db username
- * --password        db password
- * --db              db name
- * --repo            to report repo full name,such as: pingcap/tidb *
- * --branch          to report repo's branch,such as `master`
- * --timestamp       timestamp as event time.
- * --case-data-file  case run data file path.
+ * --hostname       db hostname
+ * --username       db username
+ * --password       db password
+ * --db             db name
+ * --repo           to report repo full name,such as: pingcap/tidb *
+ * --branch         to report repo's branch,such as `master`
+ * --caseDataFile   case run data file path.
  */
-const cliArgs = flags.parse(Deno.args) as _cliParams;
+
+const cliArgs = flags.parse<_cliParams>(Deno.args);
 await main(cliArgs);
