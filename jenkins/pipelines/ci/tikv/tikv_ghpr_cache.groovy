a99b07a612a2e026d8a55ab7b51943ffc93c5670
Purelind <purelind@users.noreply.github.com>
2023-03-20 10:30:40 +0800

    chore: tikv cache image add release-7.0 (#1908)

--- jenkins/pipelines/ci/tikv/tikv_ghpr_cache.groovy
+++ jenkins/pipelines/ci/tikv/tikv_ghpr_cache.groovy
@@ -107,6 +107,7 @@ RUN cd tikv-src \
         }
         
         build_branch("master")
+        build_branch("release-7.0")
         build_branch("release-6.6")
         build_branch("release-6.5")
         build_branch("release-6.1")
