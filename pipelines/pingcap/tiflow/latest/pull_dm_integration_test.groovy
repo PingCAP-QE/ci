ce618f86298f5b25dabf98b2ce6b7ea7e5497033
purelind <purelind@gmail.com>
2023-03-23 16:56:53 +0800

    chore: update group values

--- pipelines/pingcap/tiflow/latest/pull_dm_integration_test.groovy
+++ pipelines/pingcap/tiflow/latest/pull_dm_integration_test.groovy
@@ -122,7 +122,7 @@ pipeline {
                     axis {
                         name 'TEST_GROUP'
                         values 'G00', 'G01', 'G02', 'G03', 'G04', 'G05', 'G06', 'G07', 'G08',
-                            'G09', 'G10', 'G11', 'G12', 'TLS_GROUP'                      
+                            'G09', 'G10', 'G11', 'TLS_GROUP'                      
                     }
                 }
                 agent{
