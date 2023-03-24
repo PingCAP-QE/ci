9f0be87bf7d180902c68a20c76371cf21195c3b9
purelind <purelind@gmail.com>
2023-03-24 14:52:16 +0800

    feat: tiflow ci refactor to prow style

--- jobs/pingcap/tiflow/latest/pull_dm_compatibility_test.groovy
+++ jobs/pingcap/tiflow/latest/pull_dm_compatibility_test.groovy
@@ -5,65 +5,13 @@ pipelineJob('pingcap/tiflow/pull_dm_compatibility_test') {
         daysToKeep(30)
     }
     parameters {
-        stringParam("ghprbActualCommit")
-        stringParam("ghprbPullId")
-        stringParam("ghprbTargetBranch")
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
     }
     properties {
-        buildFailureAnalyzer(false) // disable failure analyze
         githubProjectUrl("https://github.com/pingcap/tiflow")
-        pipelineTriggers {
-            triggers {
-                ghprbTrigger {
-                    cron('H/5 * * * *')
-                    gitHubAuthId('') // using the default only one.
-
-                    triggerPhrase('.*/(debug-dm-compatibility-test.*)')
-                    onlyTriggerPhrase(true)
-                    skipBuildPhrase(".*skip-ci.*")
-                    buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
-                    whitelist('')
-                    orgslist('')
-                    whiteListTargetBranches {
-                        ghprbBranch { branch('master') }
-                    }
-                    // ignore when only those file changed.(
-                    //   multi line regex
-                    // excludedRegions('.*\\.md')
-                    excludedRegions('') // current the context is required in github branch protection.
-
-                    blackListLabels("") // list of GitHub labels for which the build should not be triggered.
-                    whiteListLabels("") // list of GitHub labels for which the build should only be triggered.
-                    adminlist("")
-                    blackListCommitAuthor("")
-                    includedRegions("")
-                    commentFilePath("")
-
-                    allowMembersOfWhitelistedOrgsAsAdmin(true)
-                    permitAll(true)
-                    useGitHubHooks(true)
-                    displayBuildErrorsOnDownstreamBuilds(false)
-                    autoCloseFailedPullRequests(false)
-
-                    // useless, but can not delete.
-                    commitStatusContext("--none--")
-                    msgSuccess("--none--")
-                    msgFailure("--none--")
-
-                    extensions {
-                        ghprbCancelBuildsOnUpdate { overrideGlobal(true) }
-                        ghprbSimpleStatus {
-                            commitStatusContext("debug-ci/dm-compatibility-test")
-                            statusUrl('${RUN_DISPLAY_URL}')
-                            startedStatus("")
-                            triggeredStatus("")
-                            addTestResults(false)
-                            showMatrixStatus(false)
-                        }
-                    }
-                }
-            }
-        }
     }
  
     definition {
