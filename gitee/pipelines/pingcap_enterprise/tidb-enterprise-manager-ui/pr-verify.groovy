76dd1d95708a8915d7733cfabfe185c94f061c53
wuhuizuo <wuhuizuo@126.com>
2023-04-16 23:03:33 +0800

    feat(gitee/pipelines/pingcap_enterprise):  add notify when started.
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- gitee/pipelines/pingcap_enterprise/tidb-enterprise-manager-ui/pr-verify.groovy
+++ gitee/pipelines/pingcap_enterprise/tidb-enterprise-manager-ui/pr-verify.groovy
@@ -20,6 +20,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
+                addGiteeMRComment(comment: "-  :point_right:  ${JOB_NAME} [started](${BUILD_URL})")
                 dir(REFS.repo) {
                     script {
                         cache(path: "./", filter: '**/*', key: prow.getCacheKey('gitee', REFS), restoreKeys: prow.getRestoreKeys('gitee', REFS)) {
@@ -64,7 +65,7 @@ pipeline {
     }
     post {
         always {
-            addGiteeMRComment(comment: "- ${JOB_NAME} `[${currentBuild.result}](${BUILD_URL})`")
+            addGiteeMRComment(comment: "- ${JOB_NAME} [${currentBuild.result}](${BUILD_URL})")
         }
     }
 }
