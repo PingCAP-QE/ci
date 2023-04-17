fc233fc87158663f1f1c9ef4e35b48190fd4963c
wuhuizuo <wuhuizuo@126.com>
2023-04-17 17:48:24 +0800

    fix(libraries/tipipeline/vars):  fix git functions
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- libraries/tipipeline/vars/git.groovy
+++ libraries/tipipeline/vars/git.groovy
@@ -3,7 +3,7 @@ def setSshKey(String credentialsId, String gitSshHost = 'github.com') {
     withCredentials([sshUserPrivateKey(credentialsId: credentialsId, keyFileVariable: 'SSH_KEY')]) {
         sh label: 'Set git ssh key', script: """
             [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
-            cp "\$SSH_KEY" > ~/.ssh/id_rsa
+            cp "\$SSH_KEY" ~/.ssh/id_rsa
             chmod 400 ~/.ssh/id_rsa
             ssh-keyscan -t rsa,dsa ${gitSshHost} >> ~/.ssh/known_hosts            
         """
