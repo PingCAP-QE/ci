500291106d465c4e7cd3fd289f136bf0a8975c36
wuhuizuo <wuhuizuo@126.com>
2023-04-17 17:21:18 +0800

    fix(gitee/pipelines/pingcap_enterprise):  add debug steps for checkout ssh (#2002)
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- libraries/tipipeline/vars/git.groovy
+++ libraries/tipipeline/vars/git.groovy
@@ -5,7 +5,7 @@ def setSshKey(String credentialsId, String gitHost = 'github.com') {
             [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
             echo "\$SSH_KEY" > ~/.ssh/id_rsa
             chmod 400 ~/.ssh/id_rsa
-            ssh-keyscan ${gitHost} >> ~/.ssh/known_hosts            
+            ssh-keyscan -t rsa,dsa ${gitSshHost} >> ~/.ssh/known_hosts            
         """
     }
 }
