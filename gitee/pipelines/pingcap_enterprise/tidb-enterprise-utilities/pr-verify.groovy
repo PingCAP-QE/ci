7780ff4757196c24faf36898af921ec61f74b58b
wuhuizuo <wuhuizuo@126.com>
2023-04-18 15:58:18 +0800

    feat(gitee/pipelines/pingcap_enterprise/tidb-enterprise-utilities): add mysql tests
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- gitee/pipelines/pingcap_enterprise/tidb-enterprise-utilities/pr-verify.groovy
+++ gitee/pipelines/pingcap_enterprise/tidb-enterprise-utilities/pr-verify.groovy
@@ -4,6 +4,7 @@
 @Library('tipipeline') _
 
 final GIT_CREDENTIALS_ID = 'gitee-bot-ssh'
+final GITHUB_CREDENTIALS_ID = 'github-bot-ssh'
 final POD_TEMPLATE_FILE = 'gitee/pipelines/pingcap_enterprise/tidb-enterprise-utilities/pod-pr-verify.yaml'
 final REFS = gitee.composeRefFromEventPayload(env.jsonBody)
 
@@ -26,6 +27,15 @@ pipeline {
                         cache(path: "./", filter: '**/*', key: prow.getCacheKey('gitee', REFS), restoreKeys: prow.getRestoreKeys('gitee', REFS)) {
                             retry(2) {
                                 prow.checkoutPrivateRefs(REFS, GIT_CREDENTIALS_ID, timeout = 5, gitSshHost = 'gitee.com')
+                            }
+                        }
+                    }
+                }
+                dir("tidb-test") {
+                    cache(path: "./", filter: '**/*', key: "git/pingcap/tidb-test/rev-${REFS.pulls[0].sha}", restoreKeys: ['git/pingcap/tidb-test/rev-']) {
+                        retry(2) {
+                            script {
+                                component.checkout('git@github.com:pingcap/tidb-test.git', 'tidb-test', REFS.base_ref, REFS.pulls[0].title, GITHUB_CREDENTIALS_ID)
                             }
                         }
                     }
@@ -68,7 +78,7 @@ pipeline {
             steps {
                 container('golang') {
                     dir(REFS.repo) {
-                        sh script: './build_tidb.sh'
+                        sh script: './build_ci_tidb.sh'
                     }
                 }
             }
@@ -76,6 +86,46 @@ pipeline {
                 success {
                     dir(REFS.repo) {
                         archiveArtifacts(artifacts: 'bin/*', fingerprint: true, allowEmptyArchive: true)
+                    }
+                }
+            }
+        }
+        stage("Integration-Test") {
+            stages {
+                stage('MySQL Tests') {
+                    matrix {
+                        axes {
+                            axis {
+                                name 'PART'
+                                values '1', '2', '3', '4'
+                            }
+                        }
+                        stages {
+                            stage("Test") {
+                                options {
+                                    lock('mysql-test')
+                                    timeout(time: 30, unit: 'MINUTES')
+                                }
+                                steps {
+                                    container('golang') {
+                                        dir(REFS.repo) {
+                                            sh label: 'tidb-server', script: 'ls bin/tidb-server && chmod +x bin/tidb-server'
+                                        }
+                                        dir('tidb-test/mysql_test') {
+                                            sh label: "part ${PART}", script: "TIDB_SERVER_PATH=${WORKSPACE}/${REFS.repo}/bin/tidb-server ./test.sh -backlist=1 -part=${PART}"
+                                        }
+                                    }
+                                }
+                                post {
+                                    always {
+                                        junit(testResults: "**/result.xml")
+                                    }
+                                    failure {
+                                        archiveArtifacts(artifacts: 'mysql-test.out*', allowEmptyArchive: true)
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
