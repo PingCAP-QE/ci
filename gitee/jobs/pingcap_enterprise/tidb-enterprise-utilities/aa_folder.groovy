15179d4c8140bf6bdef514d2271f01b664519d6b
wuhuizuo <wuhuizuo@126.com>
2023-04-21 17:05:34 +0800

    feat(gitee): move out the jobs and pipeline files
    It was stored in other git repo at gitee.com.
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- gitee/jobs/pingcap_enterprise/tidb-enterprise-utilities/aa_folder.groovy
+++
@@ -1,3 +0,0 @@
-folder('pingcap_enterprise/tidb-enterprise-utilities') {
-    description("Folder for pipelines of pingcap_enterprise/tidb-enterprise-utilities repo")
-}
--- gitee/jobs/pingcap_enterprise/tidb-security-advanced/aa_folder.groovy
+++
@@ -1,3 +0,0 @@
-folder('pingcap_enterprise/tidb-security-advanced') {
-    description("Folder for pipelines of pingcap_enterprise/tidb-security-advanced repo")
-}
