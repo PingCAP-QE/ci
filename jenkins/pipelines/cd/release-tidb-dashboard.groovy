bf47905d47c01a3d64ae776f28faaea6418fb0fb
lijie <lijie0123@users.noreply.github.com>
2023-04-23 15:08:29 +0800

    chore: update staging tiup address

--- jenkins/pipelines/cd/release-tidb-dashboard.groovy
+++ jenkins/pipelines/cd/release-tidb-dashboard.groovy
@@ -278,7 +278,7 @@ pipeline {
                     defaultContainer 'tiup'
                 }
             }
-            environment {TIUP_MIRRORS = 'http://172.16.5.139:8988'; TIUPKEY_JSON = credentials('tiup-prod-key') }
+            environment {TIUP_MIRRORS = 'http://tiup.pingcap.net:8988'; TIUPKEY_JSON = credentials('tiup-prod-key') }
             steps{
                 sh 'set +x;curl https://tiup-mirrors.pingcap.com/root.json -o /root/.tiup/bin/root.json; mkdir -p /root/.tiup/keys; cp $TIUPKEY_JSON  /root/.tiup/keys/private.json'
                 sh """
