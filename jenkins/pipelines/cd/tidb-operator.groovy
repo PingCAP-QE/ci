41489ebfafed11fd2d9d4765c623ba0333cbb764
csuzhangxc <csuzhangxc@gmail.com>
2023-05-09 06:38:27 +0000

    fix

--- jenkins/pipelines/cd/tidb-operator.groovy
+++ jenkins/pipelines/cd/tidb-operator.groovy
@@ -372,7 +372,7 @@ pipeline {
                         }
                         stage("sync to registry br-federation") {
                             when { expression { BrFederation } }
-                            def component="br-federation-manager"
+                            environment { component="br-federation-manager" }
                             stages {
                                 stage("harbor") {
                                     environment { HUB = credentials('harbor-pingcap') }
