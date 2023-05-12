a0268b718955dc74aa2a9a8b98eef42fd20cc2d9
wuhuizuo <wuhuizuo@126.com>
2023-05-12 17:50:46 +0800

    refactor(pipelines/pingcap/tidb/release-7.1): refactor build and UT pipelines to include new extensions
    Ref: #2056
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- pipelines/pingcap/tidb/release-7.1/ghpr_build.groovy
+++ pipelines/pingcap/tidb/release-7.1/ghpr_build.groovy
@@ -42,107 +42,61 @@ pipeline {
         stage('Checkout') {
             // FIXME(wuhuizuo): catch AbortException and set the job abort status
             // REF: https://github.com/jenkinsci/git-plugin/blob/master/src/main/java/hudson/plugins/git/GitSCM.java#L1161
-            parallel {   
-                stage('tidb') {
-                    steps {
-                        dir('tidb') {
-                            cache(path: "./", filter: '**/*', key: "git/pingcap/tidb/rev-${REFS.pulls[0].sha}", restoreKeys: ['git/pingcap/tidb/rev-']) {
-                                retry(2) {
-                                    script {
-                                        prow.checkoutRefs(REFS)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                stage("enterprise-plugin") {
-                    steps {
-                        dir("enterprise-plugin") {
-                            cache(path: "./", filter: '**/*', key: "git/pingcap/enterprise-plugin/rev-${REFS.pulls[0].sha}", restoreKeys: ['git/pingcap/enterprise-plugin/rev-']) {
-                                retry(2) {
-                                    script {
-                                        component.checkout('git@github.com:pingcap/enterprise-plugin.git', 'plugin', REFS.base_ref, REFS.pulls[0].title, GIT_CREDENTIALS_ID)
-                                    }
-                                }
+            steps {
+                dir('tidb') {
+                    cache(path: "./", filter: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
+                        script {
+                            git.setSshKey(GIT_CREDENTIALS_ID)
+                            retry(2) {
+                                prow.checkoutRefs(REFS, timeout = 5, credentialsId = '', gitBaseUrl = 'https://github.com', withSubmodule=true)
                             }
                         }
                     }
                 }
             }
         }
-        stage("Build tidb-server and plugin"){
-            parallel {
-                stage("Build tidb-server") {
-                    stages {
-                        stage("Build"){
-                            steps {
-                                dir("tidb") {                                     
-                                    sh "make bazel_build"
-                                }
-                            }
-                            post {       
-                                // TODO: statics and report logic should not put in pipelines.
-                                // Instead should only send a cloud event to a external service.
-                                always {
-                                    dir("tidb") {
-                                        archiveArtifacts(
-                                            artifacts: 'importer.log,tidb-server-check.log',
-                                            allowEmptyArchive: true,
-                                        )
-                                    }            
-                                }
-                            }
-                        }
-                        stage("Upload") {
-                            options {
-                                timeout(time: 5, unit: 'MINUTES')
-                            }
-                            steps {
-                                dir("tidb") {
-                                    sh label: "create tidb-server tarball", script: """
-                                        rm -rf .git
-                                        tar czvf tidb-server.tar.gz ./*
-                                        echo "pr/${REFS.pulls[0].sha}" > sha1
-                                        echo "done" > done
-                                        """
-                                    sh label: 'upload to tidb dir', script: """
-                                        filepath="builds/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].sha}/centos7/tidb-server.tar.gz"
-                                        donepath="builds/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].sha}/centos7/done"
-                                        refspath="refs/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].number}/sha1"
-                                        curl -F \${filepath}=@tidb-server.tar.gz \${FILE_SERVER_URL}/upload
-                                        curl -F \${donepath}=@done \${FILE_SERVER_URL}/upload
-                                        curl -F \${refspath}=@sha1 \${FILE_SERVER_URL}/upload
-                                        """
-                                    sh label: 'upload to tidb-checker dir', script: """
-                                        filepath="builds/pingcap/tidb-check/pr/${REFS.pulls[0].sha}/centos7/tidb-server.tar.gz"
-                                        donepath="builds/pingcap/tidb-check/pr/${REFS.pulls[0].sha}/centos7/done"
-                                        curl -F \${filepath}=@tidb-server.tar.gz \${FILE_SERVER_URL}/upload
-                                        curl -F \${donepath}=@done \${FILE_SERVER_URL}/upload                                    
-                                        """
-                                }
-                            }
-                        }
+        stage("Build tidb-server community edition"){   
+            steps {
+                dir("tidb") {                                     
+                    sh "make bazel_build"
+                }
+            }
+            post {       
+                success {
+                    dir('tidb') {
+                        sh label: "create tidb-server tarball", script: """
+                            rm -rf .git
+                            tar czvf tidb-server.tar.gz ./*
+                            echo "pr/${REFS.pulls[0].sha}" > sha1
+                            echo "done" > done
+                            """
+                        sh label: 'upload to tidb dir', script: """
+                            filepath="builds/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].sha}/centos7/tidb-server.tar.gz"
+                            donepath="builds/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].sha}/centos7/done"
+                            refspath="refs/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].number}/sha1"
+                            curl -F \${filepath}=@tidb-server.tar.gz \${FILE_SERVER_URL}/upload
+                            curl -F \${donepath}=@done \${FILE_SERVER_URL}/upload
+                            curl -F \${refspath}=@sha1 \${FILE_SERVER_URL}/upload
+                            """
+                        sh label: 'upload to tidb-checker dir', script: """
+                            filepath="builds/pingcap/tidb-check/pr/${REFS.pulls[0].sha}/centos7/tidb-server.tar.gz"
+                            donepath="builds/pingcap/tidb-check/pr/${REFS.pulls[0].sha}/centos7/done"
+                            curl -F \${filepath}=@tidb-server.tar.gz \${FILE_SERVER_URL}/upload
+                            curl -F \${donepath}=@done \${FILE_SERVER_URL}/upload                                    
+                            """
                     }
                 }
-                stage("Build plugins") {
-                    steps {
-                        timeout(time: 15, unit: 'MINUTES') {
-                            sh label: 'build pluginpkg tool', script: 'cd tidb/cmd/pluginpkg && go build'
-                        }
-                        dir('enterprise-plugin/whitelist') {
-                            sh label: 'build plugin whitelist', script: '''
-                                GO111MODULE=on go mod tidy
-                                ../../tidb/cmd/pluginpkg/pluginpkg -pkg-dir . -out-dir .
-                                '''
-                        }
-                        dir('enterprise-plugin/audit') {
-                            sh label: 'build plugin: audit', script: '''
-                                GO111MODULE=on go mod tidy
-                                ../../tidb/cmd/pluginpkg/pluginpkg -pkg-dir . -out-dir .
-                                '''
-                        }
+                always {
+                    dir("tidb") {
+                        archiveArtifacts(artifacts: 'importer.log,tidb-server-check.log', allowEmptyArchive: true)
                     }
+                }
+            }
+        }
+        stage("Build tidb-server enterprise edition") {
+            steps {
+                dir("tidb") {
+                    sh "make enterprise-prepare enterprise-server-build && ./bin/tidb-server -V"
                 }
             }
         }
