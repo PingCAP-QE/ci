dcef973b3c6dda8869d6b288cfcc55568b766496
Purelind <purelind@users.noreply.github.com>
2023-05-13 21:40:41 +0800

    fix: tmp fix cache contains untraced files (#2067)
    * fix: tmp fix cache contains untraced files
    
    * chore: add debug file

--- pipelines/pingcap/tidb/release-6.5/ghpr_build.groovy
+++ pipelines/pingcap/tidb/release-6.5/ghpr_build.groovy
@@ -46,7 +46,7 @@ pipeline {
                 stage('tidb') {
                     steps {
                         dir('tidb') {
-                            cache(path: "./", filter: '**/*', key: "git/pingcap/tidb/rev-${REFS.pulls[0].sha}", restoreKeys: ['git/pingcap/tidb/rev-']) {
+                            cache(path: "./", filter: '**/*', key: "git/pingcap/tidb/rev-update-${REFS.pulls[0].sha}") {
                                 retry(2) {
                                     script {
                                         prow.checkoutRefs(REFS)
@@ -77,7 +77,12 @@ pipeline {
                     stages {
                         stage("Build"){
                             steps {
-                                dir("tidb") {                                     
+                                dir("tidb") {
+                                    sh """
+                                    git status
+                                    git log -5
+                                    ls -alh ./extension
+                                    """                                   
                                     sh "make bazel_build"
                                 }
                             }
