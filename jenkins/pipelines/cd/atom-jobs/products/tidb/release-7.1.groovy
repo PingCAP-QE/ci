7503154218f09760321d392f073bda3f5fd40621
lijie <lijie@pingcap.com>
2023-05-16 19:54:05 +0800

    feat: support enterprise plugin.so
    Signed-off-by: lijie <lijie@pingcap.com>

--- jenkins/pipelines/cd/atom-jobs/products/tidb/release-7.1.groovy
+++ jenkins/pipelines/cd/atom-jobs/products/tidb/release-7.1.groovy
@@ -9,19 +9,14 @@ def getBinDownloadURL={
     return "${FILE_SERVER_URL}/download/${getBinPath()}"
 }
 
+def getPluginPath={
+    return "builds/devbuild/tidb/optimization/${params.Version}/${params.GitHash}/tidb-plugin-${OS}-${ARCH}.tar.gz"
+}
+def getPluginDownloadURL={
+    return "${FILE_SERVER_URL}/download/${getPluginPath()}"
+}
 
-def build = {
-        def skip = false
-        stage("check"){
-            result = sh(script: "curl -I ${getBinDownloadURL()}|grep \"200 OK\"", returnStatus: true)
-            if (result == 0) {
-                skip = true
-            }
-        }
-        if (skip) {
-            echo "Binary exists, skip build"
-            return
-        }
+def buildTidb = {
         stage("checkout"){
             sh """
             if curl --output /dev/null --silent --head --fail ${FILE_SERVER_URL}/download/cicd/daily-cache-code/src-tidb.tar.gz; then
@@ -60,9 +55,9 @@ def build = {
             sh BUILD_CMD
             sh """
                 rm -rf output
-                mkdir -p output/bin
-                cp bin/* output/bin/ 
-                tar -czvf tidb.tar.gz -C output/bin/ . 
+                mkdir -p output
+                cp bin/* output/
+                tar -czvf tidb.tar.gz -C output/ . 
                 sha256sum tidb.tar.gz | cut -d ' ' -f 1 >tidb.tar.gz.sha256
             """
         }
@@ -73,23 +68,110 @@ def build = {
         }
 }
 
+def buildEnterprisePlugin = {
+    stage("build enterprise plugin"){
+        checkout changelog: false, poll: true,
+                        scm: [$class: 'GitSCM', branches: [[name: "${PluginGitHash}"]], doGenerateSubmoduleConfigurations: false,
+                            extensions: [[$class: 'CheckoutOption', timeout: 30],
+                                        [$class: 'CloneOption', timeout: 60],
+                                        [$class: 'PruneStaleBranch'],
+                                        [$class: 'SubmoduleOption', timeout: 30, disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, trackingSubmodules: false, reference: ''],
+                                        [$class: 'CleanBeforeCheckout']], submoduleCfg: [],
+                            userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh',
+                                                refspec      : specRef,
+                                                url          : "git@github.com:pingcap/enterprise-plugin.git"]]]
+        sh """
+        go version
+        cd ../tidb/cmd/pluginpkg
+        go build 
+        cd ../../../enterprise-plugin
+        cd whitelist
+        go mod tidy
+        cd ..
+        ../tidb/cmd/pluginpkg/pluginpkg -pkg-dir whitelist -out-dir whitelist
+        md5sum whitelist/whitelist-1.so > whitelist/whitelist-1.so.md5
+        cd audit
+        go mod tidy
+        cd ..
+        ../tidb/cmd/pluginpkg/pluginpkg -pkg-dir audit -out-dir audit
+        md5sum audit/audit-1.so > audit/audit-1.so.md5
+        """ 
+        sh """
+        rm -rf output
+        mkdir output
+        cp whitelist/whitelist-1.so.md5 output
+        cp whitelist/whitelist-1.so output
+        cp audit/audit-1.so.md5 output
+        cp audit/audit-1.so output
+        tar -czvf enterpise-plugin.tar.gz -C output/ . 
+        sha256sum  enterpise-plugin.tar.gz | cut -d ' ' -f 1 > enterpise-plugin.tar.gz.sha256
+        """
+        sh "curl -F $PluginBinPath=@enterprise-plugin.tar.gz ${FILE_SERVER_URL}/upload"
+        sh "curl -F ${PluginBinPath}.sha256=@enterprise-plugin.tar.gz.sha256 ${FILE_SERVER_URL}/upload"
+    }
+}
+
+
+def buildBin={
+    stage("check"){
+        def binExist = sh(script: "curl -I ${getBinDownloadURL()}|grep \"200 OK\"", returnStatus: true)
+        if (params.Edition == "enterprise") {
+            def pluginExist = sh(script: "curl -I ${getPluginDownloadURL()}|grep \"200 OK\"", returnStatus: true)
+            if (binExist  == 0 && pluginExist == 0) {
+                skip = true
+            }
+        }else{
+            if (binExist  == 0) {
+                skip = true
+            }
+        }
+    }
+    if (skip) {
+        echo "Binary exists, skip build"
+        return
+    }
+    dir('tidb'){
+        buildTidb()
+    }
+    if (params.Edition == "enterprise") {
+        dir('enterprise-plugin'){
+            buildEnterprisePlugin()
+        }
+    }
+}
+
 
 def buildDocker={
     sh 'printenv HUB_PSW | docker login -u $HUB_USR --password-stdin hub.pingcap.net'
-    sh """
-        curl --fail --retry 3 -o Dockerfile https://raw.githubusercontent.com/PingCAP-QE/artifacts/main/dockerfiles/tidb.Dockerfile
-        curl --fail --retry 3 -o tidb.tar.gz ${getBinDownloadURL()}
-        tar -xzvf tidb.tar.gz
-        rm -f tidb.tar.gz
-        docker build -t ${DockerImage}-$ARCH .
-        docker push ${DockerImage}-$ARCH
-    """ 
+    if (params.Edition == "enterprise"){
+        sh """
+            curl --fail --retry 3 -o Dockerfile https://raw.githubusercontent.com/PingCAP-QE/artifacts/main/dockerfiles/tidb-enterprise.Dockerfile
+            curl --fail --retry 3 -o tidb.tar.gz ${getBinDownloadURL()}
+            curl --fail --retry 3 -o plugin.tar.gz ${getPluginDownloadURL()}
+            tar -xzvf tidb.tar.gz
+            tar -xzvf plugin.tar.gz
+            rm -f tidb.tar.gz
+            rm -f plugin.tar.gz
+            docker build -t ${DockerImage}-$ARCH .
+            docker push ${DockerImage}-$ARCH
+        """ 
+    }else{
+        sh """
+            curl --fail --retry 3 -o Dockerfile https://raw.githubusercontent.com/PingCAP-QE/artifacts/main/dockerfiles/tidb.Dockerfile
+            curl --fail --retry 3 -o tidb.tar.gz ${getBinDownloadURL()}
+            tar -xzvf tidb.tar.gz
+            rm -f tidb.tar.gz
+            docker build -t ${DockerImage}-$ARCH .
+            docker push ${DockerImage}-$ARCH
+        """ 
+    }
 }
 
 
 pipeline{
     parameters {
         string(name: 'GitHash', description: 'the git tag or commit or branch or pull/id of repo')
+        string(name: 'PluginGitHash', description: 'plugin git hash')
         string(name: 'Version', description: 'important, the Version for cli --Version and profile choosing, eg. v6.5.0')
         choice(name: 'Edition', choices : ["community", "enterprise"])
         string(name: 'BuildCmd', description: 'the build command', defaultValue: '')
@@ -122,7 +204,6 @@ pipeline{
                 }
                 steps{
                     script{ container('golang'){
-                        build()
                     }}
                 }
             }
@@ -153,11 +234,14 @@ spec:
                 }
                 steps{
                     script{ 
-                        build()
+                        dir('tidb'){
+                            buildBin()
+                        }
                     }
                 }
             }
             stage("darwin/amd64"){
+                when { beforeAgent true; equals expected: "community", actual: params.Edition }
                 agent { node { label 'darwin && amd64' } }
                 environment {
                     OS = "darwin"
@@ -166,11 +250,12 @@ spec:
                 }
                 steps{
                     script{
-                        build()
+                        buildBin()
                     }
                 }
             }
             stage("darwin/arm64"){
+                when { beforeAgent true; equals expected: "community", actual: params.Edition }
                 agent { node { label 'darwin && arm64' } }
                 environment {
                     OS = "darwin"
@@ -179,14 +264,8 @@ spec:
                 }
                 steps{
                     script{
-                        build()
+                        buildBin()
                     }
-                }
-            }
-            stage("enterprise-plugin"){
-                when { equals expected: "enterprise", actual: params.Edition }
-                steps{
-                    echo "enterprise plugin build"
                 }
             }
         }
