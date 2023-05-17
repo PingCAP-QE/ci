ef91be9a96a38eee45b5233d4ff729c0618d0d33
purelind <purelind@gmail.com>
2023-05-17 18:18:01 +0800

    chore: revert idleMinutes to 0

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_kafka_integration_test.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_kafka_integration_test.groovy
@@ -164,7 +164,7 @@ def run_with_pod(Closure body) {
     podTemplate(label: label,
             cloud: cloud,
             namespace: namespace,
-            idleMinutes: 120,
+            idleMinutes: 0,
             containers: [
                     containerTemplate(
                         name: 'golang', alwaysPullImage: true,
