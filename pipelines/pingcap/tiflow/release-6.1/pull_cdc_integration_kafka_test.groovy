a55ae422ee68544166bd86da4640d78e412e827e
purelind <purelind@gmail.com>
2023-06-07 15:12:47 +0800

    debug: temp disable failfast

--- pipelines/pingcap/tiflow/release-6.1/pull_cdc_integration_kafka_test.groovy
+++ pipelines/pingcap/tiflow/release-6.1/pull_cdc_integration_kafka_test.groovy
@@ -24,7 +24,7 @@ pipeline {
     }
     options {
         timeout(time: 65, unit: 'MINUTES')
-        parallelsAlwaysFailFast()
+        // parallelsAlwaysFailFast()
     }
     stages {
         stage('Debug info') {
