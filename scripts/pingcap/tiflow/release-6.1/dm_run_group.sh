ad5921a05e83c90197690e62f78f41becfbbe790
Purelind <purelind@users.noreply.github.com>
2023-06-08 10:49:41 +0800

    fix: set path to use correct gh-ost version (#2134)

--- scripts/pingcap/tiflow/release-6.1/dm_run_group.sh
+++ scripts/pingcap/tiflow/release-6.1/dm_run_group.sh
@@ -25,7 +25,9 @@ groups=(
 	["G09"]="import_v10x sharding2 ha"
 	["G10"]="start_task print_status http_apis new_relay all_mode"
 	# `others others_2 others_3` tests of old pipeline
-	["G11"]="full_mode adjust_gtid async_checkpoint_flush case_sensitive checkpoint_transaction check_task downstream_diff_index downstream_more_column drop_column_with_index duplicate_event expression_filter extend_column fake_rotate_event gbk gtid ha_cases http_proxies lightning_mode metrics load_task new_collation_off only_dml openapi s3_dumpling_lighting sequence_sharding_optimistic sequence_sharding_removemeta shardddl_optimistic slow_relay_writer sql_mode sync_collation tracker_ignored_ddl validator_basic validator_checkpoint"
+	["G11"]="others"
+	["G12"]="others_2"
+	["G13"]="others_3"
 	["TLS_GROUP"]="tls"
 )
 
