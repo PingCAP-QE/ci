2665a2ee376607c7ca2b6c927b924f6fd3e7c139
lijie <lijie@pingcap.com>
2023-06-19 18:27:26 +0800

    chore: change nightly version to be v7.3.0-alpha
    Signed-off-by: lijie <lijie@pingcap.com>

--- jenkins/pipelines/cd/nightly/release-all-images-by-branch.groovy
+++ jenkins/pipelines/cd/nightly/release-all-images-by-branch.groovy
@@ -84,7 +84,7 @@ HARBOR_PROJECT_PREFIX = "hub.pingcap.net/qa"
 
 
 // for master branch: use default local tag: v6.1.0-nightly
-RELEASE_TAG = "v7.2.0-alpha"
+RELEASE_TAG = "v7.3.0-alpha"
 if (GIT_BRANCH.startsWith("release-")) {
     RELEASE_TAG = "v" + trimPrefix(GIT_BRANCH) + ".0-nightly"
 }
