2da882f13e2bba8d3fa9a435f63cd48937ccfdec
Purelind <purelind@users.noreply.github.com>
2023-06-20 11:50:41 +0800

    fix: add schema_registry binary (#2182)
    * fix: add schema_registry binary
    
    * fix: correct tar file name
    
    * fix: fix shell env escape error
    
    * format: remove lines

--- jenkins/pipelines/ci/ticdc/cdc_ghpr_kafka_integration_test.groovy
+++ jenkins/pipelines/ci/ticdc/cdc_ghpr_kafka_integration_test.groovy
@@ -20,11 +20,11 @@ if (ghprbPullId == null || ghprbPullId == "") {
 GO_VERSION = "go1.20"
 POD_GO_IMAGE = ""
 GO_IMAGE_MAP = [
-    "go1.13": "hub.pingcap.net/wulifu/golang-tini:1.13",
-    "go1.16": "hub.pingcap.net/wulifu/golang-tini:1.16",
-    "go1.18": "hub.pingcap.net/wulifu/golang-tini:1.18",
-    "go1.19": "hub.pingcap.net/wulifu/golang-tini:1.19",
-    "go1.20": "hub.pingcap.net/wulifu/golang-tini:1.20",
+    "go1.13": "hub.pingcap.net/jenkins/golang-tini:1.13",
+    "go1.16": "hub.pingcap.net/jenkins/golang-tini:1.16",
+    "go1.18": "hub.pingcap.net/jenkins/golang-tini:1.18",
+    "go1.19": "hub.pingcap.net/jenkins/golang-tini:1.19",
+    "go1.20": "hub.pingcap.net/jenkins/golang-tini:1.20",
 ]
 POD_LABEL_MAP = [
     "go1.13": "${JOB_NAME}-go1130-build-${BUILD_NUMBER}",
@@ -488,11 +488,3 @@ if (params.containsKey("triggered_by_upstream_ci")  && params.get("triggered_by_
         ]
     }
 }
-
-
-
-
-
-
-
-
