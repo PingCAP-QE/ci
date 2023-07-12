a226b748bbdb7d2a86ff5b044077d91befa00239
Purelind <purelind@users.noreply.github.com>
2023-07-12 10:56:13 +0800

    feat: add presubmit for some integration-test (#2254)
    * feat: add new ci podtemplate
    
    * feat: add new presubmit job dsl
    
    * feat: add pull-tiflash-test
    
    * feat: add pull-common-test
    
    * feat: add pull-it-ddl-test
    
    * feat: add new pull-integration-common-test
    
    * feat: add new pull-sqllogic-test
    
    * feat: add presubmit trigger, skip report now

--- pipelines/pingcap/tidb/latest/pull_integration_jdbc_test.groovy
+++ pipelines/pingcap/tidb/latest/pull_integration_jdbc_test.groovy
@@ -54,7 +54,7 @@ pipeline {
                     cache(path: "./", filter: '**/*', key: "git/pingcap/tidb-test/rev-${REFS.base_sha}", restoreKeys: ['git/pingcap/tidb-test/rev-']) {
                         retry(2) {
                             script {
-                                component.checkout('git@github.com:pingcap/tidb-test.git', 'tidb-test', REFS.base_ref, "", GIT_CREDENTIALS_ID)
+                                component.checkout('git@github.com:pingcap/tidb-test.git', 'tidb-test', REFS.base_ref, REFS.pulls[0].title, GIT_CREDENTIALS_ID)
                             }
                         }
                     }
