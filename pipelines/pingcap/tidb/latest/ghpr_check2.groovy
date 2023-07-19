af860942a5b6585c08042940ad289dcb49c35e8d
wuhuizuo <wuhuizuo@126.com>
2023-07-19 16:20:36 +0800

    fix(pipelines/pingcap/tidb): fix coverage data path and refactor unit job
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- pipelines/pingcap/tidb/latest/ghpr_check2.groovy
+++ pipelines/pingcap/tidb/latest/ghpr_check2.groovy
@@ -125,9 +125,8 @@ pipeline {
                             }
                             success {
                                 dir("tidb") {
-                                    sh 'ls -alh test_coverage/ || true'
                                     script {
-                                        prow.uploadCoverageToCodecov(REFS, 'integration', 'test_coverage/coverage.dat')
+                                        prow.uploadCoverageToCodecov(REFS, 'integration', './coverage.dat')
                                     }
                                 }
                             }
