ba12425ee879b59b01cc947438d8dbd09aa2b852
purelind <purelind@gmail.com>
2023-07-20 18:18:28 +0800

    fix: correct folder

--- jobs/pingcap/tiflow/release-7.1.0/aa_folder.groovy
+++ jobs/pingcap/tiflow/release-7.1.0/aa_folder.groovy
@@ -1,3 +1,3 @@
-folder('pingcap/tiflow/release-7.1') {
-    description("Folder for pipelines of pingcap/tiflow repo for v7.1")
+folder('pingcap/tiflow/release-7.1.0') {
+    description("Folder for pipelines of pingcap/tiflow repo for v7.1.0")
 }
