8433b3a2fb7151a131190535e20ef9ee2f918f19
purelind <purelind@gmail.com>
2023-07-26 14:48:42 +0800

    feat: add mysql connector-c test on tidb

---
+++ pipelines/pingcap/tidb/latest/merged_mysql_connector_test.groovy
@@ -0,0 +1,95 @@
+// REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
+// Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
+// should triggerd for master and latest release branches
+@Library('tipipeline') _
+
+final K8S_NAMESPACE = "jenkins-tidb"
+final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
+final POD_TEMPLATE_FILE = 'pipelines/pingcap/tidb/latest/pod-merged_mysql_connector_test.yaml'
+final REFS = readJSON(text: params.JOB_SPEC).refs
+
+pipeline {
+    agent {
+        kubernetes {
+            namespace K8S_NAMESPACE
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer 'golang'
+        }
+    }
+    environment {
+        FILE_SERVER_URL = 'http://fileserver.pingcap.net'
+        GITHUB_TOKEN = credentials('github-bot-token')
+        CI = "1"
+    }
+    options {
+        timeout(time: 40, unit: 'MINUTES')
+        parallelsAlwaysFailFast()
+    }
+    stages {
+        stage('Debug info') {
+            steps {
+                sh label: 'Debug info', script: """
+                    printenv
+                    echo "-------------------------"
+                    go env
+                    echo "-------------------------"
+                    echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
+                """
+                container(name: 'net-tool') {
+                    sh 'dig github.com'
+                }
+            }
+        }
+        stage('Checkout') {
+            options { timeout(time: 10, unit: 'MINUTES') }
+            steps {
+                dir("tidb") {
+                    cache(path: "./", filter: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
+                        retry(2) {
+                            script {
+                                prow.checkoutRefs(REFS)
+                            }
+                        }
+                    }
+                }
+                dir("tidb-test") {
+                    cache(path: "./", filter: '**/*', key: "git/pingcap/tidb-test/rev-${REFS.base_sha}", restoreKeys: ['git/pingcap/tidb-test/rev-']) {
+                        retry(2) {
+                            script {
+                                component.checkoutV2('git@github.com:pingcap/tidb-test.git', 'tidb-test', REFS.base_ref, REFS.pulls[0].title, GIT_CREDENTIALS_ID)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage('Prepare') {
+            steps {
+                dir('tidb') {
+                    sh label: 'tidb-server', script: '[ -f bin/tidb-server ] || make'
+                }
+                dir('tidb-test') {
+                    sh 'touch ws-${BUILD_TAG}'
+                    sh label: 'prepare thirdparty binary', script: """
+                    mkdir -p bin/
+                    cp ../tidb/bin/tidb-server bin/ && chmod +x bin/tidb-server
+                    ls -alh bin/
+                    ./bin/tidb-server -V
+                    """
+                }
+            }
+        }
+        stage('MySQL Connector Tests') {
+            steps {
+                container('mysql-client-test') {
+                    dir('tidb-test') {
+                        sh label: "run test", script: """
+                            #!/usr/bin/env bash
+                            make mysql_client_test WITH_TIPROXY=0
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
