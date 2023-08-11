03b7a700ebf6713386cdd4a39e8ea1728df0b0c0
purelind <purelind@gmail.com>
2023-08-11 17:45:54 +0800

    fix: disable coverage upload

--- pipelines/pingcap/tidb/latest/merged_integration_br_test.groovy
+++ pipelines/pingcap/tidb/latest/merged_integration_br_test.groovy
@@ -130,15 +130,6 @@ pipeline {
                                 """
                                 archiveArtifacts artifacts: "log-${TEST_GROUP}.tar.gz", fingerprint: true 
                             }
-                            success {
-                                dir('tidb'){
-                                    sh label: "upload coverage", script: """
-                                        ls -alh /tmp/group_cover
-                                        gocovmerge /tmp/group_cover/cov.* > coverage.txt
-                                        codecov --rootDir . --flags integration --file coverage.txt --branch origin/${REFS.base_ref} --sha ${REFS.base_sha}
-                                    """
-                                }
-                            }
                         }
                     }
                 }
