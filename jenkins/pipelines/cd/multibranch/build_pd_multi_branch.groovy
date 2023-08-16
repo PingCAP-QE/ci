d415185f0fed7dc5d186dcad4c4ff7888b021ad6
Purelind <purelind@users.noreply.github.com>
2023-08-16 15:05:03 +0800

    feat(build): multi branch build support go1.21 (#2358)
    * feat: use go1.21 for master branch build
    
    * feat: use go1.21

--- jenkins/pipelines/cd/multibranch/build_pd_multi_branch.groovy
+++ jenkins/pipelines/cd/multibranch/build_pd_multi_branch.groovy
@@ -59,6 +59,10 @@ def upload_result_to_db() {
 def selectGoVersion(branchNameOrTag) {
     if (branchNameOrTag.startsWith("v")) {
         println "This is a tag"
+        if (branchNameOrTag >= "v7.4") {
+            println "tag ${branchNameOrTag} use go 1.21"
+            return "go1.21"
+        }
         if (branchNameOrTag >= "v7.0") {
             println "tag ${branchNameOrTag} use go 1.20"
             return "go1.20"
@@ -83,12 +87,16 @@ def selectGoVersion(branchNameOrTag) {
             println "tag ${branchNameOrTag} use go 1.13"
             return "go1.13"
         }
-        println "tag ${branchNameOrTag} use default version go 1.20"
-        return "go1.20"
+        println "tag ${branchNameOrTag} use default version go 1.21"
+        return "go1.21"
     } else { 
         println "this is a branch"
         if (branchNameOrTag == "master") {
-            println("branchNameOrTag: master  use go1.20")
+            println("branchNameOrTag: master  use go1.21")
+            return "go1.21"
+        }
+        if (branchNameOrTag.startsWith("release-") && branchNameOrTag >= "release-7.4") {
+            println("branchNameOrTag: ${branchNameOrTag}  use go1.21")
             return "go1.20"
         }
         if (branchNameOrTag.startsWith("release-") && branchNameOrTag >= "release-7.0") {
@@ -112,15 +120,18 @@ def selectGoVersion(branchNameOrTag) {
             println("branchNameOrTag: ${branchNameOrTag}  use go1.13")
             return "go1.13"
         }
-        println "branchNameOrTag: ${branchNameOrTag}  use default version go1.20"
-        return "go1.20"
+        println "branchNameOrTag: ${branchNameOrTag}  use default version go1.21"
+        return "go1.21"
     }
 }
 
 
-def GO_BUILD_SLAVE = "build_go1200"
+def GO_BUILD_SLAVE = "build_go1210"
 def goVersion = selectGoVersion(env.BRANCH_NAME)
 switch(goVersion) {
+    case "go1.21":
+        GO_BUILD_SLAVE = "build_go1210"
+        break
     case "go1.20":
         GO_BUILD_SLAVE = "build_go1200"
         break
@@ -137,7 +148,7 @@ switch(goVersion) {
         GO_BUILD_SLAVE = "build_go1130"
         break
     default:
-        GO_BUILD_SLAVE = "build_go1200"        
+        GO_BUILD_SLAVE = "build_go1210"        
         break
 }
 println "This build use ${goVersion}"
