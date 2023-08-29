e4eef486db3f171e594f24552fe6b796ca191dd1
djshow832 <zhangming@pingcap.com>
2023-08-29 16:56:08 +0800

    feat(tiproxy): add more tiproxy tests (#2376)
    * add more tiproxy tests
    
    * fix postsubmits
    
    * fix comment
    
    * add to kustomization
    
    * add max_concurrency
    
    * fix cache key

---
+++ pipelines/pingcap/tiproxy/latest/merged_sqllogic_test.groovy
@@ -0,0 +1,167 @@
+// REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
+// Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
+@Library('tipipeline') _
+
+final K8S_NAMESPACE = "jenkins-tidb"
+final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
+final GIT_FULL_REPO_NAME = 'pingcap/tidb-test'
+final POD_TEMPLATE_FILE = 'pipelines/pingcap/tiproxy/latest/pod-merged_sqllogic_test.yaml'
+final REFS = readJSON(text: params.JOB_SPEC).refs
+
+pipeline {
+    agent {
+        kubernetes {
+            namespace K8S_NAMESPACE
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer 'golang'
+        }
+    }
+    environment {
+        FILE_SERVER_URL = 'http://fileserver.pingcap.net'
+    }
+    options {
+        timeout(time: 60, unit: 'MINUTES')
+    }
+    stages {
+        stage('Debug info') {
+            steps {
+                sh label: 'Debug info', script: """
+                    printenv
+                    echo "-------------------------"
+                    go env
+                    echo "-------------------------"
+                    ls -l /dev/null
+                    echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
+                """
+                container(name: 'net-tool') {
+                    sh 'dig github.com'
+                }
+            }
+        }
+        stage('Checkout') {
+            options { timeout(time: 5, unit: 'MINUTES') }
+            steps {
+                dir("tiproxy") {
+                    cache(path: "./", filter: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
+                        retry(2) {
+                            script {
+                                prow.checkoutRefs(REFS)
+                            }
+                        }
+                    }
+                }
+                dir("tidb-test") {
+                    cache(path: "./", filter: '**/*', key: "git/pingcap/tidb-test/rev-${REFS.pulls[0].sha}", restoreKeys: ['git/pingcap/tidb-test/rev-']) {
+                        retry(2) {
+                            script {
+                                component.checkoutV2('git@github.com:pingcap/tidb-test.git', 'tidb-test', "master", REFS.pulls[0].title, GIT_CREDENTIALS_ID)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage('Prepare') {
+            steps {
+                dir('tiproxy') {
+                    sh label: 'tiproxy', script: '[ -f bin/tiproxy ] || make'
+                }
+                dir('tidb-test') {
+                    cache(path: "./", filter: '**/*', key: "ws/${BUILD_TAG}") {
+                        sh "touch ws-${BUILD_TAG}"
+                        sh label: 'prepare thirdparty binary', script: """
+                        chmod +x download_binary.sh
+                        ./download_binary.sh --tidb=master --pd=master --tikv=master
+                        cp ../tiproxy/bin/tiproxy ./bin/
+                        ls -alh bin/
+                        ./bin/tidb-server -V
+                        ./bin/pd-server -V
+                        ./bin/tikv-server -V
+                        ./bin/tiproxy --version
+                        """
+                    }
+                }
+            }
+        }
+        stage('TestsGroup1') {
+            matrix {
+                axes {
+                    axis {
+                        name 'TEST_PATH_STRING'
+                        values 'index/between/1 index/between/10 index/between/100', 'index/between/100 index/between/1000 index/commute/10',
+                            'index/commute/100 index/commute/1000_n1 index/commute/1000_n2',
+                            'index/delete/1 index/delete/10 index/delete/100 index/delete/1000 index/delete/10000',
+                            'random/aggregates_n1 random/aggregates_n2 random/expr' , 'random/expr random/select_n1 random/select_n2',
+                            'select random/groupby'
+                    }
+                }
+                agent{
+                    kubernetes {
+                        namespace K8S_NAMESPACE
+                        yamlFile POD_TEMPLATE_FILE
+                        defaultContainer 'golang'
+                    }
+                }
+                stages {
+                    stage("Test") {
+                        options { timeout(time: 40, unit: 'MINUTES') }
+                        steps {
+                            dir('tidb-test') {
+                                cache(path: "./sqllogic_test", filter: '**/*', key: "ws/${BUILD_TAG}") {
+                                    sh label: "test_path: ${TEST_PATH_STRING}", script: """
+                                        #!/usr/bin/env bash
+                                        path_array=(${TEST_PATH_STRING})
+                                        for path in \${path_array[@]}; do
+                                            echo "test path: \${path}"
+                                            SQLLOGIC_TEST_PATH="/git/sqllogictest/test/\${path}" \
+                                            make deploy-sqllogictest ARGS="-x -c y -s tikv -p ${SQLLOGIC_TEST_PATH}"
+                                        done
+                                    """
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage('TestsGroup2') {
+            matrix {
+                axes {
+                    axis {
+                        name 'TEST_PATH_STRING'
+                        values 'index/in/10 index/in/1000_n1 index/in/1000_n2', 'index/orderby/10 index/orderby/100',
+                           'index/orderby/1000_n1 index/orderby/1000_n2', 'index/orderby_nosort/10 index/orderby_nosort/100',
+                           'index/orderby_nosort/1000_n1 index/orderby_nosort/1000_n2', 'index/random/10 index/random/100 index/random/1000'
+                       }
+                }
+                agent{
+                    kubernetes {
+                        namespace K8S_NAMESPACE
+                        yamlFile POD_TEMPLATE_FILE
+                        defaultContainer 'golang'
+                    }
+                }
+                stages {
+                    stage("Test") {
+                        options { timeout(time: 40, unit: 'MINUTES') }
+                        steps {
+                            dir('tidb-test') {
+                                cache(path: "./sqllogic_test", filter: '**/*', key: "ws/${BUILD_TAG}") {
+                                    sh label: "test_path: ${TEST_PATH_STRING}", script: """
+                                        #!/usr/bin/env bash
+                                        path_array=(${TEST_PATH_STRING})
+                                        for path in \${path_array[@]}; do
+                                            echo "test path: \${path}"
+                                            SQLLOGIC_TEST_PATH="/git/sqllogictest/test/\${path}" \
+                                            make deploy-sqllogictest ARGS="-x -c y -s tikv -p ${SQLLOGIC_TEST_PATH}"
+                                        done
+                                    """
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
