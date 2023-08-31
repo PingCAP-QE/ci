c9500e85ce7ad71584cd4335ee0d63ebc759ff41
purelind <purelind@gmail.com>
2023-08-31 13:57:43 +0800

    chore

--- pipelines/pingcap/tidb/release-6.5/pull_br_integration_test.groovy
+++ pipelines/pingcap/tidb/release-6.5/pull_br_integration_test.groovy
@@ -94,7 +94,7 @@ pipeline {
                 axes {
                     axis {
                         name 'TEST_GROUP'
-                        values 'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6',  'G7', 'G8', 'G9', 'G10', 'G11', 'G12', 'G13', 'G14', 'G15'
+                        values 'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9', 'G10', 'G11', 'G12', 'G13', 'G14', 'G15'
                     }
                 }
                 agent{
