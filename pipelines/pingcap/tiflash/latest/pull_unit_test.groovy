b98fc64ae1347dba1c469f9a8efc02d43723fb13
purelind <purelind@gmail.com>
2023-09-05 14:46:06 +0800

    update prepare tools

--- pipelines/pingcap/tiflash/latest/pull_unit_test.groovy
+++ pipelines/pingcap/tiflash/latest/pull_unit_test.groovy
@@ -8,6 +8,7 @@ final GIT_FULL_REPO_NAME = 'pingcap/tiflash'
 final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
 final POD_TEMPLATE_FILE = 'pipelines/pingcap/tiflash/latest/pod-pull_unit_test.yaml'
 final REFS = readJSON(text: params.JOB_SPEC).refs
+final dependency_dir = "/home/jenkins/agent/dependency"
 
 pipeline {
     agent {
@@ -54,29 +55,84 @@ pipeline {
             }
         }
         stage("Build") {
-            stages("prepare tools") {
-                stage("CCache") {
+            stages("Prepare tools") {
+                parallel {
+                    stage("CCache") {
+                        sh label: "install ccache", script: """
+                            if ! command -v ccache &> /dev/null; then
+                                echo "ccache not found! Installing..."
+                                rpm -Uvh '${dependency_dir}/ccache.x86_64.rpm'
+                            else
+                                echo "ccache is already installed!"
+                            fi
+                        """
+                    }
+                    stage("Cmake") {
+                        sh label: "install cmake3", script: """
+                            if ! command -v cmake &> /dev/null; then
+                                echo "cmake not found! Installing..."
+                                sh ${dependency_dir}/cmake-3.22.3-linux-${arch_id}.sh --prefix=/usr --skip-license --exclude-subdir
+                            else
+                                echo "cmake is already installed!"
+                            fi
+                        """
+                    }
+                    stage("Clang-Format") {
+                        sh label: "install clang-format", script: """
+                            if ! command -v clang-format &> /dev/null; then
+                                echo "clang-format not found! Installing..."
+                                cp '${dependency_dir}/clang-format-12' '/usr/local/bin/clang-format'
+                                chmod +x '/usr/local/bin/clang-format'
+                            else
+                                echo "clang-format is already installed!"
+                            fi
+                        """
+                    }
+                    stage("Clang-Format-15") {
+                        sh label: "install clang-format-15", script: """
+                            if ! command -v clang-format-15 &> /dev/null; then
+                                echo "clang-format-15 not found! Installing..."
+                                cp '${dependency_dir}/clang-format-15' '/usr/local/bin/clang-format-15'
+                                chmod +x '/usr/local/bin/clang-format-15'
+                            else
+                                echo "clang-format-15 is already installed!"
+                            fi
+                        """
 
-                }
-                stage("Cmake") {
-
-                }
-                stage("Clang-Format") {
-
-                }
-                stage("Clang-Format-15") {
-
-                }
-                stage("Clang-Tidy") {
-
-                }
-                stage("Coverage") {
-
+                    }
+                    stage("Clang-Tidy") {
+                        sh label: "install clang-tidy", script: """
+                            if ! command -v clang-tidy &> /dev/null; then
+                                echo "clang-tidy not found! Installing..."
+                                cp '${dependency_dir}/clang-tidy-12' '/usr/local/bin/clang-tidy'
+                                chmod +x '/usr/local/bin/clang-tidy'
+                                cp '${dependency_dir}/lib64-clang-12-include.tar.gz' '/tmp/lib64-clang-12-include.tar.gz'
+                                cd /tmp && tar zxf lib64-clang-12-include.tar.gz
+                            else
+                                echo "clang-tidy is already installed!"
+                            fi
+                        """
+                    }
+                    stage("Coverage") {
+                        sh label: "install gcovr", script: """
+                            if ! command -v gcovr &> /dev/null; then
+                                echo "lcov not found! Installing..."
+                                cp '${dependency_dir}/gcovr.tar' '/tmp/'
+                                cd /tmp
+                                tar xvf gcovr.tar && rm -rf gcovr.tar
+                                ln -sf /tmp/gcovr/gcovr /usr/bin/gcovr
+                            else
+                                echo "lcov is already installed!"
+                            fi
+                        """
+                    }
                 }
             }
             stages("Prepare Cache") {
-                stage("CCache") {}
-                stage("Proxy Cache") {}
+                parallel {
+                    stage("CCache") {}
+                    stage("Proxy Cache") {}
+                }
             }
             stages("Build Dependency and Utils") {
                 stage("Cluster Manage") {}
