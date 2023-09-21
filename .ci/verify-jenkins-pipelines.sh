623dada13c8e84ac4e034d130ade4e736e98b410
lijie <lijie@pingcap.com>
2023-09-21 12:00:00 +0800

    feat: ci check jenkins/pipelines
    Signed-off-by: lijie <lijie@pingcap.com>

--- .ci/verify-jenkins-pipelines.sh
+++ .ci/verify-jenkins-pipelines.sh
@@ -7,7 +7,7 @@
 # JENKINS_CRUMB is needed if your Jenkins controller has CRSF protection enabled as it should
 
 JENKINS_CRUMB=$(curl -fsS "$JENKINS_URL/crumbIssuer/api/json" | jq .crumb)
-for f in $(find pipelines -name "*.groovy"); do
+for f in $(find pipelines jenkins/pipelines -name "*.groovy"); do
     echo -ne "validating $f:\\t"
     result="$(curl -fsS -X POST -H "$JENKINS_CRUMB" -F "jenkinsfile=<${f}" "$JENKINS_URL/pipeline-model-converter/validate")"
     echo "$result"
