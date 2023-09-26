6f75f0182e4640d0b2f6b41bf81429acb3a000bf
purelind <purelind@gmail.com>
2023-09-26 11:00:57 +0800

    feat: add tidb release-7.4 jobs
    Signed-off-by: purelind <purelind@gmail.com>

---
+++ pipelines/pingcap/tidb/release-7.4/ghpr_build.groovy
@@ -0,0 +1,112 @@
+// REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
+// Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
+@Library('tipipeline') _
+
+final K8S_NAMESPACE = "jenkins-tidb"
+final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
+final GIT_FULL_REPO_NAME = 'pingcap/tidb'
+final POD_TEMPLATE_FILE = 'pipelines/pingcap/tidb/release-7.4/pod-ghpr_build.yaml'
+final REFS = readJSON(text: params.JOB_SPEC).refs
+
+pipeline {
+    agent {
+        kubernetes {
+            namespace K8S_NAMESPACE
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer 'golang'
+        }
+    }
+    environment {
+        FILE_SERVER_URL = 'http://fileserver.pingcap.net'
+    }
+    options {
+        timeout(time: 60, unit: 'MINUTES')
+        parallelsAlwaysFailFast()
+    }
+    stages {
+        stage('Debug info') {
+            steps {
+                sh label: 'Debug info', script: """
+                    printenv
+                    echo "-------------------------"
+                    go env
+                    echo "-------------------------"
+                    ls -l /dev/null
+                    echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
+                """
+                container(name: 'net-tool') {
+                    sh 'dig github.com'
+                }
+            }
+        }
+        stage('Checkout') {         
+            steps {
+                dir('tidb') {                            
+                    cache(path: "./", filter: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
+                        script {
+                            git.setSshKey(GIT_CREDENTIALS_ID)
+                            retry(2) {
+                                prow.checkoutRefs(REFS, timeout = 5, credentialsId = '', gitBaseUrl = 'https://github.com', withSubmodule=true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        stage("Build tidb-server community edition"){   
+            steps {
+                dir("tidb") {                                     
+                    sh "make bazel_build"
+                }
+            }
+            post {
+                success {
+                    dir('tidb') {
+                        sh label: "create tidb-server tarball", script: """
+                            rm -rf .git
+                            tar czvf tidb-server.tar.gz ./*
+                            echo "pr/${REFS.pulls[0].sha}" > sha1
+                            echo "done" > done
+                            """
+                        sh label: 'upload to tidb dir', script: """
+                            filepath="builds/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].sha}/centos7/tidb-server.tar.gz"
+                            donepath="builds/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].sha}/centos7/done"
+                            refspath="refs/${GIT_FULL_REPO_NAME}/pr/${REFS.pulls[0].number}/sha1"
+                            curl -F \${filepath}=@tidb-server.tar.gz \${FILE_SERVER_URL}/upload
+                            curl -F \${donepath}=@done \${FILE_SERVER_URL}/upload
+                            curl -F \${refspath}=@sha1 \${FILE_SERVER_URL}/upload
+                            """
+                        sh label: 'upload to tidb-checker dir', script: """
+                            filepath="builds/pingcap/tidb-check/pr/${REFS.pulls[0].sha}/centos7/tidb-server.tar.gz"
+                            donepath="builds/pingcap/tidb-check/pr/${REFS.pulls[0].sha}/centos7/done"
+                            curl -F \${filepath}=@tidb-server.tar.gz \${FILE_SERVER_URL}/upload
+                            curl -F \${donepath}=@done \${FILE_SERVER_URL}/upload                                    
+                            """
+                    }
+                }
+                always {
+                    dir("tidb") {
+                        archiveArtifacts(artifacts: 'importer.log,tidb-server-check.log', allowEmptyArchive: true)
+                    }
+                }
+            }
+        }
+        stage("Build tidb-server enterprise edition") {
+            steps {
+                dir("tidb") {
+                    sh "make enterprise-prepare enterprise-server-build && ./bin/tidb-server -V"
+                }
+            }
+        }
+    }
+    post {
+        // TODO(wuhuizuo): put into container lifecyle preStop hook.
+        always {
+            container('report') {
+                sh "bash scripts/plugins/report_job_result.sh ${currentBuild.result} result.json || true"
+            }
+            archiveArtifacts(artifacts: 'result.json', fingerprint: true, allowEmptyArchive: true)
+        }
+    }
+}
---
+++ pipelines/pingcap/tidb/release-7.4/ghpr_check.groovy
@@ -0,0 +1,76 @@
+// REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
+// Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
+// should triggerd release-7.4 releases branches
+@Library('tipipeline') _
+
+final K8S_NAMESPACE = "jenkins-tidb"
+final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
+final GIT_FULL_REPO_NAME = 'pingcap/tidb'
+final POD_TEMPLATE_FILE = 'pipelines/pingcap/tidb/release-7.4/pod-ghpr_check.yaml'
+final REFS = readJSON(text: params.JOB_SPEC).refs
+
+pipeline {
+    agent {
+        kubernetes {
+            namespace K8S_NAMESPACE
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer 'golang'
+        }
+    }
+    environment {
+        CI = "1"
+    }
+    options {
+        timeout(time: 30, unit: 'MINUTES')
+        parallelsAlwaysFailFast()
+    }
+    stages {
+        stage('Debug info') {
+            steps {
+                sh label: 'Debug info', script: """
+                    printenv
+                    echo "-------------------------"
+                    go env
+                    echo "-------------------------"
+                    ls -l /dev/null
+                    echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
+                """
+                container(name: 'net-tool') {
+                    sh 'dig github.com'
+                }
+            }
+        }
+        stage('Checkout') {
+            steps {
+                dir('tidb') {
+                    cache(path: "./", filter: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
+                        script {
+                            git.setSshKey(GIT_CREDENTIALS_ID)
+                            retry(2) {
+                                prow.checkoutRefs(REFS, timeout = 5, credentialsId = '', gitBaseUrl = 'https://github.com', withSubmodule=true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage("Checks") {
+            environment { CODECOV_TOKEN = credentials('codecov-token-tidb') }
+            // !!! concurrent go builds will encounter conflicts probabilistically.
+            steps {
+                dir('tidb') {
+                    sh script: 'make gogenerate check integrationtest'
+                }
+            }
+        }
+    }
+    post {
+        // TODO(wuhuizuo): put into container lifecyle preStop hook.
+        always {
+            container('report') {                
+                sh "bash scripts/plugins/report_job_result.sh ${currentBuild.result} result.json || true"
+            }
+            archiveArtifacts(artifacts: 'result.json', fingerprint: true, allowEmptyArchive: true)
+        }
+    }
+}
