725b255f28cc21fb935dbe1a8656665ac705b2e5
wuhuizuo <wuhuizuo@126.com>
2023-10-16 17:25:55 +0800

    Update jobs/pingcap-inc/enterprise-extensions/le-release-7.4/aa_folder.groovy

--- jobs/pingcap-inc/enterprise-extensions/le-release-7.4/aa_folder.groovy
+++ jobs/pingcap-inc/enterprise-extensions/le-release-7.4/aa_folder.groovy
@@ -1,3 +1,3 @@
 folder('pingcap-inc/enterprise-extensions/le-release-7.4') {
-    description("Folder for pipelines of pingcap-inc/enterprise-extensions repo")
+    description("Folder for pipelines of pingcap-inc/enterprise-extensions repo limited for release branches <= v7.4")
 }
