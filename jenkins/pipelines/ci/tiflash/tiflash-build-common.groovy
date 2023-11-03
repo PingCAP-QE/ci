8c72e2199011a1a53b8bd4d4a126d132af16024e
JaySon <tshent@qq.com>
2023-11-03 13:08:04 +0800

    Only build bench for the master branch

--- jenkins/pipelines/ci/tiflash/tiflash-build-common.groovy
+++ jenkins/pipelines/ci/tiflash/tiflash-build-common.groovy
@@ -130,7 +130,12 @@ def getBuildTarget() {
     }
 
     if (params.BUILD_TESTS) {
-        targets = "bench_dbms gtests_dbms gtests_libcommon gtests_libdaemon ${targets}"
+        targets = "gtests_dbms gtests_libcommon gtests_libdaemon ${targets}"
+
+        // Only build bench binary for the master branch
+        if (params.TARGET_BRANCH == "master") {
+            targets = "bench_dbms ${targets}"
+        }
     }
 
     if (params.BUILD_PAGE_TOOLS) {
