817b53c552b868b57c8868326b9b36c42186db08
Xuecheng Zhang <csuzhangxc@gmail.com>
2023-11-28 16:39:18 +0800

    remove tidb-cluster and tidb-backup Helm chart for TiDB Operator CD

--- jenkins/pipelines/cd/tidb-operator.groovy
+++ jenkins/pipelines/cd/tidb-operator.groovy
@@ -8,7 +8,7 @@ def EnableE2E = false
 
 
 
-final CHART_ITEMS = 'tidb-operator tidb-cluster tidb-backup tidb-drainer tidb-lightning'
+final CHART_ITEMS = 'tidb-operator tidb-drainer tidb-lightning'
 final CHARTS_BUILD_DIR = 'output/chart'
 final K8S_CLUSTER = "kubernetes"
 final K8S_NAMESPACE="jenkins-cd"
