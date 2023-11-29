3393360239c9e21bb68fa1be167eeaa7b705ee89
purelind <purelind@gmail.com>
2023-11-29 15:01:48 +0800

    fix: download corrent dependency binary on hotfix-branch pr
    Signed-off-by: purelind <purelind@gmail.com>

--- pipelines/pingcap/tiflow/release-7.4/pull_dm_compatibility_test.groovy
+++ pipelines/pingcap/tiflow/release-7.4/pull_dm_compatibility_test.groovy
@@ -104,7 +104,7 @@ pipeline {
                             """
                             sh label: "download third_party", script: """
                                 pwd && ls -alh dm/tests/
-                                cd dm/tests && ./download-compatibility-test-binaries.sh ${REFS.base_ref} && ls -alh ./bin
+                                cd dm/tests && ./download-compatibility-test-binaries.sh release-7.4 && ls -alh ./bin
                                 cd - && cp -r dm/tests/bin/* ./bin
                                 ls -alh ./bin
                                 ./bin/tidb-server -V
