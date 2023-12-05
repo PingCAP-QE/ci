11e674f7f152e85c38d81d7664d3aec8cc0ce934
purelind <purelind@gmail.com>
2023-12-05 10:47:34 +0800

    chore: disable tiflash merge-ut
    Signed-off-by: purelind <purelind@gmail.com>

--- pipelines/pingcap/tiflash/latest/merged_unit_test.groovy
+++ pipelines/pingcap/tiflash/latest/merged_unit_test.groovy
@@ -1,13 +1,13 @@
 // REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
 // Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
 // should triggerd for master branches
-// @Library('tipipeline') _
+@Library('tipipeline') _
 
 final K8S_NAMESPACE = "jenkins-tidb"  // TODO: need to adjust namespace after test
 final GIT_FULL_REPO_NAME = 'pingcap/tiflash'
 final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
 final POD_TEMPLATE_FILE = 'pipelines/pingcap/tiflash/latest/pod-merged_unit_test.yaml'
-// final REFS = readJSON(text: params.JOB_SPEC).refs
+final REFS = readJSON(text: params.JOB_SPEC).refs
 final dependency_dir = "/home/jenkins/agent/dependency"
 final proxy_cache_dir = "/home/jenkins/agent/proxy-cache/refactor-pipelines"
 Boolean proxy_cache_ready = false
@@ -15,149 +15,11 @@ Boolean update_proxy_cache = true
 Boolean update_ccache = true
 String proxy_commit_hash = null
 
-podYAML = """
-apiVersion: v1
-kind: Pod
-spec:
-  securityContext:
-    fsGroup: 1000
-  containers:
-    - name: runner
-      image: "hub.pingcap.net/ee/ci/release-build-base-tiflash:v20231106"
-      command:
-        - "/bin/bash"
-        - "-c"
-        - "cat"
-      tty: true
-      resources:
-        requests:
-          memory: 32Gi
-          cpu: "12"
-        limits:
-          memory: 32Gi
-          cpu: "12"
-      volumeMounts:
-      - mountPath: "/home/jenkins/agent/rust"
-        name: "volume-0"
-        readOnly: false
-      - mountPath: "/home/jenkins/agent/ccache"
-        name: "volume-1"
-        readOnly: false
-      - mountPath: "/home/jenkins/agent/dependency"
-        name: "volume-2"
-        readOnly: false
-      - mountPath: "/home/jenkins/agent/ci-cached-code-daily"
-        name: "volume-4"
-        readOnly: false
-      - mountPath: "/home/jenkins/agent/proxy-cache"
-        name: "volume-5"
-        readOnly: false
-      - mountPath: "/tmp"
-        name: "volume-6"
-        readOnly: false
-      - mountPath: "/tmp-memfs"
-        name: "volume-7"
-        readOnly: false
-    - name: util
-      image: hub.pingcap.net/jenkins/ks3util
-      args: ["sleep", "infinity"]
-      resources:
-        requests:
-          cpu: "500m"
-          memory: "500Mi"
-        limits:
-          cpu: "500m"
-          memory: "500Mi"
-    - name: jnlp
-      resources:
-        requests:
-          memory: 256Mi
-          cpu: 100m
-        limits:
-          memory: 256Mi
-          cpu: 100m
-      volumeMounts:
-      - mountPath: "/home/jenkins/agent/rust"
-        name: "volume-0"
-        readOnly: false
-      - mountPath: "/home/jenkins/agent/ccache"
-        name: "volume-1"
-        readOnly: false
-      - mountPath: "/home/jenkins/agent/dependency"
-        name: "volume-2"
-        readOnly: false
-      - mountPath: "/home/jenkins/agent/ci-cached-code-daily"
-        name: "volume-4"
-        readOnly: false
-      - mountPath: "/home/jenkins/agent/proxy-cache"
-        name: "volume-5"
-        readOnly: false
-      - mountPath: "/tmp"
-        name: "volume-6"
-        readOnly: false
-      - mountPath: "/tmp-memfs"
-        name: "volume-7"
-        readOnly: false 
-    - name: net-tool
-      image: wbitt/network-multitool
-      tty: true
-      resources:
-        limits:
-          memory: 128Mi
-          cpu: 100m
-  volumes:
-    - name: "volume-0"
-      nfs:
-        path: "/data/nvme1n1/nfs/tiflash/rust"
-        readOnly: false
-        server: "10.2.12.82"
-    - name: "volume-2"
-      nfs:
-        path: "/data/nvme1n1/nfs/tiflash/dependency"
-        readOnly: true
-        server: "10.2.12.82"
-    - name: "volume-1"
-      nfs:
-        path: "/data/nvme1n1/nfs/tiflash/ccache"
-        readOnly: false
-        server: "10.2.12.82"
-    - name: "volume-4"
-      nfs:
-        path: "/data/nvme1n1/nfs/git"
-        readOnly: true
-        server: "10.2.12.82"
-    - name: "volume-5"
-      nfs:
-        path: "/data/nvme1n1/nfs/tiflash/proxy-cache"
-        readOnly: false
-        server: "10.2.12.82"
-    - name: "volume-6"
-      emptyDir: {}
-    - name: "volume-7"
-      emptyDir:
-        medium: Memory
-  affinity:
-    nodeAffinity:
-      requiredDuringSchedulingIgnoredDuringExecution:
-        nodeSelectorTerms:
-          - matchExpressions:
-              - key: kubernetes.io/arch
-                operator: In
-                values:
-                  - amd64
-              - key: ci-nvme-high-performance
-                operator: In
-                values:
-                  - "true"
-"""
-
-
 pipeline {
     agent {
         kubernetes {
             namespace K8S_NAMESPACE
-            // yamlFile POD_TEMPLATE_FILE
-            yaml podYAML
+            yamlFile POD_TEMPLATE_FILE
             defaultContainer 'runner'
             retries 5
             customWorkspace "/home/jenkins/agent/workspace/tiflash-build-common"
@@ -190,43 +52,19 @@ pipeline {
             steps {
                     dir("tiflash") {
                         script {
-                            // cache(path: "./", filter: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
-                            //     retry(2) {
-                            //         prow.checkoutRefs(REFS, timeout = 10, credentialsId = '', gitBaseUrl = 'https://github.com')
-                            //     }
-                            // }
-                            // cache(path: ".git/modules", filter: '**/*', key: prow.getCacheKey('git', REFS, 'git-modules'), restoreKeys: prow.getRestoreKeys('git', REFS, 'git-modules')) {
-                            //         sh ''
-                            //         sh """
-                            //         git submodule update --init --recursive
-                            //         git status
-                            //         git show --oneline -s
-                            //         """
-                            // }
-                            container("util") {
-                                withCredentials(
-                                    [file(credentialsId: 'ks3util-config', variable: 'KS3UTIL_CONF')]
-                                ) {
-                                    sh "ks3util -c \$KS3UTIL_CONF cp -f ks3://ee-fileserver/download/cicd/daily-cache-code/src-tics.tar.gz src-tics.tar.gz"
-                                    sh """
-                                    ls -alh
-                                    chown 1000:1000 src-tics.tar.gz
-                                    """
+                            cache(path: "./", filter: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
+                                retry(2) {
+                                    prow.checkoutRefs(REFS, timeout = 10, credentialsId = '', gitBaseUrl = 'https://github.com')
                                 }
                             }
-                            sh """
-                            printenv
-                            time tar -xzf src-tics.tar.gz --strip-components=1 && rm -rf src-tics.tar.gz
-                            ls -alh
-                            chown 1000:1000 -R ./
-                            ls -alh
-                            git version
-                            git config --global --add safe.directory '*'
-                            git submodule update --init --recursive
-                            git status
-                            git show --oneline -s
-                            """
-                            
+                            cache(path: ".git/modules", filter: '**/*', key: prow.getCacheKey('git', REFS, 'git-modules'), restoreKeys: prow.getRestoreKeys('git', REFS, 'git-modules')) {
+                                    sh ''
+                                    sh """
+                                    git submodule update --init --recursive
+                                    git status
+                                    git show --oneline -s
+                                    """
+                            }
                             dir("contrib/tiflash-proxy") {
                                 proxy_commit_hash = sh(returnStdout: true, script: 'git log -1 --format="%H"').trim()
                                 println "proxy_commit_hash: ${proxy_commit_hash}"
@@ -238,92 +76,92 @@ pipeline {
                     }
             }
         }
-        // stage("Prepare tools") {
-        //     // TODO: need to simplify this part
-        //     // all tools should be pre-install in docker image
-        //     parallel {
-        //     stage("Ccache") {
-        //         steps {
-        //             sh label: "install ccache", script: """
-        //                 if ! command -v ccache &> /dev/null; then
-        //                     echo "ccache not found! Installing..."
-        //                     rpm -Uvh '${dependency_dir}/ccache.x86_64.rpm'
-        //                 else
-        //                     echo "ccache is already installed!"
-        //                 fi
-        //             """
-        //         }
-        //     }
-        //     stage("Cmake") {
-        //         steps { 
-        //             sh label: "install cmake3", script: """
-        //                 if ! command -v cmake &> /dev/null; then
-        //                     echo "cmake not found! Installing..."
-        //                     sh ${dependency_dir}/cmake-3.22.3-linux-x86_64.sh --prefix=/usr --skip-license --exclude-subdir
-        //                 else
-        //                     echo "cmake is already installed!"
-        //                 fi
-        //             """
-        //         }
-        //     }
-        //     stage("Clang-Format") {
-        //         steps {
-        //             sh label: "install clang-format", script: """
-        //                 if ! command -v clang-format &> /dev/null; then
-        //                     echo "clang-format not found! Installing..."
-        //                     cp '${dependency_dir}/clang-format-12' '/usr/local/bin/clang-format'
-        //                     chmod +x '/usr/local/bin/clang-format'
-        //                 else
-        //                     echo "clang-format is already installed!"
-        //                 fi
-        //             """
-        //         }
-        //     }
-        //     stage("Clang-Format-15") {
-        //         steps { 
-        //             sh label: "install clang-format-15", script: """
-        //                 if ! command -v clang-format-15 &> /dev/null; then
-        //                     echo "clang-format-15 not found! Installing..."
-        //                     cp '${dependency_dir}/clang-format-15' '/usr/local/bin/clang-format-15'
-        //                     chmod +x '/usr/local/bin/clang-format-15'
-        //                 else
-        //                     echo "clang-format-15 is already installed!"
-        //                 fi
-        //             """
-        //         }
-        //     }
-        //     stage( "Clang-Tidy") {
-        //         steps { 
-        //             sh label: "install clang-tidy", script: """
-        //                 if ! command -v clang-tidy &> /dev/null; then
-        //                     echo "clang-tidy not found! Installing..."
-        //                     cp '${dependency_dir}/clang-tidy-12' '/usr/local/bin/clang-tidy'
-        //                     chmod +x '/usr/local/bin/clang-tidy'
-        //                     cp '${dependency_dir}/lib64-clang-12-include.tar.gz' '/tmp/lib64-clang-12-include.tar.gz'
-        //                     cd /tmp && tar zxf lib64-clang-12-include.tar.gz
-        //                 else
-        //                     echo "clang-tidy is already installed!"
-        //                 fi
-        //             """
-        //         }
-        //     }
-        //     stage("Coverage") {
-        //         steps {
-        //             sh label: "install gcovr", script: """
-        //                 if ! command -v gcovr &> /dev/null; then
-        //                     echo "lcov not found! Installing..."
-        //                     cp '${dependency_dir}/gcovr.tar' '/tmp/'
-        //                     cd /tmp
-        //                     tar xvf gcovr.tar && rm -rf gcovr.tar
-        //                     ln -sf /tmp/gcovr/gcovr /usr/bin/gcovr
-        //                 else
-        //                     echo "lcov is already installed!"
-        //                 fi
-        //             """
-        //         }
-        //     }
-        //     }
-        // }
+        stage("Prepare tools") {
+            // TODO: need to simplify this part
+            // all tools should be pre-install in docker image
+            parallel {
+            stage("Ccache") {
+                steps {
+                    sh label: "install ccache", script: """
+                        if ! command -v ccache &> /dev/null; then
+                            echo "ccache not found! Installing..."
+                            rpm -Uvh '${dependency_dir}/ccache.x86_64.rpm'
+                        else
+                            echo "ccache is already installed!"
+                        fi
+                    """
+                }
+            }
+            stage("Cmake") {
+                steps { 
+                    sh label: "install cmake3", script: """
+                        if ! command -v cmake &> /dev/null; then
+                            echo "cmake not found! Installing..."
+                            sh ${dependency_dir}/cmake-3.22.3-linux-x86_64.sh --prefix=/usr --skip-license --exclude-subdir
+                        else
+                            echo "cmake is already installed!"
+                        fi
+                    """
+                }
+            }
+            stage("Clang-Format") {
+                steps {
+                    sh label: "install clang-format", script: """
+                        if ! command -v clang-format &> /dev/null; then
+                            echo "clang-format not found! Installing..."
+                            cp '${dependency_dir}/clang-format-12' '/usr/local/bin/clang-format'
+                            chmod +x '/usr/local/bin/clang-format'
+                        else
+                            echo "clang-format is already installed!"
+                        fi
+                    """
+                }
+            }
+            stage("Clang-Format-15") {
+                steps { 
+                    sh label: "install clang-format-15", script: """
+                        if ! command -v clang-format-15 &> /dev/null; then
+                            echo "clang-format-15 not found! Installing..."
+                            cp '${dependency_dir}/clang-format-15' '/usr/local/bin/clang-format-15'
+                            chmod +x '/usr/local/bin/clang-format-15'
+                        else
+                            echo "clang-format-15 is already installed!"
+                        fi
+                    """
+                }
+            }
+            stage( "Clang-Tidy") {
+                steps { 
+                    sh label: "install clang-tidy", script: """
+                        if ! command -v clang-tidy &> /dev/null; then
+                            echo "clang-tidy not found! Installing..."
+                            cp '${dependency_dir}/clang-tidy-12' '/usr/local/bin/clang-tidy'
+                            chmod +x '/usr/local/bin/clang-tidy'
+                            cp '${dependency_dir}/lib64-clang-12-include.tar.gz' '/tmp/lib64-clang-12-include.tar.gz'
+                            cd /tmp && tar zxf lib64-clang-12-include.tar.gz
+                        else
+                            echo "clang-tidy is already installed!"
+                        fi
+                    """
+                }
+            }
+            stage("Coverage") {
+                steps {
+                    sh label: "install gcovr", script: """
+                        if ! command -v gcovr &> /dev/null; then
+                            echo "lcov not found! Installing..."
+                            cp '${dependency_dir}/gcovr.tar' '/tmp/'
+                            cd /tmp
+                            tar xvf gcovr.tar && rm -rf gcovr.tar
+                            ln -sf /tmp/gcovr/gcovr /usr/bin/gcovr
+                        else
+                            echo "lcov is already installed!"
+                        fi
+                    """
+                }
+            }
+            }
+        }
 
         stage("Prepare Cache") {
             parallel {
@@ -360,19 +198,11 @@ pipeline {
                     }
                 }
                 stage("Proxy-Cache") {
-                    // when {
-                    //     expression { return fileExists("/home/jenkins/agent/proxy-cache/refactor-pipelines/${proxy_commit_hash}-amd64-linux-llvm") }
-                    // }
                     steps {
                         script {
-                            // def cache_source = "/home/jenkins/agent/proxy-cache/${proxy_commit_hash}-amd64-linux-llvm"
-                            // if (fileExists(cache_source)) {
-                            //     echo "proxy cache found"
-                            //     proxy_cache_ready = true
-                            // }
                             proxy_cache_ready = fileExists("/home/jenkins/agent/proxy-cache/refactor-pipelines/${proxy_commit_hash}-amd64-linux-llvm")
                             println "proxy_cache_ready: ${proxy_cache_ready}"
-                            // /home/jenkins/agent/proxy-cache/${proxy_commit_hash}-amd64-linux-llvm
+
                             sh label: "copy proxy if exist", script: """
                             proxy_cache_file="${proxy_cache_dir}/${proxy_commit_hash}-amd64-linux-llvm"
                             if [ -f \$proxy_cache_file ]; then
