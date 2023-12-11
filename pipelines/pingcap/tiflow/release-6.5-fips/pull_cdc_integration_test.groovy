e06c9f656794518e7df9b6718b4d509d720b71dd
Purelind <purelind@users.noreply.github.com>
2023-12-11 18:48:18 +0800

    feat: tiflow support fips branch pipelines (#2669)
    tiflow support fips branch pipelines
    
    ---------
    
    Signed-off-by: purelind <purelind@gmail.com>

---
+++ pipelines/pingcap/tiflow/release-6.5-fips/pull_cdc_integration_test.groovy
@@ -0,0 +1,175 @@
+// REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
+// Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
+// should triggerd for master branches
+@Library('tipipeline') _
+
+final K8S_NAMESPACE = "jenkins-tiflow"
+final GIT_FULL_REPO_NAME = 'pingcap/tiflow'
+final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
+final GIT_CREDENTIALS_ID2 = 'github-pr-diff-token'
+final POD_TEMPLATE_FILE = 'pipelines/pingcap/tiflow/release-6.5-fips/pod-pull_cdc_integration_test.yaml'
+final REFS = readJSON(text: params.JOB_SPEC).refs
+def skipRemainingStages = false
+
+pipeline {
+    agent {
+        kubernetes {
+            namespace K8S_NAMESPACE
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer 'golang'
+        }
+    }
+    environment {
+        FILE_SERVER_URL = 'http://fileserver.pingcap.net'
+    }
+    options {
+        timeout(time: 60, unit: 'MINUTES')
+        // parallelsAlwaysFailFast()
+    }
+    stages {
+        stage('Debug info') {
+            steps {
+                sh label: 'Debug info', script: """
+                    printenv
+                    echo "-------------------------"
+                    go env
+                    echo "-------------------------"
+                    echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
+                """
+                container(name: 'net-tool') {
+                    sh 'dig github.com'
+                }
+            }
+        }
+        stage('Check diff files') {
+            steps {
+                container("golang") {
+                    script {
+                        def pr_diff_files = component.getPrDiffFiles(GIT_FULL_REPO_NAME, REFS.pulls[0].number, GIT_CREDENTIALS_ID2)
+                        def pattern = /(^dm\/|^engine\/).*$/
+                        println "pr_diff_files: ${pr_diff_files}"
+                        // if all diff files start with dm/, skip cdc integration test
+                        def matched = component.patternMatchAllFiles(pattern, pr_diff_files)
+                        if (matched) {
+                            println "matched, all diff files full path start with dm/ or engine/, current pr is dm/engine's pr(not related to ticdc), skip cdc integration test"
+                            currentBuild.result = 'SUCCESS'
+                            skipRemainingStages = true
+                            return
+                        }
+                    }
+                }
+            }
+        }
+        stage('Checkout') {
+            when { expression { !skipRemainingStages} }
+            options { timeout(time: 10, unit: 'MINUTES') }
+            steps {
+                dir("tiflow") {
+                    cache(path: "./", includes: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
+                        retry(2) {
+                            script {
+                                prow.checkoutRefs(REFS)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage("prepare") {
+            when { expression { !skipRemainingStages} }
+            options { timeout(time: 20, unit: 'MINUTES') }
+            steps {
+                dir("third_party_download") {
+                    retry(2) {
+                        sh label: "download third_party", script: """
+                            cd ../tiflow
+                            chmod +x ../scripts/pingcap/tiflow/release-6.5-fips/ticdc_download_integration_test_binaries.sh
+                            ${WORKSPACE}/scripts/pingcap/tiflow/release-6.5-fips/ticdc_download_integration_test_binaries.sh
+                            ls -alh ./bin/
+                            make check_third_party_binary
+                            cd - && mkdir -p bin && mv ../tiflow/bin/* ./bin/
+                            ls -alh ./bin
+                            ./bin/tidb-server -V
+                            ./bin/pd-server -V
+                            ./bin/tikv-server -V
+                            ./bin/tiflash --version
+                            ./bin/sync_diff_inspector --version
+                        """
+                    }
+                }
+                dir("tiflow") {
+                    cache(path: "./bin", includes: '**/*', key: prow.getCacheKey('binary', REFS, 'cdc-integration-test')) { 
+                        // build cdc, kafka_consumer, cdc.test for integration test
+                        // only build binarys if not exist, use the cached binarys if exist
+                        sh label: "prepare", script: """
+                            ls -alh ./bin
+                            [ -f ./bin/cdc ] || make cdc
+                            [ -f ./bin/cdc_kafka_consumer ] || make kafka_consumer
+                            [ -f ./bin/cdc.test ] || make integration_test_build
+                            ls -alh ./bin
+                            ./bin/cdc version
+                        """
+                    }
+                    cache(path: "./", includes: '**/*', key: "ws/${BUILD_TAG}/tiflow-cdc") { 
+                        sh label: "prepare", script: """
+                            cp -r ../third_party_download/bin/* ./bin/
+                            ls -alh ./bin
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Tests') {
+            when { expression { !skipRemainingStages} }
+            matrix {
+                axes {
+                    axis {
+                        name 'TEST_GROUP'
+                        values 'G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6',  'G7', 'G8', 'G9', 'G10', 'G11', 'G12', 'G13', 
+                            'G14', 'G15'
+                    }
+                }
+                agent{
+                    kubernetes {
+                        namespace K8S_NAMESPACE
+                        yamlFile POD_TEMPLATE_FILE
+                        defaultContainer 'golang'
+                    }
+                } 
+                stages {
+                    stage("Test") {
+                        options { timeout(time: 40, unit: 'MINUTES') }
+                        environment { 
+                            TICDC_CODECOV_TOKEN = credentials('codecov-token-tiflow') 
+                            TICDC_COVERALLS_TOKEN = credentials('coveralls-token-tiflow')    
+                        }
+                        steps {
+                            dir('tiflow') {
+                                cache(path: "./", includes: '**/*', key: "ws/${BUILD_TAG}/tiflow-cdc") {
+                                    sh label: "${TEST_GROUP}", script: """
+                                        rm -rf /tmp/tidb_cdc_test && mkdir -p /tmp/tidb_cdc_test
+                                        ln -s ${WORKSPACE}/tiflow/bin ${WORKSPACE}/tiflow/tests/bin
+                                        chmod +x ../scripts/pingcap/tiflow/release-6.5-fips/cdc_run_group.sh
+                                        cp ../scripts/pingcap/tiflow/release-6.5-fips/cdc_run_group.sh tests/integration_tests/
+                                        ./tests/integration_tests/cdc_run_group.sh mysql ${TEST_GROUP}
+                                    """
+                                }
+                            }
+                        }
+                        post {
+                            failure {
+                                sh label: "collect logs", script: """
+                                    ls /tmp/tidb_cdc_test/
+                                    tar -cvzf log-${TEST_GROUP}.tar.gz \$(find /tmp/tidb_cdc_test/ -type f -name "*.log")    
+                                    ls -alh  log-${TEST_GROUP}.tar.gz  
+                                """
+                                archiveArtifacts artifacts: "log-${TEST_GROUP}.tar.gz", fingerprint: true 
+                            }
+                        }
+                    }
+                }
+            }        
+        }
+    }
+}
---
+++ scripts/pingcap/tiflow/release-6.5-fips/ticdc_download_integration_test_binaries.sh
@@ -0,0 +1,106 @@
+#! /usr/bin/env bash
+
+# ticdc_download_integration_test_binaries.sh will
+# * download all the binaries you need for integration testing
+
+# Notice:
+# Please don't try the script locally,
+# it downloads files for linux platform. We only use it in docker container.
+
+set -o errexit
+set -o pipefail
+
+# Specify which branch to be utilized for executing the test, which is
+# exclusively accessible when obtaining binaries from
+# http://fileserver.pingcap.net.
+branch=${1:-release-6.5-fips}
+default_target_branch="release-6.5"
+
+set -o nounset
+
+# See https://misc.flogisoft.com/bash/tip_colors_and_formatting.
+color-green() { # Green
+	echo -e "\x1B[1;32m${*}\x1B[0m"
+}
+color-green "Download binaries from branch ${branch}"
+
+
+function download() {
+	local url=$1
+	local file_name=$2
+	local file_path=$3
+	if [[ -f "${file_path}" ]]; then
+		echo "file ${file_name} already exists, skip download"
+		return
+	fi
+	echo ">>>"
+	echo "download ${file_name} from ${url}"
+	wget --no-verbose --retry-connrefused --waitretry=1 -t 3 -O "${file_path}" "${url}"
+}
+
+function download_binaries() {
+	color-green "Download binaries..."
+	# PingCAP file server URL.
+	file_server_url="http://fileserver.pingcap.net"
+
+	# Get sha1 based on branch name.
+	tidb_sha1=$(curl "${file_server_url}/download/refs/pingcap/tidb/${branch}/sha1")
+	tikv_sha1=$(curl "${file_server_url}/download/refs/pingcap/tikv/${branch}/sha1")
+	pd_sha1=$(curl "${file_server_url}/download/refs/pingcap/pd/${branch}/sha1")
+	tiflash_sha1=$(curl "${file_server_url}/download/refs/pingcap/tiflash/${default_target_branch}/sha1")
+
+	# All download links.
+	tidb_download_url="${file_server_url}/download/builds/pingcap/tidb/${tidb_sha1}/centos7/tidb-server.tar.gz"
+	tikv_download_url="${file_server_url}/download/builds/pingcap/tikv/${tikv_sha1}/centos7/tikv-server.tar.gz"
+	pd_download_url="${file_server_url}/download/builds/pingcap/pd/${pd_sha1}/centos7/pd-server.tar.gz"
+	tiflash_download_url="${file_server_url}/download/builds/pingcap/tiflash/${default_target_branch}/${tiflash_sha1}/centos7/tiflash.tar.gz"
+	minio_download_url="${file_server_url}/download/minio.tar.gz"
+	go_ycsb_download_url="${file_server_url}/download/builds/pingcap/go-ycsb/test-br/go-ycsb"
+	etcd_download_url="${file_server_url}/download/builds/pingcap/cdc/etcd-v3.4.7-linux-amd64.tar.gz"
+	sync_diff_inspector_url="${file_server_url}/download/builds/pingcap/cdc/sync_diff_inspector_hash-00998a9a_linux-amd64.tar.gz"
+	jq_download_url="${file_server_url}/download/builds/pingcap/test/jq-1.6/jq-linux64"
+
+	download "$tidb_download_url" "tidb-server.tar.gz" "tmp/tidb-server.tar.gz"
+	tar -xz -C third_bin bin/tidb-server -f tmp/tidb-server.tar.gz && mv third_bin/bin/tidb-server third_bin/
+	download "$pd_download_url" "pd-server.tar.gz" "tmp/pd-server.tar.gz"
+	tar -xz -C third_bin 'bin/*' -f tmp/pd-server.tar.gz && mv third_bin/bin/* third_bin/
+	download "$tikv_download_url" "tikv-server.tar.gz" "tmp/tikv-server.tar.gz"
+	tar -xz -C third_bin bin/tikv-server -f tmp/tikv-server.tar.gz && mv third_bin/bin/tikv-server third_bin/
+	download "$tiflash_download_url" "tiflash.tar.gz" "tmp/tiflash.tar.gz"
+	tar -xz -C third_bin -f tmp/tiflash.tar.gz
+	mv third_bin/tiflash third_bin/_tiflash
+	mv third_bin/_tiflash/* third_bin && rm -rf third_bin/_tiflash
+	download "$minio_download_url" "minio.tar.gz" "tmp/minio.tar.gz"
+	tar -xz -C third_bin -f tmp/minio.tar.gz
+
+	download "$go_ycsb_download_url" "go-ycsb" "third_bin/go-ycsb"
+	download "$jq_download_url" "jq" "third_bin/jq"
+	download "$etcd_download_url" "etcd.tar.gz" "tmp/etcd.tar.gz"
+	tar -xz -C third_bin etcd-v3.4.7-linux-amd64/etcdctl -f tmp/etcd.tar.gz
+	mv third_bin/etcd-v3.4.7-linux-amd64/etcdctl third_bin/ && rm -rf third_bin/etcd-v3.4.7-linux-amd64
+	download "$sync_diff_inspector_url" "sync_diff_inspector.tar.gz" "tmp/sync_diff_inspector.tar.gz"
+	tar -xz -C third_bin -f tmp/sync_diff_inspector.tar.gz
+
+	chmod a+x third_bin/*
+}
+
+# Some temporary dir.
+rm -rf tmp
+rm -rf third_bin
+
+mkdir -p third_bin
+mkdir -p tmp
+mkdir -p bin
+
+download_binaries
+
+# Copy it to the bin directory in the root directory.
+rm -rf tmp
+rm -rf bin/bin
+mv third_bin/* ./bin
+rm -rf bin/bin
+rm -rf third_bin
+
+ls -alh ./bin
+
+color-green "Download SUCCESS"
