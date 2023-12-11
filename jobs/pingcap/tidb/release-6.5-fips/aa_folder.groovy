52faa2341967689632a84dfbb8336ecfcd0090e8
wuhuizuo <wuhuizuo@126.com>
2023-12-11 18:29:41 +0800

    Update jobs/pingcap/tidb/release-6.5-fips/aa_folder.groovy

--- jobs/pingcap/tidb/release-6.5-fips/aa_folder.groovy
+++ jobs/pingcap/tidb/release-6.5-fips/aa_folder.groovy
@@ -1,3 +1,3 @@
 folder('pingcap/tidb/release-6.5-fips') {
-    description("Folder for pipelines of pingcap/tidb repo for v6.5")
+    description("Folder for pipelines of pingcap/tidb repo for feature/release-6.5-fips")
 }
