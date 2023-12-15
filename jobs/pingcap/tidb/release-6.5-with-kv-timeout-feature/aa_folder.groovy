32a190a79b338a870e295559ec343d0c4b778c9f
purelind <purelind@gmail.com>
2023-12-15 15:57:08 +0800

    chore: update description
    Signed-off-by: purelind <purelind@gmail.com>

--- jobs/pingcap/tidb/release-6.5-with-kv-timeout-feature/aa_folder.groovy
+++ jobs/pingcap/tidb/release-6.5-with-kv-timeout-feature/aa_folder.groovy
@@ -1,3 +1,3 @@
 folder('pingcap/tidb/release-6.5-with-kv-timeout-feature') {
-    description("Folder for pipelines of pingcap/tidb repo for v6.5")
+    description("Folder for pipelines of pingcap/tidb repo for branch release-6.5-with-kv-timeout-feature")
 }
