908e9521ade8717f4679774cd0c335c6b3afeb0b
Purelind <purelind@users.noreply.github.com>
2023-12-15 12:15:20 +0800

    chore: use the fips-branch tidb-test (#2682)
    use the fips-branch tidb-test
    
    ---------
    
    Signed-off-by: purelind <purelind@gmail.com>

--- pipelines/pingcap/tidb/release-6.5-fips/ghpr_mysql_test.groovy
+++ pipelines/pingcap/tidb/release-6.5-fips/ghpr_mysql_test.groovy
@@ -56,8 +56,7 @@ pipeline {
                     cache(path: "./", includes: '**/*', key: "git/PingCAP-QE/tidb-test/rev-${REFS.pulls[0].sha}", restoreKeys: ['git/PingCAP-QE/tidb-test/rev-']) {
                         retry(2) {
                             script {
-                                // TODO: need to create new branch feature/release-6.5-fips on tidb-test repo or not?
-                                component.checkout('git@github.com:PingCAP-QE/tidb-test.git', 'tidb-test', "release-6.5", REFS.pulls[0].title, GIT_CREDENTIALS_ID)
+                                component.checkout('git@github.com:PingCAP-QE/tidb-test.git', 'tidb-test', "feature/release-6.5-fips", REFS.pulls[0].title, credentialsId=GIT_CREDENTIALS_ID, trunkBranch="feature/release-6.5-fips")
                             }
                         }
                     }
