d5d6e6609fba1de2873dac81c6690a7a1df55618
purelind <purelind@gmail.com>
2023-12-27 16:59:03 +0800

    feat: update jobdsl and presubmit to support prow trigger
    Signed-off-by: purelind <purelind@gmail.com>

--- jobs/pingcap/tidb-engine-ext/release-6.1/ghpr_unit_test.groovy
+++ jobs/pingcap/tidb-engine-ext/release-6.1/ghpr_unit_test.groovy
@@ -1,70 +1,18 @@
 // REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
-// For trunk and latest release branches.
+// For release-6.1 branches.
 pipelineJob('pingcap/tidb-engine-ext/release-6.1/ghpr_unit_test') {
     logRotator {
-        daysToKeep(90)
+        daysToKeep(30)
     }
     parameters {
-        stringParam("ghprbActualCommit")
-        stringParam("ghprbPullId")
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
     }
     properties {
         // priority(0) // 0 fast than 1
         githubProjectUrl("https://github.com/pingcap/tidb-engine-ext")
-        pipelineTriggers {
-            triggers {
-                ghprbTrigger {
-                    cron('H/5 * * * *')
-                    gitHubAuthId('') // using the default only one.
-
-                    triggerPhrase('.*/(run-(all-tests|unit-test).*)')
-                    onlyTriggerPhrase(true)
-                    skipBuildPhrase(".*skip-ci.*")
-                    buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
-                    whitelist('')
-                    orgslist('')
-                    blackListTargetBranches {
-                        ghprbBranch { branch('master') }
-                        ghprbBranch { branch('raftstore-proxy') }
-                        ghprbBranch { branch('^(raftstore-proxy-)?6\\.[2-9]\\d*(\\.\\d+)?(\\-.*)?$') }
-                    }
-                    // ignore when only those file changed.(
-                    //   multi line regex
-                    // excludedRegions('.*\\.md')
-                    excludedRegions('') // current the context is required in github branch protection.
-
-                    blackListLabels("")
-                    whiteListLabels("")
-                    adminlist("")
-                    blackListCommitAuthor("")
-                    includedRegions("")
-                    commentFilePath("")
-
-                    allowMembersOfWhitelistedOrgsAsAdmin(true)
-                    permitAll(true)
-                    useGitHubHooks(true)
-                    displayBuildErrorsOnDownstreamBuilds(false)
-                    autoCloseFailedPullRequests(false)
-
-                    // useless, but can not delete.
-                    commitStatusContext("--none--")
-                    msgSuccess("--none--")
-                    msgFailure("--none--")
-
-                    extensions {
-                        ghprbCancelBuildsOnUpdate { overrideGlobal(true) }
-                        ghprbSimpleStatus {
-                            commitStatusContext('pull-unit-test')
-                            statusUrl('${RUN_DISPLAY_URL}')
-                            startedStatus('')
-                            triggeredStatus('')
-                            addTestResults(false)
-                            showMatrixStatus(false)
-                        }
-                    }
-                }
-            }
-        }
     }
  
     definition {
