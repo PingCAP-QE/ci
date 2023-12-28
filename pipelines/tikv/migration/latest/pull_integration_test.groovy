347cb842a379283f9bca98aabef8087f21f73ede
Purelind <purelind@users.noreply.github.com>
2023-12-28 15:07:58 +0800

    feat(tikv/migration): support prow trigger job (#2704)
    support prow trigger job
    
    ---------
    
    Signed-off-by: purelind <purelind@gmail.com>

--- pipelines/tikv/migration/latest/pull_integration_test.groovy
+++ pipelines/tikv/migration/latest/pull_integration_test.groovy
@@ -7,6 +7,7 @@ final K8S_NAMESPACE = "jenkins-tidb"
 final GIT_FULL_REPO_NAME = 'tikv/migration'
 final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
 final POD_TEMPLATE_FILE = 'pipelines/tikv/migration/latest/pod-pull_integration_test.yaml'
+final REFS = readJSON(text: params.JOB_SPEC).refs
 
 pipeline {
     agent {
@@ -43,27 +44,17 @@ pipeline {
             options { timeout(time: 5, unit: 'MINUTES') }
             steps {
                 dir("migration") {
-                    cache(path: "./", includes: '**/*', key: "git/tikv/migration/rev-${ghprbActualCommit}", restoreKeys: ['git/tikv/migration/rev-']) {
+                    cache(path: "./", includes: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
                         retry(2) {
-                            checkout(
-                                changelog: false,
-                                poll: false,
-                                scm: [
-                                    $class: 'GitSCM', branches: [[name: ghprbActualCommit]],
-                                    doGenerateSubmoduleConfigurations: false,
-                                    extensions: [
-                                        [$class: 'PruneStaleBranch'],
-                                        [$class: 'CleanBeforeCheckout'],
-                                        [$class: 'CloneOption', timeout: 15],
-                                    ],
-                                    submoduleCfg: [],
-                                    userRemoteConfigs: [[
-                                        refspec: "+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*",
-                                        url: "https://github.com/${GIT_FULL_REPO_NAME}.git",
-                                    ]],
-                                ]
-                            )
+                            script {
+                                prow.checkoutRefs(REFS)
+                            }
                         }
+                        sh """
+                        git rev-parse --show-toplevel
+                        git status
+                        git status -s .
+                        """
                     }
                 }
             }
@@ -73,7 +64,7 @@ pipeline {
                 dir('migration') {
                     cache(path: "./cdc", includes: '**/*', key: "ws/${BUILD_TAG}/tikvcdc") {
                         container("golang") {
-                            sh label: 'integration test prepare', script: """
+                            sh label: 'integration test prepare', script: """#!/usr/bin/env bash
                             cd cdc/
                             make prepare_test_binaries 
                             make check_third_party_binary 
@@ -105,7 +96,7 @@ pipeline {
                         steps {
                             dir('migration') {
                                cache(path: "./cdc", includes: '**/*', key: "ws/${BUILD_TAG}/tikvcdc") {  
-                                    sh label: "TEST_GROUP ${TEST_GROUP}",script: """
+                                    sh label: "TEST_GROUP ${TEST_GROUP}",script: """#!/usr/bin/env bash
                                         cd cdc/
                                         ./tests/integration_tests/run_group.sh ${TEST_GROUP}
                                     """
@@ -114,7 +105,7 @@ pipeline {
                         }
                     }
                 }
-            }        
+            }
         }
     }
 }
