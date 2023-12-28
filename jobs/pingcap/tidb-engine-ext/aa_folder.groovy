782603d9a649fee79d38784ea86262feee88a97c
Purelind <purelind@users.noreply.github.com>
2023-12-28 11:48:28 +0800

    feat(pingcap/tidb-engine-ext): tidb engine ext support prow trigger (#2703)
    *  support prow trigger
    *  just use prow job to run ci task, remove jenkins jobs
    
    presubmit config about clone_depth could be found in here
    https://pkg.go.dev/k8s.io/test-infra/prow/config#UtilityConfig
    
    ---------
    
    Signed-off-by: purelind <purelind@gmail.com>

--- jobs/pingcap/tidb-engine-ext/aa_folder.groovy
+++
@@ -1,3 +0,0 @@
-folder('pingcap/tidb-engine-ext') {
-    description("Folder for pipelines of pingcap/tidb-engine-ext repo")
-}
--- jobs/pingcap/tidb-engine-ext/release-6.1/aa_folder.groovy
+++
@@ -1,3 +0,0 @@
-folder('pingcap/tidb-engine-ext/release-6.1') {
-    description("Folder for pipelines of pingcap/tidb-engine-ext repo for v6.1")
-}
