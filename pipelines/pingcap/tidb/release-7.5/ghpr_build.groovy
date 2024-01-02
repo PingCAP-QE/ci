1af9cbae76e5d4b68771ba2df93847bb5197762c
wuhuizuo <wuhuizuo@126.com>
2024-01-02 14:27:31 +0800

    fix(pingcap/tidb): add plugin test stage (#2713)
    - v7.1.x
    - v7.5.x
    - trunk branch
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- pipelines/pingcap/tidb/release-7.5/ghpr_build.groovy
+++ pipelines/pingcap/tidb/release-7.5/ghpr_build.groovy
@@ -39,9 +39,9 @@ pipeline {
                 }
             }
         }
-        stage('Checkout') {         
+        stage('Checkout') {
             steps {
-                dir('tidb') {                            
+                dir(REFS.repo) {
                     cache(path: "./", includes: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
                         script {
                             git.setSshKey(GIT_CREDENTIALS_ID)
@@ -53,16 +53,15 @@ pipeline {
                 }
             }
         }
-
-        stage("Build tidb-server community edition"){   
+        stage("Build tidb-server community edition"){
             steps {
-                dir("tidb") {                                     
+                dir(REFS.repo) {
                     sh "make bazel_build"
                 }
             }
             post {
                 success {
-                    dir('tidb') {
+                    dir(REFS.repo) {
                         sh label: "create tidb-server tarball", script: """
                             rm -rf .git
                             tar czvf tidb-server.tar.gz ./*
@@ -81,12 +80,12 @@ pipeline {
                             filepath="builds/pingcap/tidb-check/pr/${REFS.pulls[0].sha}/centos7/tidb-server.tar.gz"
                             donepath="builds/pingcap/tidb-check/pr/${REFS.pulls[0].sha}/centos7/done"
                             curl -F \${filepath}=@tidb-server.tar.gz \${FILE_SERVER_URL}/upload
-                            curl -F \${donepath}=@done \${FILE_SERVER_URL}/upload                                    
+                            curl -F \${donepath}=@done \${FILE_SERVER_URL}/upload
                             """
                     }
                 }
                 always {
-                    dir("tidb") {
+                    dir(REFS.repo) {
                         archiveArtifacts(artifacts: 'importer.log,tidb-server-check.log', allowEmptyArchive: true)
                     }
                 }
@@ -94,9 +93,41 @@ pipeline {
         }
         stage("Build tidb-server enterprise edition") {
             steps {
-                dir("tidb") {
+                dir(REFS.repo) {
                     sh "make enterprise-prepare enterprise-server-build && ./bin/tidb-server -V"
                 }
+            }
+        }
+        stage("Test plugin") {
+            steps {
+                dir('enterprise-plugin') {
+                    cache(path: "./", includes: '**/*', key: "git/pingcap-inc/enterprise-plugin/rev-${REFS.pulls[0].sha}", restoreKeys: ['git/pingcap-inc/enterprise-plugin/rev-']) {
+                        retry(2) {
+                            script {
+                                component.checkout('git@github.com:pingcap-inc/enterprise-plugin.git', 'plugin', REFS.base_ref, REFS.pulls[0].title, GIT_CREDENTIALS_ID)
+                            }
+                        }
+                    }
+                }
+                sh label: 'Test plugins', script: """
+                    mkdir -p plugin-so
+
+                    # compile go plugin: audit
+                    pushd enterprise-plugin/audit && go mod tidy && popd
+                    pushd ${REFS.repo} && go run ./cmd/pluginpkg -pkg-dir ../enterprise-plugin/audit -out-dir ../plugin-so && popd
+
+                    # compile go plugin: whitelist
+                    pushd enterprise-plugin/whitelist && go mod tidy && popd
+                    pushd ${REFS.repo} && go run ./cmd/pluginpkg -pkg-dir ../enterprise-plugin/whitelist -out-dir ../plugin-so && popd
+
+                    # test them.
+                    make server -C ${REFS.repo}
+                    ./${REFS.repo}/bin/tidb-server -plugin-dir=./plugin-so -plugin-load=audit-1,whitelist-1 | tee ./loading-plugin.log &
+
+                    sleep 30
+                    ps aux | grep tidb-server
+                    killall -9 -r tidb-server
+                """
             }
         }
     }
