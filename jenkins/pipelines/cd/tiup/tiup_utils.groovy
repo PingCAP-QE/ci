7facf0e5159506cb400fb5d97f326c463e676b76
lijie <lijie@pingcap.com>
2024-01-08 13:35:56 +0800

    fix: fix tiup version to 1.13
    Signed-off-by: lijie <lijie@pingcap.com>

--- jenkins/pipelines/cd/tiup/tiup_utils.groovy
+++ jenkins/pipelines/cd/tiup/tiup_utils.groovy
@@ -1,7 +1,7 @@
 def install_tiup(bin_dir,private_key) {
     sh """
-    wget -q https://tiup-mirrors.pingcap.com/tiup-linux-amd64.tar.gz
-    sudo tar -zxf tiup-linux-amd64.tar.gz -C ${bin_dir}
+    wget -q https://tiup-mirrors.pingcap.com/tiup-v1.13.0-linux-amd64.tar.gz
+    sudo tar -zxf tiup-v1.13.0-linux-amd64.tar.gz -C ${bin_dir}
     sudo chmod 755 ${bin_dir}/tiup
     rm -rf ~/.tiup
     mkdir -p /home/jenkins/.tiup/bin/
@@ -15,8 +15,8 @@ def install_tiup(bin_dir,private_key) {
 
 def install_tiup_without_key(bin_dir) {
     sh """
-    wget -q ${TIUP_MIRROR}/tiup-linux-amd64.tar.gz
-    tar -zxf tiup-linux-amd64.tar.gz -C ${bin_dir}
+    wget -q ${TIUP_MIRROR}/tiup-v1.13.0-linux-amd64.tar.gz
+    tar -zxf tiup-v1.13.0-linux-amd64.tar.gz -C ${bin_dir}
     chmod 755 ${bin_dir}/tiup
     rm -rf ~/.tiup
     mkdir -p ~/.tiup/bin
