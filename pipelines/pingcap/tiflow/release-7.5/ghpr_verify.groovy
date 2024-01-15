9cab4e24e38d412e76a875ddd926c385e982173b
Purelind <purelind@users.noreply.github.com>
2024-01-15 15:27:15 +0800

    fix(tiflow): not upload to codecov on release branch (#2745)
    Not upload to codecov on lts release branch.

--- pipelines/pingcap/tiflow/release-7.5/ghpr_verify.groovy
+++ pipelines/pingcap/tiflow/release-7.5/ghpr_verify.groovy
@@ -83,21 +83,6 @@ pipeline {
                             }
                         }
                         post {
-                            success {
-                                dir('tiflow') {
-                                    script {
-                                        def testConfigs = [
-                                            dm_unit_test_in_verify_ci: [flags: "unit", coverage_file: "/tmp/dm_test/cov.unit_test.out"],
-                                            unit_test_in_verify_ci: [flags: "unit", coverage_file: "/tmp/tidb_cdc_test/cov.unit.out"],
-                                            engine_unit_test_in_verify_ci: [flags: "unit", coverage_file: "/tmp/engine_test/cov.unit_test.out"]
-                                        ]
-                                        def config = testConfigs[TEST_CMD]
-                                        if (config && config.coverage_file) {
-                                            prow.uploadCoverageToCodecov(REFS, config.flags, config.coverage_file)
-                                        }
-                                    }
-                                }
-                            }
                             always {
                                 junit(testResults: "**/tiflow/*-junit-report.xml", allowEmptyResults : true)  
                             }
