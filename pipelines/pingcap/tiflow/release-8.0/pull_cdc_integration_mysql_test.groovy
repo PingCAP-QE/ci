89f18647fa9a9999d6e769cdfdd3d71ae29196e2
Purelind <purelind@users.noreply.github.com>
2024-03-18 18:02:12 +0800

    fix(tiflow): correct job name on 8.0 (#2856)
    Correct job name on 8.0
    
    Signed-off-by: purelind <purelind@gmail.com>

--- pipelines/pingcap/tiflow/release-8.0/pull_cdc_integration_mysql_test.groovy
+++ pipelines/pingcap/tiflow/release-8.0/pull_cdc_integration_mysql_test.groovy
@@ -7,7 +7,7 @@ final K8S_NAMESPACE = "jenkins-tiflow"
 final GIT_FULL_REPO_NAME = 'pingcap/tiflow'
 final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
 final GIT_CREDENTIALS_ID2 = 'github-pr-diff-token'
-final POD_TEMPLATE_FILE = 'pipelines/pingcap/tiflow/release-8.0/pod-pull_cdc_integration_test.yaml'
+final POD_TEMPLATE_FILE = 'pipelines/pingcap/tiflow/release-8.0/pod-pull_cdc_integration_mysql_test.yaml'
 final REFS = readJSON(text: params.JOB_SPEC).refs
 def skipRemainingStages = false
 
