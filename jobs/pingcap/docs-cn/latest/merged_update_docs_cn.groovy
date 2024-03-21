fe1394a1ff99ea3fd0da58937fe07cd5d3528cbe
Purelind <purelind@users.noreply.github.com>
2024-03-21 14:32:13 +0800

    feat(pingcap/docs-cn): add docs-cn postsubmit (#2736)
    add docs-cn postsubmit.

---
+++ jobs/pingcap/docs-cn/latest/merged_update_docs_cn.groovy
@@ -0,0 +1,38 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For trunk and latest release branches.
+pipelineJob('pingcap/docs-cn/merged_update_docs_cn') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
+    }
+    properties {
+        githubProjectUrl("https://github.com/pingcap/docs-cn")
+    }
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/pingcap/docs-cn/latest/merged_update_docs_cn.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        } 
+                    }
+                }
+            }
+        }
+    }
+}
