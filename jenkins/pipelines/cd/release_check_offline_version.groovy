f880d480739143c74689f50ae13adea7f20b0fd4
purelind <purelind@gmail.com>
2024-04-01 10:31:46 +0800

    feat: update release check pipeline

---
+++ jenkins/pipelines/cd/release_check_offline_version.groovy
@@ -0,0 +1,156 @@
+
+podYaml = """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: check
+    image: hub.pingcap.net/jenkins/release-check-version:v20240326
+    imagePullPolicy: Always
+    args: ["sleep", "infinity"]
+"""
+
+pipeline {
+    parameters {
+        string(
+            defaultValue: "v8.0.0",
+            description: 'The release version to check',
+            name: 'VERSION',
+            trim: true
+        )
+        string(
+            defaultValue: 'https://raw.githubusercontent.com/purelind/test-ci/main/components-v8.0.0.json' ,
+            name: 'COMPONENT_JSON_URL', 
+            description: 'The URL of the component json file',
+            trim: true
+        )
+    }
+    agent none
+    options {
+        timeout(time: 40, unit: 'MINUTES')
+    }
+    stages {
+        stage("Check URL") {
+            agent {
+                kubernetes {
+                    yaml podYaml
+                    defaultContainer 'check'
+                    nodeSelector "kubernetes.io/arch=amd64"
+                }
+            }
+            steps {
+                script {
+                    if (params.COMPONENT_JSON_URL == "") {
+                        error("The COMPONENT_JSON_URL is empty")
+                    }
+                    dir("release-check-offline") { 
+                        script {
+                            sh """
+
+                            git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                            cd scripts/ops/release-check-version
+                            python3 check_offline_package.py quick ${VERSION} community amd64 --components_url="${params.COMPONENT_JSON_URL}"
+                            """
+                        }
+                    }
+                }
+            }
+        }
+        stage("MultiPlatform check") {
+            parallel {
+                stage('linux/amd64 community') {
+                    agent {
+                        kubernetes {
+                            yaml podYaml
+                            defaultContainer 'check'
+                            nodeSelector "kubernetes.io/arch=amd64"
+                        }
+                    }
+                    steps {
+                        dir("release-check-version") {
+                            script {
+                                sh """
+                                tiup --version
+                                python3 --version
+
+                                git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                                cd scripts/ops/release-check-version
+                                python3 check_offline_package.py details ${params.VERSION} community amd64 --components_url="${params.COMPONENT_JSON_URL}"
+                                """
+                            }
+                        }
+                    }
+                }
+                stage('linux/arm64 community') {
+                    agent {
+                        kubernetes {
+                            yaml podYaml
+                            defaultContainer 'check'
+                            nodeSelector "kubernetes.io/arch=arm64"
+                        }
+                    }
+                    steps {
+                        dir("release-check-version") {
+                            script {
+                                sh """
+                                tiup --version
+                                python3 --version
+
+                                git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                                cd scripts/ops/release-check-version
+                                python3 check_offline_package.py details ${params.VERSION} community arm64 --components_url="${params.COMPONENT_JSON_URL}"
+                                """
+                            }
+                        }
+                    }
+                }
+                stage('linux/amd64 enterprise') {
+                    agent {
+                        kubernetes {
+                            yaml podYaml
+                            defaultContainer 'check'
+                            nodeSelector "kubernetes.io/arch=amd64"
+                        }
+                    }
+                    steps {
+                        dir("release-check-version") {
+                            script {
+                                sh """
+                                tiup --version
+                                python3 --version
+
+                                git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                                cd scripts/ops/release-check-version
+                                python3 check_offline_package.py details ${params.VERSION} enterprise amd64 --components_url="${params.COMPONENT_JSON_URL}"
+                                """
+                            }
+                        }
+                    }
+                }
+                stage('linux/arm64 enterprise') {
+                    agent {
+                        kubernetes {
+                            yaml podYaml
+                            defaultContainer 'check'
+                            nodeSelector "kubernetes.io/arch=arm64"
+                        }
+                    }
+                    steps {
+                        dir("release-check-version") {
+                            script {
+                                sh """
+                                tiup --version
+                                python3 --version
+
+                                git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                                cd scripts/ops/release-check-version
+                                python3 check_offline_package.py details ${params.VERSION} enterprise arm64 --components_url="${params.COMPONENT_JSON_URL}"
+                                """
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- jenkins/pipelines/cd/release_check_version.groovy
+++ jenkins/pipelines/cd/release_check_version.groovy
@@ -19,6 +19,10 @@ spec:
       value: ""
     - name: DOCKER_HOST
       value: tcp://localhost:2375
+    resources:
+      requests:
+        cpu: 4
+        memory: 8Gi
     securityContext:
       privileged: true
     tty: true
@@ -32,14 +36,20 @@ spec:
 pipeline {
     parameters {
         booleanParam(
-          defaultValue: true,
+          defaultValue: false,
           description: 'Whether this is a release candidate build check',
           name: 'IS_RC_BUILD'
         )
         string(
-            defaultValue: 'https://raw.githubusercontent.com/purelind/test-ci/main/components.json' ,
+            defaultValue: 'https://raw.githubusercontent.com/purelind/test-ci/main/components-v8.0.0.json' ,
             name: 'COMPONENT_JSON_URL', 
             description: 'The URL of the component json file',
+            trim: true
+        )
+        string(
+            defaultValue: 'v8.0.0' ,
+            name: 'VERSION', 
+            description: 'The release version to check',
             trim: true
         )
     }
@@ -65,6 +75,7 @@ pipeline {
                                 tiup --version
                                 python3 --version
                                 docker version
+                                docker system info
                                 git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
                                 cd scripts/ops/release-check-version
                                 python3 main.py image --components_url="${params.COMPONENT_JSON_URL}"
@@ -88,6 +99,7 @@ pipeline {
                                 tiup --version
                                 python3 --version
                                 docker version
+                                docker system info
                                 git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
                                 cd scripts/ops/release-check-version
                                 python3 main.py image --components_url="${params.COMPONENT_JSON_URL}"
@@ -96,96 +108,96 @@ pipeline {
                         }
                     }
                 }
-                stage('linux/amd64 tiup') {
-                    agent {
-                        kubernetes {
-                            yaml podYaml
-                            defaultContainer 'check'
-                            nodeSelector "kubernetes.io/arch=amd64"
-                        }
-                    }
-                    steps {
-                        dir("release-check-version") {
-                            script {
-                                sh """
-                                git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
-                                cd scripts/ops/release-check-version
-                                python3 main.py tiup --components_url="${params.COMPONENT_JSON_URL}"
-                                """
-                            }
-                        }
-                    }
-                }
-                stage('linux/arm64 tiup') {
-                    agent {
-                        kubernetes {
-                            yaml podYaml
-                            defaultContainer 'check'
-                            nodeSelector "kubernetes.io/arch=arm64"
-                        }
-                    }
-                    steps {
-                        dir("release-check-version") {
-                            script {
-                                sh """
-                                git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
-                                cd scripts/ops/release-check-version
-                                python3 main.py tiup --components_url="${params.COMPONENT_JSON_URL}"
-                                """
-                            }
-                        }
-                    }
-                }
+                // stage('linux/amd64 tiup') {
+                //     agent {
+                //         kubernetes {
+                //             yaml podYaml
+                //             defaultContainer 'check'
+                //             nodeSelector "kubernetes.io/arch=amd64"
+                //         }
+                //     }
+                //     steps {
+                //         dir("release-check-version") {
+                //             script {
+                //                 sh """
+                //                 git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                //                 cd scripts/ops/release-check-version
+                //                 python3 main.py tiup --components_url="${params.COMPONENT_JSON_URL}"
+                //                 """
+                //             }
+                //         }
+                //     }
+                // }
+                // stage('linux/arm64 tiup') {
+                //     agent {
+                //         kubernetes {
+                //             yaml podYaml
+                //             defaultContainer 'check'
+                //             nodeSelector "kubernetes.io/arch=arm64"
+                //         }
+                //     }
+                //     steps {
+                //         dir("release-check-version") {
+                //             script {
+                //                 sh """
+                //                 git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                //                 cd scripts/ops/release-check-version
+                //                 python3 main.py tiup --components_url="${params.COMPONENT_JSON_URL}"
+                //                 """
+                //             }
+                //         }
+                //     }
+                // }
 
-                stage('darwin/arm64 tiup') {
-                    agent {
-                        node {
-                            label 'darwin && arm64'
-                        }
-                    }
-                    steps {
-                        dir("release-check-tiup") {  
-                            deleteDir()
-                            sh """
-                                hostname
-                                ifconfig | grep 172
-                                git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
-                                cd scripts/ops/release-check-version
-                                python3 -m venv .venv
-                                source .venv/bin/activate
-                                pip install -r requirements.txt
-                                export PATH=\$PATH:\$HOME/.tiup/bin
-                                which tiup
-                                python3 main.py tiup --components_url="${params.COMPONENT_JSON_URL}"
-                            """
-                        }
+                // stage('darwin/arm64 tiup') {
+                //     agent {
+                //         node {
+                //             label 'darwin && arm64'
+                //         }
+                //     }
+                //     steps {
+                //         dir("release-check-tiup") {  
+                //             deleteDir()
+                //             sh """
+                //                 hostname
+                //                 ifconfig | grep 172
+                //                 git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                //                 cd scripts/ops/release-check-version
+                //                 python3 -m venv .venv
+                //                 source .venv/bin/activate
+                //                 pip install -r requirements.txt
+                //                 export PATH=\$PATH:\$HOME/.tiup/bin
+                //                 which tiup
+                //                 python3 main.py tiup --components_url="${params.COMPONENT_JSON_URL}"
+                //             """
+                //         }
                         
-                    }
-                }
-                stage('darwin/amd64 tiup') {
-                    agent {
-                        node {
-                            label 'darwin && amd64'
-                        }
-                    }
-                    steps {
-                        dir("release-check-tiup") { 
-                            deleteDir()
-                            sh """
-                                hostname
-                                ifconfig | grep 172
-                                git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
-                                cd scripts/ops/release-check-version
-                                python3 -m venv .venv
-                                source .venv/bin/activate
-                                pip install -r requirements.txt
-                                export PATH=\$PATH:\$HOME/.tiup/bin
-                                which tiup
-                                python3 main.py tiup --components_url="${params.COMPONENT_JSON_URL}" 
-                            """
-                        }
-                    }
-                }
+                //     }
+                // }
+                // stage('darwin/amd64 tiup') {
+                //     agent {
+                //         node {
+                //             label 'darwin && amd64'
+                //         }
+                //     }
+                //     steps {
+                //         dir("release-check-tiup") { 
+                //             deleteDir()
+                //             sh """
+                //                 hostname
+                //                 ifconfig | grep 172
+                //                 git clone --branch purelind/add-release-check-version --depth 1 https://github.com/purelind/ci-1.git .
+                //                 cd scripts/ops/release-check-version
+                //                 python3 -m venv .venv
+                //                 source .venv/bin/activate
+                //                 pip install -r requirements.txt
+                //                 export PATH=\$PATH:\$HOME/.tiup/bin
+                //                 which tiup
+                //                 python3 main.py tiup --components_url="${params.COMPONENT_JSON_URL}" 
+                //             """
+                //         }
+                //     }
+                // }
             }
         }
     }
