e5fa71132d1e7a17d95cfb1623a76d6f9f60fdf3
Purelind <purelind@users.noreply.github.com>
2024-04-02 20:33:17 +0800

    feat: enable debug test for tikv refactor ut (#2844)
    enable debug test for tikv refactor ut.

--- pipelines/tikv/tikv/latest/pull_unit_test.groovy
+++ pipelines/tikv/tikv/latest/pull_unit_test.groovy
@@ -8,7 +8,7 @@ final GIT_FULL_REPO_NAME = 'tikv/tikv'
 final POD_TEMPLATE_FILE = 'pipelines/tikv/tikv/latest/pod-pull_unit_test.yaml'
 final REFS = readJSON(text: params.JOB_SPEC).refs
 
-final EXTRA_NEXTEST_ARGS = "-j 16"
+final EXTRA_NEXTEST_ARGS = "-j 8"
 
 
 pipeline {
@@ -17,14 +17,15 @@ pipeline {
             namespace K8S_NAMESPACE
             yamlFile POD_TEMPLATE_FILE
             defaultContainer 'runner'
-            workspaceVolume emptyDirWorkspaceVolume(memory: true)
+            // workspaceVolume emptyDirWorkspaceVolume(memory: true)
         }
     }
     environment {
         FILE_SERVER_URL = 'http://fileserver.pingcap.net'
+        TIKV_TEST_MEMORY_DISK_MOUNT_POINT = "/home/jenkins/agent/memvolume"
     }
     options {
-        timeout(time: 45, unit: 'MINUTES')
+        timeout(time: 50, unit: 'MINUTES')
         parallelsAlwaysFailFast()
         skipDefaultCheckout()
     }
@@ -34,7 +35,10 @@ pipeline {
                 sh label: 'Debug info', script: """
                     printenv
                     echo "-------------------------"
-                    go env
+                    env
+                    hostname
+                    df -h
+                    free -hm
                     echo "-------------------------"
                     echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
                 """
@@ -54,6 +58,12 @@ pipeline {
                             }
                         }
                     }
+                    sh """
+                        ls -alh
+                        ln -s \$HOME/tikv-target ${WORKSPACE}/tikv/target
+                        pwd && ls -alh
+                    """
+                    
                 }
             }
         }
@@ -62,13 +72,11 @@ pipeline {
                 dir("tikv") {
                     retry(2) {
                         sh label: 'Run lint: format', script: """
-                            cd \$HOME/tikv-src
                             export RUSTFLAGS=-Dwarnings
                             make format
                             git diff --quiet || (git diff; echo Please make format and run tests before creating a PR; exit 1)
                         """
                         sh label: 'Run lint: clippy', script: """
-                            cd \$HOME/tikv-src
                             export RUSTFLAGS=-Dwarnings
                             export FAIL_POINT=1
                             export ROCKSDB_SYS_SSE=1
@@ -87,7 +95,6 @@ pipeline {
                 dir("tikv") {
                     retry(2) {
                         sh label: 'Build test artifact', script: """
-                            cd \$HOME/tikv-src
                             export RUSTFLAGS=-Dwarnings
                             export FAIL_POINT=1
                             export ROCKSDB_SYS_SSE=1
@@ -107,6 +114,19 @@ pipeline {
                             cp ${WORKSPACE}/scripts/tikv/tikv/gen_test_binary_json.py ./gen_test_binary_json.py
                             python gen_test_binary_json.py
                             cat test-binaries.json
+
+                            # archive test artifacts
+                            ls -alh archive-test-binaries
+                            tar -cvf archive-test-binaries.tar archive-test-binaries
+                            ls -alh archive-test-binaries.tar
+                            tar czf test-artifacts.tar.gz test-binaries test-binaries.json test-metadata.json Cargo.toml cmd src tests components .config `ls target/*/deps/*plugin.so 2>/dev/null`
+                            ls -alh test-artifacts.tar.gz
+                            mkdir -p /home/jenkins/archives
+                            mv test-artifacts.tar.gz archive-test-binaries.tar /home/jenkins/archives/
+                        """
+                        sh label: 'archive test artifact', script: """
+                            cp test-metadata.json ${WORKSPACE}/tikv/target/
+                            cp test-binaries.json ${WORKSPACE}/tikv/target/
                         """
                     }
                 }
@@ -115,21 +135,44 @@ pipeline {
         stage("Test") {
             options { timeout(time: 30, unit: 'MINUTES') }
             steps {
-                dir('tikv') {
+                dir('/home/jenkins/agent/tikv-presubmit/unit-test') {
+                    sh label: "clean up", script: """
+                        rm -rf /home/jenkins/tikv-*
+                        ls -alh /home/jenkins/
+                        ln -s `pwd` \$HOME/tikv-src
+                        mkdir -p target/debug
+                        uname -a
+                        df -h
+                        free -hm
+
+                        # prepare test artifacts
+                        cp /home/jenkins/archives/test-artifacts.tar.gz .
+                        cp /home/jenkins/archives/archive-test-binaries.tar .
+                        tar -xf test-artifacts.tar.gz
+                        tar xf archive-test-binaries.tar --strip-components=1
+                        rm -f test-artifacts.tar.gz archive-test-binaries.tar
+                        ls -la
+                        ls -alh target/debug/deps/
+                    """
                     sh """
-                    cd \$HOME/tikv-src
-                    ls -alh
+                    ls -alh \$HOME/tikv-src
                     ls -alh /home/jenkins/tikv-src/
                     ls -alh /home/jenkins/tikv-src/target/debug/deps/
-
                     export RUSTFLAGS=-Dwarnings
                     export FAIL_POINT=1
                     export RUST_BACKTRACE=1
                     export MALLOC_CONF=prof:true,prof_active:false
-                    export CI=1  # TODO: remove this
+                    # export CI=1  # TODO: remove this
                     export LOG_FILE=/home/jenkins/tikv-src/target/my_test.log
 
-                    if cargo nextest run -P ci --binaries-metadata test-binaries.json --cargo-metadata test-metadata.json ${EXTRA_NEXTEST_ARGS}; then
+                    if cargo nextest run -P ci --binaries-metadata test-binaries.json --cargo-metadata test-metadata.json --partition count:1/2 ${EXTRA_NEXTEST_ARGS}; then
+                        echo "test pass"
+                    else
+                        # test failed
+                        gdb -c core.* -batch -ex "info threads" -ex "thread apply all bt"
+                        exit 1
+                    fi
+                    if cargo nextest run -P ci --binaries-metadata test-binaries.json --cargo-metadata test-metadata.json --partition count:2/2 ${EXTRA_NEXTEST_ARGS}; then
                         echo "test pass"
                     else
                         # test failed
