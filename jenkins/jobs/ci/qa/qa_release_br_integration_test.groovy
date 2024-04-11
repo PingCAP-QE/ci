91c97efa28d809cb8e65d3129ee086b1e9052069
purelind <purelind@gmail.com>
2024-04-11 18:30:51 +0800

    feat: add two qa-release-test pipeline
    Signed-off-by: purelind <purelind@gmail.com>

---
+++ jenkins/jobs/ci/qa/qa_release_br_integration_test.groovy
@@ -0,0 +1,41 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+pipelineJob('qa-release-br-integration-test') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam {
+            name('RELEASE_TAG')
+            defaultValue('v8.1.0')
+            description('release tag')
+            trim(true)
+        }
+        stringParam {
+            name('RELEASE_BRANCH')
+            defaultValue('release-8.1')
+            description('release branch')
+            trim(true)
+        }
+    }
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("jenkins/pipelines/qa/qa-release-br-integration-test.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
