2b4081ae5af9747d83e716c9f5277ab25d8eae4b
wuhuizuo <wuhuizuo@126.com>
2024-05-26 17:55:06 +0800

    Define code ownership using "OWNERS" files in the central CI config repo
    Fixes #2974
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

---
+++ .ci/update-prow-job-kustomization.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+yq -i ".configMapGenerator[0].files = []" prow-jobs/kustomization.yaml
+key_index=0
+for f in $(find prow-jobs -name "*.yaml" -type f | sed 's|prow-jobs/||' | grep -v kustomization.yaml | sort); do
+    # I want format the key_index with 3 char width.
+    ((++key_index))
+    key=$(printf "%03d.yaml" $key_index)
+    yq -i ".configMapGenerator[0].files += \"$key=$f\"" prow-jobs/kustomization.yaml
+done
+
+# use git diff to judge if the file is changed, if yes ,then print the error message and exit with 1.
+git diff --exit-code prow-jobs/kustomization.yaml
+if [ $? -ne 0 ]; then
+    echo "prow-jobs/kustomization.yaml is not up to date, please run '.ci/update-prow-job-kustomization.sh' and commit the changes"
+    exit 1
+fi
---
+++ prow-jobs/README.md
@@ -0,0 +1,10 @@
+Prow job definations
+===
+
+## How to add a job
+
+1. Add in a existed yaml job yaml file or create a new job yaml file and write the job yaml.
+2. Run the updating script : [](../.ci/update-prow-job-kustomization.sh)
+   ```bash
+   .ci/update-prow-job-kustomization.sh
+   ```
