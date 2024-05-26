38a564754587a693f9a062e1cc78cc385e6ff821
wuhuizuo <wuhuizuo@126.com>
2024-05-26 19:40:14 +0800

    ci(pingcap-qe/ci): fix presubmits job
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- .ci/update-prow-job-kustomization.sh
+++ .ci/update-prow-job-kustomization.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-yq -i ".configMapGenerator[0].files = []" prow-jobs/kustomization.yaml
+sed -i 's/^[[:blank:]]*"files": \[\]$/&/' prow-jobs/kustomization.yaml
 key_index=0
 for f in $(find prow-jobs -name "*.yaml" -type f | sed 's|prow-jobs/||' | grep -v kustomization.yaml | sort); do
     # I want format the key_index with 3 char width.
-    ((++key_index))
+    key_index=$((key_index + 1))
     key=$(printf "%03d.yaml" $key_index)
-    yq -i ".configMapGenerator[0].files += \"$key=$f\"" prow-jobs/kustomization.yaml
+    sed -i "s/^[[:blank:]]*\"files\": \[$/$&\\n    \"$key=$f\",/" prow-jobs/kustomization.yaml
 done
 
 # use git diff to judge if the file is changed, if yes ,then print the error message and exit with 1.
-git diff --exit-code prow-jobs/kustomization.yaml
-if [ $? -ne 0 ]; then
+if ! git diff --exit-code prow-jobs/kustomization.yaml >/dev/null; then
     echo "prow-jobs/kustomization.yaml is not up to date, please run '.ci/update-prow-job-kustomization.sh' and commit the changes"
     exit 1
 fi
