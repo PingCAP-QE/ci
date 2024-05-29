3b65b8e61dc147aac13fe5f82c66c09d570439e1
wuhuizuo <wuhuizuo@126.com>
2024-05-29 19:44:48 +0800

    ci: fix script update-prow-job-kustomization.sh
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- .ci/update-prow-job-kustomization.sh
+++ .ci/update-prow-job-kustomization.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-sed -i 's/^[[:blank:]]*"files": \[\]$/&/' prow-jobs/kustomization.yaml
+yq -i ".configMapGenerator[0].files = []" prow-jobs/kustomization.yaml
 key_index=0
 for f in $(find prow-jobs -name "*.yaml" -type f | sed 's|prow-jobs/||' | grep -v kustomization.yaml | sort); do
     # I want format the key_index with 3 char width.
-    key_index=$((key_index + 1))
+    ((++key_index))
     key=$(printf "%03d.yaml" $key_index)
-    sed -i "s/^[[:blank:]]*\"files\": \[$/$&\\n    \"$key=$f\",/" prow-jobs/kustomization.yaml
+    yq -i ".configMapGenerator[0].files += \"$key=$f\"" prow-jobs/kustomization.yaml
 done
 
 # use git diff to judge if the file is changed, if yes ,then print the error message and exit with 1.
