b635361ff1812e8749ff813ef4539af99cf0da95
wuhuizuo <wuhuizuo@126.com>
2024-06-05 13:48:05 +0000

    feat(prow-jobs/tikv/raft-engine): add prow job
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- .ci/update-prow-job-kustomization.sh
+++ .ci/update-prow-job-kustomization.sh
@@ -2,7 +2,7 @@
 
 yq -i ".configMapGenerator[0].files = []" prow-jobs/kustomization.yaml
 key_index=0
-for f in $(find prow-jobs -name "*.yaml" -type f | sed 's|prow-jobs/||' | grep -v kustomization.yaml | sort); do
+for f in $(find prow-jobs -name "*.yaml" -type f | sed 's|prow-jobs/||' | grep -v kustomization.yaml | LC_COLLATE=C sort); do
     # I want format the key_index with 3 char width.
     ((++key_index))
     key=$(printf "%03d.yaml" $key_index)
@@ -10,7 +10,7 @@ for f in $(find prow-jobs -name "*.yaml" -type f | sed 's|prow-jobs/||' | grep -
 done
 
 # use git diff to judge if the file is changed, if yes ,then print the error message and exit with 1.
-if ! git diff --exit-code prow-jobs/kustomization.yaml >/dev/null; then
+if ! git diff --exit-code prow-jobs/kustomization.yaml; then
     echo "prow-jobs/kustomization.yaml is not up to date, please run '.ci/update-prow-job-kustomization.sh' and commit the changes"
     exit 1
 fi
