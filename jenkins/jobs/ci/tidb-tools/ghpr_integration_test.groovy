e6b214c01cc9077595a7f24163942db58826396c
Purelind <purelind@users.noreply.github.com>
2024-06-14 17:34:14 +0800

    feat: add jobdsl for old tidb-tools pipeline (#3001)
    add jobdsl for old tidb-tools pipeline

---
+++ jenkins/jobs/ci/tidb-tools/ghpr_integration_test.groovy
@@ -0,0 +1,89 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+pipelineJob('tidb_tools_ghpr_integration_test') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam("ghprbActualCommit")
+        stringParam("ghprbPullId")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tidb-tools")
+        pipelineTriggers {
+            triggers {
+                ghprbTrigger {
+                    cron('H/5 * * * *')
+                    gitHubAuthId('8b25795b-a680-4dce-9904-89ef40d73159')
+
+                    triggerPhrase('.*/(run(-all-tests|-integration-test|-integration-common-test).*)')
+                    onlyTriggerPhrase(false)
+                    skipBuildPhrase(".*skip-ci.*")
+                    buildDescTemplate('PR #$pullId: $abbrTitle\n$url')
+                    orgslist("pingcap")
+                    whiteListTargetBranches {
+                        ghprbBranch { branch('master') }
+                        ghprbBranch { branch('^(release-)?4\\.\\d+(\\.\\d+)?(\\-.*)?$') }
+                        ghprbBranch { branch('^(release-)?[5]\\.[0-4](\\.\\d+)?(\\-.*)?$') }
+                    }
+                    // ignore when only those file changed.(
+                    //   multi line regex
+                    // excludedRegions('.*\\.md')
+                    excludedRegions('') // current the context is required in github branch protection.
+
+                    blackListLabels("")
+                    whiteListLabels("")
+                    adminlist("")
+                    blackListCommitAuthor("")
+                    includedRegions("")
+                    commentFilePath("")
+
+                    allowMembersOfWhitelistedOrgsAsAdmin(true)
+                    permitAll(true)
+                    useGitHubHooks(true)
+                    displayBuildErrorsOnDownstreamBuilds(false)
+                    autoCloseFailedPullRequests(false)
+
+                    // useless, but can not delete.
+                    commitStatusContext("--none--")
+                    msgSuccess("--none--")
+                    msgFailure("--none--")
+
+                    extensions {
+                        ghprbCancelBuildsOnUpdate { overrideGlobal(true) }
+                        ghprbSimpleStatus {
+                            commitStatusContext("idc-jenkins-ci-tidb-tools/integration-test")
+                            statusUrl('${RUN_DISPLAY_URL}')
+                            startedStatus("Jenkins job is running.")
+                            triggeredStatus("Jenkins job triggered.")
+                            addTestResults(false)
+                            showMatrixStatus(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("jenkins/pipelines/ci/tidb-tools/tools_ghpr_integration_test.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
