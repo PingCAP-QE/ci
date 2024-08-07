0a04b03187659234aa0bc6d1c4e6975943921fa8
wuhuizuo <wuhuizuo@126.com>
2024-08-07 11:46:37 +0800

    feat(pingcap/tiflash): add refactored jobs for release-8.1
    Fixes #3063
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

---
+++ jobs/pingcap/tiflash/release-8.1/pull_unit_test.groovy
@@ -0,0 +1,41 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+final folder = 'pingcap/tiflash/release-8.1'
+final jobName = 'pull_unit_test'
+
+pipelineJob("${folder}/${jobName}") {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC", "", "Prow job spec struct data")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tiflash")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/${folder}/${jobName}.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
