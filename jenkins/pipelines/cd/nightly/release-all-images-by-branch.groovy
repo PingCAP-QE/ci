02e9f8139872c9b9f2c5d3cb6f869003a7b762e8
purelind <purelind@gmail.com>
2024-10-10 11:34:14 +0800

    feat: bump nightly build version to v8.5.0-alpha

--- jenkins/pipelines/cd/nightly/release-all-images-by-branch.groovy
+++ jenkins/pipelines/cd/nightly/release-all-images-by-branch.groovy
@@ -46,7 +46,7 @@ HARBOR_PROJECT_PREFIX = "hub.pingcap.net/qa"
 
 
 // for master branch: use default local tag: v6.1.0-nightly
-RELEASE_TAG = "v8.4.0-alpha"
+RELEASE_TAG = "v8.5.0-alpha"
 if (GIT_BRANCH.startsWith("release-")) {
     RELEASE_TAG = "v" + trimPrefix(GIT_BRANCH) + ".0-nightly"
 }
