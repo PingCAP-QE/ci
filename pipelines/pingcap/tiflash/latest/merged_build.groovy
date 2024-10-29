ce3d42c0adc918eae28c92f962d582b2f866c7c5
purelind <purelind@gmail.com>
2024-10-29 15:46:02 +0800

    feat: add merge-build to update build ccache

--- pipelines/pingcap/tiflash/latest/merged_build.groovy
+++ pipelines/pingcap/tiflash/latest/merged_build.groovy
@@ -6,7 +6,7 @@
 final K8S_NAMESPACE = "jenkins-tiflash"  // TODO: need to adjust namespace after test
 final GIT_FULL_REPO_NAME = 'pingcap/tiflash'
 final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
-final POD_TEMPLATE_FILE = 'pipelines/pingcap/tiflash/latest/pod-merged_unit_test.yaml'
+final POD_TEMPLATE_FILE = 'pipelines/pingcap/tiflash/latest/pod-merged_build.yaml'
 final REFS = readJSON(text: params.JOB_SPEC).refs
 final PARALLELISM = 12
 final dependency_dir = "/home/jenkins/agent/dependency"
@@ -96,40 +96,41 @@ pipeline {
             parallel {
                 stage("Ccache") {
                     steps {
-                    script {
-                        dir("tiflash") {
-                            sh label: "copy ccache if exist", script: """
-                            pwd & ls -alh
-                            ccache_tar_file="/home/jenkins/agent/ccache-4.10.2/pagetools-tests-amd64-linux-llvm-debug-${REFS.base_ref}-failpoints.tar"
-                            if [ -f \$ccache_tar_file ]; then
-                                echo "ccache found"
-                                cd /tmp
-                                cp -r \$ccache_tar_file ccache.tar
-                                tar -xf ccache.tar
-                                ls -lha /tmp
-                            else
-                                echo "ccache not found"
-                            fi
-                            """
-                            sh label: "config ccache", script: """
-                            ccache -o cache_dir="/tmp/.ccache"
-                            ccache -o max_size=2G
-                            ccache -o limit_multiple=0.99
-                            ccache -o hash_dir=false
-                            ccache -o compression=true
-                            ccache -o compression_level=6
-                            ccache -o read_only=true
-                            ccache -z
-                            """
+                        script { 
+                            dir("tiflash") {
+                                sh label: "copy ccache if exist", script: """
+                                ccache_tar_file="/home/jenkins/agent/ccache-4.10.2/tiflash-amd64-linux-llvm-debug-${REFS.base_ref}-failpoints.tar"
+                                if [ -f \$ccache_tar_file ]; then
+                                    echo "ccache found"
+                                    cd /tmp
+                                    cp -r \$ccache_tar_file ccache.tar
+                                    tar -xf ccache.tar
+                                    ls -lha /tmp
+                                else
+                                    echo "ccache not found"
+                                fi
+                                """
+                                sh label: "config ccache", script: """
+                                ccache -o cache_dir="/tmp/.ccache"
+                                ccache -o max_size=2G
+                                ccache -o limit_multiple=0.99
+                                ccache -o hash_dir=false
+                                ccache -o compression=true
+                                ccache -o compression_level=6
+                                ccache -o read_only=true
+                                ccache -z
+                                """
+                            }
                         }
                     }
-                    }
+
                 }
                 stage("Proxy-Cache") {
                     steps {
                         script {
-                            proxy_cache_ready = fileExists("/home/jenkins/agent/proxy-cache/${proxy_commit_hash}-amd64-linux-llvm")
+                            proxy_cache_ready = sh(script: "test -f /home/jenkins/agent/proxy-cache/${proxy_commit_hash}-amd64-linux-llvm && echo 'true' || echo 'false'", returnStdout: true).trim() == 'true'
                             println "proxy_cache_ready: ${proxy_cache_ready}"
+
                             sh label: "copy proxy if exist", script: """
                             proxy_suffix="amd64-linux-llvm"
                             proxy_cache_file="/home/jenkins/agent/proxy-cache/${proxy_commit_hash}-\${proxy_suffix}"
@@ -143,54 +144,36 @@ pipeline {
                                 echo "proxy cache not found"
                             fi
                             """
-                            sh label: "link cargo cache", script: """
-                                mkdir -p ~/.cargo/registry
-                                mkdir -p ~/.cargo/git
-                                mkdir -p /home/jenkins/agent/rust/registry/cache
-                                mkdir -p /home/jenkins/agent/rust/registry/index
-                                mkdir -p /home/jenkins/agent/rust/git/db
-                                mkdir -p /home/jenkins/agent/rust/git/checkouts
-
-                                rm -rf ~/.cargo/registry/cache && ln -s /home/jenkins/agent/rust/registry/cache ~/.cargo/registry/cache
-                                rm -rf ~/.cargo/registry/index && ln -s /home/jenkins/agent/rust/registry/index ~/.cargo/registry/index
-                                rm -rf ~/.cargo/git/db && ln -s /home/jenkins/agent/rust/git/db ~/.cargo/git/db
-                                rm -rf ~/.cargo/git/checkouts && ln -s /home/jenkins/agent/rust/git/checkouts ~/.cargo/git/checkouts
-
-                                rm -rf ~/.rustup/tmp
-                                rm -rf ~/.rustup/toolchains
-                                mkdir -p /home/jenkins/agent/rust/rustup-env/tmp
-                                mkdir -p /home/jenkins/agent/rust/rustup-env/toolchains
-                                ln -s /home/jenkins/agent/rust/rustup-env/tmp ~/.rustup/tmp
-                                ln -s /home/jenkins/agent/rust/rustup-env/toolchains ~/.rustup/toolchains
-                            """
                         }   
                     }
                 }
-            }
-        }
-        stage("Build Dependency and Utils") {
-            parallel {
-                stage("Cluster Manage") { 
+                stage("Cargo-Cache") {
                     steps {
-                        // NOTE: cluster_manager is deprecated since release-6.0 (include)
-                        echo "cluster_manager is deprecated"
-                    }
-                }
-                stage("TiFlash Proxy") {
-                    steps {
-                        script {
-                            if (proxy_cache_ready) {
-                                echo "skip becuase of cache"
-                            } else {
-                               echo "skip because proxy build is integrated(llvm)"
-                            }
-                        }
+                        sh label: "link cargo cache", script: """
+                            mkdir -p ~/.cargo/registry
+                            mkdir -p ~/.cargo/git
+                            mkdir -p /home/jenkins/agent/rust/registry/cache
+                            mkdir -p /home/jenkins/agent/rust/registry/index
+                            mkdir -p /home/jenkins/agent/rust/git/db
+                            mkdir -p /home/jenkins/agent/rust/git/checkouts
+
+                            rm -rf ~/.cargo/registry/cache && ln -s /home/jenkins/agent/rust/registry/cache ~/.cargo/registry/cache
+                            rm -rf ~/.cargo/registry/index && ln -s /home/jenkins/agent/rust/registry/index ~/.cargo/registry/index
+                            rm -rf ~/.cargo/git/db && ln -s /home/jenkins/agent/rust/git/db ~/.cargo/git/db
+                            rm -rf ~/.cargo/git/checkouts && ln -s /home/jenkins/agent/rust/git/checkouts ~/.cargo/git/checkouts
+
+                            rm -rf ~/.rustup/tmp
+                            rm -rf ~/.rustup/toolchains
+                            mkdir -p /home/jenkins/agent/rust/rustup-env/tmp
+                            mkdir -p /home/jenkins/agent/rust/rustup-env/toolchains
+                            ln -s /home/jenkins/agent/rust/rustup-env/tmp ~/.rustup/tmp
+                            ln -s /home/jenkins/agent/rust/rustup-env/toolchains ~/.rustup/toolchains
+                        """
                     }
                 }
             }
         }
         stage("Configure Project") {
-            // TODO: need to simplify this part, all config and build logic should be in script in tiflash repo
             steps {
                 script {
                     def toolchain = "llvm"
@@ -221,7 +204,7 @@ pipeline {
                             -DCMAKE_BUILD_TYPE=Debug \\
                             -DCMAKE_PREFIX_PATH='/usr/local' \\
                             -DCMAKE_INSTALL_PREFIX=${WORKSPACE}/install/tiflash \\
-                            -DENABLE_TESTS=true \\
+                            -DENABLE_TESTS=false \\
                             -DUSE_CCACHE=true \\
                             -DDEBUG_WITHOUT_DEBUG_INFO=true \\
                             -DUSE_INTERNAL_TIFLASH_PROXY=${!proxy_cache_ready} \\
@@ -233,96 +216,73 @@ pipeline {
         }
         stage("Build TiFlash") {
             steps {
-                dir("${WORKSPACE}/tiflash") {
-                sh """
-                    cmake --build '${WORKSPACE}/build' --target gtests_dbms gtests_libcommon gtests_libdaemon --parallel ${PARALLELISM}
+                dir("${WORKSPACE}/tiflash") {  
+                    sh """
+                    cmake --build '${WORKSPACE}/build' --target tiflash --parallel 12
                     """
                     sh """
-                    cp '${WORKSPACE}/build/dbms/gtests_dbms' '${WORKSPACE}/install/tiflash/'
-                    cp '${WORKSPACE}/build/libs/libcommon/src/tests/gtests_libcommon' '${WORKSPACE}/install/tiflash/'
-                    cmake --install ${WORKSPACE}/build --component=tiflash-gtest --prefix='${WORKSPACE}/install/tiflash'
+                    cmake --install '${WORKSPACE}/build' --component=tiflash-release --prefix='${WORKSPACE}/install/tiflash'
                     """
-                }
-                dir("${WORKSPACE}/build") {
-                    sh """
-                    target=`realpath \$(find . -executable | grep -v gtests_libdaemon.dir | grep gtests_libdaemon)`
-                    cp \$target '${WORKSPACE}/install/tiflash/'
-                    """
-                }
-                dir("${WORKSPACE}/tiflash") {
                     sh """
                     ccache -s
-                    ls -lha ${WORKSPACE}/install/tiflash/
+                    ls -lha ${WORKSPACE}/install/tiflash
                     """
                 }
             }
         }
-
+        stage("License check") {
+            steps {
+                dir("${WORKSPACE}/tiflash") {
+                    // TODO: add license-eye to docker image
+                    sh label: "license header check", script: """
+                        echo "license check"
+                        if [[ -f .github/licenserc.yml ]]; then
+                            wget -q -O license-eye http://fileserver.pingcap.net/download/cicd/ci-tools/license-eye_v0.4.0
+                            chmod +x license-eye
+                            ./license-eye -c .github/licenserc.yml header check
+                        else
+                            echo "skip license check"
+                            exit 0
+                        fi
+                    """
+                }
+            }
+        }
         stage("Post Build") {
             parallel {
-                stage("Archive Build Artifacts") {
+                stage("Upload Build Artifacts") {
                     steps {
                         dir("${WORKSPACE}/install") {
-                            sh """
+                            sh label: "archive tiflash binary", script: """
                             tar -czf 'tiflash.tar.gz' 'tiflash'
                             """
                             archiveArtifacts artifacts: "tiflash.tar.gz"
-                        }
-                    }
-                }
-                stage("Archive Build Data") {
-                    steps {
-                        dir("${WORKSPACE}/build") {
                             sh """
-                            tar -cavf build-data.tar.xz \$(find . -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name "*.hpp" -o -name "*.gcno" -o -name "*.gcna")
+                            du -sh tiflash.tar.gz
+                            rm -rf tiflash.tar.gz
                             """
-                            archiveArtifacts artifacts: "build-data.tar.xz", allowEmptyArchive: true
-                        }
-                        dir("${WORKSPACE}/tiflash") {
-                            sh """
-                            tar -cavf source-patch.tar.xz \$(find . -name "*.pb.h" -o -name "*.pb.cc")
-                            """
-                            archiveArtifacts artifacts: "source-patch.tar.xz", allowEmptyArchive: true
                         }
                     }
                 }
                 stage("Upload Ccache") {
                     steps {
-                        sh label: "upload ccache", script: """
+                        dir("${WORKSPACE}/tiflash") {
+                            sh label: "upload ccache", script: """
                             cd /tmp
+                            rm -rf ccache.tar
                             tar -cf ccache.tar .ccache
-                            cp ccache.tar /home/jenkins/agent/ccache-4.10.2/pagetools-tests-amd64-linux-llvm-debug-${REFS.base_ref}-failpoints.tar
+                            cp ccache.tar /home/jenkins/agent/ccache-4.10.2/tiflash-amd64-linux-llvm-debug-${REFS.base_ref}-failpoints.
                             cd -
-                        """
+                            """
+                        }
                     }
                 }
-            }
-        }
-
-        stage("Unit Test Prepare") {
-            steps {
-                sh label: "link unit test dir", script: """
-                ln -sf ${WORKSPACE}/install/tiflash /tiflash
-                ls -lha ${WORKSPACE}/tiflash
-                ln -sf ${WORKSPACE}/tiflash/tests /tests
-                """
-            }
-        }
-        stage("Run Tests") {
-            steps {
-                dir("${WORKSPACE}/tiflash") {
-                    sh label: "run unit tests", script: """
-                    parallelism=${PARALLELISM}
-                    rm -rf /tmp-memfs/tiflash-tests
-                    mkdir -p /tmp-memfs/tiflash-tests
-                    export TIFLASH_TEMP_DIR=/tmp-memfs/tiflash-tests
-
-                    mkdir -p /root/.cache
-                    source /tests/docker/util.sh
-                    export LLVM_PROFILE_FILE="/tiflash/profile/unit-test-%\${parallelism}m.profraw"
-                    show_env
-                    ENV_VARS_PATH=/tests/docker/_env.sh OUTPUT_XML=true NPROC=\${parallelism} /tests/run-gtest.sh
-                    """
+                stage("Upload Proxy Cache") {
+                    steps {
+                        sh label: "upload proxy cache", script: """
+                            echo "TODO: upload proxy cache"
+                        """
+                    }
                 }
             }
         }
