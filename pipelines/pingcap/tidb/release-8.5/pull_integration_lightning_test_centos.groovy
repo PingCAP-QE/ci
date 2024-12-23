d04cdb5aee5cd3fdc1a3f115062c44acdd6a04d5
purelind <purelind@gmail.com>
2024-12-23 20:46:09 +0800

    fix: correct download binary

--- pipelines/pingcap/tidb/release-8.5/pull_integration_lightning_test_centos.groovy
+++ pipelines/pingcap/tidb/release-8.5/pull_integration_lightning_test_centos.groovy
@@ -61,7 +61,12 @@ pipeline {
                 dir("third_party_download") {
                     retry(2) {
                         sh label: "download third_party", script: """
-                            mkdir -p bin
+                            chmod +x ../tidb/lightning/tests/*.sh
+                            ${WORKSPACE}/tidb/lightning/tests/download_integration_test_binaries.sh ${REFS.base_ref}
+                            mkdir -p bin && mv third_bin/* bin/
+                            ls -alh bin/
+                            rm -f bin/pd-* bin/tikv-* bin/tiflash bin/lib*
+
                             wget -q -O tikv-server.tar.gz "https://internal-do.pingcap.net/dl/oci-file/hub.pingcap.net/devbuild/tikv/tikv/package?tag=v8.5.0-centos7_linux_amd64&file=tikv-v8.5.0-linux-amd64.tar.gz"
                             wget -q -O pd-server.tar.gz "https://internal-do.pingcap.net/dl/oci-file/hub.pingcap.net/devbuild/tikv/pd/package?tag=v8.5.0-centos7_linux_amd64&file=pd-v8.5.0-linux-amd64.tar.gz"
                             wget -q -O tiflash.tar.gz "https://internal-do.pingcap.net/dl/oci-file/hub.pingcap.net/devbuild/pingcap/tiflash/package?tag=v8.5.0-centos7_linux_amd64&file=tiflash-v8.5.0-linux-amd64.tar.gz"
