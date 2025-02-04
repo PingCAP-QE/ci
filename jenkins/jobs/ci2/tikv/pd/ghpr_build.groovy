61c9c22accc428d31488c82d09c36e321dde3d14
wuhuizuo <wuhuizuo@126.com>
2025-02-04 16:15:55 +0800

    refactor(tikv/pd): move build job to prow style job
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- jenkins/jobs/ci2/tikv/pd/ghpr_build.groovy
+++ jenkins/jobs/ci2/tikv/pd/ghpr_build.groovy
@@ -1,5 +1,6 @@
 // REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
 pipelineJob('pd_ghpr_build') {
+    disabled(true)
     logRotator {
         daysToKeep(30)
     }
@@ -64,7 +65,7 @@ pipelineJob('pd_ghpr_build') {
             }
         }
     }
- 
+
     definition {
         cpsScm {
             lightweight(true)
