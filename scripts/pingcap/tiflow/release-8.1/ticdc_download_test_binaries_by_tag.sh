b1589478e1246c56edfe6122cef1ac8012f85ad2
purelind <purelind@gmail.com>
2025-02-12 17:23:35 +0800

    feat: download script support only download one component
    Signed-off-by: purelind <purelind@gmail.com>

--- scripts/pingcap/tiflow/release-8.1/ticdc_download_test_binaries_by_tag.sh
+++ scripts/pingcap/tiflow/release-8.1/ticdc_download_test_binaries_by_tag.sh
@@ -2,12 +2,48 @@
 
 # ticdc_download_test_binaries_by_tag.sh will
 # * download all the binaries you need for integration testing
+# Usage: 
+#   ./ticdc_download_test_binaries_by_tag.sh <version> [components...] [--os=<os>] [--arch=<arch>]
+# Example:
+#   ./ticdc_download_test_binaries_by_tag.sh v8.1.0 tidb pd             # download tidb and pd for linux/amd64
+#   ./ticdc_download_test_binaries_by_tag.sh v8.1.0                     # download all for linux/amd64
+#   ./ticdc_download_test_binaries_by_tag.sh v8.1.0 --os=darwin --arch=arm64 tidb  # specify os/arch
 
 set -euo pipefail
 
-VERSION=${1}
-OS=${2:-linux}
-ARCH=${3:-amd64}
+# Declare associative arrays
+declare -A COMPONENT_URLS
+declare -A COMPONENT_EXTRACT_PATHS
+
+# Default values
+OS="linux"
+ARCH="amd64"
+VERSION=${1:-""}
+shift 1
+
+if [ -z "$VERSION" ]; then
+	echo "Error: Version is required"
+	exit 1
+fi
+
+# Parse arguments
+COMPONENTS=()
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		--os=*)
+			OS="${1#*=}"
+			shift
+			;;
+		--arch=*)
+			ARCH="${1#*=}"
+			shift
+			;;
+		*)
+			COMPONENTS+=("$1")
+			shift
+			;;
+	esac
+done
 
 # Constants
 # Allow override through environment variable, fall back to default if not set
@@ -39,24 +75,31 @@ download_file() {
 
 download_binaries() {
 	log_green "Downloading binaries..."
-
-	local pingcap_base_url="${FILE_SERVER_URL}/download/builds/pingcap"
-	local tikv_base_url="${FILE_SERVER_URL}/download/builds/tikv"
 	
-	# Define download URLs for main components
-	local tidb_url="${pingcap_base_url}/tidb/tag/${VERSION}/${OS}_${ARCH}/tidb.tar.gz"
-	local tiflash_url="${pingcap_base_url}/tiflash/tag/${VERSION}/${OS}_${ARCH}/tiflash.tar.gz"
-	local ctl_url="${pingcap_base_url}/ctl/tag/${VERSION}/${OS}_${ARCH}/ctl.tar.gz"
-	local tikv_url="${tikv_base_url}/tikv/tag/${VERSION}/${OS}_${ARCH}/tikv.tar.gz"
-	local pd_url="${tikv_base_url}/pd/tag/${VERSION}/${OS}_${ARCH}/pd.tar.gz"
-
-	# Download and extract binaries
-	download_and_extract "$tidb_url" "tidb.tar.gz" "tidb-server"
-	download_and_extract "$pd_url" "pd.tar.gz" "pd-server"
-	download_and_extract "$tikv_url" "tikv.tar.gz" "tikv-server"
-	download_and_extract "$tiflash_url" "tiflash.tar.gz"
-	download_and_extract "$ctl_url" "ctl.tar.gz" "pd-ctl"
-
+	# If no components specified, download all
+	if [ ${#COMPONENTS[@]} -eq 0 ]; then
+		COMPONENTS=(tidb pd tikv tiflash ctl)
+	fi
+	
+	# Validate components
+	for component in "${COMPONENTS[@]}"; do
+		if [[ ! ${COMPONENT_URLS[$component]+_} ]]; then
+			echo "Error: Invalid component '$component'"
+			echo "Available components: ${!COMPONENT_URLS[@]}"
+			exit 1
+		fi
+	done
+	
+	# Download specified components
+	for component in "${COMPONENTS[@]}"; do
+		local url=${COMPONENT_URLS[$component]}
+		local file_name=$(basename "$url")
+		local extract_path=${COMPONENT_EXTRACT_PATHS[$component]}
+		
+		echo "Downloading component: $component"
+		download_and_extract "$url" "$file_name" "$extract_path"
+	done
+	
 	chmod a+x ${THIRD_BIN_DIR}/*
 }
 
@@ -92,8 +135,30 @@ setup() {
 	mkdir -p ${THIRD_BIN_DIR} ${TMP_DIR} ${BIN_DIR}
 }
 
+setup_component_configs() {
+	local pingcap_base_url="${FILE_SERVER_URL}/download/builds/pingcap"
+	local tikv_base_url="${FILE_SERVER_URL}/download/builds/tikv"
+	
+	COMPONENT_URLS=(
+		["tidb"]="${pingcap_base_url}/tidb/tag/${VERSION}/${OS}_${ARCH}/tidb.tar.gz"
+		["pd"]="${tikv_base_url}/pd/tag/${VERSION}/${OS}_${ARCH}/pd.tar.gz"
+		["tikv"]="${tikv_base_url}/tikv/tag/${VERSION}/${OS}_${ARCH}/tikv.tar.gz"
+		["tiflash"]="${pingcap_base_url}/tiflash/tag/${VERSION}/${OS}_${ARCH}/tiflash.tar.gz"
+		["ctl"]="${pingcap_base_url}/ctl/tag/${VERSION}/${OS}_${ARCH}/ctl.tar.gz"
+	)
+	
+	COMPONENT_EXTRACT_PATHS=(
+		["tidb"]="tidb-server"
+		["pd"]="pd-server"
+		["tikv"]="tikv-server"
+		["tiflash"]=""
+		["ctl"]="pd-ctl"
+	)
+}
+
 main() {
 	setup
+	setup_component_configs
 	download_binaries
 	# Move binaries to final location
 	mv ${THIRD_BIN_DIR}/* ./${BIN_DIR}
