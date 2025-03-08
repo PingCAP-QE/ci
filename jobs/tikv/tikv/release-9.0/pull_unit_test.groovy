ef2ec79292eb7885040900b44df1a4adb9247a51
purelind <purelind@gmail.com>
2025-03-08 14:16:13 +0800

    feat: add tikv and pd jobs of release-9.0-beta.1

---
+++ jobs/tikv/tikv/release-9.0/pull_unit_test.groovy
@@ -0,0 +1,41 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+final folder = 'tikv/tikv/release-9.0'
+final jobName = 'pull_unit_test'
+
+pipelineJob("${folder}/${jobName}") {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC", "", "Prow job spec struct data")
+    }
+    properties {
+        buildFailureAnalyzer(false) // disable failure analyze
+        githubProjectUrl("https://github.com/tikv/tikv")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/${folder}/${jobName}.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        } 
+                    }
+                }
+            }
+        }
+    }
+}
