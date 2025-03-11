6464f38e04ea96bc7f09060da9b58e1f76f0bbcf
Purelind <purelind@users.noreply.github.com>
2025-03-11 14:43:33 +0800

    feat(tikv & pd): add tikv and pd pipelines of branch release-9.0-beta.1 (#3408)
    Add tikv and pd pipelines of branch release-9.0-beta.1.

---
+++ jobs/tikv/pd/release-9.0-beta/pull_integration_copr_test.groovy
@@ -0,0 +1,39 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For release branches.
+pipelineJob('tikv/pd/release-9.0-beta/pull_integration_copr_test') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC", "", "Prow job spec struct data")
+    }
+    properties {
+        buildFailureAnalyzer(false) // disable failure analyze
+        githubProjectUrl("https://github.com/tikv/pd")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/tikv/pd/release-9.0-beta/pull_integration_copr_test.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        } 
+                    }
+                }
+            }
+        }
+    }
+}
