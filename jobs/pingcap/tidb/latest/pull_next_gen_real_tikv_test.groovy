1a561c576d5b34a092559ce32755091520f49e89
purelind <purelind@gmail.com>
2025-04-27 15:04:56 +0800

    feat: add jobdsl of next gen real tikv test

---
+++ jobs/pingcap/tidb/latest/pull_next_gen_real_tikv_test.groovy
@@ -0,0 +1,38 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For feature/next-gen-tidb branch.
+pipelineJob('pingcap/tidb/pull_next_gen_real_tikv_test') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC", "", "Prow job spec struct data")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tidb")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/pingcap/tidb/latest/pull_next_gen_real_tikv_test.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        } 
+                    }
+                }
+            }
+        }
+    }
+}
