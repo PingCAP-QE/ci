d29c850401c0b805df8423190542ddc27349a826
purelind <purelind@gmail.com>
2025-05-28 16:39:49 +0800

    chore(pingcap/tidb): rename pull_vector_search_test to  pull_tiflash_integration_test
    This commit introduces a new Jenkins pipeline job for the Tiflash integration test, defined in `pull_tiflash_integration_test.groovy`. It includes a pod specification in `pod-pull_tiflash_integration_test.yaml` with resource requests and limits for the Golang and network tool containers. Additionally, the `latest-presubmits.yaml` file is updated to register the new job, ensuring it can be triggered and rerun as needed.

--- jobs/pingcap/tidb/latest/pull_tiflash_integration_test.groovy
+++ jobs/pingcap/tidb/latest/pull_tiflash_integration_test.groovy
@@ -1,5 +1,5 @@
 // REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
-pipelineJob('pingcap/tidb/pull_vector_search_test') {
+pipelineJob('pingcap/tidb/pull_tiflash_integration_test') {
     logRotator {
         daysToKeep(30)
     }
@@ -16,7 +16,7 @@ pipelineJob('pingcap/tidb/pull_vector_search_test') {
     definition {
         cpsScm {
             lightweight(true)
-            scriptPath('pipelines/pingcap/tidb/latest/pull_vector_search_test.groovy')
+            scriptPath('pipelines/pingcap/tidb/latest/pull_tiflash_integration_test.groovy')
             scm {
                 git{
                     remote {
