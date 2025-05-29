fbb0299b937e444ffa0134c1e455ea1323c5291d
Purelind <purelind@users.noreply.github.com>
2025-05-29 10:29:55 +0800

    feat(pipelines/pingcap/tiflash): add support for next-gen branch integration tests (#3539)
    Close https://github.com/PingCAP-QE/ci/issues/3544
    
    This commit introduces logic to handle the 'feature/next-gen' branch in
    the integration test pipeline.
    * It adds new variables to manage next-gen specific configurations
    * modifies cache checks for proxy and libclara
    * and updates the CMake command to include a flag for enabling next-gen
    features.
    * Additionally, it adjusts the TiKV image tag based on the branch type,
    enhancing the CI process for next-gen development.
    
    ---------
    
    Co-authored-by: JaySon <tshent@qq.com>

---
+++ jobs/pingcap/tiflash/latest/pull_unit_next_gen.groovy
@@ -0,0 +1,39 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For trunk and latest release branches.
+pipelineJob('pingcap/tiflash/pull_unit_next_gen') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/pingcap/tiflash")
+    }
+ 
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/pingcap/tiflash/latest/pull_unit_next_gen.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
