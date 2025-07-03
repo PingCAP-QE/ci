a14df7ed226099ac07067b525c203f27c5e2a3e3
wuhuizuo <wuhuizuo@126.com>
2025-07-03 15:55:06 +0800

    feat(pingcap-qe/tidb-test): add next-gen presubmit jobs

---
+++ jobs/pingcap-qe/tidb-test/latest/pull_tiproxy_mysql_test_next_gen.groovy
@@ -0,0 +1,42 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+final fullRepo = 'pingcap-qe/tidb-test'
+final branchAlias = 'latest' // For trunk and latest release branches.
+final jobName = 'pull_tiproxy_mysql_test_next_gen'
+
+pipelineJob("${fullRepo}/${jobName}") {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
+    }
+    properties {
+        // priority(0) // 0 fast than 1
+        githubProjectUrl("https://github.com/${fullRepo}")
+    }
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/${fullRepo}/${branchAlias}/${jobName}/pipeline.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
