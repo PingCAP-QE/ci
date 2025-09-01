e1f989b07f7155bd6c114356677d598809728c01
wuhuizuo <wuhuizuo@126.com>
2025-09-01 17:21:48 +0800

    refactor(cd): migrate to push devbuild images to GAR (#3713)
    - Push images to GAR for us-docker.pkg.dev paths in docker-common.groovy
    for dev builds.
    - Create multi-arch manifests in GAR for matching image paths.
    - Update default dev build image prefix to use GAR instead of Harbor.

--- jenkins/pipelines/cd/atom-jobs/manifest-multiarch-common.groovy
+++ jenkins/pipelines/cd/atom-jobs/manifest-multiarch-common.groovy
@@ -1,15 +1,24 @@
 node("delivery") {
     container("delivery") {
         stage("build multi-arch") {
-            withCredentials([usernamePassword(credentialsId: 'harbor-pingcap', usernameVariable: 'harborUser', passwordVariable: 'harborPassword')]) {
-                sh """
-            printenv harborPassword | docker login -u ${harborUser} --password-stdin hub.pingcap.net
-            export DOCKER_CLI_EXPERIMENTAL=enabled
-            docker manifest create ${MULTI_ARCH_IMAGE}  -a ${AMD64_IMAGE} -a ${ARM64_IMAGE}
-            docker manifest push ${MULTI_ARCH_IMAGE}
-            """
+            if (params.MULTI_ARCH_IMAGE.startsWith("us-docker.pkg.dev/pingcap-testing-account/")) {
+                docker.withRegistry("https://us-docker.pkg.dev", "pingcap-testing-account") {
+                    sh """
+                        export DOCKER_CLI_EXPERIMENTAL=enabled
+                        docker manifest create ${MULTI_ARCH_IMAGE}  -a ${AMD64_IMAGE} -a ${ARM64_IMAGE}
+                        docker manifest push ${MULTI_ARCH_IMAGE}
+                    """
+                }
+            } else {
+                docker.withRegistry("https://hub.pingcap.net", "harbor-pingcap") {
+                    sh """
+                        export DOCKER_CLI_EXPERIMENTAL=enabled
+                        docker manifest create ${MULTI_ARCH_IMAGE}  -a ${AMD64_IMAGE} -a ${ARM64_IMAGE}
+                        docker manifest push ${MULTI_ARCH_IMAGE}
+                    """
+                }
             }
+            println "multi arch image: ${MULTI_ARCH_IMAGE}"
         }
     }
-    println "multi arch image: ${MULTI_ARCH_IMAGE}"
 }
