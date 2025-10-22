feb81c9a0ebd153abc50be7bf1c2b2c6a7ac113f
Tenghui Zhong <tenfy@tenfy.cn>
2025-10-22 13:52:28 +0800

    ticdc: Add next-gen integration tests for Kafka, MySQL, Pulsar, and Storage. (#3831)
    This pull request introduces new Jenkins pipeline files for running
    integration tests for TiCDC. These pipelines are designed to run on the
    "next-gen" infrastructure, indicated by the `_next_gen.groovy` naming
    convention.
    
    The primary motivation for these changes is to expand the integration
    testing capabilities for TiCDC by adding support for Pulsar and
    improving the existing Kafka and MySQL integration tests.
    
    Here's a summary of the changes:
    
    *   **New Jenkins Job DSL Files:**
        *   Added `pull_cdc_kafka_integration_heavy_next_gen.groovy`
        *   Added `pull_cdc_kafka_integration_light_next_gen.groovy`
        *   Added `pull_cdc_mysql_integration_heavy_next_gen.groovy`
        *   Added `pull_cdc_mysql_integration_light_next_gen.groovy`
        *   Added `pull_cdc_pulsar_integration_light_next_gen.groovy`
        *   Added `pull_cdc_storage_integration_heavy_next_gen.groovy`
        *   Added `pull_cdc_storage_integration_light_next_gen.groovy`
    
    *   **New Jenkins Pipeline Files:**
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_kafka_integration_heavy_next_gen/pipeline.groovy`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_kafka_integration_heavy_next_gen/pod.yaml`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_kafka_integration_light_next_gen/pipeline.groovy`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_kafka_integration_light_next_gen/pod.yaml`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_mysql_integration_heavy_next_gen/pipeline.groovy`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_mysql_integration_heavy_next_gen/pod.yaml`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_mysql_integration_light_next_gen/pipeline.groovy`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_mysql_integration_light_next_gen/pod.yaml`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_pulsar_integration_light_next_gen/pipeline.groovy`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_pulsar_integration_light_next_gen/pod.yaml`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_storage_integration_heavy_next_gen/pipeline.groovy`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_storage_integration_heavy_next_gen/pod.yaml`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_storage_integration_light_next_gen/pipeline.groovy`
    * Added
    `pipelines/pingcap/ticdc/latest/pull_cdc_storage_integration_light_next_gen/pod.yaml`
    
    *   **New Prow Job Configuration:**
    * Added `prow-jobs/pingcap/ticdc/latest-presubmits-next-gen.yaml` to
    integrate the new pipelines into Prow.
    
    * **"Next-Gen" Infrastructure Support:** All new pipelines are
    configured to run on the "next-gen" Jenkins infrastructure, leveraging
    updated pod templates and artifact download scripts.
    
    * **Integration Test Coverage:** The new files provide dedicated
    pipelines for testing TiCDC's integration with Kafka, MySQL, Pulsar, and
    various storage backends in both "light" and "heavy" test
    configurations.
    
    * **Standardized Pipeline Structure:** The new pipelines follow a
    consistent structure, including stages for debugging, checkout,
    preparation (downloading dependencies and building binaries), and
    running tests.
    
    * **Matrix-Based Testing:** The 'Tests' stage utilizes a matrix strategy
    to run tests in parallel across different `TEST_GROUP` values, improving
    test execution efficiency.
    
    * **Artifact Archiving:** Failed test runs will archive relevant logs
    for easier debugging.
    
    ---------
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

---
+++ pipelines/pingcap/ticdc/latest/pull_cdc_mysql_integration_heavy_next_gen/pipeline.groovy
@@ -0,0 +1,165 @@
+// REF: https://www.jenkins.io/doc/book/pipeline/syntax/#declarative-pipeline
+// Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
+// should triggerd for master branches
+@Library('tipipeline') _
+
+final K8S_NAMESPACE = "jenkins-tiflow"
+final GIT_FULL_REPO_NAME = 'pingcap/ticdc'
+final GIT_CREDENTIALS_ID = 'github-sre-bot-ssh'
+final BRANCH_ALIAS = 'latest'
+final POD_TEMPLATE_FILE = 'pipelines/${GIT_FULL_REPO_NAME}/${BRANCH_ALIAS}/${JOB_BASE_NAME}/pod.yaml'
+final REFS = readJSON(text: params.JOB_SPEC).refs
+
+final TARGET_BRANCH_PD = (REFS.base_ref ==~ /release-.*/ ? REFS.base_ref : "master")
+final TARGET_BRANCH_TIDB = (REFS.base_ref ==~ /release-.*/ ? REFS.base_ref : "master")
+final TARGET_BRANCH_TIFLASH = (REFS.base_ref ==~ /release-.*/ ? REFS.base_ref : "master")
+final TARGET_BRANCH_TIKV = (REFS.base_ref ==~ /release-.*/ ? REFS.base_ref : "dedicated")
+
+pipeline {
+    agent {
+        kubernetes {
+            namespace K8S_NAMESPACE
+            yamlFile POD_TEMPLATE_FILE
+            defaultContainer 'golang'
+        }
+    }
+    environment {
+        OCI_ARTIFACT_HOST = 'us-docker.pkg.dev/pingcap-testing-account/hub'
+        NEXT_GEN = 1
+    }
+    options {
+        timeout(time: 80, unit: 'MINUTES')
+        // parallelsAlwaysFailFast()
+    }
+    stages {
+        stage('Debug info') {
+            steps {
+                sh label: 'Debug info', script: """
+                    printenv
+                    echo "-------------------------"
+                    go env
+                    echo "-------------------------"
+                    echo "debug command: kubectl -n ${K8S_NAMESPACE} exec -ti ${NODE_NAME} bash"
+                """
+                container(name: 'net-tool') {
+                    sh 'dig github.com'
+                    script {
+                        prow.setPRDescription(REFS)
+                    }
+                }
+            }
+        }
+        stage('Checkout') {
+            options { timeout(time: 10, unit: 'MINUTES') }
+            steps {
+                dir(REFS.repo) {
+                    cache(path: "./", includes: '**/*', key: prow.getCacheKey('git', REFS), restoreKeys: prow.getRestoreKeys('git', REFS)) {
+                        retry(2) {
+                            script {
+                                prow.checkoutRefs(REFS)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage("prepare") {
+            options { timeout(time: 20, unit: 'MINUTES') }
+            steps {
+                dir(REFS.repo) {
+                    cache(path: "./bin", includes: '**/*', key: prow.getCacheKey('ng-binary', REFS, 'cdc-mysql-integration')) {
+                        // build cdc, kafka_consumer, storage_consumer, cdc.test for integration test
+                        // only build binarys if not exist, use the cached binarys if exist
+                        sh label: "prepare", script: """
+                            ls -alh ./bin
+                            [ -f ./bin/cdc ] || make cdc
+                            [ -f ./bin/cdc.test ] || make integration_test_build
+                            ls -alh ./bin
+                            ./bin/cdc version
+                        """
+                    }
+                    container("utils") {
+                        dir("bin") {
+                            script {
+                                retry(2) {
+                                    sh label: "download third_party", script: """
+                                        export next_gen_artifact_script=${WORKSPACE}/scripts/artifacts/download_pingcap_oci_artifact.sh"
+                                        chmod +x $next_gen_artifact_script
+                                        ${next_gen_artifact_script} \
+                                            --pd=${TARGET_BRANCH_PD}-next-gen \
+                                            --tikv=${TARGET_BRANCH_TIKV}-next-gen \
+                                            --tikv-worker=${TARGET_BRANCH_TIKV}-next-gen \
+                                            --tidb=${TARGET_BRANCH_TIDB}-next-gen \
+                                            --minio=RELEASE.2025-07-23T15-54-02Z
+                                    """
+                                }
+                            }
+                        }
+                        script {
+                            retry(2) {
+                                sh label: "download third_party", script: """
+                                    ./tests/scripts/download-integration-test-binaries-next-gen.sh && ls -alh ./bin
+                                """
+                            }
+                        }
+                        cache(path: "./", includes: '**/*', key: "ws/${BUILD_TAG}/ticdc") {
+                            sh label: "prepare", script: """
+                                ls -alh ./bin
+                            """
+                        }
+                    }
+                }
+            }
+        }
+
+        stage('Tests') {
+            matrix {
+                axes {
+                    axis {
+                        name 'TEST_GROUP'
+                        values 'G00', 'G01', 'G02', 'G03', 'G04', 'G05', 'G06', 'G07', 'G08', 'G09', 'G10', 'G11', 'G12', 'G13', 'G14', 'G15'
+                    }
+                }
+                agent{
+                    kubernetes {
+                        namespace K8S_NAMESPACE
+                        yamlFile POD_TEMPLATE_FILE
+                        defaultContainer 'golang'
+                    }
+                }
+                stages {
+                    stage("Test") {
+                        options { timeout(time: 60, unit: 'MINUTES') }
+                        steps {
+                            dir(REFS.repo) {
+                                cache(path: "./", includes: '**/*', key: "ws/${BUILD_TAG}/ticdc") {
+                                    sh """
+                                        make check_third_party_binary
+                                        ls -alh ./bin
+                                        ./bin/tidb-server -V
+                                        ./bin/pd-server -V
+                                        ./bin/tikv-server -V
+                                        ./bin/tiflash --version
+                                    """
+                                }
+                                sh label: "${TEST_GROUP}", script: """
+                                    ./tests/integration_tests/run_heavy_it_in_ci.sh mysql ${TEST_GROUP}
+                                """
+                            }
+                        }
+                        post {
+                            failure {
+                                sh label: "collect logs", script: """
+                                    ls /tmp/tidb_cdc_test/
+                                    tar --warning=no-file-changed  -cvzf log-${TEST_GROUP}.tar.gz \$(find /tmp/tidb_cdc_test/ -type f -name "*.log")
+                                    ls -alh  log-${TEST_GROUP}.tar.gz
+                                """
+                                archiveArtifacts artifacts: "log-${TEST_GROUP}.tar.gz", fingerprint: true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
