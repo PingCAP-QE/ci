228d27dc27166c2f25282dbae9d063af7c5c4816
wuhuizuo <wuhuizuo@126.com>
2026-01-28 01:10:55 +0800

    fix(pipelines/pingcap/tidb): Increase timeout from 30 to 60 minutes for ghpr_check job

--- pipelines/pingcap/tidb/latest/ghpr_check.groovy
+++ pipelines/pingcap/tidb/latest/ghpr_check.groovy
@@ -19,7 +19,7 @@ pipeline {
         }
     }
     options {
-        timeout(time: 30, unit: 'MINUTES')
+        timeout(time: 60, unit: 'MINUTES')
         parallelsAlwaysFailFast()
     }
     stages {
