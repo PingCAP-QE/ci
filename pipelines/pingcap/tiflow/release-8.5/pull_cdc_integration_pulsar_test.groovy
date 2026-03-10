8cefc1b674b5a3bd37d18628edd74a935f7192a1
Ruihao Chen <joechenrh@gmail.com>
2026-03-10 10:27:50 +0800

    pipeline(tiflow): add ci for syncdiff in release-8.5 (#4299)
    ~~Wait https://github.com/pingcap/tiflow/pull/12529 merged first.~~
    
    Wait testing CI with https://github.com/pingcap/tiflow/pull/12537, then
    we can merge it.
    
    ---------
    
    Signed-off-by: Ruihao Chen <joechenrh@gmail.com>
    Co-authored-by: wuhuizuo <wuhuizuo@126.com>

--- pipelines/pingcap/tiflow/release-8.5/pull_cdc_integration_pulsar_test.groovy
+++ pipelines/pingcap/tiflow/release-8.5/pull_cdc_integration_pulsar_test.groovy
@@ -127,7 +127,6 @@ pipeline {
                                 ./bin/pd-server -V
                                 ./bin/tikv-server -V
                                 ./bin/tiflash --version
-                                ./bin/sync_diff_inspector --version
                             """
                         }
                     }
