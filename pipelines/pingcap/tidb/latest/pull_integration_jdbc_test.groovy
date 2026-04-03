fd0a9f139bbb8fcef07f655581ab63ec717bce36
weizheng <wei.zheng@pingcap.com>
2026-04-03 15:19:01 +0800

    fix(pipelines/tidb): remove redundant helper link in jdbc test

--- pipelines/pingcap/tidb/latest/pull_integration_jdbc_test.groovy
+++ pipelines/pingcap/tidb/latest/pull_integration_jdbc_test.groovy
@@ -129,19 +129,12 @@ pipeline {
                                                 export TIDB_SERVER_PATH="${WORKSPACE}/tidb-test/bin/tidb-server"
                                                 export TIKV_PATH="127.0.0.1:2379"
                                                 export TIDB_TEST_STORE_NAME="tikv"
+                                                cd \${TEST_DIR} && chmod +x *.sh && \${TEST_SCRIPT}
                                             else
                                                 export TIDB_SERVER_PATH="${WORKSPACE}/tidb-test/bin/tidb-server"
                                                 export TIDB_TEST_STORE_NAME="unistore"
+                                                cd \${TEST_DIR} && chmod +x *.sh && \${TEST_SCRIPT}
                                             fi
-                                            cd \${TEST_DIR}
-                                            if [[ ! -f ../_helper.sh ]]; then
-                                                if [[ -f ../../_helper.sh ]]; then
-                                                    ln -sf ../../_helper.sh ../_helper.sh
-                                                elif [[ -f ${WORKSPACE}/tidb-test/_helper.sh ]]; then
-                                                    ln -sf ${WORKSPACE}/tidb-test/_helper.sh ../_helper.sh
-                                                fi
-                                            fi
-                                            chmod +x *.sh && \${TEST_SCRIPT}
                                         """
                                     }
                                 }
