5eb7b7f88562028aa5a95b5b12dff0a9cd6bf627
Ti Chi Robot <ti-community-prow-bot@tidb.io>
2026-05-15 11:00:16 +0800

    feat(libraries): implement Jenkins run matrix caching (#4583)
    - **feat(libraries): add matrixCache library for parallel stage
    caching**
    - **ci(libraries): remove gitee.groovy library function**
    
    ---------
    
    Co-authored-by: wuhuizuo <wuhuizuo@126.com>

--- pipelines/ti-community-infra/test-prod/prow_debug.groovy
+++ pipelines/ti-community-infra/test-prod/prow_debug.groovy
@@ -2,13 +2,11 @@
 // Keep small than 400 lines: https://issues.jenkins.io/browse/JENKINS-37984
 // should triggerd for master and latest release branches
 @Library('tipipeline') _
-final POD_TEMPLATE_FILE = 'pipelines/ti-community-infra/test-prod/pod-prow_debug.yaml'
+final REFS = params.JOB_SPEC ? readJSON(text: params.JOB_SPEC).refs : [org: 'ti-community-infra', repo: 'ci', base_sha: 'local']
 
 pipeline {
     agent {
-        kubernetes {
-            yamlFile POD_TEMPLATE_FILE
-        }
+        kubernetes {}
     }
     options { skipDefaultCheckout() }
     stages {
@@ -22,6 +20,38 @@ pipeline {
                     sh "pwd && ls -l"
                 }
                 sh "pwd && ls -l"
+            }
+        }
+
+        stage('Matrix Cache Debug') {
+            matrix {
+                axes {
+                    axis {
+                        name 'AXIS_OS'
+                        values 'linux', 'darwin'
+                    }
+                    axis {
+                        name 'AXIS_ARCH'
+                        values 'amd64', 'arm64'
+                    }
+                }
+                stages {
+                    stage('Verify Matrix Cache Key Inputs') {
+                        when {
+                            expression {
+                                return matrixCache.shouldSkip(REFS, env.STAGE_NAME)
+                            }
+                        }
+                        steps {
+                            echo 'matrixCache debug axis=${AXIS_OS}/${AXIS_ARCH}'
+                        }
+                        post {
+                            success {
+                                script { matrixCache.markDone(REFS, env.STAGE_NAME) }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
