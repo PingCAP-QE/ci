1a8fc0c06e9c5ef55b67259876f47a94ba6c6651
JaySon-Huang <tshent@qq.com>
2026-05-25 18:10:53 +0800

    add pull_integration_next_gen_columnar.groovy
    Signed-off-by: JaySon-Huang <tshent@qq.com>

---
+++ jobs/pingcap/tiflash/latest/pull_integration_next_gen_columnar.groovy
@@ -0,0 +1,35 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For trunk and latest release branches.
+pipelineJob('pingcap/tiflash/pull_integration_next_gen_columnar') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
+    }
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath("pipelines/pingcap/tiflash/latest/pull_integration_next_gen_columnar.groovy")
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
