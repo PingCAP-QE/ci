fb3838a9551adf6260445c71fa98ec0f252c8f34
yebin <50281525+lybcodes@users.noreply.github.com>
2026-06-15 17:29:30 +0800

    ci(tidb): migrate remaining tidb latest jenkins jobs to GCP (#4687)
    Issue Number ref : https://github.com/PingCAP-QE/ci/issues/4678
    
    Signed-off-by: lyb <yebin.li@pingcap.com>

--- pipelines/pingcap/tidb/latest/pull_integration_common_test.groovy
+++ pipelines/pingcap/tidb/latest/pull_integration_common_test.groovy
@@ -17,6 +17,7 @@ pipeline {
             namespace K8S_NAMESPACE
             yaml pod_label.withCiLabels(POD_TEMPLATE_FILE, REFS)
             retries 2
+            workspaceVolume genericEphemeralVolume(accessModes: 'ReadWriteOnce', requestsSize: '150Gi', storageClassName: 'hyperdisk-rwo')
             defaultContainer 'golang'
         }
     }
@@ -95,6 +96,7 @@ pipeline {
                         namespace K8S_NAMESPACE
                         yaml pod_label.withCiLabels(POD_TEMPLATE_FILE, REFS)
                         retries 2
+                        workspaceVolume genericEphemeralVolume(accessModes: 'ReadWriteOnce', requestsSize: '150Gi', storageClassName: 'hyperdisk-rwo')
                         defaultContainer 'golang'
                     }
                 }
