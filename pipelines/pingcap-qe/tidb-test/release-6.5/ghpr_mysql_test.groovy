a469a15efd85d0f3ccd80962d66ee96c56d8c7dc
wuhuizuo <wuhuizuo@126.com>
2026-07-03 17:49:24 +0800

    refactor(pipelines/tidb-test): align release-6.2 with release-8.5

--- pipelines/pingcap-qe/tidb-test/release-6.5/ghpr_mysql_test.groovy
+++ pipelines/pingcap-qe/tidb-test/release-6.5/ghpr_mysql_test.groovy
@@ -12,8 +12,9 @@ pipeline {
     agent {
         kubernetes {
             namespace K8S_NAMESPACE
-            yamlFile POD_TEMPLATE_FILE
+            yaml pod_label.withCiLabels(POD_TEMPLATE_FILE, REFS)
             retries 2
+            workspaceVolume genericEphemeralVolume(accessModes: 'ReadWriteOnce', requestsSize: '150Gi', storageClassName: 'hyperdisk-rwo')
             defaultContainer 'golang'
         }
     }
@@ -75,8 +76,9 @@ pipeline {
                     kubernetes {
                         namespace K8S_NAMESPACE
                         defaultContainer 'golang'
-                        yamlFile POD_TEMPLATE_FILE
+                        yaml pod_label.withCiLabels(POD_TEMPLATE_FILE, REFS)
                         retries 2
+                        workspaceVolume genericEphemeralVolume(accessModes: 'ReadWriteOnce', requestsSize: '150Gi', storageClassName: 'hyperdisk-rwo')
                     }
                 }
                 when {
