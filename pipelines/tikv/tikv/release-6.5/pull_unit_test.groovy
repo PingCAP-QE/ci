43e95676f01e0a0bb8755ed76116a18f0ff2e314
wuhuizuo <wuhuizuo@126.com>
2026-08-11 20:25:11 +0800

    fix(pipelines): guard devtoolset-8 enable in tikv pull_unit_test pipelines

--- pipelines/tikv/tikv/release-6.5/pull_unit_test.groovy
+++ pipelines/tikv/tikv/release-6.5/pull_unit_test.groovy
@@ -71,8 +71,10 @@ pipeline {
                             export ROCKSDB_SYS_SSE=1
                             export RUST_BACKTRACE=1
                             export LOG_LEVEL=INFO
-                            echo using gcc 8
-                            source /opt/rh/devtoolset-8/enable
+                            if [ -f /opt/rh/devtoolset-8/enable ]; then
+                              echo "using devtoolset-8 (gcc 8)"
+                              source /opt/rh/devtoolset-8/enable
+                            fi
                             make clippy || (echo Please fix the clippy error; exit 1)
                         """
                     }
@@ -92,8 +94,10 @@ pipeline {
                             export LOG_LEVEL=INFO
                             export CARGO_INCREMENTAL=0
                             export RUSTDOCFLAGS="-Z unstable-options --persist-doctests"
-                            echo using gcc 8
-                            source /opt/rh/devtoolset-8/enable
+                            if [ -f /opt/rh/devtoolset-8/enable ]; then
+                              echo "using devtoolset-8 (gcc 8)"
+                              source /opt/rh/devtoolset-8/enable
+                            fi
                             set -e
                             set -o pipefail
 
