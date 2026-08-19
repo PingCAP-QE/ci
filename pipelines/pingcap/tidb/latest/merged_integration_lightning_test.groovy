2f125cbe83f398ec076aa3795ffba70dc06e8a48
wuhuizuo <wuhuizuo@126.com>
2026-08-19 17:20:12 +0800

    fix(pipelines): fix unclosed quote in OCI_ARTIFACT_HOST_COMMUNITY

--- pipelines/pingcap/tidb/latest/merged_integration_lightning_test.groovy
+++ pipelines/pingcap/tidb/latest/merged_integration_lightning_test.groovy
@@ -27,7 +27,7 @@ pipeline {
     }
     environment {
         OCI_ARTIFACT_HOST = "${env._JENKINS_OCI_ARTIFACT_HOST_HUB}"
-        OCI_ARTIFACT_HOST_COMMUNITY = env._JENKINS_OCI_ARTIFACT_HOST_HUB
+        OCI_ARTIFACT_HOST_COMMUNITY = "${env._JENKINS_OCI_ARTIFACT_HOST_HUB}"
     }
     options {
         timeout(time: 60, unit: 'MINUTES')
