fd4a68ba808177a6526b4b7dd7c3f90e8969f7bc
wuhuizuo <wuhuizuo@126.com>
2026-09-09 18:53:27 +0800

    refactor(pipelines): remove unused tiflow coveralls credential declarations

--- pipelines/pingcap/tiflow/release-6.5/pull_cdc_integration_mysql_test.groovy
+++ pipelines/pingcap/tiflow/release-6.5/pull_cdc_integration_mysql_test.groovy
@@ -132,7 +132,6 @@ pipeline {
                         options { timeout(time: 60, unit: 'MINUTES') }
                         environment {
                             TICDC_CODECOV_TOKEN = credentials('codecov-token-tiflow')
-                            TICDC_COVERALLS_TOKEN = credentials('coveralls-token-tiflow')
                         }
                         steps {
                             dir(REFS.repo) {
