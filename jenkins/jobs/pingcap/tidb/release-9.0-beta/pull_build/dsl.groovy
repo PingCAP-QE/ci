d379fcfb9a9ae423425a707677843bbc87e2a049
wuhuizuo <wuhuizuo@126.com>
2026-09-23 18:55:07 +0800

    refactor(jobs): migrate pingcap/tidb to the job-folder layout

---
+++ jenkins/jobs/pingcap/tidb/release-9.0-beta/pull_build/dsl.groovy
@@ -0,0 +1,36 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For release-9.0-beta.M branches.
+final ciGroovyPath = "jenkins/jobs/pingcap/tidb/release-9.0-beta/pull_build/Jenkinsfile"
+pipelineJob('pingcap/tidb/release-9.0-beta/pull_build') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
+    }
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath(ciGroovyPath)
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ jenkins/jobs/pingcap/tidb/release-9.0-beta/pull_integration_br_test/dsl.groovy
@@ -0,0 +1,34 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+final ciGroovyPath = "jenkins/jobs/pingcap/tidb/release-9.0-beta/pull_integration_br_test/Jenkinsfile"
+pipelineJob('pingcap/tidb/release-9.0-beta/pull_br_integration_test') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC", "", "Prow job spec struct data")
+    }
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath(ciGroovyPath)
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
