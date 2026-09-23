af2d9e749cbc47e4a04fc6427c2ff67742568e21
wuhuizuo <wuhuizuo@126.com>
2026-09-23 18:53:58 +0800

    refactor(jobs): migrate pingcap-qe/tidb-test to the job-folder layout

---
+++ jenkins/jobs/pingcap-qe/tidb-test/latest/ghpr_mysql_test/dsl.groovy
@@ -0,0 +1,39 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+final fullRepo = 'pingcap-qe/tidb-test'
+final branchAlias = 'latest' // For trunk and latest release branches.
+final jobName = 'ghpr_mysql_test'
+final ciGroovyPath = "jenkins/jobs/${fullRepo}/${branchAlias}/${jobName}/Jenkinsfile"
+
+pipelineJob("${fullRepo}/${jobName}") {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC")
+    }
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath(ciGroovyPath)
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
