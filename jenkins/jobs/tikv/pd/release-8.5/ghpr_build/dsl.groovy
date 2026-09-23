711fa289a712301309bc875fd0578a314153a259
wuhuizuo <wuhuizuo@126.com>
2026-09-23 18:53:26 +0800

    refactor(jobs): migrate tikv/pd to the job-folder layout

---
+++ jenkins/jobs/tikv/pd/release-8.5/ghpr_build/dsl.groovy
@@ -0,0 +1,36 @@
+// REF: https://<your-jenkins-server>/plugin/job-dsl/api-viewer/index.html
+// For release branches.
+final ciGroovyPath = "jenkins/jobs/tikv/pd/release-8.5/ghpr_build/Jenkinsfile"
+pipelineJob('tikv/pd/release-8.5/ghpr_build') {
+    logRotator {
+        daysToKeep(30)
+    }
+    parameters {
+        // Ref: https://docs.prow.k8s.io/docs/jobs/#job-environment-variables
+        stringParam("BUILD_ID")
+        stringParam("PROW_JOB_ID")
+        stringParam("JOB_SPEC", "", "Prow job spec struct data")
+    }
+
+    definition {
+        cpsScm {
+            lightweight(true)
+            scriptPath(ciGroovyPath)
+            scm {
+                git{
+                    remote {
+                        url('https://github.com/PingCAP-QE/ci.git')
+                    }
+                    branch('main')
+                    extensions {
+                        cloneOptions {
+                            depth(1)
+                            shallow(true)
+                            timeout(5)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
